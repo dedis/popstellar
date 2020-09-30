cfa14cc81aa3e594e4866715d3590129e099ebff
maxime <maxime.wursch@epfl.ch>
2020-09-30 02:05:32 +0200

    Base of pop app in React Native

---
+++ PopApp/Navigation/Navigation.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import Launch from '../Components/Launch'
+import Home from '../Components/Home'
+import Connect from '../Components/Connect'
+import {Dimensions} from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+
+const TopTabNavigator = createMaterialTopTabNavigator()
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <SafeAreaView style={{flex:1}}>
+            <TopTabNavigator.Navigator
+                initialLayout={ width= Dimensions.get('window').width }
+                >
+            <TopTabNavigator.Screen name="Home" component={Home} />
+            <TopTabNavigator.Screen name="Connect" component={Connect} />
+            <TopTabNavigator.Screen name="Launch" component={Launch} />
+            </TopTabNavigator.Navigator>
+            </SafeAreaView>
+        </NavigationContainer>
+    );
+  }
