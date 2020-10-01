cbaf7f3f03f92be192f2f2c0c8f124fd09f06fd1
romain46 <romain.pugin@epfl.ch>
2020-10-01 14:16:04 +0200

    -

--- src/client.go
+++ src/client.go
@@ -11,25 +11,25 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 
 	// init
-   tcpAddr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:8080")
-   if err != nil {
-   		fmt.Printf("TCP resolve failed:", err.Error())
-   		os.Exit(1)
-   }
-   conn, err := net.DialTCP("tcp", nil, tcpAddr)
-   if err != nil {
-	   fmt.Printf("TCP dial failed:", err.Error())
-	   os.Exit(1)
-   }
-	for{
+	tcpAddr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:8080")
+	if err != nil {
+		fmt.Printf("TCP resolve failed:", err.Error())
+		os.Exit(1)
+	}
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	if err != nil {
+		fmt.Printf("TCP dial failed:", err.Error())
+		os.Exit(1)
+	}
+	for {
 		fmt.Printf("Send Message :")
 		text, _ := reader.ReadString('\n')
-	   // send message
+		// send message
 		_, err = conn.Write([]byte(text))
-	   if err != nil {
-		   println("Write to server failed:", err.Error())
-		   os.Exit(1)
-	   }
+		if err != nil {
+			println("Write to server failed:", err.Error())
+			os.Exit(1)
+		}
 
 		fmt.Println("Write to server  : ", text)
 
