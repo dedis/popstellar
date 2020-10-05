c78b94cae8132aecde1e910394ec63130431e06a
celinecamacho <celine.camacho@epfl.ch>
2020-10-05 18:58:43 +0200

    Implement and tested tabs and Home UI without LAO's list

--- app/src/main/java/com/github/dedis/student20_pop/MainActivity.java
+++ app/src/main/java/com/github/dedis/student20_pop/MainActivity.java
@@ -1,17 +1,54 @@
 package com.github.dedis.student20_pop;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-/*
-    Activity used to display "Hello World"
- */
-public class MainActivity extends AppCompatActivity {
+/**
+ * Activity used to display the different UIs
+**/
+public final class MainActivity extends FragmentActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (findViewById(R.id.fragment_container) != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, new HomeFragment()).commit();
+        }
+    }
+
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.tab_home:
+                showFragment(new HomeFragment(), HomeFragment.TAG);
+                break;
+            case R.id.tab_connect:
+                showFragment(new ConnectFragment(), ConnectFragment.TAG);
+                break;
+            case R.id.tab_launch:
+                showFragment(new LaunchFragment(), LaunchFragment.TAG);
+                break;
+            default:
+        }
+    }
+
+    private void showFragment(Fragment fragment, String TAG) {
+        if (!fragment.isVisible()) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment, TAG)
+                    .addToBackStack(TAG).commit();
+        }
     }
 }
