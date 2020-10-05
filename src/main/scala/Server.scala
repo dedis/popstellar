71ba5a029f9d2ffe3a99194b218636eb4a64ac78
Sébastien Fulpius <sebastien.fulpius@epfl.ch>
2020-10-05 17:27:15 +0200

    Prepare code for writing messages in the database

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,12 +1,16 @@
-import akka.actor.typed.ActorSystem
+import Database.Write
+import akka.actor.typed.{ActorSystem, Behavior}
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.HttpMethods.GET
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.ws.{Message, TextMessage}
 import akka.http.scaladsl.server.Directives._
-import akka.stream.scaladsl.Flow
+import akka.stream.scaladsl.{BroadcastHub, Flow, Keep, MergeHub, Sink, Source}
 
+import scala.concurrent.Future
 import scala.io.StdIn
+import scala.util.{Failure, Success}
 
 object Server {
 
@@ -15,34 +19,58 @@ object Server {
    */
   def main(args: Array[String]): Unit = {
 
-    implicit val system = ActorSystem(Behaviors.empty, "my-system")
-    // needed for the future flatMap/onComplete in the end
-    implicit val executionContext = system.executionContext
+    val root = Behaviors.setup[Nothing]{ context =>
+      implicit val system = context.system
+      val db = context.spawn(Database(), "db")
 
-    //Route for HTTP request
-    val route =
-      path("hello") {
-        get {
-          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+      //Route for HTTP request
+      val route =
+        path("hello") {
+          get {
+            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+          }
         }
+
+      //Route for websocket requests
+      def wsRoute = path("ws") {
+        //Reply to a message with the content of the received message
+        handleWebSocketMessages(
+          Flow[Message].collect {
+            case TextMessage.Strict(text) =>
+              db ! Write(text, text)
+              TextMessage("You said " + text)
+          }
+        )
       }
 
-    //Route for websocket requests
-    def wsRoute = path("ws") {
-      //Reply to a message with the content of the received message
-      handleWebSocketMessages(
-        Flow[Message].collect {
-          case TextMessage.Strict(text) => TextMessage("You said " + text)
-        }
-      )
+      implicit val executionContext = system.executionContext
+      val bindingFuture = Http().newServerAt("localhost", 8080).bind(route ~ wsRoute)
+      bindingFuture.onComplete {
+        case Success(value) => println("Server online at http://localhost:8080/")
+        case Failure(exception) => println("Server failed to start")
+          system.terminate()
+      }
+
+      Behaviors.empty
     }
 
-    val bindingFuture = Http().newServerAt("localhost", 8080).bind(route ~ wsRoute)
+    val system = ActorSystem[Nothing](root, "pop")
 
-    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
-    bindingFuture
-      .flatMap(_.unbind()) // trigger unbinding from the port
-      .onComplete(_ => system.terminate()) // and shutdown when done
+
   }
+}
+
+object Database {
+  sealed trait DBAction
+  final case class Write(key: String, value : String) extends DBAction
+  def apply(): Behavior[DBAction]  = db()
+
+  private def db(): Behavior[DBAction] =
+    Behaviors.receiveMessage {
+      case Write(key, value) =>
+        //TODO: write in the database here
+        println(key + ": " + value)
+        Behaviors.same
+    }
 }
