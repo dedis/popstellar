e799a125184ffa16ec22c799d8aafab3f3d38ec2
celinecamacho <celine.camacho@epfl.ch>
2020-10-05 18:58:43 +0200

    Implement and tested tabs and Home UI without LAO's list

---
+++ app/src/androidTest/java/com/github/dedis/student20_pop/MainActivityTest.java
@@ -0,0 +1,43 @@
+package com.github.dedis.student20_pop;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> rule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Before
+    public void launchActivity() {
+        ActivityScenario.launch(MainActivity.class);
+    }
+
+    @Test
+    public void onClickHomeTest() {
+        onView(withId(R.id.tab_home)).perform(click());
+        onView(withId(R.id.fragment_home)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void onClickConnectTest() {
+        onView(withId(R.id.tab_connect)).perform(click());
+        onView(withId(R.id.fragment_connect)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void onClickLaunchTest() {
+        onView(withId(R.id.tab_launch)).perform(click());
+        onView(withId(R.id.fragment_launch)).check(matches(isDisplayed()));
+    }
+}
---
+++ app/src/main/java/com/github/dedis/student20_pop/LaunchFragment.java
@@ -0,0 +1,24 @@
+package com.github.dedis.student20_pop;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+/**
+ * Fragment used to display the Launch UI
+**/
+public final class LaunchFragment extends Fragment {
+
+    public static final String TAG = LaunchFragment.class.getSimpleName();
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_launch, container, false);
+    }
+}
