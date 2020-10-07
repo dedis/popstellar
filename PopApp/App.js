94954e8c75ac96b4ee939bd98213793ff1d84c43
maximelaval <maxime.wursch@epfl.ch>
2020-10-07 15:38:55 +0200

    Connect UI
    Create the design of the connect UI and the navigation between all the steps.
    
    For now the app cannot access to native component, so the QR code reader will be implement later

--- PopApp/App.js
+++ PopApp/App.js
@@ -1,21 +1,29 @@
 import 'react-native-gesture-handler'
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, StatusBar, Platform } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import Navigation from './Navigation/Navigation'
+import { NavigationContainer } from '@react-navigation/native';
 
 /*
 * The start point of the app
 *
 * It open the navigation component in a safeAreaProvider to be able use
 * SafeAreaView in order to resolve issue with status bar
+*
+*  The Platform.OS is to put the statusBar in IOS in black, otherwise it is not readable
 */
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <Navigation/>
+      {Platform.OS === "ios" && 
+        <StatusBar barStyle='dark-content' backgroundColor='white'/>
+      }
+      <NavigationContainer>
+        <Navigation/>
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
