3ef7f35671a0e764b5bbe4deb1aff39c5babd0d9
BuildTools <unconfigured@null.spigotmc.org>
2020-10-10 23:35:26 +0200

    Working prototype for the connect fragment

---
+++ app/src/main/java/com/github/dedis/student20_pop/qrcode/QRFocusingProcessor.java
@@ -0,0 +1,57 @@
+package com.github.dedis.student20_pop.qrcode;
+
+import android.util.SparseArray;
+
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.FocusingProcessor;
+import com.google.android.gms.vision.Tracker;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+public class QRFocusingProcessor extends FocusingProcessor<Barcode> {
+
+    public QRFocusingProcessor(BarcodeDetector detector, QRCodeListener listener) {
+        super(detector, new BarcodeTracker(listener));
+    }
+
+    @Override
+    public int selectFocus(Detector.Detections<Barcode> detections) {
+        //Find most centered qrcode
+        SparseArray<Barcode> barcodes = detections.getDetectedItems();
+        double centerX = detections.getFrameMetadata().getWidth()/2d;
+        double centerY = detections.getFrameMetadata().getHeight()/2d;
+        double minSquaredDistance = Double.MAX_VALUE;
+        int id = -1;
+
+        for(int i = 0; i < barcodes.size(); i++) {
+            int key = barcodes.keyAt(i);
+            Barcode curBarcode = barcodes.get(key);
+
+            double dx = centerX - curBarcode.getBoundingBox().centerX();
+            double dy = centerY - curBarcode.getBoundingBox().centerY();
+            double squaredDist = dx * dx + dy * dy;
+
+            if(squaredDist < minSquaredDistance) {
+                minSquaredDistance = squaredDist;
+                id = key;
+            }
+        }
+
+        return id;
+    }
+
+    private static class BarcodeTracker extends Tracker<Barcode> {
+
+        private final QRCodeListener listener;
+
+        public BarcodeTracker(QRCodeListener listener) {
+            this.listener = listener;
+        }
+
+        @Override
+        public void onNewItem(int id, Barcode barcode) {
+            if(barcode.valueFormat == Barcode.URL)
+                listener.onQRCodeDetected(barcode.url.url);
+        }
+    }
+}
