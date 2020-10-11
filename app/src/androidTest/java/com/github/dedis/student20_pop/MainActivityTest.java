3c44266b2020bb8c10cf107e3d326c26c5768b39
celinecamacho <celine.camacho@epfl.ch>
2020-10-11 17:05:51 +0200

    Add comment for onClick method

--- app/src/androidTest/java/com/github/dedis/student20_pop/MainActivityTest.java
+++ app/src/androidTest/java/com/github/dedis/student20_pop/MainActivityTest.java
@@ -1,10 +1,8 @@
 package com.github.dedis.student20_pop;
 
 import androidx.test.core.app.ActivityScenario;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
--- app/src/main/java/com/github/dedis/student20_pop/ui/HomeFragment.java
+++ app/src/main/java/com/github/dedis/student20_pop/ui/HomeFragment.java
@@ -10,7 +10,6 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.github.dedis.student20_pop.R;
-import com.github.dedis.student20_pop.ui.ConnectFragment;
 
 /**
  * Fragment used to display the Connect UI
