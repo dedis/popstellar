74aab4740ef4398fcb2484761ee7c809f1032d2f
Jean-Baptiste Michel <jean-baptiste.michel@epfl.ch>
2020-10-13 17:04:20 +0200

    modified

--- app/src/main/java/com/github/dedis/student20_pop/attendee/PlaceholderFragment.java
+++ app/src/main/java/com/github/dedis/student20_pop/attendee/PlaceholderFragment.java
@@ -49,7 +49,7 @@ public class PlaceholderFragment extends Fragment {
             Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
         final TextView textView = root.findViewById(R.id.section_label);
-        pageViewModel.getText().observe(this, new Observer<String>() {
+        pageViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
