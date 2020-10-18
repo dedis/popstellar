fd78efbb988540d887bf818cf7d85aa8ff8e2921
Nicolas Raulin <nicolas.raulin@epfl.ch>
2020-10-18 21:31:18 +0200

    Basics of Json parsing + communication protocol

--- src/main/scala/JsonCommunicationProtocol.scala
+++ src/main/scala/JsonCommunicationProtocol.scala
@@ -1,20 +1,83 @@
-import spray.json.{DefaultJsonProtocol}
+import spray.json._
+import types._
+
 
 object JsonCommunicationProtocol extends DefaultJsonProtocol {
 
-  implicit val attendeeFormat = jsonFormat2(Attendee)
+  // Parsing for case classes
+  implicit val attendeeFormat: RootJsonFormat[Attendee] = jsonFormat2(Attendee)
+  implicit val laoCreationFormat: RootJsonFormat[LaoCreationMessage] = jsonFormat4(LaoCreationMessage)
 
+
+  implicit val messageAnswerFormat: RootJsonFormat[JsonMessageAnswer] = jsonFormat2(JsonMessageAnswer)
+
+  // Parsing for classes
+  implicit object TestClassFormat extends RootJsonFormat[TestClassJsonParsing] {
+    override def read(json: JsValue): TestClassJsonParsing = json match {
+      case JsArray(Vector(JsString(name), JsNumber(number), JsBoolean(b), JsArray(keys), JsArray(attendees), additional)) =>
+        new TestClassJsonParsing(
+          name,
+          number.toInt,
+          b,
+          keys.map(_.convertTo[Key]).toList,
+          attendees.map(_.convertTo[Attendee]).toList,
+          additional match {
+            case JsNumber(d) => Some(d.toDouble)
+            case _ => None
+          }
+        )
+      case _ => throw DeserializationException("invalid TestClassFormat input")
+    }
+
+    override def write(obj: TestClassJsonParsing): JsValue = {
+      JsArray(
+        JsString(obj.name),
+        JsNumber(obj.number),
+        JsBoolean(obj.b),
+        JsArray(obj.keys.map(e => e.toJson).toVector),
+        JsArray(obj.attendees.map(e => e.toJson).toVector),
+        obj.additional match {
+          case Some(d) => JsNumber(d)
+          case _ => JsNull
+        }
+      )
+    }
+
+  }
 }
 
 
-
-// Test object for the protocol
-//type Key = String // hex value in string formal
-
-abstract class Participant
-//case class Organizer(name: String, id: Key)
-//case class Witness(name: String, id: Key)
-case class Attendee(name: String, id: String)
+package object types {
+  type Key = String // hex value in string formal
+  type Timestamp = String // ???
+  type Hash = String //  ???
+  type Hex = String // hex in string format
+}
 
 
+sealed abstract class Participant
+//case class Organizer(name: String, id: Key) extends Participant
+//case class Witness(name: String, id: Key) extends Participant
+case class Attendee(name: String, id: Key) extends Participant
 
+
+sealed abstract class JsonMessage
+case class LaoCreationMessage(
+  name: String,
+  organizer: Key,
+  witnesses: List[Key],
+  attestation: Hex
+) extends JsonMessage
+
+
+case class JsonMessageAnswer(success: Boolean, error: Option[String]) extends JsonMessage
+
+
+sealed class TestClassJsonParsing(
+ val name: String,
+ val number: Int,
+ val b: Boolean,
+ val keys: List[Key],
+ val attendees: List[Attendee],
+ val additional: Option[Double]
+)
--- src/main/scala/JsonParser.scala
+++ src/main/scala/JsonParser.scala
@@ -6,18 +6,35 @@ object JsonParser {
 
 
   def main(args: Array[String]): Unit = {
-    println("Json parser tests!")
 
-    val source = """{ "some": "JSON source" }"""
-    val jsonAst = source.parseJson // or JsonParser(source)
+    /* Simple Json parsing example */
+    //val source = """{ "some": "JSON source" }"""
+    //val jsonAst = source.parseJson // or JsonParser(source)
 
-    val json: String = jsonAst.prettyPrint // or .compactPrint
-    val json2: String = jsonAst.compactPrint
+    //val json: String = jsonAst.prettyPrint // or .compactPrint
 
-    val toJsonAttendee = Attendee("Nicolas", "123").toJson
-    println(toJsonAttendee.prettyPrint)
-    val fromJsonAttendee = toJsonAttendee.convertTo[Attendee]
-    println(fromJsonAttendee.name)
-    
+
+    /* Simple Json Case class parsing */
+    val toJsonLao: JsValue = LaoCreationMessage("Event0", "pkOrganizer", List("pkW1", "pkW2", "pkW3"), "attestation").toJson
+    //println(toJsonLao.prettyPrint)
+    val fromJsonEvent: LaoCreationMessage = toJsonLao.convertTo[LaoCreationMessage]
+    //println(s"Organizer's key : ${fromJsonEvent.organizer}")
+
+    // Simulate answers to frontEnd
+    val positiveAnswerJson = JsonMessageAnswer(success = true, None).toJson
+    //println(positiveAnswerJson.prettyPrint)
+    val negativeAnswerJson = JsonMessageAnswer(success = false, Some("Error :> invalid format!")).toJson
+    //println(negativeAnswerJson.prettyPrint)
+
+
+    /* Class Json parsing */
+    val testClassInst: TestClassJsonParsing = new TestClassJsonParsing(
+      "test", 101, true, List("k1", "k2"), List(Attendee("Attendee1", "key1"), Attendee("Attendee2", "key2")), Some(3.5)
+    )
+    val testClassInstJson = testClassInst.toJson
+    println(testClassInstJson.prettyPrint)
+
+    val testClassInstCon = testClassInstJson.convertTo[TestClassJsonParsing]
+    //println(s"TestClass attendees : ${testClassInstCon.attendees}")
   }
 }
