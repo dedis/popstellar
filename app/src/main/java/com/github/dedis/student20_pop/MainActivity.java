e932cf5069f7e37b9b247ee88deab24ad4707f66
Jean-Baptiste Michel <jean-baptiste.michel@epfl.ch>
2020-10-20 13:42:58 +0200

    make attendee UI depend on Event class

--- app/src/main/java/com/github/dedis/student20_pop/MainActivity.java
+++ app/src/main/java/com/github/dedis/student20_pop/MainActivity.java
@@ -5,9 +5,13 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ExpandableListView;
+import android.widget.Toast;
 
+import com.github.dedis.student20_pop.attendeeUI.AttendeeFragment;
 import com.github.dedis.student20_pop.attendeeUI.ExpandableListViewAdapter;
+import com.github.dedis.student20_pop.model.Lao;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -28,8 +32,9 @@ public final class MainActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_attendee);
+        setContentView(R.layout.activity_main);
 
+        /*
         expandableListView = findViewById(R.id.exp_list_view);
 
         showList();
@@ -38,20 +43,51 @@ public final class MainActivity extends FragmentActivity {
         expandableListView.setAdapter(listViewAdapter);
         expandableListView.expandGroup(1);
         expandableListView.expandGroup(2);
+        */
 
+        if (findViewById(R.id.fragment_container) != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, new HomeFragment()).commit();
+        }
     }
 
+
+    /**
+     * Manage the fragment change after clicking a specific view.
+     *
+     * @param view the clicked view
+     */
     public void onClick(View view) {
-        switch(view.getId()) {
+        switch (view.getId()) {
             case R.id.tab_home:
                 showFragment(new HomeFragment(), HomeFragment.TAG);
                 break;
             case R.id.tab_connect:
-                showFragment(new ConnectFragment(), ConnectFragment.TAG);
+                //showFragment(new ConnectFragment(), ConnectFragment.TAG);
+                showFragment(new AttendeeFragment(), AttendeeFragment.TAG);
                 break;
             case R.id.tab_launch:
                 showFragment(new LaunchFragment(), LaunchFragment.TAG);
                 break;
+            /*case R.id.button_launch:
+                String name = ((EditText) findViewById(R.id.entry_box_launch)).getText().toString();
+                // Creating the LAO but not sending the information for now
+                Lao lao = new Lao(name);
+                showFragment(new HomeFragment(), LaunchFragment.TAG);
+                Toast.makeText(this,
+                        getResources().getString(R.string.message_launch_successful, name),
+                        Toast.LENGTH_LONG)
+                        .show();
+                break;
+            case R.id.button_cancel_launch:
+                ((EditText) findViewById(R.id.entry_box_launch)).getText().clear();
+                showFragment(new HomeFragment(), LaunchFragment.TAG);
+                break;
+             */
             default:
         }
     }
@@ -61,10 +97,13 @@ public final class MainActivity extends FragmentActivity {
             getSupportFragmentManager()
                     .beginTransaction()
                     .replace(R.id.fragment_container, fragment, TAG)
-                    .addToBackStack(TAG).commit();
+                    .addToBackStack(TAG)
+                    .commit();
         }
     }
 
+
+    /**
     private void showList(){
         eventTypes = new ArrayList<String>();
         eventList = new HashMap<String, List<String>>();
@@ -94,6 +133,7 @@ public final class MainActivity extends FragmentActivity {
         eventList.put(eventTypes.get(2), presentEvents);
         eventList.put(eventTypes.get(3), futureEvents);
     }
+     **/
 
 
 
---
+++ app/src/main/java/com/github/dedis/student20_pop/attendeeUI/AttendeeFragment.java
@@ -0,0 +1,108 @@
+package com.github.dedis.student20_pop.attendeeUI;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.github.dedis.student20_pop.R;
+import com.github.dedis.student20_pop.model.Event;
+import com.github.dedis.student20_pop.model.Event.EventCategory;
+import com.github.dedis.student20_pop.model.Lao;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+//How do we pass the info from Connect UI to Attendee UI ?
+//What does an attendee listen for ? is it passed through the LAO?
+
+public class AttendeeFragment extends Fragment {
+
+    public static final String TAG = AttendeeFragment.class.getSimpleName();
+    ExpandableListViewEventAdapter listViewEventAdapter;
+    ExpandableListView expandableListView;
+    List<EventCategory> categories;
+    HashMap<EventCategory, List<Event>> events;
+    Lao lao = new Lao("My first LAO");  //given from intent or previous fragment
+    boolean d = instantiateEvents(lao);
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_attendee, container, false);
+
+        expandableListView = rootView.findViewById(R.id.exp_list_view);
+
+        getEvents();
+
+        listViewEventAdapter = new ExpandableListViewEventAdapter(this.getActivity(), categories, events);
+        expandableListView.setAdapter(listViewEventAdapter);
+        expandableListView.expandGroup(0);
+        expandableListView.expandGroup(1);
+
+
+        return rootView;
+
+
+    }
+
+    //retrieve events from LAO
+    public void getEvents(){
+        categories = new ArrayList<EventCategory>();
+        categories.add(EventCategory.PAST);
+        categories.add(EventCategory.PRESENT);
+        categories.add(EventCategory.FUTURE);
+
+        events = new HashMap<EventCategory, List<Event>>();
+        events.put(EventCategory.PAST, new ArrayList<Event>());
+        events.put(EventCategory.PRESENT, new ArrayList<Event>());
+        events.put(EventCategory.FUTURE, new ArrayList<Event>());
+
+        for (Event e: lao.getEvents()){
+            switch (e.getCategory()){
+                case PAST:
+                    events.get(EventCategory.PAST).add(e);
+                    break;
+                case PRESENT:
+                    events.get(EventCategory.PRESENT).add(e);
+                    break;
+                case FUTURE:
+                    events.get(EventCategory.FUTURE).add(e);
+                    break;
+                default:
+            }
+        }
+
+        //Still need to order the events in chronological order
+    }
+
+    private boolean instantiateEvents(Lao lao){
+        Event e1 = new Event("Event 1", "Right here", Event.EventType.MEETING, EventCategory.FUTURE);
+        Event e2 = new Event("Event 2", "Somewhere", Event.EventType.DISCUSSION, EventCategory.FUTURE);
+        Event e3 = new Event("Event 3", "I don't know where", Event.EventType.POLL, EventCategory.PRESENT);
+        Event e4 = new Event("Event 4", "Right here", Event.EventType.MEETING, EventCategory.PRESENT);
+        Event e5 = new Event("Event 5", "Not here", Event.EventType.ROLL_CALL, EventCategory.PRESENT);
+        Event e6 = new Event("Event 6", "EPFL", Event.EventType.MEETING, EventCategory.PAST);
+
+        lao.addEvent(e1);
+        lao.addEvent(e2);
+        lao.addEvent(e3);
+        lao.addEvent(e4);
+        lao.addEvent(e5);
+        lao.addEvent(e6);
+
+        return true;
+    }
+
+}
