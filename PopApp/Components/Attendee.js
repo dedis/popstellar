b62acaf608e68eb12aa971e7c52b56c58770e339
maxime <maxime.wursch@epfl.ch>
2020-10-23 21:39:14 +0200

    Apply the coding convention

--- PopApp/Components/Attendee.js
+++ PopApp/Components/Attendee.js
@@ -1,30 +1,25 @@
-import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
 
-import STRINGS from "../res/strings";
+import STRINGS from '../res/strings';
 import { Views } from '../Styles';
-
 
 /**
 * The Attendee component
 *
 * Manage the Attendee screen
 */
-class Attendee extends React.Component {
-    render() {
-        return(
-            <View style={styles.container}>
-                <Text>{STRINGS.attendee_description}</Text>
-            </View>
-        )
-    }
-}
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        ...Views.base,
-    },
-  });
+  container: {
+    flex: 1,
+    ...Views.base,
+  },
+});
 
-export default Attendee
+const Attendee = () => (
+  <View style={styles.container}>
+    <Text>{STRINGS.attendee_description}</Text>
+  </View>
+);
+
+export default Attendee;
--- PopApp/Components/CameraButton.js
+++ PopApp/Components/CameraButton.js
@@ -1,39 +1,45 @@
-import React from 'react'
-import { StyleSheet, Image, View, TouchableOpacity } from 'react-native'
+import React from 'react';
+import {
+  StyleSheet, Image, View, TouchableOpacity,
+} from 'react-native';
+import PropTypes from 'prop-types';
 
-import { Colors } from '../Styles'
-
+import { Colors } from '../Styles';
 
 /**
 * Camera button
 * use action parameter to define the onPress action
 */
 
-class CameraButton extends React.Component {
-    render() {
-        return(
-            <View>
-                <TouchableOpacity style={styles.button} onPress={ () => { this.props.action() } }>
-                    <Image style={styles.icon} source={require("../res/img/ic_camera.png")}/>
-                </TouchableOpacity>
-            </View>
-        )
-    }
-}
+const cameraIc = require('../res/img/ic_camera.png');
 
 const styles = StyleSheet.create({
-    icon:{
-        width: 64,
-        height: 64,
-    },
-    button: {
-        justifyContent: "center",
-        alignItems: "center",
-        width: 80,
-        height: 80,
-        backgroundColor: Colors.blue,
-        borderRadius: 80,
-    }
-  });
+  icon: {
+    width: 64,
+    height: 64,
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 80,
+    height: 80,
+    backgroundColor: Colors.blue,
+    borderRadius: 80,
+  },
+});
 
-export default CameraButton
+function CameraButton({ action }) {
+  return (
+    <View>
+      <TouchableOpacity style={styles.button} onPress={() => { action(); }}>
+        <Image style={styles.icon} source={cameraIc} />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+CameraButton.propTypes = {
+  action: PropTypes.func.isRequired,
+};
+
+export default CameraButton;
