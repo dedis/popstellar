d26bad77b6491fbc78406af219beb83449295e54
Jean-Baptiste Michel <jean-baptiste.michel@epfl.ch>
2020-10-29 18:30:12 +0100

    up to date

--- app/src/test/java/com/github/dedis/student20_pop/KeysTest.java
+++ app/src/test/java/com/github/dedis/student20_pop/KeysTest.java
@@ -1,31 +1,25 @@
 package com.github.dedis.student20_pop;
+
 import com.github.dedis.student20_pop.model.Keys;
 
 import org.junit.Test;
-
-import java.security.GeneralSecurityException;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertThrows;
-
 public class KeysTest {
 
     Keys keyPair1 = new Keys();
     Keys keyPair2 = new Keys();
 
-
     @Test
-    public void getMethodsReturnInTheCorrectFormat(){
-        System.out.print(keyPair1.getPrivateKey());
-        System.out.print(keyPair1.getPublicKey());
+    public void keysAreDisplayedCorrectly(){
+        System.out.println(keyPair1.getPublicKey());
+        System.out.println(keyPair2.getPrivateKey());
     }
 
     @Test
-    public void doesNotCreateTheSameKeypairTwice(){
-            assertNotEquals(keyPair1.getPrivateKey(), keyPair2.getPrivateKey());
-            assertNotEquals(keyPair1.getPublicKey(), keyPair2.getPublicKey());
+    public void doesNotCreateTheSameKeyPair(){
+        assertNotEquals(keyPair1.getPrivateKey(), keyPair2.getPrivateKey());
+        assertNotEquals(keyPair1.getPublicKey(), keyPair2.getPublicKey());
     }
 
     @Test
