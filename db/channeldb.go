efb7d142f3c71272f822db4da3c948bd7659d9e5
Huntsekkern <44240415+Huntsekkern@users.noreply.github.com>
2020-11-02 18:20:29 +0100

    New structure???

--- db/channeldb.go
+++ db/channeldb.go
@@ -16,7 +16,7 @@ const UserName = "user.db"
  * opens the User DB. creates it if not exists.
  * don't forget to close the database afterwards
  */
-func OpenUserDB() (*bolt.DB, error) {
+func OpenChannelDB() (*bolt.DB, error) {
 	db, err := bolt.Open(UserName, 0600, nil)
 	if err != nil {
 		return nil, err
@@ -28,7 +28,7 @@ func OpenUserDB() (*bolt.DB, error) {
 /**
  * Function will return an error if the DB was already initialized
  */
-func InitUserDB(db *bolt.DB) error {
+func InitChannelDB(db *bolt.DB) error {
 	err := db.Update(func(tx *bolt.Tx) error {
 		b, err1 := tx.CreateBucket([]byte("ids"))
 		if err1 != nil {
@@ -47,7 +47,7 @@ func InitUserDB(db *bolt.DB) error {
  * Function to create a new user and store it in the DB
  * @returns : the id of the created user (+ event error)
  */
-func CreateUser(id string) (error) {
+func CreateChannel(id string) (error) {
 
 	db, e := OpenUserDB()
 	defer db.Close()
@@ -77,27 +77,32 @@ func CreateUser(id string) (error) {
 /**
  * Check that the attestation of a user is correct
  */
-func checkUserValidity(id []byte) bool {
+ /*
+func checkChannelValidity(id []byte) bool {
 	user, err := GetFromID(id)
 	attestation := lao.Attestation
 
 	//TODO do something??
 
 	return computed == attestation
-}
+}*/
 
 
 /**
 * Retrieve value from a given ID key, and update it with a new subscribtion or publish rights
 * returns error message
 */
-func SubscribeUserDB (userId []byte, channelId []byte) error {
+func UpdateChannelDB (userId []byte, channelId []byte, action []byte) error {
 
 	//TODO correct the if checks
 	// TODO create functions in jsonHelper addSubscribe, addPublish
-
-	updatedString := addSubscribe(oldString, channelId)
-
+	if(action == "subscriber") {
+		updatedString := addSubscribe(oldString, channelId)
+	} else if(action == "publisher") {
+		updatedString := addPublish(oldString, channelId)
+	} else {
+		return errors.New("action not recognized")
+	}
 
 	db, e := OpenUserDB()
 	defer db.Close()
@@ -121,11 +126,13 @@ func SubscribeUserDB (userId []byte, channelId []byte) error {
 	return err
 }
 
+// ADD Helpers for UpdateChannelDB
+
 
 /**
  * Returns a string which contains the subscribe and publish rights in the user database which matches the id passed an argument
  */
-func GetUserDataFromID(userid []byte) ([]byte, error) {
+func GetChannelDataFromID(userid []byte) ([]byte, error) {
 
 	var data []byte
 
