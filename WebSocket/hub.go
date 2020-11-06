2ae3cf0a784bd01063e21bcd6f1515d4f1aaf205
romain46 <romain.pugin@epfl.ch>
2020-11-06 16:16:08 +0100

    pseudo code structure

--- WebSocket/hub.go
+++ WebSocket/hub.go
@@ -13,11 +13,13 @@ type hub struct {
 	// Registered connections.
 	connections map[*connection]struct{}
 
-	// Inbound messages from the connections.
+	// message to send to the channel
 	message chan []byte
-
 	//channel in wich we have to send the info
 	channel chan []byte
+
+	//msg recieved from the webskt
+	recievedMessage chan []byte
 
 	logMx sync.RWMutex
 	log   [][]byte
@@ -25,22 +27,45 @@ type hub struct {
 
 func NewHub() *hub {
 	h := &hub{
-		connectionsMx: sync.RWMutex{},
-		message:       make(chan []byte),
-		connections:   make(map[*connection]struct{}),
+		connectionsMx:   sync.RWMutex{},
+		message:         make(chan []byte),
+		recievedMessage: make(chan []byte),
+		connections:     make(map[*connection]struct{}),
 	}
 	//publish subscribe go routine !
+	/*
+		go func() {
+			for {
+				msg := <-h.message
+				channel := <-h.channel
+				if channel != 0 {
+					send_to := getSubscribersFromChannel(channel)
+				} else {
+					if(isSubchannel()){
+						send_to := getSubsrcriberFromParentChannel( channel) //Askip Bryan veut autre chose lol
+					}
+					send_to := h.connections
+				}
+				h.connectionsMx.RLock()
+				for c := range send_to {
+					select {
+					case c.send <- msg:
+					// stop trying to send to this connection after trying for 1 second.
+					// if we have to stop, it means that a reader died so remove the connection also.
+					case <-time.After(1 * time.Second):
+						log.Printf("shutting down connection %s", c)
+						h.removeConnection(c)
+					}
+				}
+				h.connectionsMx.RUnlock()
+			}
+		}()*/
 	go func() {
 		for {
-			msg := <-h.message
-			channel := <-h.channel
-			if channel != 0 {
-				send_to := getSubscribersFromChannel(channel)
-			} else {
-				send_to := h.connections
-			}
+
+			msg := <-h.recievedMessage
 			h.connectionsMx.RLock()
-			for c := range send_to {
+			for c := range h.connections {
 				select {
 				case c.send <- msg:
 				// stop trying to send to this connection after trying for 1 second.
