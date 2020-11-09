170106b92e5fd42b9fbaa022f129c649dbc09081
Gabriel Fleischer <gabriel.fleischer@epfl.ch>
2020-11-09 09:01:04 +0100

    Clean everything for the pull request

--- app/src/test/java/com/github/dedis/student20_pop/utility/network/SimpleSocketTest.java
+++ app/src/test/java/com/github/dedis/student20_pop/utility/network/SimpleSocketTest.java
@@ -18,13 +18,20 @@ import javax.websocket.OnMessage;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
+/**
+ * Test the web socket implementation
+ */
 public class SimpleSocketTest {
 
     private static Waiter waiter;
     private static Queue<String> messages;
 
+    /**
+     * Currently test that the mocked server receive every string the client sends.
+     * This wil change once the publish-subscribe protocol is implemented
+     */
     @Test
-    public void startServer() throws DeploymentException, TimeoutException, InterruptedException {
+    public void simpleMessageExchange() throws DeploymentException, TimeoutException, InterruptedException {
         //Setup
         List<String> toSend = new ArrayList<>();
         toSend.add("Test 1");
@@ -37,9 +44,9 @@ public class SimpleSocketTest {
         Server server = new Server("localhost", 2000, "", MockServerEndpoint.class);
         server.start();
 
-        new Thread(() -> {
+        Thread t = new Thread(() -> {
             try {
-                ClientSession session = PoPClientEndpoint.connectToServer(URI.create("ws://localhost:2000/"));
+                ClientProxy session = PoPClientEndpoint.connectToServer(URI.create("ws://localhost:2000/"));
                 for(String s : toSend)
                     session.getSession().getBasicRemote().sendText(s);
                 session.getSession().close();
@@ -47,9 +54,11 @@ public class SimpleSocketTest {
             } catch (DeploymentException | IOException e) {
                 e.printStackTrace();
             }
-        }).start();
+        });
+        t.setDaemon(true);
+        t.start();
 
-        waiter.await(10000);
+        waiter.await(10000, toSend.size() + 1);
 
         server.stop();
     }
@@ -60,6 +69,7 @@ public class SimpleSocketTest {
         @OnMessage
         public void onMessage(String message, Session session) {
             waiter.assertEquals(messages.poll(), message);
+            waiter.resume();
         }
     }
 }
