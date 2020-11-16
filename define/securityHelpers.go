0b2079c4ce84083126f92bc07082269c3c4202e6
romain46 <romain.pugin@epfl.ch>
2020-11-16 14:27:37 +0100

    syntaxic changes

--- define/securityHelpers.go
+++ define/securityHelpers.go
@@ -1,21 +1,22 @@
 package define
 
+import "time"
 
 const MaxSecondsElapsedBetweenLAOCreationAndPublish = 600
 
 func LAOCreatedIsValid(data DataCreateLAO, message Message) bool {
 	isValid := true
-	//the last modified timestamp is equal to the creation timestamp, 
-	if(data.Creation != data.LastModified) {
+	//the last modified timestamp is equal to the creation timestamp,
+	if data.Creation != data.LastModified {
 		isValid = false
 	}
-	//the timestamp is reasonably recent with respect to the server’s clock, 
-	if(data.Creation > time.Now().Unix() || (data.Creation -time.Now().Unix()) > MaxSecondsElapsedBetweenLAOCreationAndPublish) {
+	//the timestamp is reasonably recent with respect to the server’s clock,
+	if data.Creation > time.Now().Unix() || data.Creation-time.Now().Unix() > MaxSecondsElapsedBetweenLAOCreationAndPublish {
 		isValid = false
 	}
-	//the attestation is valid, 
+	//the attestation is valid,
 	// TODO hash function
-	if(message.MessageID != hashTBD(message.Data + message.Signature)) {
+	if message.MessageID != hashTBD(message.Data+message.Signature) {
 		isValid = false
 	}
 	//potentially more checks
