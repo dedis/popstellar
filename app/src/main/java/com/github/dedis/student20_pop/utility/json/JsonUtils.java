a721e2a2bd5dca8fc733288b38294aa790c2c1d3
Gabriel Fleischer <gabriel.fleischer@epfl.ch>
2020-11-17 10:37:23 +0100

    Add a createGson() Method in the JsonUtils file

--- app/src/main/java/com/github/dedis/student20_pop/utility/json/JsonUtils.java
+++ app/src/main/java/com/github/dedis/student20_pop/utility/json/JsonUtils.java
@@ -1,5 +1,10 @@
 package com.github.dedis.student20_pop.utility.json;
 
+import com.github.dedis.student20_pop.model.network.level.high.Message;
+import com.github.dedis.student20_pop.model.network.level.low.ChanneledMessage;
+import com.github.dedis.student20_pop.model.network.level.low.result.Result;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 
@@ -9,6 +14,8 @@ public final class JsonUtils {
     public static final String JSON_RPC_VERSION = "2.0";
 
     public static final String JSON_REQUEST_ID = "id";
+
+    private JsonUtils() {}
 
     public static void testRPCVersion(JsonObject object) throws JsonParseException {
         if(!object.has(JSON_RPC))
@@ -20,5 +27,13 @@ public final class JsonUtils {
     public static void testRPCVersion(String version) throws JsonParseException {
         if(!version.equals(JSON_RPC_VERSION))
             throw new JsonParseException("Unable to parse jsonrpc version : " + version);
+    }
+
+    public static Gson createGson() {
+        return new GsonBuilder()
+                .registerTypeAdapter(ChanneledMessage.class, new JsonLowMessageSerializer())
+                .registerTypeAdapter(Message.class, new JsonMessageSerializer())
+                .registerTypeAdapter(Result.class, new JsonResultSerializer())
+                .create();
     }
 }
