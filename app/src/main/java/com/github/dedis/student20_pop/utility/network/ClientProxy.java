2f575eec591ace54e4d0df37188dde701b4bf0ad
Gabriel Fleischer <gabriel.fleischer@epfl.ch>
2020-11-17 14:14:12 +0100

    Add some proxy implementation
    Add a request-result system

--- app/src/main/java/com/github/dedis/student20_pop/utility/network/ClientProxy.java
+++ app/src/main/java/com/github/dedis/student20_pop/utility/network/ClientProxy.java
@@ -1,36 +1,89 @@
 package com.github.dedis.student20_pop.utility.network;
 
+import android.os.Build;
+import android.util.Base64;
+
+import androidx.annotation.RequiresApi;
+
 import com.github.dedis.student20_pop.model.network.level.high.Message;
+import com.github.dedis.student20_pop.model.network.level.low.Catchup;
+import com.github.dedis.student20_pop.model.network.level.low.ChanneledMessage;
+import com.github.dedis.student20_pop.model.network.level.low.LowLevelMessage;
+import com.github.dedis.student20_pop.model.network.level.low.Publish;
+import com.github.dedis.student20_pop.model.network.level.low.Request;
+import com.github.dedis.student20_pop.model.network.level.low.Subscribe;
+import com.github.dedis.student20_pop.model.network.level.low.Unsubscribe;
+import com.github.dedis.student20_pop.model.network.level.low.result.Failure;
+import com.github.dedis.student20_pop.model.network.level.low.result.Result;
+import com.github.dedis.student20_pop.model.network.level.low.result.Success;
+import com.github.dedis.student20_pop.model.network.level.mid.MessageContainer;
+import com.github.dedis.student20_pop.utility.json.JsonUtils;
+import com.github.dedis.student20_pop.utility.security.Hash;
+import com.github.dedis.student20_pop.utility.security.Signature;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.SplittableRandom;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
 
 import javax.websocket.Session;
 
 /**
  * A proxy of a connection to a websocket. It encapsulate the publish-subscribe protocol
  */
+@RequiresApi(api = Build.VERSION_CODES.N)
 public final class ClientProxy {
 
     private final Session session;
 
+    private final Gson gson = JsonUtils.createGson();
+    private final Map<Integer, CompletableFuture<JsonElement>> requests = new ConcurrentHashMap<>();
+    private SplittableRandom rand = new SplittableRandom();
+
     public ClientProxy(Session session) {
         this.session = session;
+    }
+
+    private <T> CompletableFuture<T> makeRequest(Class<T> requestType, Function<Integer, Request> requestSupplier) {
+        CompletableFuture<JsonElement> result = new CompletableFuture<>();
+        // Put the result to a random id, if it is already taken, generate a new id until it fits
+        int id;
+        do {
+            id = rand.nextInt();
+        }while(requests.putIfAbsent(id, result) != null);
+
+        Request request = requestSupplier.apply(id);
+        session.getAsyncRemote().sendText(gson.toJson(request, ChanneledMessage.class));
+
+        return result.thenApply(elem -> gson.fromJson(elem, requestType));
     }
 
     /**
      * Subscribe to a channel
      *
      * @param channel to subscribe to
+     * @return a completable future holding the response value
      */
-    public void subscribe(String channel) {
-        //TODO
+    public CompletableFuture<Integer> subscribe(String channel) {
+        return makeRequest(Integer.class, id -> new Subscribe(channel, id));
     }
 
     /**
      * Unsubscribe to a channel
      *
      * @param channel to subscribe to
+     * @return a completable future holding the response value
      */
-    public void unsubscribe(String channel) {
-        //TODO
+    public CompletableFuture<Integer> unsubscribe(String channel) {
+        return makeRequest(Integer.class, id -> new Unsubscribe(channel, id));
     }
 
     /**
@@ -38,18 +91,26 @@ public final class ClientProxy {
      *
      * @param channel to publish the event on
      * @param message to publish
+     *
+     * @return a completable future holding the response value
      */
-    public void publish(String channel, Message message) {
-        //TODO
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public CompletableFuture<Integer> publish(String sender, String channel, Message message) {
+        String data = Base64.encodeToString(gson.toJson(message, Message.class).getBytes(StandardCharsets.UTF_8), Base64.DEFAULT);
+        String signature = Signature.sign(/* TODO */ "sign", data);
+        String msgId = Hash.hash(data + signature);
+        MessageContainer container = new MessageContainer(sender, data, signature, msgId, new ArrayList<>());
+        return makeRequest(Integer.class, id -> new Publish(channel, id, container));
     }
 
     /**
-     * fetch events from the given channel
+     * Catchup on missed messages from a given channel
      *
      * @param channel to fetch from
      */
-    public void catchup(String channel) {
-        //TODO fetch data. Is it a request ?
+    public CompletableFuture<List<Message>> catchup(String channel) {
+        CompletableFuture<Message[]> future = makeRequest(Message[].class, id -> new Catchup(channel, id));
+        return future.thenApply(Arrays::asList);
     }
 
     /**
@@ -57,7 +118,39 @@ public final class ClientProxy {
      * @param msg received
      */
     public void onMessage(String msg) {
-        //TODO convert msg to object with json
+        JsonObject obj = gson.fromJson(msg, JsonObject.class);
+        if(obj.has("method")) {
+            handleMessage(gson.fromJson(obj, LowLevelMessage.class));
+        } else {
+            handleResult(gson.fromJson(obj, Result.class));
+        }
+    }
+
+    private void handleResult(Result result) {
+        CompletableFuture<JsonElement> future = requests.remove(result.getId());
+        if(future == null)
+            throw new Error("Received unknown result id");
+
+        // I love match cases
+        if(result instanceof Success) {
+            Success success = (Success) result;
+            future.complete(success.getResult());
+        } else if(result instanceof Failure) {
+            Failure failure = (Failure) result;
+            future.completeExceptionally(new RuntimeException("Error code " + failure.getError().getCode() + " : " + failure.getError().getDescription()));
+        } else {
+            throw new Error("Unknown result class");
+        }
+    }
+
+    private void handleMessage(LowLevelMessage lowLevelMessage) {
+        MessageContainer container = lowLevelMessage.getMessage();
+        Message message = gson.fromJson(
+                new String(
+                        Base64.decode(container.getData(), Base64.DEFAULT),
+                        StandardCharsets.UTF_8),
+                Message.class);
+        System.out.println(message);
     }
 
     // TODO remove this once not needed for testing (when the tests uses the publish-subscribe protocol
