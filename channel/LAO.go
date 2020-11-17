bf669eda2b6edaa6862ddcc8a3257ba8d86b2bac
romain46 <romain.pugin@epfl.ch>
2020-11-17 10:03:40 +0100

    adapted LAO for DB

--- channel/LAO.go
+++ channel/LAO.go
@@ -1,42 +1,32 @@
 package channel
 
 import (
+	"encoding/json"
 	"errors"
 	"github.com/boltdb/bolt"
 	"student20_pop/db"
 	"student20_pop/define"
-	"encoding/json"
 )
 
 // would be nice to have an interface that contains methods add, remove and edit for LAO, event and vote
-
-/*
- * opens the LAO DB. creates it if not exists.
- * don't forget to close the database afterwards
- * TODO : Keep it ? or put everything in the Channel DB ?
- */
-func OpenLAODB() (*bolt.DB, error) {
-	return db.OpenDB(db.DatabaseChannel)
-}
 
 /**
  * Function to create a new LAO and store it in the DB
  * @returns : error
  */
 func CreateLAO(data define.LAO) error {
-	// TODO openLAODB might change if we have a single DB
-	db, e := OpenLAODB()
+	db, e := db.OpenChannelDB()
 	defer db.Close()
 	if e != nil {
 		return e
 	}
 
 	err := db.Update(func(tx *bolt.Tx) error {
-		b, err1 := tx.CreateBucketIfNotExists([]byte("LAO"))
+		b, err1 := tx.CreateBucketIfNotExists([]byte(bucketChannel))
 		if err1 != nil {
 			return err1
 		}
-
+		//checks if there is already an entry with that ID
 		key := b.Get(data.ID)
 		if key != nil {
 			return errors.New("unable to create new LAO because of hash collision")
@@ -48,14 +38,10 @@ func CreateLAO(data define.LAO) error {
 			return err2
 		}
 		err3 := b.Put(data.ID, dt)
-		if err3 != nil {
-			return err3
-		}
 
-		return nil
+		return err3
 	})
 
-	//TODO cleanup if failed
 	return err
 }
 
