59f96b0fb33c69198e31fee64d416a8412ddf538
Huntsekkern <44240415+Huntsekkern@users.noreply.github.com>
2020-11-17 09:48:44 +0100

    add createResponse, createBroadcast + start errors

--- define/jsonhelper.go
+++ define/jsonhelper.go
@@ -3,6 +3,7 @@ package define
 import (
 	"encoding/json"
 	"strconv"
+	"log"
 )
 
 /*Most generic message structure*/
@@ -158,22 +159,47 @@ func SliceToJson(title string, data [][]byte) string {
 	return str
 }
 
+func CreateResponse(err error, generic Generic) []byte {
+	e := ErrToInt(err)
+	return []byte (ResponseToSenderInJson(e, generic.id))
+}
+
+func CreateBroadcast(message Message, generic Generic) []byte {
+	broadc := Generic{
+		jsonrpc:	generic.jsonrpc,
+		Method:		"message",
+		Params: 	generic.Params,
+		id:			generic.id,
+	}
+	b, err := json.Marshal(broadc)
+
+	if err != nil {
+		log.Fatal("couldn't Marshal the message to broadcast")
+	}
+	
+	return []byte (b)
+}
+
+
+
 /*
 * Function that converts a Lao to a Json byte array
 * we suppose error is in the good range
  */
-func ResponseToSenderInJson(error int) string {
+func ResponseToSenderInJson(err int, id string) string {
 	str := "{\"jsonrpc\": \"2.0\","
-	if error != 0 {
+	if err != 0 {
 		str += "{ \"error\": { \"code\":"
-		str += strconv.Itoa(error)
+		str += strconv.Itoa(err)
 		str += ",\"description\":"
-		str += selectDescriptionError(error)
+		str += selectDescriptionError(err)
 		str += "}"
 	} else {
 		str += " \"result\": 0"
 	}
-	str += ",\"id\": 3}"
+	str += ",\"id\": "
+	str += id
+	str += "}"
 	return str
 }
 
@@ -181,7 +207,7 @@ func ResponseToSenderInJson(error int) string {
 *	return the associate description error
 *	we check the validity (error vetween -1 and -5) before the function
  */
-func selectDescriptionError(err int) string {
+func selectDescriptionError(err error) string {
 	switch err {
 	case -1:
 		return "\"invalid action\""
