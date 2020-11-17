0a4591ab2d7b95af8987a9d07e48f1349646e4eb
romain46 <romain.pugin@epfl.ch>
2020-11-17 10:36:56 +0100

    adapted LAO for DB

--- channel/LAO.go
+++ channel/LAO.go
@@ -2,7 +2,6 @@ package channel
 
 import (
 	"encoding/json"
-	"errors"
 	"github.com/boltdb/bolt"
 	"student20_pop/db"
 	"student20_pop/define"
@@ -14,7 +13,7 @@ import (
  * Function to create a new LAO and store it in the DB
  * @returns : error
  */
-func CreateLAO(data define.LAO) error {
+func writeLAO(lao define.LAO, secure bool) error {
 	db, e := db.OpenChannelDB()
 	defer db.Close()
 	if e != nil {
@@ -26,18 +25,24 @@ func CreateLAO(data define.LAO) error {
 		if err1 != nil {
 			return err1
 		}
-		//checks if there is already an entry with that ID
-		key := b.Get(data.ID)
-		if key != nil {
-			return errors.New("unable to create new LAO because of hash collision")
+		//checks if there is already an entry with that ID if secure is true
+		if secure {
+			key := b.Get(lao.ID)
+			if key != nil {
+				return define.ErrResourceAlreadyExists
+			}
+		} else {
+			exists := b.Get(lao.ID)
+			if exists == nil {
+				return define.ErrInvalidResource
+			}
 		}
-
 		// Marshal the LAO and store it
-		dt, err2 := json.Marshal(data)
+		dt, err2 := json.Marshal(lao)
 		if err2 != nil {
 			return err2
 		}
-		err3 := b.Put(data.ID, dt)
+		err3 := b.Put(lao.ID, dt)
 
 		return err3
 	})
@@ -45,14 +50,29 @@ func CreateLAO(data define.LAO) error {
 	return err
 }
 
-//TODO
-func UpdateLao(lao define.LAO) error {
-	// TODO adapt struct
-	return nil
+/*writes a lao to the DB, returns an error if ID already is key in DB*/
+func CreateLAO(lao define.LAO) error {
+	return writeLAO(lao, true)
 }
 
-func GetFromID(id []byte) (define.LAO, error) {
-	// TODO adapt struct
-	//TODO
-	return define.LAO{}, nil
+/*writes a lao to the DB, regardless of ID already exists*/
+func UpdateLao(lao define.LAO) error {
+	return writeLAO(lao, false)
+}
+
+/*returns channel data from a given ID */
+func GetFromID(id []byte) []byte {
+	db, e := db.OpenChannelDB()
+	defer db.Close()
+	if e != nil {
+		return nil
+	}
+	var data []byte
+	e = db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucketChannel))
+		data = b.Get(id)
+		return nil
+	})
+
+	return data
 }
