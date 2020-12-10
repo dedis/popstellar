4ca95f37268415f04fdb39544fec22fcba91e2e1
Nicolas Raulin <nicolas.raulin@epfl.ch>
2020-12-10 06:12:05 +0100

    Cherry-pick websocket communication with server

---
+++ PopApp/websockets/WebsocketAnswer.js
@@ -0,0 +1,129 @@
+// eslint-disable-next-line import/no-cycle
+import WebsocketLink from './WebsocketLink';
+import {
+  actions, JSON_RPC_VERSION, objects, PendingRequest, SERVER_ANSWER_FIELD_COUNT, fromString64,
+  getCurrentLao,
+} from './WebsocketUtils';
+import { getStore } from '../Store/configureStore';
+
+/** Maximum amount of identical queries retries */
+const MAX_QUERY_RETRIES = 3;
+/** Enumeration of all message object fields in a server answer */
+const answerProperties = Object.freeze({
+  JSONRPC: 'jsonrpc',
+  RESULT: 'result',
+  ERROR: 'error',
+  ID: 'id',
+});
+
+/** Enumeration of all error object fields in a server answer */
+const errorProperties = Object.freeze({
+  CODE: 'code',
+  DESCRIPTION: 'description',
+});
+
+/** Handles callbacks when receiving a server answer */
+const handleServerAnswer = (message) => {
+  const obj = JSON.parse(message.data);
+
+  console.log('handling a new answer : ', obj);
+
+  /* check that the answer is of a valid format (positive or negative) */
+
+  // check that the object has exactly SERVER_ANSWER_FIELD_COUNT fields
+  if (Object.keys(obj).length !== SERVER_ANSWER_FIELD_COUNT) return;
+
+  // check that the object has both required field and that the protocol used is correct
+  if (!Object.prototype.hasOwnProperty.call(obj, answerProperties.JSONRPC)
+      || obj.jsonrpc !== JSON_RPC_VERSION
+  ) return;
+  if (!Object.prototype.hasOwnProperty.call(obj, answerProperties.ID)
+      || !Number.isInteger(obj.id)
+  ) return;
+
+  // check that the object has exactly one of the two optional fields (XOR)
+  if (Object.prototype.hasOwnProperty.call(obj, answerProperties.RESULT)
+      === Object.prototype.hasOwnProperty.call(obj, answerProperties.ERROR)
+  ) return;
+
+  // if the message answers un unknown request, then drop the message
+  if (!WebsocketLink.getPendingProperties().has(obj.id)) return;
+
+  // processes server answer
+  if (Object.prototype.hasOwnProperty.call(obj, answerProperties.RESULT)) {
+    // processes positive server answer
+    if (obj.result === 0) {
+      // general positive answer
+      const answer = WebsocketLink.getPendingProperties().get(obj.id);
+
+      // handle "callbacks" if needed
+      switch (answer.requestObject) { // update_lao
+        case objects.LAO:
+          if (answer.requestAction === actions.CREATE) {
+            // callback for a successful create LAO request
+            const jsonMessage = answer.message;
+            jsonMessage.params.channel = fromString64(jsonMessage.params.channel);
+            jsonMessage.params.message.data = JSON.parse(fromString64(jsonMessage.params.message.data));
+
+            // store new LAO
+            getStore().dispatch({ type: 'SET_CURRENT_LAO', value: jsonMessage });
+          } else if (answer.requestAction === actions.UPDATE_PROPERTIES) {
+            // callback for a successful update LAO request
+            const jsonMessageData = answer.message.params.message.data;
+            const updatedLao = JSON.parse(JSON.stringify(getCurrentLao()));
+
+            // modify elements
+            updatedLao.params.message.data.name = jsonMessageData.name;
+            updatedLao.params.message.data.last_modified = jsonMessageData.last_modified;
+            updatedLao.params.message.data.witnesses = jsonMessageData.witnesses;
+
+            // store updated LAO
+            getStore().dispatch({ type: 'SET_CURRENT_LAO', value: updatedLao });
+          } else if (answer.requestAction === actions.STATE) {
+            console.error('TODO (in WebsocketAnswer) : case (answer.requestAction === actions.STATE)');
+          }
+          break;
+
+        case objects.MESSAGE:
+          console.error('TODO (in WebsocketAnswer) : case (objects.MESSAGE)');
+          break;
+
+        case objects.MEETING:
+          console.error('TODO (in WebsocketAnswer) : case (objects.MEETING)');
+          break;
+
+        default:
+          break;
+      }
+
+      WebsocketLink.getPendingProperties().delete(obj.id);
+    } else {
+      console.error('TODO handle propagate (+ what if none of those two possibilities, silent return?)');
+    }
+  } else {
+    // processes negative server answer (error)
+    const { error } = obj;
+
+    if (typeof error !== 'object' || error === null) return;
+    if (!Object.prototype.hasOwnProperty.call(error, errorProperties.CODE)
+        || !Object.prototype.hasOwnProperty.call(error, errorProperties.DESCRIPTION)
+    ) return;
+    if (!Number.isInteger(error.code) || error.code < -5 || error.code > -1) return;
+    if (typeof error.description !== 'string') return;
+
+    const query = WebsocketLink.getPendingProperties().get(obj.id);
+    const { retryCount } = WebsocketLink.getPendingProperties().get(obj.id);
+    if (retryCount < MAX_QUERY_RETRIES) {
+      WebsocketLink.getPendingProperties().set(
+        obj.id,
+        new PendingRequest(query.message, query.requestObject, query.requestAction, retryCount + 1),
+      );
+      WebsocketLink.sendRequestToServer(query.message, true);
+    } else {
+      console.error(`max retryCount (${MAX_QUERY_RETRIES}) reached! Failing query : `, query);
+      WebsocketLink.getPendingProperties().delete(obj.id);
+    }
+  }
+};
+
+export default handleServerAnswer;
---
+++ PopApp/websockets/WebsocketLink.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import { w3cwebsocket as W3CWebSocket } from 'websocket';
+// eslint-disable-next-line import/no-cycle
+import handleServerAnswer from './WebsocketAnswer';
+import { PendingRequest } from './WebsocketUtils';
+
+/* global window */ // do not touch! EsLint required comment!
+
+const WEBSOCKET_READYSTATE_INTERVAL_MS = 10;
+const WEBSOCKET_READYSTATE_MAX_ATTEMPTS = 100;
+
+/* TEMP */
+const SERVER_ADDRESS = {
+  address: '127.0.0.1',
+  port: '8000', // '8080',
+  path: '', // 'ps's
+};
+
+export default class WebsocketLink {
+  // websocket connexion
+  static #ws;
+
+  // map of pending queries sent to server
+  static #pendingQueries;
+
+  /**
+   * Send a request (query) to the server
+   *
+   * @param message (JsonObject) message for the server
+   * @param requestObject (objects enum) object of the message
+   * @param requestAction (actions enum) action of the message
+   * @param retry (boolean) is the query a retry?
+   */
+  static sendRequestToServer(message, requestObject, requestAction, retry = false) {
+    if (this.#ws == null) {
+      WebsocketLink._initWebsocket(
+        SERVER_ADDRESS.address, SERVER_ADDRESS.port, SERVER_ADDRESS.path,
+      );
+    }
+
+    WebsocketLink._sendMessage(message, requestObject, requestAction, retry);
+  }
+
+  /** Return a map of (id -> PendingRequest) which are pending requests sent to the server */
+  static getPendingProperties() { return this.#pendingQueries; }
+
+  /** Initialize a websocket connexion */
+  static _initWebsocket(address = '127.0.0.1', port = '8000', path = '') {
+    let p = path;
+    if (path !== '') { p = `/${path}`; }
+    const ws = new W3CWebSocket(`ws://${address}:${port}${p}`);
+
+    ws.onopen = () => { console.log(`initiating web socket : ws://${address}:${port}`); };
+    ws.onmessage = (message) => { handleServerAnswer(message); };
+    // TODO on error
+
+    this.#ws = ws;
+    this.#pendingQueries = new Map();
+  }
+
+  /** Signal when an open websocket connexion is ready to be used */
+  static _waitWebsocketReady(resolveWebsocketReady, rejectWebsocketReady) {
+
+    if (!this.#ws.readyState) {
+      let count = 0;
+
+      const id = window.setInterval(() => {
+        if (!this.#ws.readyState && count < WEBSOCKET_READYSTATE_MAX_ATTEMPTS) {
+          count += 1;
+        } else {
+          if (count === WEBSOCKET_READYSTATE_MAX_ATTEMPTS) {
+            rejectWebsocketReady(
+              `Maximum waiting time for websocket to be ready reached : 
+              ${WEBSOCKET_READYSTATE_MAX_ATTEMPTS * WEBSOCKET_READYSTATE_INTERVAL_MS}
+              [ms] (_waitWebsocketReady)`,
+            );
+          } else resolveWebsocketReady();
+          window.clearInterval(id);
+        }
+      }, WEBSOCKET_READYSTATE_INTERVAL_MS);
+    } else {
+      resolveWebsocketReady();
+    }
+  }
+
+  /**
+   * Effectively send a message to the server
+   *
+   * @param message (JsonObject) message for the server
+   * @param requestObject (objects enum) object of the message
+   * @param requestAction (actions enum) action of the message
+   * @param retry (boolean) is the query a retry?
+   */
+  static _sendMessage(message, requestObject, requestAction, retry) {
+
+    // Check that the websocket connection is ready
+    if (!this.#ws.readyState) {
+      let promise = new Promise((resolveWebsocketReady, rejectWebsocketReady) => {
+        this._waitWebsocketReady(resolveWebsocketReady, rejectWebsocketReady);
+      });
+
+      promise.then(
+        () => this._sendMessage(message, requestObject, requestAction, retry),
+        (error) => console.error('(TODO)', error),
+      );
+    } else {
+      // websocket ready to be used, message can be sent
+      if (!retry) {
+        this.#pendingQueries.set(
+          message.id,
+          new PendingRequest(message, requestObject, requestAction),
+        );
+      }
+      this.#ws.send(JSON.stringify(message));
+    }
+  }
+}
