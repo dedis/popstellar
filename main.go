002789f2899cdf4dea205946b9a6a339291d6564
romain46 <romain.pugin@epfl.ch>
2020-12-16 14:26:25 +0100

    added flag parsing

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 
 import (
 	"flag"
-	"os"
 	"strings"
 	"student20_pop/network"
 	"text/template"
@@ -15,26 +14,31 @@ import (
 // this function basically makes the webserver run
 func main() {
 
-	mode := os.Args[1]
+	var mode = flag.String("m", "o", "server mode")
+	var address = flag.String("a", "", "IP on which to run the server")
+	var port = flag.String("p", "8000", "port on which the server listens for websocket connections")
+	var pkey = flag.String("k", "oui", "actor's public key")
+	var file = flag.String("f", "org.db", "file for the actor to store it's database")
+
 	flag.Parse()
 	tpl := template.Must(template.ParseFiles("index.html"))
 
-	switch strings.ToLower(mode) {
+	switch strings.ToLower(*mode) {
 	case "o":
-		h := network.NewOrganizerHub(os.Args[3], os.Args[4])
-		router := http.NewServeMux()
-		//router.Handle("/", network.HomeHandler(tpl))
-		router.Handle("/", network.NewWSHandler(h))
-		log.Printf("serving organizer on address " + os.Args[2])
-		log.Fatal(http.ListenAndServe(os.Args[2], router)) //here to change the srv address
-
-	case "w":
-		h := network.NewWitnessHub(os.Args[3], os.Args[4])
+		h := network.NewOrganizerHub(*pkey, *file)
 		router := http.NewServeMux()
 		router.Handle("/", network.HomeHandler(tpl))
 		router.Handle("/ws", network.NewWSHandler(h))
-		log.Printf("serving witness on adress " + os.Args[2])
-		log.Fatal(http.ListenAndServe(os.Args[2], router)) //here to change the srv address
+		log.Printf("serving organizer on address " + *address + ":" + *port)
+		log.Fatal(http.ListenAndServe(*address+":"+*port, router)) //here to change the srv address
+
+	case "w":
+		h := network.NewWitnessHub(*pkey, *file)
+		router := http.NewServeMux()
+		router.Handle("/", network.HomeHandler(tpl))
+		router.Handle("/ws", network.NewWSHandler(h))
+		log.Printf("serving witness on adress " + *address + ":" + *port)
+		log.Fatal(http.ListenAndServe(*address+":"+*port, router)) //here to change the srv address
 
 	}
 
