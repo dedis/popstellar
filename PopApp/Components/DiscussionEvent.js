11daa93af39a1faf436314aebc28ce78cbcdd2cf
maximelaval <maxime.wursch@epfl.ch>
2020-12-24 11:40:53 +0100

    Correct lint problems

--- PopApp/Components/DiscussionEvent.js
+++ PopApp/Components/DiscussionEvent.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-cycle */
 import React from 'react';
 import {
   StyleSheet, View, Text, FlatList, TextInput, Button,
--- PopApp/Components/Launch.js
+++ PopApp/Components/Launch.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+// TODO remove the line above when console will not be use
 import React from 'react';
 import {
   StyleSheet, View, Text, Button, TextInput,
