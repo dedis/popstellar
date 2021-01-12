c9e73893782830c15e6c785a79ffb819e47a58a0
celinecamacho <celine.camacho@epfl.ch>
2021-01-12 15:28:05 +0100

    Comment and test Catchup.java

--- app/src/main/java/com/github/dedis/student20_pop/model/network/layer/base/Catchup.java
+++ app/src/main/java/com/github/dedis/student20_pop/model/network/layer/base/Catchup.java
@@ -1,10 +1,16 @@
 package com.github.dedis.student20_pop.model.network.layer.base;
 
 /**
- * Query to catchup on missed messages.
+ * Query to catchup on missed messages
  */
 public final class Catchup extends Query {
 
+    /**
+     * Constructor for a Catchup
+     *
+     * @param channel name of the channel
+     * @param id request ID of the catchup
+     */
     public Catchup(String channel, int id) {
         super(channel, id);
     }
