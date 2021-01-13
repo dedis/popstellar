88973e636a289055db2d6567842f70eaa72fad82
Gabriel Fleischer <gabriel.fleischer@epfl.ch>
2021-01-13 10:19:27 +0100

    Change Json Schema validation library to accept 2019-09 draft

--- app/src/test/java/com/github/dedis/student20_pop/utility/json/TestJson.java
+++ app/src/test/java/com/github/dedis/student20_pop/utility/json/TestJson.java
@@ -23,16 +23,22 @@ import com.github.dedis.student20_pop.model.network.method.message.data.message.
 import com.github.dedis.student20_pop.model.network.method.message.data.rollcall.CloseRollCall;
 import com.github.dedis.student20_pop.model.network.method.message.data.rollcall.CreateRollCall;
 import com.github.dedis.student20_pop.model.network.method.message.data.rollcall.OpenRollCall;
-import com.github.fge.jsonschema.core.exceptions.ProcessingException;
-import com.github.fge.jsonschema.main.JsonSchema;
-import com.github.fge.jsonschema.main.JsonSchemaFactory;
 import com.google.gson.Gson;
+import com.networknt.schema.JsonMetaSchema;
+import com.networknt.schema.JsonSchema;
+import com.networknt.schema.JsonSchemaFactory;
+import com.networknt.schema.SchemaValidatorsConfig;
+import com.networknt.schema.ValidationMessage;
+import com.networknt.schema.uri.URIFactory;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.net.URI;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Set;
 
 /**
  * Test object encoding and decoding with Gson
@@ -45,102 +51,134 @@ public class TestJson {
 
     private final ObjectMapper mapper = new ObjectMapper();
     private JsonSchema lowSchema;
-    private JsonSchema highSchema;
+    private JsonSchemaFactory factory;
 
     @Before
-    public void setupSchema() throws ProcessingException {
-        lowSchema = JsonSchemaFactory.byDefault().getJsonSchema("resource:/schema/genericMessage.json");
-        highSchema = JsonSchemaFactory.byDefault().getJsonSchema("resource:/schema/query/method/message/data/data.json");
+    public void setupSchema() {
+        factory = new JsonSchemaFactory.Builder()
+                .defaultMetaSchemaURI(JsonMetaSchema.getV201909().getUri())
+                .addMetaSchema(JsonMetaSchema.getV201909())
+                .uriFactory(new URIFactory() {
+                    @Override
+                    public URI create(String uri) {
+                        return URI.create(uri.replace("https://github.com/dedis/student20_pop/tree/proto-specs/", "resource:/schema/"));
+                    }
+
+                    @Override
+                    public URI create(URI baseURI, String segment) {
+                        return URI.create("resource:/schema/" + segment);
+                    }
+                }, "https")
+                .build();
+        lowSchema = factory.getSchema(URI.create("resource:/schema/genericMessage.json"), new SchemaValidatorsConfig());
     }
 
-    private void testChanneledMessage(Message msg) throws JsonProcessingException, ProcessingException {
+    private void testMessage(Message msg) throws JsonProcessingException {
         String json = gson.toJson(msg, Message.class);
-        Assert.assertTrue(lowSchema.validate(mapper.readTree(json)).isSuccess());
+        Set<ValidationMessage> errors = lowSchema.validate(mapper.readTree(json));
+        if (errors.size() != 0) System.out.println(errors);
+        Assert.assertEquals(0, errors.size());
         Assert.assertEquals(msg, gson.fromJson(json, Message.class));
     }
 
-    private void testResult(Answer msg) throws JsonProcessingException, ProcessingException {
+    private void testResult(Answer msg) throws JsonProcessingException {
         String json = gson.toJson(msg, Answer.class);
-        Assert.assertTrue(lowSchema.validate(mapper.readTree(json)).isSuccess());
+        Set<ValidationMessage> errors = lowSchema.validate(mapper.readTree(json));
+    if (errors.size() != 0) System.out.println(errors);
+        Assert.assertEquals(0, errors.size());
         Assert.assertEquals(msg, gson.fromJson(json, Answer.class));
     }
 
-    private void testData(Data msg) throws JsonProcessingException, ProcessingException {
+    private void testData(Data msg) throws JsonProcessingException {
         String json = gson.toJson(msg, Data.class);
-        Assert.assertTrue(highSchema.validate(mapper.readTree(json)).isSuccess());
+        JsonSchema schema = factory.getSchema(URI.create("resource:/schema/query/method/message/data/data" + msg.getClass().getSimpleName() + ".json"), new SchemaValidatorsConfig());
+        Set<ValidationMessage> errors = schema.validate(mapper.readTree(json));
+        if (errors.size() != 0) System.out.println(errors);
+        Assert.assertEquals(0, errors.size());
         Assert.assertEquals(msg, gson.fromJson(json, Data.class));
     }
 
     @Test
-    public void testSubscribe() throws JsonProcessingException, ProcessingException {
-        testChanneledMessage(new Subscribe("test", 0));
+    public void testSubscribe() throws JsonProcessingException {
+        testMessage(new Subscribe("/root/test", 0));
     }
 
     @Test
-    public void testUnsubscribe() throws JsonProcessingException, ProcessingException {
-        testChanneledMessage(new Unsubscribe("test", 0));
+    public void testUnsubscribe() throws JsonProcessingException {
+        testMessage(new Unsubscribe("/root/test", 0));
     }
 
 
     @Test
-    public void testPublish() throws JsonProcessingException, ProcessingException {
-        testChanneledMessage(new Publish("test", 0,
-                new MessageGeneral("sender", "data", "signature", "id", Arrays.asList("witness1", "witness2"))));
+    public void testPublish() throws JsonProcessingException {
+        testMessage(new Publish("/root", 0,
+                new MessageGeneral("sender", "data", "signature", "id", Collections.emptyList())));
     }
 
     @Test
-    public void testCatchup() throws JsonProcessingException, ProcessingException {
-        testChanneledMessage(new Catchup("test", 0));
+    public void testCatchup() throws JsonProcessingException {
+        testMessage(new Catchup("/root/test", 0));
     }
 
     @Test
-    public void testMessageLow() throws JsonProcessingException, ProcessingException {
-        testChanneledMessage(new Broadcast("test",
-                new MessageGeneral("sender", "data", "signature", "id", Arrays.asList("witness1", "witness2"))));
+    public void testBroadcast() throws JsonProcessingException {
+        testMessage(new Broadcast("/root/test",
+                new MessageGeneral("sender", "data", "signature", "id", Collections.emptyList())));
     }
 
     @Test
-    public void testSuccess() throws JsonProcessingException, ProcessingException {
-        testResult(new Result(0, gson.toJsonTree(40)));
+    public void testMessageGeneral() throws JsonProcessingException {
+        MessageGeneral msg = new MessageGeneral("sender", "data", "signature", "id", Collections.emptyList());
+        String json = gson.toJson(msg, MessageGeneral.class);
+        JsonSchema schema = factory.getSchema(URI.create("resource:/schema/query/method/message/MessageGeneral.json"), new SchemaValidatorsConfig());
+        Set<ValidationMessage> errors = schema.validate(mapper.readTree(json));
+        if (errors.size() != 0) System.out.println(errors);
+        Assert.assertEquals(0, errors.size());
+        Assert.assertEquals(msg, gson.fromJson(json, MessageGeneral.class));
     }
 
     @Test
-    public void testFailure() throws JsonProcessingException, ProcessingException {
-        testResult(new Error(4, new ErrorCode(4, "Test")));
+    public void testSuccess() throws JsonProcessingException {
+        testResult(new Result(4, gson.toJsonTree(0)));
     }
 
     @Test
-    public void testCreateLao() throws JsonProcessingException, ProcessingException {
+    public void testError() throws JsonProcessingException {
+        testResult(new Error(4, new ErrorCode(-4, "Test")));
+    }
+
+    @Test
+    public void testCreateLao() throws JsonProcessingException {
         testData(new CreateLao("id", "name", 12L, "organizer", Arrays.asList("witness1", "witness2")));
     }
 
     @Test
-    public void testStateLao() throws JsonProcessingException, ProcessingException {
+    public void testStateLao() throws JsonProcessingException {
         testData(new StateLao("id", "name", 12L, 202L, "organizer", Arrays.asList("witness1", "witness2")));
     }
 
     @Test
-    public void testUpdateLao() throws JsonProcessingException, ProcessingException {
+    public void testUpdateLao() throws JsonProcessingException {
         testData(new UpdateLao("id", "name", 202L, Arrays.asList("witness1", "witness2")));
     }
 
     @Test
-    public void testCreateMeeting() throws JsonProcessingException, ProcessingException {
-        testData(new CreateMeeting("id", "name", 12L, 202L, "location", 40, 231));
+    public void testCreateMeeting() throws JsonProcessingException {
+        testData(new CreateMeeting("id", "name", 12L, "location", 40, 231));
     }
 
     @Test
-    public void testStateMeeting() throws JsonProcessingException, ProcessingException {
-        testData(new StateMeeting("id", "name", 12L, 202L, "location", 40, 231));
+    public void testStateMeeting() throws JsonProcessingException {
+        testData(new StateMeeting("id", "name", 12L, 202L, "location", 40, 231, "modId", Collections.emptyList()));
     }
 
     @Test
-    public void testWitnessMessage() throws JsonProcessingException, ProcessingException {
+    public void testWitnessMessage() throws JsonProcessingException {
         testData(new WitnessMessage("id", "signature"));
     }
 
     @Test
-    public void testCreateRollCall() throws JsonProcessingException, ProcessingException {
+    public void testCreateRollCall() throws JsonProcessingException {
         testData(new CreateRollCall("id", "name", 432, 231, CreateRollCall.StartType.NOW, "loc", "desc"));
         testData(new CreateRollCall("id", "name", 432, 231, CreateRollCall.StartType.NOW, "loc", null));
         testData(new CreateRollCall("id", "name", 432, 231, CreateRollCall.StartType.SCHEDULED, "loc", "desc"));
@@ -148,12 +186,12 @@ public class TestJson {
     }
 
     @Test
-    public void testOpenRollCall() throws JsonProcessingException, ProcessingException {
+    public void testOpenRollCall() throws JsonProcessingException {
         testData(new OpenRollCall("id", 32));
     }
 
     @Test
-    public void testCloseRollCall() throws JsonProcessingException, ProcessingException {
+    public void testCloseRollCall() throws JsonProcessingException {
         testData(new CloseRollCall("id", 32, 342, Arrays.asList("1", "2", "3")));
     }
 }
