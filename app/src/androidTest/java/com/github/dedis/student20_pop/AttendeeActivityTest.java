824ccf152916aaf78b93d7d0648024f45362d890
Gabriel Fleischer <gabriel.fleischer@epfl.ch>
2021-01-17 14:38:17 +0100

    Start tests update

--- app/src/androidTest/java/com/github/dedis/student20_pop/AttendeeActivityTest.java
+++ app/src/androidTest/java/com/github/dedis/student20_pop/AttendeeActivityTest.java
@@ -1,9 +1,18 @@
 package com.github.dedis.student20_pop;
 
-import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import com.github.dedis.student20_pop.model.Keys;
+import com.github.dedis.student20_pop.model.Lao;
+import com.github.dedis.student20_pop.model.event.Event;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
@@ -11,12 +20,40 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static com.github.dedis.student20_pop.model.event.EventType.DISCUSSION;
+import static com.github.dedis.student20_pop.model.event.EventType.MEETING;
+import static com.github.dedis.student20_pop.model.event.EventType.POLL;
 
 public class AttendeeActivityTest {
 
+    @Rule
+    public ActivityScenarioRule<AttendeeActivity> activityScenarioRule =
+            new ActivityScenarioRule<>(AttendeeActivity.class);
+
     @Before
     public void launchActivity() {
-        ActivityScenario.launch(AttendeeActivity.class);
+        activityScenarioRule.getScenario().onActivity(
+                activity -> {
+                    PoPApplication app = (PoPApplication) activity.getApplication();
+
+                    String notMyPublicKey = new Keys().getPublicKey();
+                    Lao lao1 = new Lao("LAO 1", notMyPublicKey);
+                    Lao lao2 = new Lao("LAO 2", notMyPublicKey);
+                    Lao lao3 = new Lao("My LAO 3", app.getPerson().getId());
+
+                    List<Event> events2 = Arrays.asList(
+                            new Event("Future Event 1", lao2.getId(), 2617547969L, "EPFL", POLL),
+                            new Event("Present Event 1", lao2.getId(), Instant.now().getEpochSecond(), "Somewhere", DISCUSSION),
+                            new Event("Past Event 1", lao2.getId(), 1481643086L, "Here", MEETING));
+
+                    lao2.setEvents(events2);
+
+                    app.createLao(lao1);
+                    app.createLao(lao2);
+                    app.createLao(lao3);
+                    app.setCurrentLao(lao2);
+                }
+        );
     }
 
     @Test
