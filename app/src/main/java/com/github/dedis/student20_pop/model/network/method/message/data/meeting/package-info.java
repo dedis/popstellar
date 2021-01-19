67a65752b0ae05b5db29d9fb98339e30cf576ec7
Gabriel Fleischer <gabriel.fleischer@epfl.ch>
2021-01-19 18:32:32 +0100

    Some files were not committed in last commit

--- app/src/main/java/com/github/dedis/student20_pop/model/network/method/message/data/meeting/package-info.java
+++ app/src/main/java/com/github/dedis/student20_pop/model/network/method/message/data/meeting/package-info.java
@@ -1,7 +1,6 @@
 /**
  * Classes used to model the data of a Meeting message being sent or received on a channel
- * <p>
- *     Possible actions are Create and State
- * </p>
- **/
+ *
+ * <p>Possible actions are Create and State
+ */
 package com.github.dedis.student20_pop.model.network.method.message.data.meeting;
--- app/src/main/java/com/github/dedis/student20_pop/model/network/method/message/data/message/package-info.java
+++ app/src/main/java/com/github/dedis/student20_pop/model/network/method/message/data/message/package-info.java
@@ -1,7 +1,6 @@
 /**
  * Classes used to model the data of a Message object being sent or received on a channel
- * <p>
- *     Possible action is Witness
- * </p>
- **/
+ *
+ * <p>Possible action is Witness
+ */
 package com.github.dedis.student20_pop.model.network.method.message.data.message;
