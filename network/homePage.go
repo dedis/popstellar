86a899369f32e1578b3ec11cfac19c129b989367
romain46 <romain.pugin@epfl.ch>
2021-01-20 07:13:45 +0100

    added fuctions to interface actor.go, better use of config file

--- network/homePage.go
+++ network/homePage.go
@@ -5,13 +5,15 @@ package network
 import (
 	"log"
 	"net/http"
+	"strconv"
+	"student20_pop/config"
 	"text/template"
 )
 
 // HomeHandler serves the http homepage
 func HomeHandler(tpl *template.Template) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		err := tpl.Execute(w, r)
+		err := tpl.Execute(w, config.ADDRESS+":"+strconv.Itoa(config.PORT))
 		if err != nil {
 			log.Flags()
 		}
