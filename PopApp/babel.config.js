5b56aa17f39ac301a3b60ab4b1ee2e7737566dea
Pierluca Borsò <pierluca@borso.net>
2021-02-08 20:14:05 +0100

    Added TypeScript support (untested)

--- PopApp/babel.config.js
+++ PopApp/babel.config.js
@@ -2,5 +2,14 @@ module.exports = (api) => {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+        [
+            'module-resolver',
+            {
+                root: ['./'],
+                extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json']
+            }
+	]
+    ]
   };
 };
