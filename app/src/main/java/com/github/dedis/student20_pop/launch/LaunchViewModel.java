3588da4ef6d4e89b1b73b6bddbcfc227954632d6
celinecamacho <celine.camacho@epfl.ch>
2021-02-09 18:43:04 +0100

    Create view model for launch

---
+++ app/src/main/java/com/github/dedis/student20_pop/launch/LaunchViewModel.java
@@ -0,0 +1,47 @@
+package com.github.dedis.student20_pop.launch;
+
+import android.app.Application;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.github.dedis.student20_pop.Event;
+
+public class LaunchViewModel extends AndroidViewModel {
+
+    private final Context mContext;
+
+    private final MutableLiveData<Event<Boolean>> mLaunchLaoEvent = new MutableLiveData<>();
+
+    private final MutableLiveData<Event<Boolean>> mCancelLaunchEvent = new MutableLiveData<>();
+
+    private final MutableLiveData<String> laoName = new MutableLiveData<>();
+
+    public LaunchViewModel(@NonNull Application application) {
+        super(application);
+        mContext = application.getApplicationContext();
+    }
+
+    public LiveData<Event<Boolean>> getLaunchLaoEvent() {
+        return mLaunchLaoEvent;
+    }
+
+    public LiveData<Event<Boolean>> getCancelLaunchEvent() {
+        return mCancelLaunchEvent;
+    }
+
+    public MutableLiveData<String> getLaoName() {
+        return laoName;
+    }
+
+    public void launchLao() {
+        mLaunchLaoEvent.setValue(new Event<>(true));
+    }
+
+    public void cancelLaunch() {
+        mCancelLaunchEvent.setValue(new Event<>(true));
+    }
+}
--- app/src/main/java/com/github/dedis/student20_pop/ui/LaunchFragment.java
+++ app/src/main/java/com/github/dedis/student20_pop/ui/LaunchFragment.java
@@ -7,12 +7,21 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.github.dedis.student20_pop.Event;
 import com.github.dedis.student20_pop.R;
+import com.github.dedis.student20_pop.ViewModelFactory;
+import com.github.dedis.student20_pop.launch.LaunchViewModel;
+import com.github.dedis.student20_pop.utility.ActivityUtils;
 
 /** Fragment used to display the Launch UI */
 public final class LaunchFragment extends Fragment {
 
   public static final String TAG = LaunchFragment.class.getSimpleName();
+
+  private LaunchViewModel mViewModel;
 
   public static LaunchFragment newInstance() {
     return new LaunchFragment();
@@ -21,9 +30,58 @@ public final class LaunchFragment extends Fragment {
   @Nullable
   @Override
   public View onCreateView(
-      @NonNull LayoutInflater inflater,
-      @Nullable ViewGroup container,
-      @Nullable Bundle savedInstanceState) {
+          @NonNull LayoutInflater inflater,
+          @Nullable ViewGroup container,
+          @Nullable Bundle savedInstanceState) {
+
+    mViewModel = obtainViewModel(this);
+
+    // Subscribe to "launch LAO" event
+    mViewModel.getLaunchLaoEvent().observe(this, new Observer<Event<Boolean>>() {
+      @Override
+      public void onChanged(Event<Boolean> booleanEvent) {
+        Boolean action = booleanEvent.getContentIfNotHandled();
+        if (action != null) {
+          launchLao();
+        }
+      }
+    });
+
+    // Subscribe to "cancel launch" event
+    mViewModel.getCancelLaunchEvent().observe(this, new Observer<Event<Boolean>>() {
+      @Override
+      public void onChanged(Event<Boolean> booleanEvent) {
+        Boolean action = booleanEvent.getContentIfNotHandled();
+        if (action != null) {
+          setupHomeFragment();
+        }
+      }
+    });
+
     return inflater.inflate(R.layout.fragment_launch, container, false);
+  }
+
+  public static LaunchViewModel obtainViewModel(Fragment fragment) {
+    ViewModelFactory factory = ViewModelFactory.getInstance(fragment.getActivity().getApplication());
+    LaunchViewModel viewModel = new ViewModelProvider(fragment, factory).get(LaunchViewModel.class);
+
+    return viewModel;
+  }
+
+  private void launchLao() {
+    // get text from edit text
+    // create lao
+  }
+
+  private void setupHomeFragment() {
+    // simulate click open home
+    HomeFragment homeFragment = (HomeFragment) getActivity().getSupportFragmentManager()
+            .findFragmentById(R.id.fragment_container_main);
+    if (homeFragment == null) {
+      homeFragment = HomeFragment.newInstance();
+      ActivityUtils.replaceFragmentInActivity(
+              getActivity().getSupportFragmentManager(), homeFragment, R.id.fragment_container_main
+      );
+    }
   }
 }
