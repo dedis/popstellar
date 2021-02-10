d7d148db7ef27ab612e031d5437f2219b4496da1
Nicolas Raulin <nicolas.raulin@epfl.ch>
2021-02-11 00:26:37 +0100

    Applied PR209 suggestions

--- fe1-web/Model/Objects/hash.ts
+++ fe1-web/Model/Objects/hash.ts
@@ -4,21 +4,31 @@ import { Base64Data } from "./base64";
 export class Hash extends Base64Data {
 
     /**
-     * Create a base64 encoded hash of data according to the communication protocol
+     * Create a base64 encoded hash of an array of strings according to the communication protocol
      *
-     * @param data value to be hashed
+     * @param data values to be hashed
      * @return resulting hash
      */
-    public static fromString(...data: string[]): Hash {
+    public static fromStringArray(...data: string[]): Hash {
         let str = '';
         data.forEach((item) => { str += `"${Hash._escapeString(item)}",`; });
         // remove the last comma and add square brackets around
         str = `[${str.slice(0, -1)}]`;
 
+        return Base64Data.encode(this.fromString(str));
+    }
+
+    /**
+     * Create a hash of a string
+     *
+     * @param data value to be hashed
+     * @return resulting hash
+     */
+    public static fromString(data: string): string {
         const hash = sha256.create();
 
-        const bString = hash.update(str).array();
-        return Base64Data.encode(String.fromCharCode(...bString));
+        const bString = hash.update(data).array();
+        return String.fromCharCode(...bString);
     }
 
     /**
