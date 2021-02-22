cc33f0880055ea8acef958f48cc6e840f3b30e27
Nicolas Raulin <nicolas.raulin@epfl.ch>
2021-02-22 02:07:42 +0100

    Added OperationError for server errors

---
+++ fe1-web/network/OperationError.ts
@@ -0,0 +1,20 @@
+/**
+ * This class allows us to distinguish operation errors
+ * (server sending back and error) from other JS errors.
+ */
+export class OperationError extends Error {
+  public readonly errorCode: number;
+
+  constructor(m: string, errorCode: number) {
+    super(m);
+    this.errorCode = errorCode;
+
+    // set prototype explicitly, needs to happen immediately after super() call
+    Object.setPrototypeOf(this, OperationError.prototype);
+
+    // Maintains proper stack trace for where our error was thrown, if available
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, OperationError);
+    }
+  }
+}
