6094ecab12485cee26ff69382042139a4b148a0d
Pierluca Borsò <pierluca@borso.net>
2021-02-24 05:41:47 +0100

    Skeleton for event objects

--- fe1-web/model/objects/index.ts
+++ fe1-web/model/objects/index.ts
@@ -7,3 +7,6 @@ export * from './Signature';
 export * from './Timestamp';
 export * from './WitnessSignature';
 export * from './Lao';
+export * from './RollCall';
+export * from './Event';
+export * from './EventBuilder';
