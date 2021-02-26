3d373cc6e798a113630145a41d66a80fc64cfe23
Nicolas Raulin <nicolas.raulin@epfl.ch>
2021-02-26 19:56:45 +0100

    Eslint warning fix

--- fe1-web/parts/lao/attendee/Attendee.tsx
+++ fe1-web/parts/lao/attendee/Attendee.tsx
@@ -6,6 +6,8 @@ import PROPS_TYPE from 'res/Props';
 import EventListCollapsible from 'components/eventList/EventListCollapsible';
 import LaoProperties from 'components/eventList/LaoProperties';
 
+const laoToProperties = (events: any) => [[], ...events];
+
 /**
  * Attendee screen: lists LAO properties and past/ongoing/future events
  *
@@ -13,19 +15,6 @@ import LaoProperties from 'components/eventList/LaoProperties';
  * TODO use the data receive by the organization server
 */
 // FIXME refactor when Event storage available
-const laoToProperties = (events: any) => [[], ...events];
-
-const propTypes = {
-  events: PropTypes.arrayOf(PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    data: PropTypes.arrayOf(
-      PropTypes.oneOfType([PROPS_TYPE.event, PROPS_TYPE.property]),
-    ).isRequired,
-  })).isRequired,
-};
-
-type IPropTypes = PropTypes.InferProps<typeof propTypes>;
-
 const Attendee = (props: IPropTypes) => {
   const { events } = props;
   const data = laoToProperties(events);
@@ -53,7 +42,17 @@ const Attendee = (props: IPropTypes) => {
   );
 };
 
+const propTypes = {
+  events: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    data: PropTypes.arrayOf(
+      PropTypes.oneOfType([PROPS_TYPE.event, PROPS_TYPE.property]),
+    ).isRequired,
+  })).isRequired,
+};
 Attendee.propTypes = propTypes;
+
+type IPropTypes = PropTypes.InferProps<typeof propTypes>;
 
 const mapStateToProps = (state: any) => ({
   events: state.currentEvents.events,
