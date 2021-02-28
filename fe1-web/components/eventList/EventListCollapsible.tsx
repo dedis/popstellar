1a3dbb1e16732ad07e0e9ffd3df50b9943a86f9d
Nicolas Raulin <nicolas.raulin@epfl.ch>
2021-02-28 16:06:20 +0100

    Reworked organizer event list

--- fe1-web/components/eventList/EventListCollapsible.tsx
+++ fe1-web/components/eventList/EventListCollapsible.tsx
@@ -1,10 +1,52 @@
 import React from 'react';
-import { SectionList } from 'react-native';
+import {
+  SectionList, StyleSheet, Text, TextStyle, View, ViewStyle,
+} from 'react-native';
 import PropTypes from 'prop-types';
 
 import PROPS_TYPE from 'res/Props';
 import TextBlock from 'components/TextBlock';
+import { Spacing, Typography } from 'styles';
+import * as RootNavigation from 'navigation/RootNavigation';
+import STRINGS from 'res/strings';
 import Event from './events';
+
+const styles = StyleSheet.create({
+  flexBox: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  } as ViewStyle,
+  buttonMatcher: {
+    ...Typography.base,
+    paddingLeft: Spacing.m,
+    opacity: 0,
+  } as TextStyle,
+  expandButton: {
+    ...Typography.base,
+    paddingRight: Spacing.m,
+  } as TextStyle,
+});
+
+function renderSectionHeader(title: string, isOrganizer: boolean) {
+  const sectionTitle = <TextBlock bold text={title} />;
+  const expandSign: string = '+';
+
+  return (isOrganizer && title === 'Future')
+    ? (
+      <View style={styles.flexBox}>
+        <Text style={styles.buttonMatcher}>{expandSign}</Text>
+        { sectionTitle }
+        <Text
+          style={styles.expandButton}
+          onPress={() => RootNavigation.navigate(STRINGS.organizer_navigation_tab_create_event, {})}
+        >
+          {expandSign}
+        </Text>
+      </View>
+    )
+    : sectionTitle;
+}
 
 /**
  * Collapsible list of events: contains 3 lists of events for
@@ -14,6 +56,7 @@ import Event from './events';
 */
 const EventListCollapsible = (props: IPropTypes) => {
   const { data } = props;
+  const { isOrganizer } = props;
 
   const renderItemFn = (
     ({ item }: any) => <Event event={item} renderItemFn={renderItemFn} />
@@ -24,7 +67,7 @@ const EventListCollapsible = (props: IPropTypes) => {
       sections={data}
       keyExtractor={(item, index) => `${item?.object}-${item?.id}-${index}`}
       renderItem={renderItemFn}
-      renderSectionHeader={({ section: { title } }) => <TextBlock bold text={title} />}
+      renderSectionHeader={({ section: { title } }) => renderSectionHeader(title, !!isOrganizer)}
     />
   );
 };
@@ -34,8 +77,13 @@ const propTypes = {
     title: PropTypes.string.isRequired,
     data: PropTypes.arrayOf(PROPS_TYPE.event).isRequired,
   }).isRequired).isRequired,
+  isOrganizer: PropTypes.bool,
 };
 EventListCollapsible.propTypes = propTypes;
+
+EventListCollapsible.defaultProps = {
+  isOrganizer: false,
+};
 
 type IPropTypes = PropTypes.InferProps<typeof propTypes>;
 
