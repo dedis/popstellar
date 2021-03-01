5bd012100fabf703bf7ad6340d4f5847718039d0
Pierluca Borsò <pierluca@borso.net>
2021-03-01 19:32:15 +0100

    Migrated React-Redux reducers to createSlice format

--- fe1-web/store/stores/OpenedLaoStore.ts
+++ fe1-web/store/stores/OpenedLaoStore.ts
@@ -1,23 +1,25 @@
-import { Lao, LaoState } from 'model/objects';
+import { Lao } from 'model/objects';
 import { dispatch, getStore } from '../Storage';
-import { ActionOpenedLaoReducer } from '../Actions';
+import { addLao, connectToLao, makeCurrentLao } from '../reducers';
+
+const currentLao = makeCurrentLao();
 
 export namespace OpenedLaoStore {
 
   export function store(lao: Lao): void {
-    dispatch({
-      type: ActionOpenedLaoReducer.SET_OPENED_LAO,
-      value: lao.toState(),
-    });
+    const laoState = lao.toState();
+    dispatch(addLao(laoState));
+    dispatch(connectToLao(lao.id));
   }
 
+  // Consider using an alternative way to access the store wherever possible
   export function get(): Lao {
-    const laoState: LaoState = getStore().getState().openedLao;
+    const lao = currentLao(getStore().getState());
 
-    if (laoState === null || laoState === undefined) {
+    if (!lao) {
       throw new Error('Error encountered while accessing storage : no currently opened LAO');
     }
 
-    return Lao.fromState(laoState);
+    return lao;
   }
 }
