8e8e3064131a4d5fa6063d1d550d161fd00576c3
Pierluca Borsò <pierluca@borso.net>
2021-03-02 19:27:14 +0100

    Applied review comment

--- fe1-web/model/objects/RollCall.ts
+++ fe1-web/model/objects/RollCall.ts
@@ -38,14 +38,27 @@ export class RollCall implements LaoEvent {
       + 'undefined/null parameters');
     }
 
-    if (!obj.id) throw new Error("Undefined 'id' when creating 'RollCall'");
-    if (!obj.name) throw new Error("Undefined 'name' when creating 'RollCall'");
-    if (!obj.location) throw new Error("Undefined 'location' when creating 'RollCall'");
-    if (!obj.description) throw new Error("Undefined 'description' when creating 'RollCall'");
-    if (!obj.creation) throw new Error("Undefined 'creation' when creating 'RollCall'");
-    if (!obj.start) throw new Error("Undefined 'start' when creating 'RollCall'");
-    if (!obj.end) throw new Error("Undefined 'end' when creating 'RollCall'");
-    if (!obj.ongoing) throw new Error("Undefined 'ongoing' when creating 'RollCall'");
+    if (obj.id === undefined) {
+      throw new Error("Undefined 'id' when creating 'RollCall'");
+    }
+    if (obj.name === undefined) {
+      throw new Error("Undefined 'name' when creating 'RollCall'");
+    }
+    if (obj.location === undefined) {
+      throw new Error("Undefined 'location' when creating 'RollCall'");
+    }
+    if (obj.description === undefined) {
+      throw new Error("Undefined 'description' when creating 'RollCall'");
+    }
+    if (obj.creation === undefined) {
+      throw new Error("Undefined 'creation' when creating 'RollCall'");
+    }
+    if (obj.start === undefined) {
+      throw new Error("Undefined 'start' when creating 'RollCall'");
+    }
+    if (obj.end === undefined) {
+      throw new Error("Undefined 'end' when creating 'RollCall'");
+    }
 
     this.id = obj.id;
     this.name = obj.name;
@@ -54,7 +67,7 @@ export class RollCall implements LaoEvent {
     this.creation = obj.creation;
     this.start = obj.start;
     this.end = obj.end;
-    this.ongoing = obj.ongoing;
+    this.ongoing = obj.ongoing || false;
   }
 
   public static fromState(rc: RollCallState): RollCall {
