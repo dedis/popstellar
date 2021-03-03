ad7f19552bae0cfcf15a987601bc57a644cb9e01
Pierluca Borsò <pierluca@borso.net>
2021-03-03 01:42:41 +0100

    Fixed Timestamp mock

--- fe1-web/model/objects/__mocks__/Timestamp.ts
+++ fe1-web/model/objects/__mocks__/Timestamp.ts
@@ -1,7 +1,15 @@
-export const mockTime = 1609455600;
+const mockEpoch = 1609455600;
 
 export class Timestamp extends Number {
   public static EpochNow() {
-    return mockTime;
+    return new Timestamp(mockEpoch);
+  }
+
+  public before(other: Timestamp): boolean {
+    return this.valueOf() < other.valueOf();
+  }
+
+  public after(other: Timestamp): boolean {
+    return this.valueOf() > other.valueOf();
   }
 }
