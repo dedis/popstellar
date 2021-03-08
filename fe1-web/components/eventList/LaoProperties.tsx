7537578f24a86578a2a3bcb6aac47c4d98149392
CaiMusso <47753346+CaiMusso@users.noreply.github.com>
2021-03-08 16:27:34 +0100

    Added the possibility of editing LAO name and creation time on UI
    The LAO properties component in the event list now offers the possibility to edit the name of the LAO and the creation time by using the implemented edit text buttons.
    This implementation uses the EdiText library from react-editext. 
    
    NOTE - the modifications are purely on a visual level, no back-end implementation is done and thus the actual lao.name property does not change on the onSave() call of the button. For the moment, the new name or creation are simply logged on the console.
    
    NOTE - for the moment this modification is available to all members (organiser, witnesses, attendees) which should not be the case. A future commit will allow this action only to the organiser.

--- fe1-web/components/eventList/LaoProperties.tsx
+++ fe1-web/components/eventList/LaoProperties.tsx
@@ -1,21 +1,48 @@
-import React, { useState } from 'react';
-import {
-  TouchableOpacity, View, ViewStyle,
-} from 'react-native';
-import { Spacing } from 'styles';
-import TextBlock from 'components/TextBlock';
-import styleEventView from 'styles/stylesheets/eventView';
-import ListCollapsibleIcon from 'components/eventList/ListCollapsibleIcon';
-import { useSelector } from 'react-redux';
-import ParagraphBlock from 'components/ParagraphBlock';
-import { Lao } from 'model/objects';
-import { makeCurrentLao } from 'store/reducers';
+import React, { useState } from "react";
+import { TouchableOpacity, View, ViewStyle } from "react-native";
+import { Spacing } from "styles";
+import TextBlock from "components/TextBlock";
+import styleEventView from "styles/stylesheets/eventView";
+import ListCollapsibleIcon from "components/eventList/ListCollapsibleIcon";
+import { useSelector } from "react-redux";
+import ParagraphBlock from "components/ParagraphBlock";
+import { Lao } from "model/objects";
+import { makeCurrentLao } from "store/reducers";
+import EdiText from "react-editext";
 
 function renderProperties(lao: Lao) {
+  const style = {
+    fontFamily: "Helvetica Bold",
+    fontSize: "13px",
+    width: 200
+  };
   return (
     <>
-      <ParagraphBlock text={`Lao name: ${lao.name}`} />
-      <ParagraphBlock text={`Lao creation: ${lao.creation.toString()}`} />
+      <ParagraphBlock text="Lao name: " />
+      <EdiText
+        hint="type the new LAO name"
+        viewProps={{ style: style }}
+        inputProps={{ style: style }}
+        type="text"
+        onSave={
+          (newLaoName: string) => console.log(" The new name is " + newLaoName)
+          // TODO: carry out the necessary LAO update interactions with the backend here
+        }
+        value={`${lao.name}`}
+      />
+      <ParagraphBlock text="Lao creation: " />
+      <EdiText
+        hint="type the new creation date"
+        viewProps={{ style: style }}
+        inputProps={{ style: style }}
+        type="text"
+        onSave={
+          (newCreation: string) =>
+            console.log(" The new creation date is " + newCreation)
+          // TODO: carry out the necessary LAO update interactions with the backend here
+        }
+        value={`${lao.creation.toString()}`}
+      />
     </>
   );
 }
@@ -26,17 +53,20 @@ const LaoProperties = () => {
 
   const [toggleChildrenVisible, setToggleChildrenVisible] = useState(false);
 
-  const toggleChildren = () => (setToggleChildrenVisible(!toggleChildrenVisible));
+  const toggleChildren = () => setToggleChildrenVisible(!toggleChildrenVisible);
 
   return (
     <>
       <TextBlock bold text="Lao Properties" />
       <View style={[styleEventView.default, { marginTop: Spacing.s }]}>
-        <TouchableOpacity onPress={toggleChildren} style={{ textAlign: 'right' } as ViewStyle}>
+        <TouchableOpacity
+          onPress={toggleChildren}
+          style={{ textAlign: "right" } as ViewStyle}
+        >
           <ListCollapsibleIcon isOpen={toggleChildrenVisible} />
         </TouchableOpacity>
 
-        { toggleChildrenVisible && lao && renderProperties(lao) }
+        {toggleChildrenVisible && lao && renderProperties(lao)}
       </View>
     </>
   );
