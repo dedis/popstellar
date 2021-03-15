5eda5e0ef4dba3bae5fa650358210b2c9ab4ff8b
Nicolas Raulin <nicolas.raulin@epfl.ch>
2021-03-15 01:50:58 +0100

    Added requests/responses creation model

---
+++ be2-scala/src/main/scala/ch/epfl/pop/model/network/JsonRpcRequest.scala
@@ -0,0 +1,24 @@
+package ch.epfl.pop.model.network
+import ch.epfl.pop.model.network.method.ParamsSimple
+import ch.epfl.pop.model.network.method.message.data.MessageData
+
+case class JsonRpcRequest(
+                           jsonrpc: String,
+                           method: Method.Method,
+                           params: ParamsSimple,
+                           id: Option[Int]
+                         ) extends JsonRpcMessage
+
+object JsonRpcRequest extends Parsable {
+  def apply(
+             jsonrpc: String,
+             method: Method.Method,
+             params: ParamsSimple,
+             id: Option[Int]
+           ): JsonRpcRequest = {
+    // FIXME add checks
+    new JsonRpcRequest(jsonrpc, method, params, id)
+  }
+
+  override def buildFromJson(messageData: MessageData, payload: String): JsonRpcRequest = ???
+}
