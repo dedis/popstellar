fd94b2f20cc540c3acca5b2a5541c15fc5fb167f
Gaurav Narula <gnarula94@gmail.com>
2021-03-24 17:21:15 +0100

    Java CI: remove redundant fields from mock Injection class

--- fe2-android/app/src/mock/java/com/github/dedis/student20_pop/Injection.java
+++ fe2-android/app/src/mock/java/com/github/dedis/student20_pop/Injection.java
@@ -1,8 +1,5 @@
 package com.github.dedis.student20_pop;
 
-import android.app.Application;
-import android.content.Context;
-import com.github.dedis.student20_pop.model.data.LAODatabase;
 import com.github.dedis.student20_pop.model.data.LAORepository;
 import com.github.dedis.student20_pop.model.data.LAOService;
 import com.github.dedis.student20_pop.model.network.GenericMessage;
@@ -20,39 +17,19 @@ import com.github.dedis.student20_pop.utility.json.JsonMessageGeneralSerializer;
 import com.github.dedis.student20_pop.utility.json.JsonMessageSerializer;
 import com.github.dedis.student20_pop.utility.json.JsonResultSerializer;
 import com.google.android.gms.vision.CameraSource;
-import com.google.android.gms.vision.Detector;
-import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 import com.google.crypto.tink.integration.android.AndroidKeysetManager;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.tinder.scarlet.Scarlet;
-import java.io.IOException;
-import java.security.GeneralSecurityException;
 import okhttp3.OkHttpClient;
 
 public class Injection {
 
-  private static String SERVER_URL = "ws://10.0.2.2:8080";
+  private Injection() {
+  }
 
-  private static final String TAG = "INJECTION";
-
-  private static final String KEYSET_NAME = "POP_KEYSET";
-
-  private static final String SHARED_PREF_FILE_NAME = "POP_KEYSET_SP";
-
-  private static final String MASTER_KEY_URI = "android-keystore://POP_MASTER_KEY";
-
-  private static OkHttpClient OK_HTTP_CLIENT_INSTANCE;
-
-  private static Scarlet SCARLET_INSTANCE;
-
-  private static LAOService LAO_SERVICE_INSTANCE;
-
-  private static AndroidKeysetManager KEYSET_MANAGER;
-
-  public static AndroidKeysetManager provideAndroidKeysetManager(Context applicationContext)
-      throws IOException, GeneralSecurityException {
+  public static AndroidKeysetManager provideAndroidKeysetManager() {
     return null;
   }
 
@@ -68,12 +45,11 @@ public class Injection {
         .create();
   }
 
-  public static BarcodeDetector provideQRCodeDetector(Context context) {
+  public static BarcodeDetector provideQRCodeDetector() {
     return null;
   }
 
-  public static CameraSource provideCameraSource(
-      Context context, Detector<Barcode> qrDetector, int width, int height) {
+  public static CameraSource provideCameraSource() {
     return null;
   }
 
@@ -81,18 +57,15 @@ public class Injection {
     return null;
   }
 
-  public static Scarlet provideScarlet(
-      Application application, OkHttpClient okHttpClient, Gson gson) {
-    return SCARLET_INSTANCE;
+  public static Scarlet provideScarlet() {
+    return null;
   }
 
-  public static LAOService provideLAOService(Scarlet scarlet) {
-    return LAO_SERVICE_INSTANCE;
+  public static LAOService provideLAOService() {
+    return null;
   }
 
-  public static LAORepository provideLAORepository(
-      Application application, LAOService service, AndroidKeysetManager keysetManager, Gson gson) {
-    LAODatabase db = LAODatabase.getDatabase(application);
+  public static LAORepository provideLAORepository() {
     return null;
   }
 }
