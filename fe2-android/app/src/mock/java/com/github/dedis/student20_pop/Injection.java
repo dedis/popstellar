f30f7a1dd240d682bead4b651a11700e2a992ac2
Gaurav Narula <gnarula94@gmail.com>
2021-03-24 17:34:20 +0100

    Java CI: add SuppressedWarnings annotation for unused arguments in mock Injection

--- fe2-android/app/src/mock/java/com/github/dedis/student20_pop/Injection.java
+++ fe2-android/app/src/mock/java/com/github/dedis/student20_pop/Injection.java
@@ -1,5 +1,7 @@
 package com.github.dedis.student20_pop;
 
+import android.app.Application;
+import android.content.Context;
 import com.github.dedis.student20_pop.model.data.LAORepository;
 import com.github.dedis.student20_pop.model.data.LAOService;
 import com.github.dedis.student20_pop.model.network.GenericMessage;
@@ -17,11 +19,15 @@ import com.github.dedis.student20_pop.utility.json.JsonMessageGeneralSerializer;
 import com.github.dedis.student20_pop.utility.json.JsonMessageSerializer;
 import com.github.dedis.student20_pop.utility.json.JsonResultSerializer;
 import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 import com.google.crypto.tink.integration.android.AndroidKeysetManager;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.tinder.scarlet.Scarlet;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import okhttp3.OkHttpClient;
 
 public class Injection {
@@ -29,7 +35,9 @@ public class Injection {
   private Injection() {
   }
 
-  public static AndroidKeysetManager provideAndroidKeysetManager() {
+  @SuppressWarnings("unused")
+  public static AndroidKeysetManager provideAndroidKeysetManager(Context applicationContext)
+      throws GeneralSecurityException, IOException {
     return null;
   }
 
@@ -45,11 +53,14 @@ public class Injection {
         .create();
   }
 
-  public static BarcodeDetector provideQRCodeDetector() {
+  @SuppressWarnings("unused")
+  public static BarcodeDetector provideQRCodeDetector(Context context) {
     return null;
   }
 
-  public static CameraSource provideCameraSource() {
+  @SuppressWarnings("unused")
+  public static CameraSource provideCameraSource(
+      Context context, Detector<Barcode> qrDetector, int width, int height) {
     return null;
   }
 
@@ -57,15 +68,20 @@ public class Injection {
     return null;
   }
 
-  public static Scarlet provideScarlet() {
+  @SuppressWarnings("unused")
+  public static Scarlet provideScarlet(
+      Application application, OkHttpClient okHttpClient, Gson gson) {
     return null;
   }
 
-  public static LAOService provideLAOService() {
+  @SuppressWarnings("unused")
+  public static LAOService provideLAOService(Scarlet scarlet) {
     return null;
   }
 
-  public static LAORepository provideLAORepository() {
+  @SuppressWarnings("unused")
+  public static LAORepository provideLAORepository(
+      Application application, LAOService service, AndroidKeysetManager keysetManager, Gson gson) {
     return null;
   }
 }
