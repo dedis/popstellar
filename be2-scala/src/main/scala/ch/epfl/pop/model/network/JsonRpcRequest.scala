158a2daab47e93e78b699ecaa33794d46775ece1
Nicolas Raulin <nicolas.raulin@epfl.ch>
2021-03-25 21:37:19 +0100

    Fixed compiling issues

--- be2-scala/src/main/scala/ch/epfl/pop/model/network/JsonRpcRequest.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/model/network/JsonRpcRequest.scala
@@ -2,14 +2,15 @@ package ch.epfl.pop.model.network
 import ch.epfl.pop.json.JsonUtils.JsonMessageParserError
 import ch.epfl.pop.model.network.method.Params
 import ch.epfl.pop.model.network.method.message.data.MessageData
+import ch.epfl.pop.pubsub.graph.PipelineError
 
-case class JsonRpcRequest(
-                           jsonrpc: String,
-                           method: MethodType.MethodType,
-                           params: Params,
-                           id: Option[Int]
+class JsonRpcRequest(
+                           val jsonrpc: String,
+                           val method: MethodType.MethodType,
+                           val params: Params,
+                           val id: Option[Int]
                          ) extends JsonRpcMessage with Validatable {
-  override def validateContent(): Option[JsonMessageParserError] = params.validateContent() // define recursively?s
+  override def validateContent(): Option[PipelineError] = ??? // params.validateContent() // define recursively?s
 }
 
 object JsonRpcRequest extends Parsable {
--- be2-scala/src/main/scala/ch/epfl/pop/model/network/Matching.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/model/network/Matching.scala
@@ -1,6 +1,6 @@
 package ch.epfl.pop.model.network
 
-sealed trait Matching {
+trait Matching {
   // See https://stackoverflow.com/questions/3407032/comparing-string-and-enumeration
   def unapply(s: String): Boolean = s == toString
 }
