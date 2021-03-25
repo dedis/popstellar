36884c32c4da1b5c8c475916b5d74b3fdc07a35f
Gaurav Narula <gnarula94@gmail.com>
2021-03-25 09:48:36 +0100

    Add null and empty string checks for Hash.hash

--- fe2-android/app/src/test/java/com/github/dedis/student20_pop/utility/security/HashTest.java
+++ fe2-android/app/src/test/java/com/github/dedis/student20_pop/utility/security/HashTest.java
@@ -2,6 +2,7 @@ package com.github.dedis.student20_pop.utility.security;
 
 import static org.junit.Assert.assertNotNull;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class HashTest {
@@ -9,6 +10,20 @@ public class HashTest {
   @Test
   public void hashTest() {
     assertNotNull(Hash.hash("Data to hash"));
+  }
+
+  @Test
+  public void hashNullTest() {
+    Assert.assertThrows(IllegalArgumentException.class, () -> Hash.hash());
+    Assert.assertThrows(IllegalArgumentException.class, () -> Hash.hash((String) null));
+    Assert.assertThrows(IllegalArgumentException.class,
+        () -> Hash.hash((String) null, (String) null));
+  }
+
+  @Test
+  public void hashEmptyStringTest() {
+    Assert.assertThrows(IllegalArgumentException.class, () -> Hash.hash(""));
+    Assert.assertThrows(IllegalArgumentException.class, () -> Hash.hash("", ""));
   }
 
   @Test
