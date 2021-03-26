66e3d310f1cc0b35fbf633638e9c661203aa43b5
Pierluca Borsò <pierluca@borso.net>
2021-03-26 17:48:07 +0100

    Allow joining LAO as attendee

--- fe1-web/parts/connect/ConnectConfirm.tsx
+++ fe1-web/parts/connect/ConnectConfirm.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  StyleSheet, View, ScrollView, ViewStyle,
+  StyleSheet, View, ViewStyle, TextInput, TextStyle,
 } from 'react-native';
+import PropTypes from 'prop-types';
 
-import { Spacing } from 'styles';
+import { getNetworkManager, requestCreateLao } from 'network';
+import { establishLaoConnection } from 'network/CommunicationApi';
+import { Channel, channelFromId, Hash } from 'model/objects';
+
+import { Spacing, Typography } from 'styles';
+import styleContainer from 'styles/stylesheets/container';
+
 import STRINGS from 'res/strings';
 import PROPS_TYPE from 'res/Props';
+
 import TextBlock from 'components/TextBlock';
 import WideButtonView from 'components/WideButtonView';
-import PropTypes from 'prop-types';
-import styleContainer from 'styles/stylesheets/container';
 
 /**
  * Ask for confirmation to connect to a specific LAO
@@ -18,6 +24,12 @@ import styleContainer from 'styles/stylesheets/container';
  * TODO Make the confirm button make the action require in the UI specification
 */
 const styles = StyleSheet.create({
+  textInput: {
+    ...Typography.base,
+    borderBottomWidth: 2,
+    marginVertical: Spacing.s,
+    marginHorizontal: Spacing.xl,
+  } as TextStyle,
   viewCenter: {
     flex: 8,
     justifyContent: 'center',
@@ -26,31 +38,86 @@ const styles = StyleSheet.create({
   } as ViewStyle,
 });
 
-const onButtonConfirm = (navigation: any) => {
-  const parentNavigation = navigation.dangerouslyGetParent();
-  if (parentNavigation !== undefined) {
-    parentNavigation.navigate(STRINGS.navigation_tab_launch);
-  }
-};
+function connectTo(serverUrl: string): boolean {
+  try {
+    const { hostname, port, pathname } = new URL(serverUrl);
 
-const ConnectConfirm = ({ navigation }: IPropTypes) => (
-  <View style={styleContainer.flex}>
-    <TextBlock text={STRINGS.connect_confirm_description} />
-    <View style={styles.viewCenter}>
-      <ScrollView>
-        <TextBlock text={STRINGS.lorem_ipsum} />
-      </ScrollView>
+    const portNum = port ? parseInt(port, 10) : undefined;
+    const path = pathname.replace(/^\/+/g, '');
+
+    getNetworkManager().connect(hostname, portNum, path || undefined);
+  } catch (err) {
+    console.error(`Cannot connect to '${serverUrl}' as it is an invalid URL`, err);
+    return false;
+  }
+  return true;
+}
+
+function validateLaoId(laoId: string): Channel | undefined {
+  try {
+    const h = new Hash(laoId);
+    return channelFromId(h);
+  } catch (err) {
+    console.error(`Cannot connect to LAO '${laoId}' as it is an invalid LAO ID`, err);
+  }
+  return undefined;
+}
+
+const ConnectConfirm = ({ navigation }: IPropTypes) => {
+  const [serverUrl, setServerUrl] = useState('https://127.0.0.1:8080');
+  const [laoId, setLaoId] = useState('');
+
+  const onButtonConfirm = () => {
+    const parentNavigation = navigation.dangerouslyGetParent();
+    if (parentNavigation === undefined) {
+      return;
+    }
+
+    if (!connectTo(serverUrl)) {
+      return;
+    }
+
+    const channel = validateLaoId(laoId);
+    if (channel === undefined) {
+      return;
+    }
+
+    establishLaoConnection(channel)
+      .then(() => {
+        // navigate to the newly created LAO
+        navigation.navigate(STRINGS.organization_navigation_tab_attendee, {});
+      })
+      .catch((reason) => console.error(`Failed to establish lao connection: ${reason}`));
+  };
+
+  return (
+    <View style={styleContainer.flex}>
+      <View style={styles.viewCenter}>
+        <TextBlock text={STRINGS.connect_confirm_description} />
+        <TextInput
+          style={styles.textInput}
+          placeholder={STRINGS.connect_server_uri}
+          onChangeText={(input: string) => setServerUrl(input)}
+          defaultValue={serverUrl}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder={STRINGS.connect_lao_id}
+          onChangeText={(input: string) => setLaoId(input)}
+          defaultValue={laoId}
+        />
+      </View>
+      <WideButtonView
+        title={STRINGS.general_button_confirm}
+        onPress={() => onButtonConfirm()}
+      />
+      <WideButtonView
+        title={STRINGS.general_button_cancel}
+        onPress={() => navigation.navigate(STRINGS.connect_scanning_title)}
+      />
     </View>
-    <WideButtonView
-      title={STRINGS.general_button_confirm}
-      onPress={() => onButtonConfirm(navigation)}
-    />
-    <WideButtonView
-      title={STRINGS.general_button_cancel}
-      onPress={() => navigation.navigate(STRINGS.connect_scanning_title)}
-    />
-  </View>
-);
+  );
+};
 
 const propTypes = {
   navigation: PROPS_TYPE.navigation.isRequired,
