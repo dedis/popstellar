3dc7062c77eb67efaa7db7a6552569c67fcd7e5d
Carlo Musso (Cai) <carlo.musso@epfl.ch>
2021-04-05 15:22:54 +0200

    original Home.tsx re-commited

--- fe1-web/parts/Home.tsx
+++ fe1-web/parts/Home.tsx
@@ -11,8 +11,6 @@ import STRINGS from 'res/strings';
 
 import LAOItem from 'components/LAOItem';
 import TextBlock from 'components/TextBlock';
-import WideButtonView from '../components/WideButtonView';
-import { WalletCryptographyHandler } from '../model/objects/WalletCryptographyHandler';
 
 /**
  * Manage the Home screen component: if the user is not connected to any LAO, a welcome message
@@ -26,25 +24,6 @@ const styles = StyleSheet.create({
     marginTop: Spacing.s,
   },
 });
-
-let cypher: string = ' ';
-
-const onWalletCryptoHandlerButtonPressed = async () => {
-  console.log('Creation of wallet object');
-  await WalletCryptographyHandler.initWalletStorage();
-  console.log('Wallet created');
-};
-
-const onEncryptRandomToken = async () => {
-  const key: string = 'MHICAQEwBQYDK2VwBCIEINTuctv5E1hK1bbY8fdp+K06/nwoy/HU++CXqI9EdVhCoB8wHQYKKoZIhvcNAQkJFDEPDA1DdXJkbGUgQ2hhaXJzgSEAGb9ECWmEzf6FQbrBZ9w7lshQhqowtrbLDFw4rXAxZuE=';
-  cypher = await WalletCryptographyHandler.encrypt(key);
-  console.log(cypher.toString());
-};
-
-const onDecryptRandomToken = async () => {
-  const plain = await WalletCryptographyHandler.decrypt(cypher);
-  console.log(plain.toString());
-};
 
 // FIXME: define interface + types, requires availableLaosReducer to be migrated first
 function getConnectedLaosDisplay(laos: Lao[]) {
@@ -66,21 +45,6 @@ function getWelcomeMessageDisplay() {
       <TextBlock bold text={STRINGS.home_welcome} />
       <TextBlock bold text={STRINGS.home_connect_lao} />
       <TextBlock bold text={STRINGS.home_launch_lao} />
-      <TextBlock text={' '} />
-      <TextBlock text={' '} />
-      <TextBlock text={' '} />
-      <WideButtonView
-        title={STRINGS.wallet}
-        onPress={() => onWalletCryptoHandlerButtonPressed()}
-      />
-      <WideButtonView
-        title={STRINGS.walletEncryptRandomToken}
-        onPress={() => onEncryptRandomToken()}
-      />
-      <WideButtonView
-        title={STRINGS.walletDecryptRandomToken}
-        onPress={() => onDecryptRandomToken()}
-      />
     </View>
   );
 }
