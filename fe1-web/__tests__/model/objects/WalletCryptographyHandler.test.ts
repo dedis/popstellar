6b86707d89157beb513141ced32a44773b7a587a
Carlo Musso (Cai) <carlo.musso@epfl.ch>
2021-04-14 00:04:18 +0200

    re-adding test (test failing because of jest - crypto issues)

---
+++ fe1-web/__tests__/model/objects/WalletCryptographyHandler.test.ts
@@ -0,0 +1,23 @@
+import 'jest-extended';
+import { WalletCryptographyHandler } from 'model/objects/WalletCryptographyHandler';
+import { sign } from 'tweetnacl';
+
+require('fake-indexeddb/auto');
+
+describe('=== Wallet Cryptography Handler tests ===', () => {
+  describe('encryption - decryption', () => {
+    it('should correctly decrypt the previously encrypted ciphertext token', async () => {
+      const cryptoManager = new WalletCryptographyHandler();
+      await cryptoManager.initWalletStorage();
+
+      const token = sign.keyPair().secretKey;
+      const cypher: ArrayBuffer = await cryptoManager.encrypt(token);
+
+      // destroy wallet cryptography manager object
+
+      const plaintext = await cryptoManager.decrypt(cypher);
+
+      expect(plaintext).toBe(token);
+    });
+  });
+});
