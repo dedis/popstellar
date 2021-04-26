28109ed50a5ac3357d0dae1155a5d67466e13a9f
Nicolas Raulin <nicolas.raulin@epfl.ch>
2021-04-26 03:21:13 +0200

    Updated MessageDecoder graph element

--- be2-scala/src/main/scala/ch/epfl/pop/pubsub/graph/MessageDecoder.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/pubsub/graph/MessageDecoder.scala
@@ -2,15 +2,31 @@ package ch.epfl.pop.pubsub.graph
 
 import akka.NotUsed
 import akka.stream.scaladsl.Flow
-import ch.epfl.pop.model.network.{JsonRpcRequest, MethodType}
+import ch.epfl.pop.model.network.{JsonRpcRequest, JsonRpcResponse}
+import ch.epfl.pop.jsonNew.HighLevelProtocol._
+import spray.json._
+
+import scala.util.{Success, Try}
 
 object MessageDecoder {
 
-  // Message or TextMessage
   val jsonRpcParser: Flow[Either[JsonString, PipelineError], GraphMessage, NotUsed] = Flow[Either[JsonString, PipelineError]].map {
-    case Left(jsonString) =>
-      Left(JsonRpcRequest("2.0", MethodType.INVALID, ???, Some(0)))
-    // case _ => _ // TODO check if type ok. Else prob Right(pipelineError)
+    case Left(jsonString) => Try(jsonString.parseJson.asJsObject) match {
+      case Success(obj) =>
+        val fields: Set[String] = obj.fields.keySet
+
+        if (fields.contains("method")) { // FIXME check that the error (if any) is correctly propagated
+          Left(obj.convertTo[JsonRpcRequest])
+        } else {
+          Left(obj.convertTo[JsonRpcResponse])
+        }
+      case _ => Right(PipelineError(
+        ErrorCodes.INVALID_DATA.id,
+        "MessageDecoder parsing failed : input json is not correctly formatted"
+      ))
+    }
+
+    case Right(pipelineError) => Right(pipelineError) // implicit typecasting
   }
 
   val messageParser: Flow[GraphMessage, GraphMessage, NotUsed] = Flow[GraphMessage].map(parseMessage)
