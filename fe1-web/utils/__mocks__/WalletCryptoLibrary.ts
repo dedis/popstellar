15da23735e1dd271415380500e0bc304c3d95e02
Carlo Musso (Cai) <carlo.musso@epfl.ch>
2021-05-05 17:48:22 +0200

    temprorily modified test

--- fe1-web/utils/__mocks__/WalletCryptoLibrary.ts
+++ fe1-web/utils/__mocks__/WalletCryptoLibrary.ts
@@ -42,6 +42,6 @@ Object.defineProperty(crypto, 'subtle', {
 /**
  * returns the MOCK SubtleCrypto library on browser for browser context
  */
-export function getCrypto() : Crypto {
+export function getMockCrypto() : Crypto {
   return crypto as Crypto;
 }
