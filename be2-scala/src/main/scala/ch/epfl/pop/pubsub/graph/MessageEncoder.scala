ac3bf62cb02ba65ae9ce2a485702ed497d4e6d97
Nicolas Raulin <nicolas.raulin@epfl.ch>
2021-05-13 14:22:13 +0200

    Fixed MessageEncoder

--- be2-scala/src/main/scala/ch/epfl/pop/pubsub/graph/MessageEncoder.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/pubsub/graph/MessageEncoder.scala
@@ -2,13 +2,39 @@ package ch.epfl.pop.pubsub.graph
 
 import akka.NotUsed
 import akka.stream.scaladsl.Flow
+import ch.epfl.pop.model.network.{ErrorObject, JsonRpcRequest, JsonRpcResponse, MethodType, ResultObject}
+import ch.epfl.pop.pubsub.graph.validators.RpcValidator
 
 object MessageEncoder {
 
   def serializeMessage(graphMessage: GraphMessage): GraphMessage = graphMessage match {
     // Note: the output message (if successful) is an answer
-    case Left(jsonRpcMessage) => Left(???) // FIXME generate json rpc answer with id corresponding to the one in the input rpc message
-    case graphMessage@_ => graphMessage
+    // The standard output is always a JsonMessage (pipeline errors are transformed into negative answers)
+    case Left(rpcRequest: JsonRpcRequest) => rpcRequest.method match {
+      // FIXME catchup has non int result
+      case MethodType.CATCHUP => Left(JsonRpcResponse(
+        RpcValidator.JSON_RPC_VERSION, ???, None, rpcRequest.id
+      ));
+      // FIXME propagate has no answer
+      case MethodType.BROADCAST => ???
+      // Standard answer. Result == 0
+      case _ => Left(JsonRpcResponse(
+        RpcValidator.JSON_RPC_VERSION, Some(new ResultObject(0)), None, rpcRequest.id
+      ))
+    }
+
+    case Right(pipelineError: PipelineError) => Left(JsonRpcResponse(
+      RpcValidator.JSON_RPC_VERSION,
+      None,
+      Some(ErrorObject(pipelineError.code, pipelineError.description)),
+      ???
+    ))
+
+    // /!\ If something is outputted as Right(...), then there's a mistake somewhere in the graph!
+    case _ => Right(PipelineError(
+      ErrorCodes.SERVER_ERROR.id,
+      s"Internal server error: unknown reason. The MessageEncoder could not decide what to do with input $graphMessage"
+    ))
   }
 
   val serializer: Flow[GraphMessage, GraphMessage, NotUsed] = Flow[GraphMessage].map(serializeMessage)
