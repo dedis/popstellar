76195a0c89048c6f60f484d414b0e06286d09f63
Walfar <victor.carles@epfl.ch>
2021-05-14 18:28:30 +0200

    mocked test for android.util.Base64

--- fe2-android/app/src/test/java/com/github/dedis/student20_pop/model/network/method/message/data/election/ElectionSetupTest.java
+++ fe2-android/app/src/test/java/com/github/dedis/student20_pop/model/network/method/message/data/election/ElectionSetupTest.java
@@ -1,13 +1,25 @@
 package com.github.dedis.student20_pop.model.network.method.message.data.election;
 
+import android.util.Base64;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.powermock.api.mockito.PowerMockito.when;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest( {Base64.class})
 public class ElectionSetupTest {
 
     private String electionSetupName = "new election setup";
@@ -18,8 +30,14 @@ public class ElectionSetupTest {
     private List<String> ballotOptions = Arrays.asList("candidate1", "candidate2");
     private String question = "which is the best ?";
     private String laoId = "my lao id";
+    private ElectionSetup electionSetup;
 
-    private ElectionSetup electionSetup = new ElectionSetup(electionSetupName, start, end, votingMethod, writeIn, ballotOptions, question, laoId);
+    @Before
+    public void setup() {
+        PowerMockito.mockStatic(Base64.class);
+        when(Base64.encodeToString(any(), anyInt())).thenAnswer(invocation -> java.util.Base64.getUrlEncoder().encodeToString((byte[]) invocation.getArguments()[0]));
+        electionSetup = new ElectionSetup(electionSetupName, start, end, votingMethod, writeIn, ballotOptions, question, laoId);
+    }
 
     @Test
     public void electionSetupGetterReturnsCorrectName() {
