4037f8a0625257db3ee7d2f5e6e7e5fec5d6d4d4
flmichel <francois.l.michel@gmail.com>
2021-06-01 16:59:57 +0200

    Added test for the catchup order

---
+++ be1-go/hub/base_channel_test.go
@@ -0,0 +1,39 @@
+package hub
+
+import (
+	"bytes"
+	"student20_pop/message"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestBaseChannel_RollCallOrder(t *testing.T) {
+	// Create the channel
+	channel := createBaseChannel(nil, "Channel0")
+
+	// Create the messages
+	messageNumber := 10
+	messages := make([]message.Message, 0, messageNumber)
+	for i := 0; i < messageNumber; i++ {
+		// Create a new message containing only an id
+		message := message.Message{MessageID: []byte{byte(i)}}
+		messages = append(messages, message)
+
+		// Store the message in the inbox
+		channel.inbox.storeMessage(message)
+
+		// Wait before storing a new message
+		time.Sleep(time.Microsecond)
+	}
+
+	// Compute the catchup method
+	catchupAnswer := channel.Catchup(message.Catchup{ID: 0})
+
+	// Check that the order of the messages is the same in `messages` and in `catchupAnswer`
+	for i := 0; i < messageNumber; i++ {
+		require.Equal(t, bytes.Compare(messages[i].MessageID, catchupAnswer[i].MessageID), 0)
+	}
+
+}
