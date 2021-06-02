1c2299c10d6a59476f0689993c7ca08a4ce583bd
unknown <stefan.eric@epfl.ch>
2021-06-02 11:26:55 +0200

    saved messages in inbox

--- be1-go/hub/election.go
+++ be1-go/hub/election.go
@@ -1,10 +1,12 @@
 package hub
 
 import (
+	"crypto/sha256"
 	"encoding/base64"
 	"fmt"
 	"golang.org/x/xerrors"
 	"log"
+	"sort"
 	"student20_pop/message"
 	"sync"
 )
@@ -223,4 +225,60 @@ func getAllQuestionsForElectionChannel(questions []message.Question) map[string]
 		}
 	}
 	return qs
+}
+
+func (c *electionChannel) endElectionHelper(publish message.Publish) error {
+	endElectionData, ok := publish.Params.Message.Data.(*message.ElectionEndData)
+	if !ok {
+		return &message.Error{
+			Code:        -4,
+			Description: "failed to cast data to ElectionEndData",
+		}
+	}
+	if endElectionData.CreatedAt < c.end{
+		return &message.Error{
+			Code:        -4,
+			Description: "Can't send end election message before the end of the election",
+		}
+	}
+	// since we eliminated (in cast vote) the duplicate votes we are sure that the voter casted one vote for one question
+	for _,question := range c.questions{
+		hashed,err := sortHashVotes(question.validVotes)
+		if err != nil {
+			return &message.Error{
+				Code:        -4,
+				Description: "Error while hashing",
+			}
+		}
+		endElectionData.RegisteredVotes = hashed
+	}
+	msg := publish.Params.Message
+	messageID := base64.URLEncoding.EncodeToString(msg.MessageID)
+	c.inboxMu.Lock()
+	c.inbox[messageID] = *msg
+	c.inboxMu.Unlock()
+
+	return nil
+}
+func sortHashVotes(votes2 map[string]validVote)([]byte,error) {
+	type kv struct {
+		voteTime message.Timestamp
+		sender   string
+	}
+	votes := make(map[int]kv)
+	i := 0
+	for k, v := range votes2 {
+		votes[i] = kv{v.voteTime, k}
+		i += 1
+	}
+	sort.Slice(votes,
+		func(i int, j int) bool { return votes[i].voteTime < votes[j].voteTime })
+	h := sha256.New()
+	for _, v := range votes {
+		if len(v.sender) == 0 {
+			return nil, xerrors.Errorf("empty string to hash()")
+		}
+		h.Write([]byte(fmt.Sprintf("%d%s", len(v.sender), v.sender)))
+	}
+	return h.Sum(nil), nil
 }
