6753a6a232964e835e9c041c349dcd4885c45579
flmichel <francois.l.michel@gmail.com>
2021-06-06 01:12:25 +0200

    Added bat file for windows

---
+++ be1-go/make.bat
@@ -0,0 +1,47 @@
+@echo off
+
+
+IF /I "%1"=="build" GOTO build
+IF /I "%1"=="test" GOTO test
+IF /I "%1"=="test_race" GOTO test_race
+IF /I "%1"=="staticcheck" GOTO staticcheck
+IF /I "%1"=="vet" GOTO vet
+IF /I "%1"=="copy_protocol" GOTO copy_protocol
+IF /I "%1"=="clean_protocol" GOTO clean_protocol
+GOTO error
+
+:build
+	%call execute_command, go build -o pop ./cli/%
+	GOTO :EOF
+
+:test
+	%call execute_command, go test -v ./...%
+	GOTO :EOF
+
+:test_race
+	%call execute_command, go test -race -v ./...%
+	GOTO :EOF
+
+:staticcheck
+	%call execute_command, staticcheck ./...%
+	GOTO :EOF
+
+:vet
+	%call execute_command, go vet ./...%
+	GOTO :EOF
+
+:copy_protocol
+	XCOPY /Y ../protocol ./validation -r
+	GOTO :EOF
+
+:clean_protocol
+	DEL /Q validation/protocol -rf
+	GOTO :EOF
+
+:error
+    IF "%1"=="" (
+        ECHO make: *** No targets specified and no makefile found.  Stop.
+    ) ELSE (
+        ECHO make: *** No rule to make target '%1%'. Stop.
+    )
+    GOTO :EOF
