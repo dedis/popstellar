1db0f6877ded29c81eecb86fb881868d4b7115fc
lpatel99 <luke.patel@hotmail.co.uk>
2021-06-08 19:06:24 +0200

    corrected test

--- be1-go/network/serveWs_test.go
+++ be1-go/network/serveWs_test.go
@@ -19,12 +19,13 @@ func TestCreateAndServeWS(t *testing.T) {
 	buffer := bytes.Buffer{}
 	log.SetOutput(&buffer)
 
-
-
-	wg.Wait()
+	srv := CreateAndServeWS(ctx, "witness", "client", hub, 9000, wg)
+	srv.Shutdown(ctx)
 
 	str := buffer.String()
+
 	condition := strings.Contains(str, "Starting the witness WS server (for client) at 9000")
+
 	if !condition {
 		t.Errorf("server not starting")
 	}
