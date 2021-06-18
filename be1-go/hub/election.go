0170e4135c44fc3500346ee25a4be1bf605b8a0c
unknown <stefan.eric@epfl.ch>
2021-06-18 08:36:49 +0200

    added field names

--- be1-go/hub/election.go
+++ be1-go/hub/election.go
@@ -447,8 +447,8 @@ func (c *electionChannel) electionResultHelper(publish message.Publish) error{
 				}
 				log.Printf("For question of id %s we get an option of %v with count %v",question.id,option,numberOfVotesPerBallotOption[i])
 				questionResults2 = append(questionResults2, message.BallotOptionCount{
-					option,
-					numberOfVotesPerBallotOption[i],
+					Option: option,
+					Count: numberOfVotesPerBallotOption[i],
 				})
 			}
 			log.Printf("The list of the ballot options and counts shoudl be the following: %v",questionResults)
