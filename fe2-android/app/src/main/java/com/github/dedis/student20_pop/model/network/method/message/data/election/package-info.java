c62b5501cd2c3ee90d60b4d29a5bade58e417f48
celinecamacho <celine.camacho@epfl.ch>
2021-08-06 13:30:56 +0200

    Add and edit package information

---
+++ fe2-android/app/src/main/java/com/github/dedis/student20_pop/model/network/method/message/data/election/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Classes used to model the data of an Election message being sent or received on a channel
+ *
+ * <p>Possible actions are Setup and End
+ */
+package com.github.dedis.student20_pop.model.network.method.message.data.election;
