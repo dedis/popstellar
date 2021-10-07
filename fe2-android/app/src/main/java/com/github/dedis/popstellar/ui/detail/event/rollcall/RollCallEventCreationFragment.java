1fc01b153ad9ab23628f7e480c395c3894a86716
celinecamacho <celine.camacho@epfl.ch>
2021-10-07 10:28:06 +0200

    Rename variable to follow conventions

--- fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/detail/event/rollcall/RollCallEventCreationFragment.java
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/detail/event/rollcall/RollCallEventCreationFragment.java
@@ -141,6 +141,6 @@ public final class RollCallEventCreationFragment extends AbstractEventCreationFr
     String title = mFragBinding.rollCallTitleText.getText().toString();
     String description = mFragBinding.rollCallEventDescriptionText.getText().toString();
     mLaoDetailViewModel
-        .createNewRollCall(title, description, creationTimeInSeconds, startTimeInSeconds, endTimeInSeconds, open);
+        .createNewRollCall(title, description, CREATION_TIME_IN_SECONDS, startTimeInSeconds, endTimeInSeconds, open);
   }
 }
