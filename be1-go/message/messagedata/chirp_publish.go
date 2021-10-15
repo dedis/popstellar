010f0e261140f8401e4740884f1af0a9e92d9ccf
Jiabao Wen <jiabao.wen@epfl.ch>
2021-10-15 16:22:19 +0200

    remove backend files

--- be1-go/message/messagedata/chirp_publish.go
+++
@@ -1,8 +0,0 @@
-package messagedata
-
-type ChirpAdd struct {
-	Object    string `json:"object"`
-	Action    string `json:"action"`
-	Text      string `json:"text"`
-	Timestamp int64  `json:"timestamp"`
-}
