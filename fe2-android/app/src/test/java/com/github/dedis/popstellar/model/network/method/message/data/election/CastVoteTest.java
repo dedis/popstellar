b50adf4f5c690bc45bc0e9b2b5776c00e4ca16f4
Gabriel Fleischer <gabriel.fleischer@epfl.ch>
2021-10-22 16:25:19 +0200

    Remove unnecessary ArrayList creations

--- fe2-android/app/src/test/java/com/github/dedis/popstellar/model/network/method/message/data/election/CastVoteTest.java
+++ fe2-android/app/src/test/java/com/github/dedis/popstellar/model/network/method/message/data/election/CastVoteTest.java
@@ -7,7 +7,6 @@ import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -21,21 +20,10 @@ public class CastVoteTest {
   private boolean writeInEnabled = false;
   private String write_in = "My write in ballot option";
   private ElectionVote electionVote1 =
-      new ElectionVote(
-          questionId1,
-          new ArrayList<>(Arrays.asList(2, 1, 0)),
-          writeInEnabled,
-          write_in,
-          electionId);
+      new ElectionVote(questionId1, Arrays.asList(2, 1, 0), writeInEnabled, write_in, electionId);
   private ElectionVote electionVote2 =
-      new ElectionVote(
-          questionId2,
-          new ArrayList<>(Arrays.asList(0, 1, 2)),
-          writeInEnabled,
-          write_in,
-          electionId);
-  private List<ElectionVote> electionVotes =
-      new ArrayList<>(Arrays.asList(electionVote1, electionVote2));
+      new ElectionVote(questionId2, Arrays.asList(0, 1, 2), writeInEnabled, write_in, electionId);
+  private List<ElectionVote> electionVotes = Arrays.asList(electionVote1, electionVote2);
 
   CastVote castVote = new CastVote(electionVotes, electionId, laoId);
 
@@ -59,14 +47,10 @@ public class CastVoteTest {
     assertEquals(castVote, new CastVote(electionVotes, electionId, laoId));
     assertEquals(castVote, castVote);
     assertNotEquals(
-        castVote,
-        new CastVote(new ArrayList<>(Collections.singletonList(electionVote1)), electionId, laoId));
+        castVote, new CastVote(Collections.singletonList(electionVote1), electionId, laoId));
     assertNotEquals(
-        castVote,
-        new CastVote(new ArrayList<>(Collections.singletonList(electionVote1)), "random", laoId));
+        castVote, new CastVote(Collections.singletonList(electionVote1), "random", laoId));
     assertNotEquals(
-        castVote,
-        new CastVote(
-            new ArrayList<>(Collections.singletonList(electionVote1)), electionId, "random"));
+        castVote, new CastVote(Collections.singletonList(electionVote1), electionId, "random"));
   }
 }
