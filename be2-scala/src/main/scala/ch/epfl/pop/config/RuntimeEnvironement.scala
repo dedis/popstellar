17241b75c40e6439c77358dbb1bb33421256bb9d
BadrTad <56789776+BadrTad@users.noreply.github.com>
2021-10-24 20:14:16 +0200

    Enhance config functionnality by providing path at runtime

--- be2-scala/src/main/scala/ch/epfl/pop/config/RuntimeEnvironement.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/config/RuntimeEnvironement.scala
@@ -8,36 +8,34 @@ import java.util.logging
 import java.io.IOException
 import java.util.regex.Pattern
 
-/** 
-  * RuntimeConfiguration object provider This object provides application config
+/** RuntimeConfiguration object provider This object provides application config
   * for setting up akka http/actor environemnt
   *
-  * @see [[config/application.conf]]
-  * @see [[ServerConf]]
+  * @see
+  *   [[config/application.conf]]
+  * @see
+  *   [[ServerConf]]
   */
 object RuntimeEnvironment {
-  
+
   private lazy val sp = new SystemProperties()
-  
+
   private def getConfDir: String = {
+   
+   /*Get config directory path form JVM*/
+    val virtualMachineParam = "scala.config"
+    val path_config = sp(virtualMachineParam)
+    if (path_config != null && !path_config.trim.isEmpty()) path_config.trim
+    else
+      throw new RuntimeException(s"-D$virtualMachineParam was not provided.")
 
-    val path_wd = sp("user.dir")
-    val wd = path_wd.split(Pattern.quote(File.separator)).last
     
-    if( wd != "be2-scala"){
-        throw new Error("Please check that your current working directory is path\\to\\be2-scala\\")
-    }
-
-    /*Build config directory path based be2-scala*/
-    val builder = new StringBuilder(path_wd).append(File.separator);
-    Seq("src","main","scala","ch","epfl","pop","config").foldLeft(builder){case (b,s) => b.append(s + File.separator)}.toString()
-
+   
   }
 
   private lazy val appConfFile =
-    getConfDir + "application.conf"
+    getConfDir + File.separator + "application.conf"
 
   lazy val appConf: Config = ConfigFactory.parseFile(new File(appConfFile))
-    
 
 }
