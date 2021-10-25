6157c78a8d0f67989910aeeccbc5717eadeb84d4
Adrien Vauthey <adrien.vauthey@epfl.ch>
2021-10-26 00:22:33 +0200

    created SocialMediaViewModel

---
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/socialmedia/SocialMediaViewModel.java
@@ -0,0 +1,91 @@
+package com.github.dedis.popstellar.ui.socialmedia;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.github.dedis.popstellar.SingleEvent;
+import com.github.dedis.popstellar.repository.LAORepository;
+import com.google.crypto.tink.integration.android.AndroidKeysetManager;
+import com.google.gson.Gson;
+
+import io.reactivex.disposables.CompositeDisposable;
+
+public class SocialMediaViewModel extends AndroidViewModel {
+  public static final String TAG = SocialMediaViewModel.class.getSimpleName();
+
+  /*
+   * LiveData objects for capturing events
+   */
+  private final MutableLiveData<SingleEvent<Boolean>> mOpenHomeEvent = new MutableLiveData<>();
+  private final MutableLiveData<SingleEvent<Boolean>> mOpenSendEvent = new MutableLiveData<>();
+  private final MutableLiveData<SingleEvent<Boolean>> mOpenFollowingEvent = new MutableLiveData<>();
+  private final MutableLiveData<SingleEvent<Boolean>> mOpenProfileEvent = new MutableLiveData<>();
+
+  /*
+   * Dependencies for this class
+   */
+  private final Gson mGson;
+  private final LAORepository mLAORepository;
+  private final AndroidKeysetManager mKeysetManager;
+  private final CompositeDisposable disposables;
+
+  public SocialMediaViewModel(
+      @NonNull Application application,
+      LAORepository laoRepository,
+      Gson gson,
+      AndroidKeysetManager keysetManager) {
+    super(application);
+    mLAORepository = laoRepository;
+    mKeysetManager = keysetManager;
+    mGson = gson;
+    disposables = new CompositeDisposable();
+  }
+
+  @Override
+  protected void onCleared() {
+    super.onCleared();
+    disposables.dispose();
+  }
+
+  /*
+   * Getters for MutableLiveData instances declared above
+   */
+  public LiveData<SingleEvent<Boolean>> getOpenHomeEvent() {
+    return mOpenHomeEvent;
+  }
+
+  public LiveData<SingleEvent<Boolean>> getOpenSendEvent() {
+    return mOpenSendEvent;
+  }
+
+  public LiveData<SingleEvent<Boolean>> getOpenFollowingEvent() {
+    return mOpenFollowingEvent;
+  }
+
+  public LiveData<SingleEvent<Boolean>> getOpenProfileEvent() {
+    return mOpenProfileEvent;
+  }
+
+  /*
+   * Methods that modify the state or post an Event to update the UI.
+   */
+  public void openHome() {
+    mOpenHomeEvent.postValue(new SingleEvent<>(true));
+  }
+
+  public void openSend() {
+    mOpenSendEvent.postValue(new SingleEvent<>(true));
+  }
+
+  public void openFollowing() {
+    mOpenFollowingEvent.postValue(new SingleEvent<>(true));
+  }
+
+  public void openProfile() {
+    mOpenProfileEvent.postValue(new SingleEvent<>(true));
+  }
+}
