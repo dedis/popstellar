f2f146f8bad0cee92ee4a440c21745fd79fe6843
arnauds5 <arnauds5@users.noreply.github.com>
2021-10-31 20:32:48 +0100

    Add/update instance_id for consensus

--- be1-go/message/messagedata/consensus_learn.go
+++ be1-go/message/messagedata/consensus_learn.go
@@ -2,8 +2,9 @@ package messagedata
 
 // ConsensusLearn defines a message data
 type ConsensusLearn struct {
-	Object    string   `json:"object"`
-	Action    string   `json:"action"`
-	MessageID string   `json:"message_id"`
-	Acceptors []string `json:"acceptors"`
+	Object     string   `json:"object"`
+	Action     string   `json:"action"`
+	InstanceID string   `json:"instance_id"`
+	MessageID  string   `json:"message_id"`
+	Acceptors  []string `json:"acceptors"`
 }
