4b1784e475907c6f87853d18632a7dcc813ad6e6
BadrTad <56789776+BadrTad@users.noreply.github.com>
2021-11-02 16:07:58 +0100

    Modulirize test by adding a getJsonStringFromFile method

--- be2-scala/src/test/scala/ch/epfl/pop/pubsub/graph/validator/SchemaValidatorSuite.scala
+++ be2-scala/src/test/scala/ch/epfl/pop/pubsub/graph/validator/SchemaValidatorSuite.scala
@@ -5,33 +5,35 @@ import scala.io.Source
 import ch.epfl.pop.pubsub.graph.Validator._
 import ch.epfl.pop.pubsub.graph.PipelineError
 import ch.epfl.pop.pubsub.graph.ErrorCodes
+import java.io.IOException
 
 class SchemaValidatorSuite extends FunSuite with Matchers {
+
+  def getJsonStringFromFile(filePath: String): String = {
+      val source = Source.fromFile(filePath);
+      val jsonStr = source.getLines.mkString
+      source.close
+      jsonStr
+    }
 
   /* Test subscribe query without message format*/
   test("Correct subscribe JSON-RPC query"){
     val subscribePath = "../protocol/examples/query/subscribe/subscribe.json"
-    val source = Source.fromFile(subscribePath);
-    val subscribeJson = source.getLines.mkString
-    source.close
+    val subscribeJson = getJsonStringFromFile(subscribePath)
 
     validateSchema(subscribeJson) should be (Left(subscribeJson))
   }
 
   test("Incorrect subscribe query: additional params"){
     val subscribePath = "../protocol/examples/query/subscribe/wrong_subscribe__additional_params.json"
-    val source = Source.fromFile(subscribePath);
-    val subscribeJson = source.getLines.mkString
-    source.close
+    val subscribeJson = getJsonStringFromFile(subscribePath)
 
     validateSchema(subscribeJson) shouldBe a [Right[_,PipelineError]]
   }
 
   test("Incorrect subscribe query: missing channel"){
     val subscribePath = "../protocol/examples/query/subscribe/wrong_subscribe_missing_channel.json"
-    val source = Source.fromFile(subscribePath);
-    val subscribeJson = source.getLines.mkString
-    source.close
+    val subscribeJson = getJsonStringFromFile(subscribePath)
 
     validateSchema(subscribeJson) shouldBe a [Right[_,PipelineError]]
   }
@@ -39,30 +41,22 @@ class SchemaValidatorSuite extends FunSuite with Matchers {
   /* Test broadcast query with message format */
   test("Correct broadcast JSON-RPC query"){
     val broadcastPath = "../protocol/examples/query/broadcast/broadcast.json"
-    val source = Source.fromFile(broadcastPath);
-    val broadcastJson = source.getLines.mkString
-    source.close
+    val broadcastJson = getJsonStringFromFile(broadcastPath)
 
     validateSchema(broadcastJson) should be (Left(broadcastJson))
   }
 
   test("Incorrect broadcast query: additional params"){
     val broadcastPath = "../protocol/examples/query/broadcast/wrong_broadcast_additional_params.json"
-    val source = Source.fromFile(broadcastPath);
-    val broadcastJson = source.getLines.mkString
-    source.close
+    val broadcastJson =getJsonStringFromFile(broadcastPath)
 
     validateSchema(broadcastJson) shouldBe a [Right[_,PipelineError]]
   }
 
   test("Incorrect broadcast query: missing message"){
     val broadcastPath = "../protocol/examples/query/broadcast/wrong_broadcast_missing_message.json"
-    val source = Source.fromFile(broadcastPath);
-    val broadcastJson = source.getLines.mkString
-    source.close
+    val broadcastJson = getJsonStringFromFile(broadcastPath)
 
     validateSchema(broadcastJson) shouldBe a [Right[_,PipelineError]]
   }
-
-
 }
