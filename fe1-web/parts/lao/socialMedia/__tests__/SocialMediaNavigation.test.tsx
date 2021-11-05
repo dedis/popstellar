81ad996896b3eb0bb342deb4d3881b7e2271a32d
jiabaow <jiabaowenwendy@gmail.com>
2021-11-05 07:57:25 +0100

    start writing test

---
+++ fe1-web/parts/lao/socialMedia/__tests__/SocialMediaNavigation.test.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react-native';
+import STRINGS from 'res/strings';
+import SocialMediaNavigation from "../SocialMediaNavigation";
+
+describe('')
