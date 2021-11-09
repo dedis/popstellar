7c4aa89721bf3997ba906573f370b603e8e8bb13
jiabaow <jiabaowenwendy@gmail.com>
2021-11-09 21:17:34 +0100

    remove unused fireEvent

--- fe1-web/components/__tests__/WideButtonView.test.tsx
+++ fe1-web/components/__tests__/WideButtonView.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render } from '@testing-library/react-native';
+import { render } from '@testing-library/react-native';
 import WideButtonView from '../WideButtonView';
 
 let onPress: Function;
