e4c9cb23a74aff794d4837d7fad831d6f3cb980e
jiabaow <jiabaowenwendy@gmail.com>
2021-11-10 10:45:39 +0100

    try to make it works

--- fe1-web/jest.config.js
+++ fe1-web/jest.config.js
@@ -11,5 +11,23 @@ module.exports = {
   collectCoverage: true,
   testResultsProcessor: "jest-sonar-reporter",
   setupFiles: ["./jest/setup.js"],
+/*  transform: {
+    "node_modules/@expo/vector-icons/.+\\.tsx?$": "ts-jest"
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!@expo/vector-icons/.*)"
+  ]*/
+/*  transform: {
+    "node_modules/createIconSet/.+\\.tsx?$": "ts-jest"
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!createIconSet/.*)"
+  ]
+  transform: {
+    "node_modules/react-native-vector-icons/.+\\.tsx?$": "ts-jest"
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!react-native-vector-icons/.*)"
+  ]*/
 };
 
--- fe1-web/parts/lao/socialMedia/__tests__/SocialMediaNavigation.test.tsx
+++ fe1-web/parts/lao/socialMedia/__tests__/SocialMediaNavigation.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render } from '@testing-library/react-native';
+import { render } from '@testing-library/react-native';
 import SocialMediaNavigation from '../SocialMediaNavigation';
 
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
