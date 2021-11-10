2c4cfbf47db81db20b48cb09720ec1684422de9c
BadrTad <56789776+BadrTad@users.noreply.github.com>
2021-11-10 23:57:37 +0100

    Add helper method to format source Messages and comments

--- be2-scala/src/test/scala/ch/epfl/pop/json/HighLevelProtocolSuite.scala
+++ be2-scala/src/test/scala/ch/epfl/pop/json/HighLevelProtocolSuite.scala
@@ -5,12 +5,13 @@ import ch.epfl.pop.model.objects._
 import org.scalatest.{FunSuite, Matchers}
 import org.scalatest.Inspectors.{forAll,forEvery}
 import org.scalatest.Matchers._
-import ch.epfl.pop.json.MsgField._
+//import ch.epfl.pop.json.MsgField._
 import org.scalactic.Prettifier
 
 class HighLevelProtocolSuite extends FunSuite with Matchers  {
+  import MsgField._
 
-    def buildExpected(id: String, sender: String, signature: String, data: String): Message ={
+  def buildExpected(id: String, sender: String, signature: String, data: String): Message ={
       Message(
         Base64Data(data),
         PublicKey(Base64Data(sender)),
@@ -20,7 +21,7 @@ class HighLevelProtocolSuite extends FunSuite with Matchers  {
       )
     }
 
-    def getFormattedString(f: MsgField, sp: String): String = {
+  def getFormattedString(f: MsgField, sp: String): String = {
       val format =  s"""{
                                  |      "message_id": "%s",
                                  |      "sender": "%s",
@@ -37,20 +38,24 @@ class HighLevelProtocolSuite extends FunSuite with Matchers  {
           case WITNESS_SIGNATURES => String.format(format, "", "", "", "" ,sp)
       }
   }
+  def getFormattedSource(id: String, sender: String, signature: String, data: String, witnesses: String): String = {
+        s"""{
+                                 |      "message_id": "$id",
+                                 |      "sender": "$sender",
+                                 |      "signature": "$signature",
+                                 |      "data": "$data",
+                                 |      "witness_signatures": $witnesses
+                                 |    }""".stripMargin
+    }
 
   test("Parser correctly parse to Message object") {
     val id: String = "f1jTxH8TU2UGUBnikGU3wRTHjhOmIEQVmxZBK55QpsE="
     val sender: String ="to_klZLtiHV446Fv98OLNdNmi-EP5OaTtbBkotTYLic="
     val signature: String = "2VDJCWg11eNPUvZOnvq5YhqqIKLBcik45n-6o87aUKefmiywagivzD4o_YmjWHzYcb9qg-OgDBZbBNWSUgJICA=="
     val data: String = "eyJjcmVhdGlvbiI6MTYzMTg4NzQ5NiwiaWQiOiJ4aWdzV0ZlUG1veGxkd2txMUt1b0wzT1ZhODl4amdYalRPZEJnSldjR1drPSIsIm5hbWUiOiJoZ2dnZ2dnIiwib3JnYW5pemVyIjoidG9fa2xaTHRpSFY0NDZGdjk4T0xOZE5taS1FUDVPYVR0YkJrb3RUWUxpYz0iLCJ3aXRuZXNzZXMiOltdLCJvYmplY3QiOiJsYW8iLCJhY3Rpb24iOiJjcmVhdGUifQ=="
-    val source: String = s"""{
-                                 |      "message_id": "$id",
-                                 |      "sender": "$sender",
-                                 |      "signature": "$signature",
-                                 |      "data": "$data",
-                                 |      "witness_signatures": [{"witness": "wit1", "signature": "sig1"}, {"witness": "wit2", "signature": "sig2"}]
-                                 |    }""".stripMargin
+    val witnessesSigs: String = "[{\"witness\": \"wit1\", \"signature\": \"sig1\"}, {\"witness\": \"wit2\", \"signature\": \"sig2\"}]"
 
+    val source = getFormattedSource(id,sender,signature,data, witnessesSigs)
     var expected: Message = buildExpected(id, sender, signature, data)
     // Add witnesses to the expected message
     val witness = (WitnessSignaturePair(PublicKey(Base64Data("wit1")), Signature(Base64Data("sig1")))
@@ -68,14 +73,8 @@ class HighLevelProtocolSuite extends FunSuite with Matchers  {
     val sender: String = ""
     val signature: String = ""
     val data: String = ""
-    val source: String =  s"""{
-                               |      "message_id": "$id",
-                               |      "sender": "$sender",
-                               |      "signature": "$signature",
-                               |      "data": "$data",
-                               |      "witness_signatures": []
-                               |    }""".stripMargin
-
+    val witnessesSigs: String = "[]"
+    val source = getFormattedSource(id,sender,signature,data, witnessesSigs)
 
     var expected: Message = buildExpected(id, sender, signature, data)
     val message = Message.buildFromJson(source)
@@ -89,13 +88,9 @@ class HighLevelProtocolSuite extends FunSuite with Matchers  {
     val sender: String ="to_klZLtiHV446Fv98OLNdNmi-EP5OaTtbBkotTYLic="
     val signature: String = "2VDJCWg11eNPUvZOnvq5YhqqIKLBcik45n-6o87aUKefmiywagivzD4o_YmjWHzYcb9qg-OgDBZbBNWSUgJICA=="
     val data: String = "eyJjcmVhdGlvbiI6MTYzMTg4NzQ5NiwiaWQiOiJ4aWdzV0ZlUG1veGxkd2txMUt1b0wzT1ZhODl4amdYalRPZEJnSldjR1drPSIsIm5hbWUiOiJoZ2dnZ2dnIiwib3JnYW5pemVyIjoidG9fa2xaTHRpSFY0NDZGdjk4T0xOZE5taS1FUDVPYVR0YkJrb3RUWUxpYz0iLCJ3aXRuZXNzZXMiOltdLCJvYmplY3QiOiJsYW8iLCJhY3Rpb24iOiJjcmVhdGUifQ=="
-    val source: String = s"""{
-                                 |      "message_id": "$id",
-                                 |      "sender": "$sender",
-                                 |      "signature": "$signature",
-                                 |      "data": "$data",
-                                 |      "witness_signatures": []
-                                 |    }""".stripMargin
+    val witnessesSigs: String = "[]"
+
+    val source = getFormattedSource(id,sender,signature,data, witnessesSigs)
 
     val expected: Message = buildExpected(id, sender, signature, data)
     val message = Message.buildFromJson(source)
@@ -108,17 +103,30 @@ class HighLevelProtocolSuite extends FunSuite with Matchers  {
     an [spray.json.JsonParser.ParsingException] should be thrownBy(Message.buildFromJson(source))
   }
 
+  /**
+    * This tests against all not allowed key types/formats
+    */
   test("Invalid json keys format test"){
-  val set = for { p <- (MsgField.values - WITNESS_SIGNATURES)
-                  t <- Set("[]", "{}", "null", "1")
-                } yield (t,p)
+    //Forms pairs/combinations of different MsgField values (except Witness) and forbidden key types
+    //for formating
+    val set = for { p <- (MsgField.values - WITNESS_SIGNATURES)
+                    t <- Set("[]", "{}", "null", "1")
+                  } yield (t,p)
 
-  val setWitness = Set("","1", "{}", "null").map((_,WITNESS_SIGNATURES))
-  forEvery(set union setWitness){
-    case (t: String, p: MsgField) =>  {
-        val source: String = getFormattedString(p,t)
-        an [IllegalArgumentException] should be thrownBy(Message.buildFromJson(source))
+    //Form left pairs/combinations of witness signatures and its forbidden key types for formatting
+    val setWitness = Set("","1", "{}", "null").map((_,WITNESS_SIGNATURES))
+
+    //Test against all the (incorrect) combinations
+    forEvery(set union setWitness){
+      case (t: String, p: MsgField) =>  {
+          val source: String = getFormattedString(p,t)
+          an [IllegalArgumentException] should be thrownBy(Message.buildFromJson(source))
+        }
       }
-    }
   }
+}
+
+object MsgField extends Enumeration {
+  type MsgField = Value
+  val MESSAGE_ID, SENDER, SIGNATURE, DATA, WITNESS_SIGNATURES = Value
 }
