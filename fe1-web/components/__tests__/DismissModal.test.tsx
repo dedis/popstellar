216aa130453015af9caa35793eb73a27d66c4715
Kilian Schneiter <kilian.schneiter@bluewin.ch>
2021-11-13 20:23:08 +0100

    Add dismiss test for DismissModal

--- fe1-web/components/__tests__/DismissModal.test.tsx
+++ fe1-web/components/__tests__/DismissModal.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
+import { fireEvent, render } from '@testing-library/react-native';
 import DismissModal from '../DismissModal';
 
 let setModalIsVisible: Function;
@@ -19,5 +19,20 @@ describe('DismissModal', () => {
       />,
     ).toJSON();
     expect(component).toMatchSnapshot();
+  });
+
+  it('disappears correctly after dismissing', () => {
+    const { getByText, toJSON } = render(
+      <DismissModal
+        visibility
+        setVisibility={setModalIsVisible}
+        title="Title"
+        description="Description"
+        buttonText="Ok"
+      />,
+    );
+    const button = getByText('Ok');
+    fireEvent.press(button);
+    expect(toJSON()).toMatchSnapshot();
   });
 });
