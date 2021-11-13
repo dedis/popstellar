5c9e08469a490ff5b2d20855ad7ad997c5fecce2
Kilian Schneiter <kilian.schneiter@bluewin.ch>
2021-11-13 15:00:06 +0100

    Add tests for error throws

--- fe1-web/model/objects/__tests__/Chirp.test.ts
+++ fe1-web/model/objects/__tests__/Chirp.test.ts
@@ -3,7 +3,12 @@ import '__tests__/utils/matchers';
 import {
   Chirp, ChirpState,
 } from '../Chirp';
-import { ProtocolError } from '../../network';
+import { Timestamp } from '../Timestamp';
+import { Hash } from '../Hash';
+
+const TIMESTAMP = new Timestamp(12345);
+const HASH_ID = new Hash('id');
+const HASH_PARENT = new Hash('parent');
 
 describe('Chirp object', () => {
   it('can do a state round trip', () => {
@@ -19,93 +24,68 @@ describe('Chirp object', () => {
     expect(chirp.toState()).toStrictEqual(chirpState);
   });
 
-  it('throws an error when object is undefined', () => {
-    const createWrongChirp = () => {
-      const wrongChirpState: ChirpState = {};
-      Chirp.fromState(wrongChirpState);
-    };
-    expect(createWrongChirp).toThrow(Error);
-  });
-
   it('throws an error when id is undefined', () => {
     const createWrongChirp = () => {
-      const wrongChirpState: ChirpState = {
-        id: undefined,
+      Chirp.constructor({
         sender: 'me',
         text: 'text',
-        time: 1234,
+        time: TIMESTAMP,
         likes: 0,
-        parentId: '5678',
-      };
-      Chirp.fromState(wrongChirpState);
+        parentId: HASH_PARENT,
+      });
     };
     expect(createWrongChirp).toThrow(Error);
   });
 
   it('throws an error when sender is undefined', () => {
     const createWrongChirp = () => {
-      const wrongChirpState: ChirpState = {
-        id: '1234',
-        sender: undefined,
+      Chirp.constructor({
+        id: HASH_ID,
         text: 'text',
-        time: 1234,
+        time: TIMESTAMP,
         likes: 0,
-        parentId: '5678',
-      };
-      Chirp.fromState(wrongChirpState);
+        parentId: HASH_PARENT,
+      });
     };
     expect(createWrongChirp).toThrow(Error);
   });
 
   it('throws an error when text is undefined', () => {
     const createWrongChirp = () => {
-      const wrongChirpState: ChirpState = {
-        id: '1234',
+      Chirp.constructor({
+        id: HASH_ID,
         sender: 'me',
-        text: undefined,
-        time: 1234,
+        time: TIMESTAMP,
         likes: 0,
-        parentId: '5678',
-      };
-      Chirp.fromState(wrongChirpState);
+        parentId: HASH_PARENT,
+      });
     };
     expect(createWrongChirp).toThrow(Error);
   });
 
   it('throws an error when time is undefined', () => {
     const createWrongChirp = () => {
-      const wrongChirpState: ChirpState = {
-        id: '1234',
+      Chirp.constructor({
+        id: HASH_ID,
         sender: 'me',
         text: 'text',
-        time: undefined,
         likes: 0,
-        parentId: '5678',
-      };
-      Chirp.fromState(wrongChirpState);
+        parentId: HASH_PARENT,
+      });
     };
     expect(createWrongChirp).toThrow(Error);
   });
 
   it('throws an error when likes is undefined', () => {
     const createWrongChirp = () => {
-      const wrongChirpState: ChirpState = {
-        id: '1234',
+      Chirp.constructor({
+        id: HASH_ID,
         sender: 'me',
         text: 'text',
-        time: 1234,
-        likes: undefined,
-        parentId: '5678',
-      };
-      Chirp.fromState(wrongChirpState);
+        time: TIMESTAMP,
+        parentId: HASH_PARENT,
+      });
     };
     expect(createWrongChirp).toThrow(Error);
-  });
-
-  it('throws an error', () => {
-    const throwError = () => {
-      throw new ProtocolError('Error');
-    };
-    expect(throwError).toThrow('Error');
   });
 });
