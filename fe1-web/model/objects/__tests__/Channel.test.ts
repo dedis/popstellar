d4ee1e3187cab7c0c0f402ac892b6709bc6a2573
Kilian Schneiter <kilian.schneiter@bluewin.ch>
2021-11-13 23:44:31 +0100

    Add test for socialUserChannel

--- fe1-web/model/objects/__tests__/Channel.test.ts
+++ fe1-web/model/objects/__tests__/Channel.test.ts
@@ -1,18 +1,34 @@
 import 'jest-extended';
 
+import { KeyPairStore } from 'store';
 import { Hash } from '../Hash';
 import {
   channelFromIds,
   generalChirpsChannel,
   getLastChannel,
+  userSocialChannel,
 } from '../Channel';
+import { KeyPair } from '../KeyPair';
+import { PublicKey } from '../PublicKey';
 
 const FAKE_ID = Hash.fromStringArray('id');
+const PUBLIC_KEY = '1234';
+const PRIVATE_KEY = '5678';
 
 describe('Channel object', () => {
   it('channelFromIds should should return the correct channel', () => {
     expect(channelFromIds()).toEqual('/root');
     expect(channelFromIds(FAKE_ID)).toStrictEqual(`/root/${FAKE_ID}`);
+  });
+
+  it('userSocialChannel should return the correct channel', () => {
+    const keyPair = KeyPair.fromState({
+      publicKey: PUBLIC_KEY,
+      privateKey: PRIVATE_KEY,
+    });
+    KeyPairStore.store(keyPair);
+    const pk = new PublicKey(PUBLIC_KEY)
+    expect(userSocialChannel(FAKE_ID)).toStrictEqual(`/root/${FAKE_ID}/social/${pk}`);
   });
 
   it('generalChirpsChannel should return the correct channel', () => {
