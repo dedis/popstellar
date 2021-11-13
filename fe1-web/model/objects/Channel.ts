07391644e3de77720eac749319b402687d38fef8
Kilian Schneiter <kilian.schneiter@bluewin.ch>
2021-11-13 22:20:42 +0100

    Add user and general social channels in Channel.ts

--- fe1-web/model/objects/Channel.ts
+++ fe1-web/model/objects/Channel.ts
@@ -1,4 +1,5 @@
 import { Hash } from './Hash';
+import { KeyPairStore } from '../../store';
 
 export type Channel = string;
 export const ROOT_CHANNEL: Channel = '/root';
@@ -9,6 +10,25 @@ export function channelFromIds(...args: Hash[]) : Channel {
   return `${ROOT_CHANNEL}/${
     args.map((c) => c.valueOf())
       .join('/')}`;
+}
+
+/**
+ * Returns the social channel of the current user.
+ *
+ * @param laoIdHash - The hash containing the laoID of the currently opened LAO
+ */
+export function userSocialChannel(laoIdHash: Hash): Channel {
+  const userPublicKey = KeyPairStore.get().publicKey.valueOf();
+  return `${ROOT_CHANNEL}/${laoIdHash.valueOf()}/social/${userPublicKey}`;
+}
+
+/**
+ * Returns the general channel of chirps.
+ *
+ * @param laoIdHash - The hash containing the laoID of the currently opened LAO
+ */
+export function generalChirpsChannel(laoIdHash: Hash): Channel {
+  return `${ROOT_CHANNEL}/${laoIdHash.valueOf()}/social/chirps`;
 }
 
 /** Returns the last part of the channel which is usually an event id
