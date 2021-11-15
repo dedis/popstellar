0751d720ef837946a670cf97ef868bf4dc848e8a
Kilian Schneiter <kilian.schneiter@bluewin.ch>
2021-11-15 16:33:22 +0100

    Remove text from addBroadcastChirp

--- fe1-web/model/network/method/message/data/__tests__/AddBroadcastChirp.test.ts
+++ fe1-web/model/network/method/message/data/__tests__/AddBroadcastChirp.test.ts
@@ -7,7 +7,6 @@ import { AddBroadcastChirp } from '../chirp/AddBroadcastChirp';
 import { ActionType, ObjectType } from '../MessageData';
 
 const TIMESTAMP = new Timestamp(1609455600); // 1st january 2021
-const TEXT = 'test text';
 const CHANNEL = '/root/abc/social/def';
 const ID = Base64UrlData.encode('message_id');
 
@@ -15,12 +14,11 @@ const sampleAddBroadcastChirp: Partial<AddBroadcastChirp> = {
   object: ObjectType.CHIRP,
   action: ActionType.ADD_BROADCAST,
   id: ID,
-  text: TEXT,
   channel: CHANNEL,
   timestamp: TIMESTAMP,
 };
 
-const dataAddChirpBroadcast: string = `{"object": "${ObjectType.CHIRP}", "action":"${ActionType.ADD_BROADCAST}", "chirp_id": "${ID}", "text": "${TEXT}", "channel": "${CHANNEL}", "timestamp": ${TIMESTAMP}}`;
+const dataAddChirpBroadcast: string = `{"object": "${ObjectType.CHIRP}", "action":"${ActionType.ADD_BROADCAST}", "chirp_id": "${ID}", "channel": "${CHANNEL}", "timestamp": ${TIMESTAMP}}`;
 
 describe('AddBroadcastChirp', () => {
   it('should be created correctly from JSON', () => {
@@ -29,7 +27,6 @@ describe('AddBroadcastChirp', () => {
       object: ObjectType.CHIRP,
       action: ActionType.ADD_BROADCAST,
       id: ID,
-      text: TEXT,
       channel: CHANNEL,
       timestamp: TIMESTAMP,
     };
@@ -46,18 +43,6 @@ describe('AddBroadcastChirp', () => {
       const createWrongObj = () => new AddBroadcastChirp({
         object: ObjectType.CHIRP,
         action: ActionType.ADD_BROADCAST,
-        text: TEXT,
-        channel: CHANNEL,
-        timestamp: TIMESTAMP,
-      });
-      expect(createWrongObj).toThrow(ProtocolError);
-    });
-
-    it('should throw an error if text is undefined', () => {
-      const createWrongObj = () => new AddBroadcastChirp({
-        object: ObjectType.CHIRP,
-        action: ActionType.ADD_BROADCAST,
-        id: ID,
         channel: CHANNEL,
         timestamp: TIMESTAMP,
       });
@@ -69,7 +54,6 @@ describe('AddBroadcastChirp', () => {
         object: ObjectType.CHIRP,
         action: ActionType.ADD_BROADCAST,
         id: ID,
-        text: TEXT,
         channel: CHANNEL,
       });
       expect(createWrongObj).toThrow(ProtocolError);
@@ -80,7 +64,6 @@ describe('AddBroadcastChirp', () => {
         object: ObjectType.CHIRP,
         action: ActionType.ADD_BROADCAST,
         id: ID,
-        text: TEXT,
         timestamp: TIMESTAMP,
       });
       expect(createWrongObj).toThrow(ProtocolError);
--- fe1-web/model/network/method/message/data/chirp/AddBroadcastChirp.ts
+++ fe1-web/model/network/method/message/data/chirp/AddBroadcastChirp.ts
@@ -13,28 +13,17 @@ export class AddBroadcastChirp implements MessageData {
   // The ID of the received chirp
   public readonly id: Hash;
 
-  // The text of the chirp
-  public readonly text: string;
-
-  // The timestamp at which the chirp has been posted
-  public readonly timestamp: Timestamp;
-
   // The channel to which the chirp has been posted
   public readonly channel: string;
 
-  // The parent ID of the chirp (if it is a reply)
-  public readonly parentId?: Hash;
+  // The timestamp at which the chirp has been posted
+  public readonly timestamp: Timestamp;
 
   constructor(msg: Partial<AddBroadcastChirp>) {
     if (!msg.id) {
       throw new ProtocolError('Undefined \'id\' parameter encountered during \'AddBroadcastChirp\'');
     }
     this.id = msg.id;
-
-    if (!msg.text) {
-      throw new ProtocolError('Undefined \'text\' parameter encountered during \'AddBroadcastChirp\'');
-    }
-    this.text = msg.text;
 
     if (!msg.timestamp) {
       throw new ProtocolError('Undefined \'timestamp\' parameter encountered during \'AddBroadcastChirp\'');
@@ -46,10 +35,6 @@ export class AddBroadcastChirp implements MessageData {
       throw new ProtocolError('Undefined \'channel\' parameter encountered during \'AddBroadcastChirp\'');
     }
     this.channel = msg.channel;
-
-    if (msg.parentId) {
-      this.parentId = msg.parentId;
-    }
   }
 
   /**
