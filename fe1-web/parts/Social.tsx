d6d1ca8d2f8b81ef97ebbb5b83a4327056001859
Kilian Schneiter <kilian.schneiter@bluewin.ch>
2021-11-17 17:56:10 +0100

    Add chirp displaying logic in Social.ts

--- fe1-web/parts/Social.tsx
+++ fe1-web/parts/Social.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import {
+  FlatList,
   StyleSheet, TextStyle, View, ViewStyle,
 } from 'react-native';
 
@@ -8,6 +9,10 @@ import TextInputChirp from 'components/TextInputChirp';
 import STRINGS from 'res/strings';
 
 import { requestAddChirp } from 'network/MessageApi';
+import { ChirpState } from 'model/objects/Chirp';
+import { makeChirpsList } from 'store/reducers/SocialReducer';
+import { useSelector } from 'react-redux';
+import WideButtonView from '../components/WideButtonView';
 
 /**
  * UI for the Social Media component
@@ -34,13 +39,34 @@ const Social = () => {
       });
   };
 
+  let chirpList: ChirpState[] = [];
+
+  const updateChirps = () => {
+    const chirps = makeChirpsList();
+    chirpList = useSelector(chirps);
+  };
+
+  const renderChirpState = (chirp: ChirpState) => {
+    const text = `${chirp.sender} said ${chirp.text}`;
+    return <TextBlock text={text} />;
+  };
+
   return (
     <View style={styles.view}>
       <TextInputChirp
         onChangeText={setInputChirp}
         onPress={publishChirp}
       />
+      <WideButtonView
+        title="Update feed"
+        onPress={updateChirps}
+      />
       <TextBlock text={STRINGS.feed_description} />
+      <FlatList
+        data={chirpList}
+        renderItem={renderChirpState}
+        keyExtractor={(item) => item.id.toString()}
+      />
     </View>
   );
 };
