3035628b8bf8369aba3d583f3d81af0d45639dbd
Kilian Schneiter <kilian.schneiter@bluewin.ch>
2021-12-13 16:43:02 +0100

    Add tests for User list item component

---
+++ fe1-web/components/__tests__/UserListItem.test.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react-native';
+import STRINGS from 'res/strings';
+import { Channel, Hash, PublicKey } from 'model/objects';
+import UserListItem from '../UserListItem';
+import { subscribeToChannel } from '../../network/CommunicationApi';
+
+const publicKey = new PublicKey('PublicKey');
+const laoId = new Hash('LaoId');
+
+jest.mock('network/CommunicationApi.ts', () => ({
+  subscribeToChannel: jest.fn((c: Channel) => Promise.resolve(c)),
+}));
+
+describe('UserListItem', () => {
+  it('calls subscribeToChannel correctly when clicking on follow', () => {
+    const expectedChannel = '/root/LaoId/social/PublicKey';
+    const button = render(
+      <UserListItem laoId={laoId} publicKey={publicKey} />,
+    ).getByText(STRINGS.follow_button);
+    fireEvent.press(button);
+    expect(subscribeToChannel).toHaveBeenCalledTimes(1);
+    expect(subscribeToChannel).toHaveBeenCalledWith(expectedChannel);
+  });
+
+  it('renders correctly', () => {
+    const component = render(
+      <UserListItem laoId={laoId} publicKey={publicKey} />,
+    ).toJSON();
+    expect(component).toMatchSnapshot();
+  });
+
+  it('renders correctly after clicking on follow', () => {
+    const { toJSON, getByText } = render(
+      <UserListItem laoId={laoId} publicKey={publicKey} />,
+    );
+    const button = getByText(STRINGS.follow_button);
+    fireEvent.press(button);
+    expect(toJSON()).toMatchSnapshot();
+  });
+});
