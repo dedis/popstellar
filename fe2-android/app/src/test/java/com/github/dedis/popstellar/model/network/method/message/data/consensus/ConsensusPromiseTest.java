08aadf150d35c68943195f513e8e9a940d07059e
Gabriel Fleischer <gabriel.fleischer@epfl.ch>
2021-12-14 08:52:15 +0100

    Fix android build issue and make sure it doesn't happen again

--- fe2-android/app/src/test/java/com/github/dedis/popstellar/model/network/method/message/data/consensus/ConsensusPromiseTest.java
+++ fe2-android/app/src/test/java/com/github/dedis/popstellar/model/network/method/message/data/consensus/ConsensusPromiseTest.java
@@ -117,7 +117,8 @@ public class ConsensusPromiseTest {
     String dir = "protocol/examples/messageData/consensus_promise/";
     String jsonInvalid1 = JsonTestUtils.loadFile(dir + "wrong_promise_negative_created_at.json");
     String jsonInvalid2 = JsonTestUtils.loadFile(dir + "wrong_promise_negative_accepted_try.json");
-    String jsonInvalid3 = JsonTestUtils.loadFile(dir + "wrong_promise_negative_proposed_try.json");
+    String jsonInvalid3 = JsonTestUtils.loadFile(dir + "wrong_promise_negative_promised_try.json");
+
     assertThrows(JsonParseException.class, () -> JsonTestUtils.parse(jsonInvalid1));
     assertThrows(JsonParseException.class, () -> JsonTestUtils.parse(jsonInvalid2));
     assertThrows(JsonParseException.class, () -> JsonTestUtils.parse(jsonInvalid3));
