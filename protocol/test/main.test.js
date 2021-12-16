10a47ecc85f0dfa1eafe460f3b0ed88904b762bd
arnauds5 <arnauds5@users.noreply.github.com>
2021-12-16 18:27:58 +0100

    Add a message data for when Paxos failed

--- protocol/test/main.test.js
+++ protocol/test/main.test.js
@@ -209,6 +209,13 @@ test("message data: consensus", () => {
     // Learn
     learn = require("../examples/messageData/consensus_learn/learn.json");
     expect(learn).toBeValid(messageDataSchema)
+
+    // Paxos Failure
+    paxos_failure = require("../examples/messageData/consensus_paxos_failure/paxos_failure.json");
+    expect(paxos_failure).toBeValid(messageDataSchema)
+
+    paxos_failure = require("../examples/messageData/consensus_paxos_failure/wrong_paxos_failure_negative_created_at.json");
+    expect(paxos_failure).not.toBeValid(messageDataSchema)
 })
 
 function printError(errors) {
