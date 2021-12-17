675f519829a60bedfe1317daa515b6b7e44ee50c
Kilian Schneiter <kilian.schneiter@bluewin.ch>
2021-12-17 17:35:00 +0100

    Add tests

--- fe1-web/model/network/method/message/__tests__/Message.test.ts
+++ fe1-web/model/network/method/message/__tests__/Message.test.ts
@@ -1,20 +1,44 @@
 import 'jest-extended';
 import testKeyPair from 'test_data/keypair.json';
-import { Timestamp } from 'model/objects';
-import { AddChirp } from '../data';
+import {
+  Base64UrlData, KeyPair,
+  PopToken,
+  PrivateKey,
+  PublicKey,
+  Timestamp,
+} from 'model/objects';
+import { KeyPairStore } from 'store';
+import { AddChirp, encodeMessageData } from '../data';
 import { Message } from '../Message';
 
-jest.mock('store/stores/KeyPairStore.ts', () => ({
-  getPublicKey: jest.fn(() => testKeyPair.publicKey),
-  getPrivateKey: jest.fn(() => testKeyPair.privateKey),
+const mockPublicKey = testKeyPair.publicKey;
+const mockPrivateKey = testKeyPair.privateKey;
+const mockPopToken = PopToken.fromState({
+  publicKey: testKeyPair.publicKey2,
+  privateKey: testKeyPair.privateKey2,
+});
+jest.mock('model/objects/wallet/Token.ts', () => ({
+  generateToken: jest.fn(async () => mockPopToken),
 }));
+
+beforeAll(() => {
+  KeyPairStore.store(KeyPair.fromState({
+    publicKey: mockPublicKey,
+    privateKey: mockPrivateKey,
+  }));
+});
 
 describe('Message', () => {
   it('fromData signs the message correctly when adding a chirp', () => {
-    const message = new AddChirp({
+    const messageData = new AddChirp({
       text: 'text',
       timestamp: new Timestamp(1607277600),
     });
-    Message.fromData(message);
+    const encodedDataJson: Base64UrlData = encodeMessageData(messageData);
+    const signature = mockPopToken.privateKey.sign(encodedDataJson);
+    Message.fromData(messageData).then((m) => {
+      expect(m.sender).toEqual(mockPopToken.publicKey);
+      expect(m.signature).toEqual(signature);
+    });
   });
 });
