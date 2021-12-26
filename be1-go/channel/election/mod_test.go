e24e140d9f27c66d5bf72791d0086d68ab4b1925
Robin Jaccard <robin.jaccard8@gmail.com>
2021-12-26 11:26:12 +0100

    add tests to election

--- be1-go/channel/election/mod_test.go
+++ be1-go/channel/election/mod_test.go
@@ -3,6 +3,7 @@ package election
 import (
 	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/require"
 	"go.dedis.ch/kyber/v3"
@@ -21,11 +22,243 @@ import (
 	"popstellar/validation"
 	"sync"
 	"testing"
+	"time"
 )
 
-const relativeExamplePath string = "../../../protocol/examples/messageData"
+const relativeQueryExamplePath string = "../../../protocol/examples/query"
+const relativeMsgDataExamplePath string = "../../../protocol/examples/messageData"
 
-func Test_CastVoteAndResult(t *testing.T) {
+// Tests that the channel works correctly when it receives a subscribe
+func Test_Election_Channel_Subscribe(t *testing.T) {
+
+	// create election channel: election with one question
+	electChannel, _ := newFakeChannel(t)
+
+	file := filepath.Join(relativeQueryExamplePath, "subscribe", "subscribe.json")
+	buf, err := os.ReadFile(file)
+	require.NoError(t, err)
+
+	var sub method.Subscribe
+	err = json.Unmarshal(buf, &sub)
+	require.NoError(t, err)
+
+	fakeSock := &fakeSocket{id: "socket"}
+
+	err = electChannel.Subscribe(fakeSock, sub)
+	require.NoError(t, err)
+
+	// Delete returns false if the socket is not present in the store
+	require.True(t, electChannel.sockets.Delete("socket"))
+}
+
+// Tests that the channel works correctly when it receives an unsubscribe
+func Test_Election_Channel_Unsubscribe(t *testing.T) {
+
+	// create election channel: election with one question
+	electChannel, _ := newFakeChannel(t)
+
+	file := filepath.Join(relativeQueryExamplePath, "unsubscribe", "unsubscribe.json")
+	buf, err := os.ReadFile(file)
+	require.NoError(t, err)
+
+	var unsub method.Unsubscribe
+	err = json.Unmarshal(buf, &unsub)
+	require.NoError(t, err)
+
+	fakeSock := &fakeSocket{id: "socket"}
+	electChannel.sockets.Upsert(fakeSock)
+
+	err = electChannel.Unsubscribe("socket", unsub)
+	require.NoError(t, err)
+
+	// Delete returns false if the socket is not present in the store
+	require.False(t, electChannel.sockets.Delete("socket"))
+}
+
+// Test that the channel throws an error when it receives an unsubscribe from a
+// non-subscribed source
+func Test_General_Channel_Wrong_Unsubscribe(t *testing.T) {
+
+	// create election channel: election with one question
+	electChannel, _ := newFakeChannel(t)
+
+	file := filepath.Join(relativeQueryExamplePath, "unsubscribe", "unsubscribe.json")
+	buf, err := os.ReadFile(file)
+	require.NoError(t, err)
+
+	var unsub method.Unsubscribe
+	err = json.Unmarshal(buf, &unsub)
+	require.NoError(t, err)
+
+	err = electChannel.Unsubscribe("socket", unsub)
+	require.Error(t, err, "client is not subscribed to this channel")
+}
+
+// Tests that the channel works correctly when it receives a catchup
+func Test_Election_Channel_Catchup(t *testing.T) {
+
+	// create election channel: election with one question
+	electChannel, _ := newFakeChannel(t)
+
+	// Create the messages
+	numMessages := 5
+
+	messages := make([]message.Message, numMessages)
+
+	for i := 0; i < numMessages; i++ {
+		// Create a new message containing only an id
+		msg := message.Message{MessageID: fmt.Sprintf("%d", i)}
+		messages[i] = msg
+
+		// Store the message in the inbox
+		electChannel.inbox.StoreMessage(msg)
+
+		// Wait before storing a new message to be able to have an unique
+		// timestamp for each message
+		time.Sleep(time.Millisecond)
+	}
+
+	// Compute the catchup method
+	catchupAnswer := electChannel.Catchup(method.Catchup{ID: 0})
+
+	// Check that the order of the messages is the same in `messages` and in
+	// `catchupAnswer`
+	for i := 0; i < numMessages; i++ {
+		require.Equal(t, messages[i].MessageID, catchupAnswer[i].MessageID,
+			catchupAnswer)
+	}
+}
+
+// Tests that the channel throws an error when it receives a broadcast message
+func Test_Election_Channel_Broadcast(t *testing.T) {
+
+	// create election channel: election with one question
+	electChannel, _ := newFakeChannel(t)
+
+	relativePath := filepath.Join(relativeExamplePath, "query", "broadcast")
+
+	file := filepath.Join(relativePath, "broadcast.json")
+	buf, err := os.ReadFile(file)
+	require.NoError(t, err)
+
+	var broad method.Broadcast
+	err = json.Unmarshal(buf, &broad)
+	require.NoError(t, err)
+
+	// an election channel isn't supposed to broadcast a message - must fail
+	require.Error(t, electChannel.Broadcast(broad))
+}
+
+// Tests that the channel works correctly when it receives a cast vote and
+// end the election correctly
+func Test_Publish_Cast_Vote_And_End_Election(t *testing.T) {
+
+	// create election channel: election with one question
+	electChannel, pkOrganizer := newFakeChannel(t)
+
+	// create a fakeSocket that is listening to the channel
+	fakeSock := &fakeSocket{id: "socket"}
+	electChannel.sockets.Upsert(fakeSock)
+
+	// check the created election has only one question
+	require.Equal(t, 1, len(electChannel.questions))
+
+	// get cast vote message data
+	file := filepath.Join(relativeExamplePath, "messageData",
+		"vote_cast_vote", "vote_cast_vote.json")
+	buf, err := os.ReadFile(file)
+	require.NoError(t, err)
+
+	var castVote messagedata.VoteCastVote
+	err = json.Unmarshal(buf, &castVote)
+	require.NoError(t, err)
+
+	buf64 := base64.URLEncoding.EncodeToString(buf)
+
+	// wrap the cast vote in a message
+	m := message.Message{
+		Data:              buf64,
+		Sender:            pkOrganizer,
+		Signature:         "h",
+		MessageID:         messagedata.Hash(buf64, "h"),
+		WitnessSignatures: []message.WitnessSignature{},
+	}
+
+	// wrap the message in a publish
+	relativePathCreatePub := filepath.Join("..", "..", "..", "protocol",
+		"examples", "query", "publish")
+
+	fileCreatePub := filepath.Join(relativePathCreatePub, "publish.json")
+	bufCreatePub, err := os.ReadFile(fileCreatePub)
+	require.NoError(t, err)
+
+	var pub method.Publish
+
+	err = json.Unmarshal(bufCreatePub, &pub)
+	require.NoError(t, err)
+
+	pub.Params.Message = m
+	pub.Params.Channel = electChannel.channelID
+
+	// publish the cast vote on the election channel
+	require.NoError(t, electChannel.Publish(pub, socket.ClientSocket{}))
+
+	// check new vote is in the valid votes map
+	for _, question := range electChannel.questions {
+		question.validVotesMu.RLock()
+
+		// check the question has one valid vote
+		require.Equal(t, 1, len(question.validVotes))
+
+		// check that the valid vote was done by the organizer
+		_, ok := question.validVotes[pkOrganizer]
+		require.True(t, ok)
+
+		question.validVotesMu.RUnlock()
+	}
+
+	// get end election message data
+	file = filepath.Join(relativeExamplePath, "messageData",
+		"election_end", "election_end.json")
+	buf, err = os.ReadFile(file)
+	require.NoError(t, err)
+
+	var endElect messagedata.ElectionEnd
+	err = json.Unmarshal(buf, &endElect)
+	require.NoError(t, err)
+
+	buf64 = base64.URLEncoding.EncodeToString(buf)
+
+	m = message.Message{
+		Data:              buf64,
+		Sender:            pkOrganizer,
+		Signature:         "h",
+		MessageID:         messagedata.Hash(buf64, "h"),
+		WitnessSignatures: []message.WitnessSignature{},
+	}
+
+	pub.Params.Message = m
+
+	require.NoError(t, electChannel.Publish(pub, socket.ClientSocket{}))
+
+	var broad method.Broadcast
+	err = json.Unmarshal(fakeSock.msg, &broad)
+	require.NoError(t, err)
+
+	var msg message.Message = broad.Params.Message
+	require.NoError(t, err)
+
+	bufff, err := base64.URLEncoding.DecodeString(msg.Data)
+	require.NoError(t, err)
+
+	var result messagedata.ElectionResult
+	err = json.Unmarshal(bufff, &result)
+	require.NoError(t, err)
+
+	require.Equal(t, result.Action, "result")
+}
+
+func Test_Cast_Vote_And_Gather_Result(t *testing.T) {
 
 	// create election channel: election with one question
 	electChannel, pkOrganizer := newFakeChannel(t)
@@ -34,7 +267,7 @@ func Test_CastVoteAndResult(t *testing.T) {
 	require.Equal(t, 1, len(electChannel.questions))
 
 	// get cast vote message data
-	file := filepath.Join(relativeExamplePath, "vote_cast_vote", "vote_cast_vote.json")
+	file := filepath.Join(relativeExamplePath, "messageData", "vote_cast_vote", "vote_cast_vote.json")
 	buf, err := os.ReadFile(file)
 	require.NoError(t, err)
 
@@ -68,7 +301,7 @@ func Test_CastVoteAndResult(t *testing.T) {
 	}
 
 	// get election result message data
-	file = filepath.Join(relativeExamplePath, "election_result.json")
+	file = filepath.Join(relativeExamplePath, "messageData", "election_result.json")
 	buf, err = os.ReadFile(file)
 	require.NoError(t, err)
 
@@ -112,7 +345,7 @@ func newFakeChannel(t *testing.T) (*Channel, string) {
 	fakeHub, err := NewfakeHub(keypair.public, nolog, nil)
 	require.NoError(t, err)
 
-	file := filepath.Join(relativeExamplePath, "election_setup", "election_setup.json")
+	file := filepath.Join(relativeExamplePath, "messageData", "election_setup", "election_setup.json")
 	buf, err := os.ReadFile(file)
 	require.NoError(t, err)
 
