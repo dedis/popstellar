dabd47e7c4ee3abb8b874df89feb3c836889fc1b
NicolasGilliard <nicolas.gilliard@epfl.ch>
2021-12-28 12:48:01 +0100

    mock hub to try a correction for the bug

--- be1-go/cli/mod_test.go
+++ be1-go/cli/mod_test.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"io"
+	"popstellar/channel"
 	"popstellar/channel/lao"
 	"popstellar/crypto"
 	"popstellar/hub"
-	"popstellar/hub/standard_hub"
 	"popstellar/network"
 	"popstellar/network/socket"
 	"sync"
@@ -14,12 +14,13 @@ import (
 
 	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/require"
+	"go.dedis.ch/kyber/v3"
 )
 
 func TestConnectToSocket(t *testing.T) {
 	log := zerolog.New(io.Discard)
 
-	oh, err := standard_hub.NewHub(crypto.Suite.Point(), log, lao.NewChannel, hub.OrganizerHubType)
+	oh, err := newMockHub(crypto.Suite.Point(), log, lao.NewChannel, hub.OrganizerHubType)
 	require.NoError(t, err)
 	oh.Start()
 
@@ -29,7 +30,7 @@ func TestConnectToSocket(t *testing.T) {
 
 	time.Sleep(1 * time.Second)
 
-	wh, err := standard_hub.NewHub(crypto.Suite.Point(), log, lao.NewChannel, hub.WitnessHubType)
+	wh, err := newMockHub(crypto.Suite.Point(), log, lao.NewChannel, hub.WitnessHubType)
 	require.NoError(t, err)
 	wDone := make(chan struct{})
 	wh.Start()
@@ -46,4 +47,61 @@ func TestConnectToSocket(t *testing.T) {
 	wh.Stop()
 	close(wDone)
 	wg.Wait()
+}
+
+// mockHub implements the Hub interface.
+type mockHub struct {
+	hubType hub.HubType
+
+	messageChan chan socket.IncomingMessage
+
+	closedSockets chan string
+
+	pubKeyOrg kyber.Point
+
+	stop chan struct{}
+
+	laoFac channel.LaoFactory
+}
+
+// newMockHub returns a new mocked Hub.
+func newMockHub(publicOrg kyber.Point, log zerolog.Logger, laoFac channel.LaoFactory,
+	hubType hub.HubType) (*mockHub, error) {
+
+	hub := mockHub{
+		hubType:       hubType,
+		messageChan:   make(chan socket.IncomingMessage),
+		closedSockets: make(chan string),
+		pubKeyOrg:     publicOrg,
+		stop:          make(chan struct{}),
+		laoFac:        laoFac,
+	}
+
+	return &hub, nil
+}
+
+func (h *mockHub) NotifyNewServer(socket.Socket) error {
+	return nil
+}
+
+func (h *mockHub) Start() {
+	go func() {
+		<-h.stop
+	}()
+}
+
+func (h *mockHub) Stop() {
+	close(h.stop)
+}
+
+func (h *mockHub) Receiver() chan<- socket.IncomingMessage {
+	return h.messageChan
+}
+
+func (h *mockHub) OnSocketClose() chan<- string {
+	return h.closedSockets
+}
+
+func (h *mockHub) Type() hub.HubType {
+	return h.hubType
 }
