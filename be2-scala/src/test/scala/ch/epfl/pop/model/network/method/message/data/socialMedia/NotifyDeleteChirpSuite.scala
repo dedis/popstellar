1e0c8783932398b8cc90c07b2d0216536b175bd5
Tuomas <tuomas.pkknn@gmail.com>
2022-01-05 14:56:35 +0100

    refactor add_broadcast into notify_add and delete_broadcast into notify_delete

--- be2-scala/src/test/scala/ch/epfl/pop/model/network/method/message/data/socialMedia/NotifyDeleteChirpSuite.scala
+++ be2-scala/src/test/scala/ch/epfl/pop/model/network/method/message/data/socialMedia/NotifyDeleteChirpSuite.scala
@@ -6,22 +6,22 @@ import ch.epfl.pop.model.network.method.message.data.{ActionType, ObjectType}
 import ch.epfl.pop.json.MessageDataProtocol._
 import spray.json._
 
-class DeleteBroadcastChirpSuite extends FunSuite with Matchers {
+class NotifyDeleteChirpSuite extends FunSuite with Matchers {
     private final val id: Hash = Hash(Base64Data.encode(""))
     private final val channel: Channel = Channel("/root/channel")
     private final val timestamp = Timestamp(0)
-    private final val msg: DeleteBroadcastChirp = DeleteBroadcastChirp(id, channel, timestamp)
+    private final val msg: NotifyDeleteChirp = NotifyDeleteChirp(id, channel, timestamp)
 
     test("Constructor/apply works as intended"){
         msg.chirp_id should equal(id)
         msg.channel should equal(channel)
         msg.timestamp should equal(timestamp)
         msg._object should equal(ObjectType.CHIRP)
-        msg.action should equal(ActionType.DELETE_BROADCAST)
+        msg.action should equal(ActionType.NOTIFY_DELETE)
     }
 
     test("json conversions work back and forth"){
-        val msg2: DeleteBroadcastChirp = DeleteBroadcastChirp.buildFromJson(msg.toJson.toString)
+        val msg2: NotifyDeleteChirp = NotifyDeleteChirp.buildFromJson(msg.toJson.toString)
         msg2 should equal(msg)
     }
 }
