f0eeb7db31c70f9f9a28e67fa894378911d06971
Kilian Schneiter <kilian.schneiter@bluewin.ch>
2022-01-16 15:44:32 +0100

    Add social media search navigation

---
+++ fe1-web/parts/lao/socialMedia/SocialSearchNavigation.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import STRINGS from 'res/strings';
+import { PublicKey } from 'model/objects';
+import PropTypes from 'prop-types';
+import SocialSearch from './SocialSearch';
+import SocialUserProfile from './SocialUserProfile';
+
+/**
+ * Defines the social media search navigation. It goes from the list of attendees to the profile
+ * of them.
+ */
+
+const Stack = createStackNavigator();
+
+const SocialSearchNavigation = (props: IPropTypes) => {
+  const { currentUserPublicKey } = props;
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name={STRINGS.social_media_navigation_tab_search}>
+        {() => <SocialSearch currentUserPublicKey={currentUserPublicKey} />}
+      </Stack.Screen>
+      <Stack.Screen
+        name={STRINGS.social_media_navigation_tab_user_profile}
+        component={SocialUserProfile}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const propTypes = {
+  currentUserPublicKey: PropTypes.instanceOf(PublicKey).isRequired,
+};
+
+SocialSearchNavigation.prototype = propTypes;
+
+type IPropTypes = {
+  currentUserPublicKey: PublicKey,
+};
+
+export default SocialSearchNavigation;
