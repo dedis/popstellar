f80875643dbea70b36e57eef56842b756a159963
Gabriel Fleischer <gabriel.fleischer@epfl.ch>
2022-01-18 15:32:15 +0100

    Fix codesmells

--- fe2-android/app/src/main/java/com/github/dedis/popstellar/utility/error/keys/InvalidPoPTokenException.java
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/utility/error/keys/InvalidPoPTokenException.java
@@ -1,20 +1,10 @@
 package com.github.dedis.popstellar.utility.error.keys;
 
 import com.github.dedis.popstellar.model.objects.security.PoPToken;
-import com.github.dedis.popstellar.model.objects.security.PublicKey;
-
-import java.util.Set;
 
 public class InvalidPoPTokenException extends KeyException {
 
-  private final Set<PublicKey> validTokens;
-
-  public InvalidPoPTokenException(PoPToken token, Set<PublicKey> validTokens) {
+  public InvalidPoPTokenException(PoPToken token) {
     super("The token " + token.getPublicKey() + " is invalid");
-    this.validTokens = validTokens;
-  }
-
-  public Set<PublicKey> getValidTokens() {
-    return validTokens;
   }
 }
--- fe2-android/app/src/main/java/com/github/dedis/popstellar/utility/error/keys/KeyException.java
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/utility/error/keys/KeyException.java
@@ -3,11 +3,11 @@ package com.github.dedis.popstellar.utility.error.keys;
 /** This class regroup all the exceptions that can be generated when retrieving a key */
 public abstract class KeyException extends Exception {
 
-  public KeyException(String msg, Exception cause) {
+  protected KeyException(String msg, Exception cause) {
     super(msg, cause);
   }
 
-  public KeyException(String msg) {
+  protected KeyException(String msg) {
     super(msg);
   }
 }
