95965f77f93313a10237d9391fc701d0930a6af9
Kilian Schneiter <kilian.schneiter@bluewin.ch>
2022-01-19 15:48:05 +0100

    Remove ../

--- fe1-web/components/ProfileIcon.tsx
+++ fe1-web/components/ProfileIcon.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Blockies from 'react-blockies';
+import { popBlue } from 'styles/colors';
 import { PublicKey } from 'model/objects';
-import { popBlue } from '../styles/colors';
 
 const ProfileIcon = (props: IPropTypes) => {
   const { publicKey, size, scale } = props;
