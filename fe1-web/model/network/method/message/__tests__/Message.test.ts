3f74ffd4aac517bec6595a073510b61cbb8d7c1f
Kilian Schneiter <kilian.schneiter@bluewin.ch>
2022-02-11 12:43:09 +0100

    Add MessageRegistry in tests

--- fe1-web/model/network/method/message/__tests__/Message.test.ts
+++ fe1-web/model/network/method/message/__tests__/Message.test.ts
@@ -12,8 +12,8 @@ import {
 } from 'model/objects';
 import { KeyPairStore } from 'store';
 import { mockPopToken, mockPrivateKey, mockPublicKey } from '__tests__/utils/TestUtils';
-import { AddChirp, encodeMessageData, EndElection } from '../data';
-import { Message } from '../Message';
+import { AddChirp, encodeMessageData, EndElection, MessageRegistry } from '../data';
+import { configureSignatures, Message } from '../Message';
 
 const TIMESTAMP = 1603455600;
 const laoState: LaoState = {
@@ -31,6 +31,9 @@ jest.mock('model/objects/wallet/Token.ts', () => ({
 }));
 
 const pastKeyPairStoreState = KeyPairStore.get();
+
+const messageRegistry = new MessageRegistry();
+configureSignatures(messageRegistry);
 
 beforeAll(() => {
   KeyPairStore.store(KeyPair.fromState({
