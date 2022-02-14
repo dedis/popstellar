386ea05f890b4b4e7c54019244a5df1edb57622a
Nicolas Raulin <nicolas.raulin@epfl.ch>
2022-02-14 03:38:42 +0100

    Polished DiskStorage by eliminating nasty bugs

--- be2-scala/src/main/scala/ch/epfl/pop/storage/DiskStorage.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/storage/DiskStorage.scala
@@ -3,20 +3,20 @@ package ch.epfl.pop.storage
 import java.io.{File, IOException}
 import java.nio.charset.StandardCharsets
 
-import org.iq80.leveldb.{DB, Options, WriteBatch}
 import org.iq80.leveldb.impl.Iq80DBFactory.factory
+import org.iq80.leveldb.{DB, Options, WriteBatch}
 import org.slf4j.{Logger, LoggerFactory}
 
-class DiskStorage(val databaseFolder: String = DiskStorage.DATABASE_FOLDER) {
+class DiskStorage(val databaseFolder: String = DiskStorage.DATABASE_FOLDER) extends Storage {
 
   // create a logger for the database system
-  val logger: Logger = LoggerFactory.getLogger("DbLogger")
+  private val logger: Logger = LoggerFactory.getLogger("DbLogger")
 
-  val options: Options = new Options()
+  private val options: Options = new Options()
   options.createIfMissing(true)
   options.cacheSize(DiskStorage.CACHE_SIZE)
 
-  val db: DB = {
+  private val db: DB = {
     try factory.open(new File(databaseFolder), options)
     catch {
       case e: IOException =>
@@ -29,24 +29,40 @@ class DiskStorage(val databaseFolder: String = DiskStorage.DATABASE_FOLDER) {
 
 
   @throws [org.iq80.leveldb.DBException]
-  def read(key: String): String = {
-    new String(db.get(key.getBytes(StandardCharsets.UTF_8)), StandardCharsets.UTF_8)
+  def read(key: String): Option[String] = {
+    db.get(key.getBytes(StandardCharsets.UTF_8)) match {
+      case null => None
+      case bytes => Some(new String(bytes, StandardCharsets.UTF_8))
+    }
   }
 
   @throws [org.iq80.leveldb.DBException]
-  def write(keyValues: (String, String)*): Unit = {
-    val batch: WriteBatch = db.createWriteBatch()
+  def write(key: String, value: String): Unit = {
+    db.put(key.getBytes(StandardCharsets.UTF_8), value.getBytes(StandardCharsets.UTF_8))
+  }
 
-    try {
-      for (kv <- keyValues) {
-        batch.put(kv._1.getBytes(StandardCharsets.UTF_8), kv._2.getBytes(StandardCharsets.UTF_8))
+  @throws [org.iq80.leveldb.DBException]
+  def write(keyValues: (String, String)*): Unit = keyValues.size match {
+    // return early if nothing is being written
+    case 0 =>
+
+    // write a single (key -> value) pair in the db
+    case 1 => write(keyValues.head._1, keyValues.head._2)
+
+    // use a batch to write multiple (key -> value) pairs in the db
+    case _ =>
+      val batch: WriteBatch = db.createWriteBatch()
+
+      try {
+        for (kv <- keyValues) {
+          batch.put(kv._1.getBytes(StandardCharsets.UTF_8), kv._2.getBytes(StandardCharsets.UTF_8))
+        }
+
+        db.write(batch)
+
+      } finally {
+        batch.close()
       }
-
-      db.write(batch)
-
-    } finally {
-      batch.close()
-    }
   }
 
   @throws [org.iq80.leveldb.DBException]
@@ -58,5 +74,5 @@ class DiskStorage(val databaseFolder: String = DiskStorage.DATABASE_FOLDER) {
 object DiskStorage {
   val DATABASE_FOLDER: String = "database-new"
 
-  private val CACHE_SIZE: Long = 50 * 1024 * 1024 // 50 MB
+  private val CACHE_SIZE: Long = 64 * 1024 * 1024 // 64 MB
 }
