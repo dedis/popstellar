4ac84c047673070338778fbe41e0daf0a9a4de16
Pierluca Borsò <pierluca@borso.net>
2022-02-19 18:35:18 +0100

    MessageReducer gets initialized by ingestion

--- fe1-web/src/core/network/ingestion/Configure.ts
+++ fe1-web/src/core/network/ingestion/Configure.ts
@@ -1,8 +1,9 @@
-import { getStore } from 'core/redux';
+import { addReducer, getStore } from 'core/redux';
 import { getNetworkManager } from '../NetworkManager';
 import { MessageRegistry } from '../jsonrpc/messages';
 import { handleRpcRequests, setMessageRegistry } from './Handler';
 import { makeMessageStoreWatcher } from './Watcher';
+import messageReducer from './Reducer';
 
 /**
  * Configures all handlers of the system within a MessageRegistry.
@@ -12,6 +13,9 @@ import { makeMessageStoreWatcher } from './Watcher';
 export function configure(registry: MessageRegistry) {
   // configure the message handlers
   setMessageRegistry(registry);
+
+  // configure the message reducer
+  addReducer(messageReducer);
 
   // setup the handler for incoming messages
   getNetworkManager().setRpcHandler(handleRpcRequests);
