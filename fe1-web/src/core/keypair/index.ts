c3a42faf51e25780e9b15854a6b4c1cd3cb542b3
Pierluca Borsò <pierluca@borso.net>
2022-02-20 19:17:56 +0100

    Fix keypair initialization

--- fe1-web/src/core/keypair/index.ts
+++ fe1-web/src/core/keypair/index.ts
@@ -1,5 +1,6 @@
 import { addReducer } from 'core/redux';
 import keyPairReducer from './KeyPairReducer';
+import { KeyPairStore } from './KeyPairStore';
 
 export * from './KeyPairRegistry';
 export * from './KeyPairStore';
@@ -7,4 +8,7 @@ export * from './KeyPairReducer';
 
 export function configureKeyPair() {
   addReducer(keyPairReducer);
+
+  // initialize the keypair
+  KeyPairStore.get();
 }
