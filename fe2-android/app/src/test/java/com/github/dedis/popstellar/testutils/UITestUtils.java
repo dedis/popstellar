57a0573e9ce40dcbdff05f2a26b3b19ea76b2116
Gabriel Fleischer <gabriel.fleischer@epfl.ch>
2022-02-21 11:12:16 +0100

    Add documentation to UITestUtils

--- fe2-android/app/src/test/java/com/github/dedis/popstellar/testutils/UITestUtils.java
+++ fe2-android/app/src/test/java/com/github/dedis/popstellar/testutils/UITestUtils.java
@@ -17,8 +17,15 @@ import androidx.test.core.app.ApplicationProvider;
 import org.robolectric.shadows.ShadowDialog;
 import org.robolectric.shadows.ShadowToast;
 
+/** This class holds utility functions when retrieving particular elements of a view in a test */
 public class UITestUtils {
 
+  /**
+   * Assert that the latest toast showed the provided text
+   *
+   * @param resId resource of the text
+   * @param args arguments to the resource
+   */
   public static void assertToastIsShown(@StringRes int resId, Object... args) {
     assertThat("No toast have been displayed", ShadowToast.getLatestToast(), notNullValue());
 
@@ -26,6 +33,13 @@ public class UITestUtils {
     assertEquals(expected, ShadowToast.getTextOfLatestToast());
   }
 
+  /**
+   * Retrieve the last opened dialog expecting it to be of the provided type
+   *
+   * @param type of the dialog
+   * @param <D> of the dialog
+   * @return the dialog
+   */
   public static <D extends Dialog> D getLastDialog(Class<D> type) {
     Dialog dialog = ShadowDialog.getLatestDialog();
     assertThat("No dialog have been displayed", dialog, notNullValue());
@@ -34,14 +48,35 @@ public class UITestUtils {
     return (D) dialog;
   }
 
+  /**
+   * Retrieve the positive button of the latest dialog
+   *
+   * <p>For example : The Accept button or Confirm button
+   *
+   * @return the button
+   */
   public static Button dialogPositiveButton() {
     return getAlertDialogButton(DialogInterface.BUTTON_POSITIVE);
   }
 
+  /**
+   * Retrieve the negative button of the latest dialog
+   *
+   * <p>For example : The Cancel button
+   *
+   * @return the button
+   */
   public static Button dialogNegativeButton() {
     return getAlertDialogButton(DialogInterface.BUTTON_NEGATIVE);
   }
 
+  /**
+   * Retrieve a specific button from the latest Alert Dialog
+   *
+   * <p>This function aims at supporting most AlertDialog from different android versions
+   *
+   * @param buttonId the id of the Button, found in {@link DialogInterface}
+   */
   public static Button getAlertDialogButton(int buttonId) {
     Dialog dialog = getLastDialog(Dialog.class);
     if (dialog instanceof AlertDialog) {
