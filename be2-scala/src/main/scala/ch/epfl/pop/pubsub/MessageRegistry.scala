d7c6da0dc42d20414a9313c91ce53b95ec079068
Nicolas Raulin <nicolas.raulin@epfl.ch>
2022-02-23 10:43:36 +0100

    Applied review comments #841

--- be2-scala/src/main/scala/ch/epfl/pop/pubsub/MessageRegistry.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/pubsub/MessageRegistry.scala
@@ -11,9 +11,10 @@ import ch.epfl.pop.model.network.method.message.data.socialMedia._
 import ch.epfl.pop.model.network.method.message.data.witness.WitnessMessage
 import ch.epfl.pop.model.network.method.message.data.{ActionType, MessageData, ObjectType}
 import ch.epfl.pop.pubsub.MessageRegistry.RegisterEntry
+import ch.epfl.pop.pubsub.graph.SchemaVerifier.createSchemaVerifier
 import ch.epfl.pop.pubsub.graph.handlers._
 import ch.epfl.pop.pubsub.graph.validators._
-import ch.epfl.pop.pubsub.graph.{GraphMessage, JsonString, SchemaVerifier}
+import ch.epfl.pop.pubsub.graph.{GraphMessage, JsonString}
 
 import scala.util.Try
 
@@ -55,37 +56,37 @@ object MessageRegistry {
     val register: Register = new Register()
 
     // data lao
-    register.add((ObjectType.LAO, ActionType.CREATE), SchemaVerifier.createSchemaVerifier("dataCreateLao.json"), CreateLao.buildFromJson, LaoValidator.validateCreateLao, LaoHandler.handleCreateLao)
-    register.add((ObjectType.LAO, ActionType.STATE), SchemaVerifier.createSchemaVerifier("dataStateLao.json"), StateLao.buildFromJson, LaoValidator.validateStateLao, LaoHandler.handleStateLao)
-    register.add((ObjectType.LAO, ActionType.UPDATE_PROPERTIES), SchemaVerifier.createSchemaVerifier("dataUpdateLao.json"), UpdateLao.buildFromJson, LaoValidator.validateUpdateLao, LaoHandler.handleUpdateLao)
+    register.add((ObjectType.LAO, ActionType.CREATE), createSchemaVerifier("dataCreateLao.json"), CreateLao.buildFromJson, LaoValidator.validateCreateLao, LaoHandler.handleCreateLao)
+    register.add((ObjectType.LAO, ActionType.STATE), createSchemaVerifier("dataStateLao.json"), StateLao.buildFromJson, LaoValidator.validateStateLao, LaoHandler.handleStateLao)
+    register.add((ObjectType.LAO, ActionType.UPDATE_PROPERTIES), createSchemaVerifier("dataUpdateLao.json"), UpdateLao.buildFromJson, LaoValidator.validateUpdateLao, LaoHandler.handleUpdateLao)
 
     // data meeting
-    register.add((ObjectType.MEETING, ActionType.CREATE), SchemaVerifier.createSchemaVerifier("dataCreateMeeting.json"), CreateMeeting.buildFromJson, MeetingValidator.validateCreateMeeting, MeetingHandler.handleCreateMeeting)
-    register.add((ObjectType.MEETING, ActionType.STATE), SchemaVerifier.createSchemaVerifier("dataStateMeeting.json"), StateMeeting.buildFromJson, MeetingValidator.validateStateMeeting, MeetingHandler.handleStateMeeting)
+    register.add((ObjectType.MEETING, ActionType.CREATE), createSchemaVerifier("dataCreateMeeting.json"), CreateMeeting.buildFromJson, MeetingValidator.validateCreateMeeting, MeetingHandler.handleCreateMeeting)
+    register.add((ObjectType.MEETING, ActionType.STATE), createSchemaVerifier("dataStateMeeting.json"), StateMeeting.buildFromJson, MeetingValidator.validateStateMeeting, MeetingHandler.handleStateMeeting)
 
     // data roll call
-    register.add((ObjectType.ROLL_CALL, ActionType.CREATE), SchemaVerifier.createSchemaVerifier("dataCreateRollCall.json"), CreateRollCall.buildFromJson, RollCallValidator.validateCreateRollCall, RollCallHandler.handleCreateRollCall)
-    register.add((ObjectType.ROLL_CALL, ActionType.OPEN), SchemaVerifier.createSchemaVerifier("dataOpenRollCall.json"), OpenRollCall.buildFromJson, r => RollCallValidator.validateOpenRollCall(r), RollCallHandler.handleOpenRollCall)
-    register.add((ObjectType.ROLL_CALL, ActionType.REOPEN), SchemaVerifier.createSchemaVerifier("dataOpenRollCall.json"), ReopenRollCall.buildFromJson, RollCallValidator.validateReopenRollCall, RollCallHandler.handleReopenRollCall)
-    register.add((ObjectType.ROLL_CALL, ActionType.CLOSE), SchemaVerifier.createSchemaVerifier("dataCloseRollCall.json"), CloseRollCall.buildFromJson, RollCallValidator.validateCloseRollCall, RollCallHandler.handleCloseRollCall)
+    register.add((ObjectType.ROLL_CALL, ActionType.CREATE), createSchemaVerifier("dataCreateRollCall.json"), CreateRollCall.buildFromJson, RollCallValidator.validateCreateRollCall, RollCallHandler.handleCreateRollCall)
+    register.add((ObjectType.ROLL_CALL, ActionType.OPEN), createSchemaVerifier("dataOpenRollCall.json"), OpenRollCall.buildFromJson, r => RollCallValidator.validateOpenRollCall(r), RollCallHandler.handleOpenRollCall)
+    register.add((ObjectType.ROLL_CALL, ActionType.REOPEN), createSchemaVerifier("dataOpenRollCall.json"), ReopenRollCall.buildFromJson, RollCallValidator.validateReopenRollCall, RollCallHandler.handleReopenRollCall)
+    register.add((ObjectType.ROLL_CALL, ActionType.CLOSE), createSchemaVerifier("dataCloseRollCall.json"), CloseRollCall.buildFromJson, RollCallValidator.validateCloseRollCall, RollCallHandler.handleCloseRollCall)
 
     // data election
-    register.add((ObjectType.ELECTION, ActionType.SETUP), SchemaVerifier.createSchemaVerifier("dataSetupElection.json"), SetupElection.buildFromJson, ElectionValidator.validateSetupElection, ElectionHandler.handleSetupElection)
-    register.add((ObjectType.ELECTION, ActionType.CAST_VOTE), SchemaVerifier.createSchemaVerifier("dataCastVote.json"), CastVoteElection.buildFromJson, ElectionValidator.validateCastVoteElection, ElectionHandler.handleCastVoteElection)
-    register.add((ObjectType.ELECTION, ActionType.RESULT), SchemaVerifier.createSchemaVerifier("dataResultElection.json"), ResultElection.buildFromJson, ElectionValidator.validateResultElection, ElectionHandler.handleResultElection)
-    register.add((ObjectType.ELECTION, ActionType.END), SchemaVerifier.createSchemaVerifier("dataEndElection.json"), EndElection.buildFromJson, ElectionValidator.validateEndElection, ElectionHandler.handleEndElection)
+    register.add((ObjectType.ELECTION, ActionType.SETUP), createSchemaVerifier("dataSetupElection.json"), SetupElection.buildFromJson, ElectionValidator.validateSetupElection, ElectionHandler.handleSetupElection)
+    register.add((ObjectType.ELECTION, ActionType.CAST_VOTE), createSchemaVerifier("dataCastVote.json"), CastVoteElection.buildFromJson, ElectionValidator.validateCastVoteElection, ElectionHandler.handleCastVoteElection)
+    register.add((ObjectType.ELECTION, ActionType.RESULT), createSchemaVerifier("dataResultElection.json"), ResultElection.buildFromJson, ElectionValidator.validateResultElection, ElectionHandler.handleResultElection)
+    register.add((ObjectType.ELECTION, ActionType.END), createSchemaVerifier("dataEndElection.json"), EndElection.buildFromJson, ElectionValidator.validateEndElection, ElectionHandler.handleEndElection)
 
     // data witness
-    register.add((ObjectType.MESSAGE, ActionType.WITNESS), SchemaVerifier.createSchemaVerifier("dataWitnessMessage.json"), WitnessMessage.buildFromJson, WitnessValidator.validateWitnessMessage, WitnessHandler.handleWitnessMessage)
+    register.add((ObjectType.MESSAGE, ActionType.WITNESS), createSchemaVerifier("dataWitnessMessage.json"), WitnessMessage.buildFromJson, WitnessValidator.validateWitnessMessage, WitnessHandler.handleWitnessMessage)
 
     // data social media
-    register.add((ObjectType.CHIRP, ActionType.ADD), SchemaVerifier.createSchemaVerifier("dataAddChirp.json"), AddChirp.buildFromJson, SocialMediaValidator.validateAddChirp, SocialMediaHandler.handleAddChirp)
-    register.add((ObjectType.CHIRP, ActionType.DELETE), SchemaVerifier.createSchemaVerifier("dataDeleteChirp.json"), DeleteChirp.buildFromJson, SocialMediaValidator.validateDeleteChirp, SocialMediaHandler.handleDeleteChirp)
-    register.add((ObjectType.CHIRP, ActionType.NOTIFY_ADD), SchemaVerifier.createSchemaVerifier("dataNotifyAddChirp.json"), NotifyAddChirp.buildFromJson, SocialMediaValidator.validateNotifyAddChirp, SocialMediaHandler.handleNotifyAddChirp)
-    register.add((ObjectType.CHIRP, ActionType.NOTIFY_DELETE), SchemaVerifier.createSchemaVerifier("dataNotifyDeleteChirp.json"), NotifyDeleteChirp.buildFromJson, SocialMediaValidator.validateNotifyDeleteChirp, SocialMediaHandler.handleNotifyDeleteChirp)
+    register.add((ObjectType.CHIRP, ActionType.ADD), createSchemaVerifier("dataAddChirp.json"), AddChirp.buildFromJson, SocialMediaValidator.validateAddChirp, SocialMediaHandler.handleAddChirp)
+    register.add((ObjectType.CHIRP, ActionType.DELETE), createSchemaVerifier("dataDeleteChirp.json"), DeleteChirp.buildFromJson, SocialMediaValidator.validateDeleteChirp, SocialMediaHandler.handleDeleteChirp)
+    register.add((ObjectType.CHIRP, ActionType.NOTIFY_ADD), createSchemaVerifier("dataNotifyAddChirp.json"), NotifyAddChirp.buildFromJson, SocialMediaValidator.validateNotifyAddChirp, SocialMediaHandler.handleNotifyAddChirp)
+    register.add((ObjectType.CHIRP, ActionType.NOTIFY_DELETE), createSchemaVerifier("dataNotifyDeleteChirp.json"), NotifyDeleteChirp.buildFromJson, SocialMediaValidator.validateNotifyDeleteChirp, SocialMediaHandler.handleNotifyDeleteChirp)
 
-    register.add((ObjectType.REACTION, ActionType.ADD), SchemaVerifier.createSchemaVerifier("dataAddReaction.json"), AddReaction.buildFromJson, SocialMediaValidator.validateAddReaction, SocialMediaHandler.handleAddReaction)
-    register.add((ObjectType.REACTION, ActionType.DELETE), SchemaVerifier.createSchemaVerifier("dataDeleteReaction.json"), DeleteReaction.buildFromJson, SocialMediaValidator.validateDeleteReaction, SocialMediaHandler.handleDeleteReaction)
+    register.add((ObjectType.REACTION, ActionType.ADD), createSchemaVerifier("dataAddReaction.json"), AddReaction.buildFromJson, SocialMediaValidator.validateAddReaction, SocialMediaHandler.handleAddReaction)
+    register.add((ObjectType.REACTION, ActionType.DELETE), createSchemaVerifier("dataDeleteReaction.json"), DeleteReaction.buildFromJson, SocialMediaValidator.validateDeleteReaction, SocialMediaHandler.handleDeleteReaction)
 
     new MessageRegistry(register.get)
   }
