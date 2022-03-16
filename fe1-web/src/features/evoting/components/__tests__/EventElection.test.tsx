71347dea877ed6f02211b46f572ce8dd485e5577
Nico Hauser <me@tyratox.ch>
2022-03-16 18:14:56 +0100

    Update EventElection.test.tsx

--- fe1-web/src/features/evoting/components/__tests__/EventElection.test.tsx
+++ fe1-web/src/features/evoting/components/__tests__/EventElection.test.tsx
@@ -149,10 +149,6 @@ const contextValue = {
   },
 };
 
-afterEach(() => {
-  warn.mockClear();
-});
-
 describe('EventElection', () => {
   describe('Not started election', () => {
     it('renders correctly for an organizer', () => {
