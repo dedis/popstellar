bff7f672d969a9611d10a3d94ad3f079e9c1cbe7
zoemarin <zoe@marin.info>
2022-03-19 16:12:49 +0100

    Task 1 : Add some view Model

--- fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/digitalcash/DigitalCashMain.java
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/digitalcash/DigitalCashMain.java
@@ -2,6 +2,8 @@ package com.github.dedis.popstellar.ui.digitalcash;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.viewpager.widget.ViewPager;
 
 import android.app.ActionBar;
@@ -17,6 +19,9 @@ public class DigitalCashMain extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_digital_cash_main);
+
+        ViewModel digiviewmodel = new ViewModelProvider(this).get(DigitalCashViewModel.class);
+
         digitalCashPageAdapter = new DigitalCashPageAdapter(getSupportFragmentManager());
         viewPager = (ViewPager) findViewById(R.id.pager);
         viewPager.setAdapter(digitalCashPageAdapter);
---
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/digitalcash/DigitalCashViewModel.java
@@ -0,0 +1,6 @@
+package com.github.dedis.popstellar.ui.digitalcash;
+
+import androidx.lifecycle.ViewModel;
+
+public class DigitalCashViewModel extends ViewModel {
+}
