1e8f187fbb7ee4732b76c09f720565fd2e493d80
zoemarin <zoe@marin.info>
2022-03-20 11:06:13 +0100

    Task 1: Add some fragment connection

--- fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/transaction/TransactionMainActivity.java
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/transaction/TransactionMainActivity.java
@@ -1,15 +1,26 @@
 package com.github.dedis.popstellar.ui.transaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import com.github.dedis.popstellar.R;
+import com.google.android.material.navigation.NavigationView;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
-public class TransactionMainActivity extends AppCompatActivity {
+public class TransactionMainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
 
+    @Nullable
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +28,36 @@ public class TransactionMainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar_digitalcash);
         setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView nav = findViewById(R.id.nav_digital_cash);
+
+        nav.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,
+                "Open", "Close"
+                );
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new TransactionHomeFragment()).commit();
+        }
+    }
+
+    @Nullable
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.send_coin_m:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new TransactionSendFragment()).commit();
+                break;
+            case R.id.receive_coin_m:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new TransactionSendFragment()).commit();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
---
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/transaction/TransactionSendFragment.java
@@ -0,0 +1,24 @@
+package com.github.dedis.popstellar.ui.transaction;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.github.dedis.popstellar.R;
+
+import javax.annotation.Nullable;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class TransactionSendFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        return inflater.inflate(R.layout.transaction_fragment_send,container,false);
+    }
+}
