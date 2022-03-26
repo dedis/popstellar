08d6dbf04fe9de1cfd2eb89b059e935b39c5981f
ahominal <79504891+ahominal@users.noreply.github.com>
2022-03-26 12:48:34 +0100

    corect json

--- protocol/test/main.test.js
+++ protocol/test/main.test.js
@@ -149,9 +149,6 @@ test("message data: election", () => {
 
     election_setup = require("../examples/messageData/election_setup/election_setup.json")
     expect(election_setup).toBeValid(messageDataSchema)
-
-    election_open = require("../examples/messageData/election_open.json")
-    expect(election_open).toBeValid(messageDataSchema)
 })
 
 test("message data: message", () => {
