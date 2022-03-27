9d56ca8e20c4237aef31f63bec70c517081cbdd2
Tesa27 <stefan.eric@epfl.ch>
2022-03-27 11:41:06 +0200

    added a test for JsonConverter

---
+++ tests/karate/src/test/java/be/utils/JsonConverterTest.java
@@ -0,0 +1,77 @@
+package be.utils;
+
+import com.intuit.karate.Json;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class JsonConverterTest {
+
+  private Json constructJsonDataForValidLao(){
+    Map<String,Object> laoData = new LinkedHashMap<>();
+    laoData.put("object","lao");
+    laoData.put("action","create");
+    laoData.put("name","LAO");
+    laoData.put("creation",1633035721);
+    laoData.put("organizer","J9fBzJV70Jk5c-i3277Uq4CmeL4t53WDfUghaK0HpeM=");
+    String witness[] = new String[0];
+    laoData.put("witnesses",witness);
+    laoData.put("id","p_EYbHyMv6sopI5QhEXBf40MO_eNoq7V_LygBd4c9RA=");
+    Json laoDataJson = Json.of(laoData);
+    return laoDataJson;
+  }
+
+
+  @Test
+  public void testJsonDataToBase64PrintsInDesiredFormat(){
+    JsonConverter jsonConverter = new JsonConverter();
+    Map<String,Object> testMap = new LinkedHashMap<>();
+    testMap.put("test1","test2");
+    Json testJson = Json.of(testMap);
+    Json testConverter = jsonConverter.messageFromData(testJson.toString(),"publish",2, "/root");
+    String jsonString = testConverter.toString();
+    System.out.println(jsonString);
+  }
+
+  @Test
+  public void testIfMessageFromDataCorrespondsToTrueMessageForValidLao(){
+    JsonConverter jsonConverter = new JsonConverter();
+
+    String rawBase64ValidLaoData = "eyJvYmplY3QiOiJsYW8iLCJhY3Rpb24iOiJjcmVhdGUiLCJuYW1lIjoiTEFPIiwiY3JlYXRpb24iOjE2MzMwMzU3MjEsIm9yZ2FuaXplciI6Iko5ZkJ6SlY3MEprNWMtaTMyNzdVcTRDbWVMNHQ1M1dEZlVnaGFLMEhwZU09Iiwid2l0bmVzc2VzIjpbXSwiaWQiOiJwX0VZYkh5TXY2c29wSTVRaEVYQmY0ME1PX2VOb3E3Vl9MeWdCZDRjOVJBPSJ9";
+    Json laoDataJson = constructJsonDataForValidLao();
+
+    String constructedData = jsonConverter.convertJson(laoDataJson);
+    assert constructedData.equals(rawBase64ValidLaoData);
+  }
+
+  @Test
+  public void constructJsonMessageFromDataCorrespondsToTrueJsonMessage(){
+    String laoDataJsonString = constructJsonDataForValidLao().toString();
+    JsonConverter jsonConverter = new JsonConverter();
+    Json jsonValidLaoMessage= jsonConverter.messageFromData(laoDataJsonString,"publish",1,"/root");
+    Map<String,Object> validStringMessage = new LinkedHashMap<>();
+    validStringMessage.put("method","publish");
+    validStringMessage.put("id",1);
+    Map<String,Object> paramsMap = new LinkedHashMap<>();
+    paramsMap.put("channel","/root");
+    Map<String,Object> messageMap  = new LinkedHashMap<>();
+    messageMap.put("data","eyJvYmplY3QiOiJsYW8iLCJhY3Rpb24iOiJjcmVhdGUiLCJuYW1lIjoiTEFPIiwiY3JlYXRpb24iOjE2MzMwMzU3MjEsIm9yZ2FuaXplciI6Iko5ZkJ6SlY3MEprNWMtaTMyNzdVcTRDbWVMNHQ1M1dEZlVnaGFLMEhwZU09Iiwid2l0bmVzc2VzIjpbXSwiaWQiOiJwX0VZYkh5TXY2c29wSTVRaEVYQmY0ME1PX2VOb3E3Vl9MeWdCZDRjOVJBPSJ9");
+    messageMap.put("sender","J9fBzJV70Jk5c-i3277Uq4CmeL4t53WDfUghaK0HpeM=");
+    messageMap.put("signature","ONylxgHA9cbsB_lwdfbn3iyzRd4aTpJhBMnvEKhmJF_niE_pUHdmjxDXjEwFyvo5WiH1NZXWyXG27SYEpkasCA==");
+    messageMap.put("message_id","e15ae2a533d22237d5288c659dfb3f88c52acd6a573a98d67ad2247a13825504");
+    String[] witness = new String[0];
+    messageMap.put("witness_signatures",witness);
+    paramsMap.put("message",messageMap);
+    validStringMessage.put("params",paramsMap);
+    validStringMessage.put("jsonrpc","2.0");
+    Json toCompare = Json.of(validStringMessage);
+    assert toCompare.toString().equals(jsonValidLaoMessage.toString());
+
+  }
+
+
+
+
+
+}
