3abcc590ded2eb5bd493fa3adf0161f52f287657
zoemarin <zoe@marin.info>
2022-03-29 15:38:51 +0200

    Digital Cash try to create some addtransaction

--- fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/digitalcash/DigitalCashViewModel.java
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/digitalcash/DigitalCashViewModel.java
@@ -10,7 +10,12 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.LiveDataReactiveStreams;
 import androidx.lifecycle.MutableLiveData;
 
+import com.github.dedis.popstellar.R;
 import com.github.dedis.popstellar.SingleEvent;
+import com.github.dedis.popstellar.model.network.method.message.MessageGeneral;
+import com.github.dedis.popstellar.model.network.method.message.data.digitalcash.AddTransaction;
+import com.github.dedis.popstellar.model.objects.Address;
+import com.github.dedis.popstellar.model.objects.Channel;
 import com.github.dedis.popstellar.model.objects.Lao;
 import com.github.dedis.popstellar.model.objects.RollCall;
 import com.github.dedis.popstellar.model.objects.Wallet;
@@ -20,6 +25,7 @@ import com.github.dedis.popstellar.model.objects.security.privatekey.PlainPrivat
 import com.github.dedis.popstellar.repository.LAORepository;
 import com.github.dedis.popstellar.repository.LAOState;
 import com.github.dedis.popstellar.repository.remote.GlobalNetworkManager;
+import com.github.dedis.popstellar.utility.error.ErrorUtils;
 import com.github.dedis.popstellar.utility.error.keys.KeyException;
 import com.github.dedis.popstellar.utility.security.KeyManager;
 import com.google.gson.Gson;
@@ -32,6 +38,7 @@ import javax.inject.Inject;
 import dagger.hilt.android.lifecycle.HiltViewModel;
 import io.reactivex.BackpressureStrategy;
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
 
 @HiltViewModel
 public class DigitalCashViewModel extends AndroidViewModel {
@@ -46,6 +53,7 @@ public class DigitalCashViewModel extends AndroidViewModel {
   private final CompositeDisposable disposables;
 
   private static final String LAO_FAILURE_MESSAGE = "failed to retrieve lao";
+  private static final String PUBLISH_MESSAGE = "sending publish message";
 
   @Inject Wallet wallet;
 
@@ -171,7 +179,7 @@ public class DigitalCashViewModel extends AndroidViewModel {
    * @param receiver_address String
    */
 
-  public void sendCoin(int amount ,@Nullable String sender_address, @Nullable String receiver_address){
+  public void sendCoin(int amount , @Nullable Address sender_address, @Nullable Address receiver_address){
     Log.d(TAG, "Sending a transaction");
     Lao lao = getCurrentLao();
     if (lao == null) {
@@ -179,8 +187,28 @@ public class DigitalCashViewModel extends AndroidViewModel {
       return;
     }
 
+    AddTransaction addTransaction = new AddTransaction(amount,sender_address,receiver_address);
 
+    try {
+      PoPToken token = keyManager.getValidPoPToken(lao);
+      Channel channel =
+              lao.getChannel().subChannel(DIGITAL_CASH).subChannel(token.getPublicKey().getEncoded());
+      Log.d(TAG, PUBLISH_MESSAGE);
+      MessageGeneral msg = new MessageGeneral(token, addTransaction, gson);
 
+      Disposable disposable =
+              networkManager
+                      .getMessageSender()
+                      .publish(token, channel, addTransaction)
+                      .subscribe(
+                              () -> Log.d(TAG, "sent some transaction" + msg.getMessageId()),
+                              error ->
+                                      ErrorUtils.logAndShow(
+                                              getApplication(), TAG, error, R.string.error_sending_coin));
+      disposables.add(disposable);
+    } catch (KeyException e) {
+      ErrorUtils.logAndShow(getApplication(), TAG, e, R.string.error_retrieve_own_token);
+    }
   }
 
   @Override
