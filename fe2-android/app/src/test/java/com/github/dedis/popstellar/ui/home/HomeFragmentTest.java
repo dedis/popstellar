b82a83e3a2f04bcfb67ed3279a3f71ffe74f988f
Gabriel Fleischer <gabriel.fleischer@epfl.ch>
2022-03-30 14:24:48 +0200

    Create base class
    Signed-off-by: Gabriel Fleischer <gabriel.fleischer@epfl.ch>

---
+++ fe2-android/app/src/test/java/com/github/dedis/popstellar/ui/home/HomeFragmentTest.java
@@ -0,0 +1,11 @@
+package com.github.dedis.popstellar.ui.home;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+import dagger.hilt.android.testing.HiltAndroidTest;
+
+@HiltAndroidTest
+@RunWith(AndroidJUnit4.class)
+public class HomeFragmentTest {}
