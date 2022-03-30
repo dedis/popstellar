eb7c2b31d0739edddad83bcf78034cb9e895faa0
Gabriel Fleischer <gabriel.fleischer@epfl.ch>
2022-03-30 14:33:54 +0200

    Add basic rules
    Signed-off-by: Gabriel Fleischer <gabriel.fleischer@epfl.ch>

--- fe2-android/app/src/test/java/com/github/dedis/popstellar/ui/home/HomeFragmentTest.java
+++ fe2-android/app/src/test/java/com/github/dedis/popstellar/ui/home/HomeFragmentTest.java
@@ -2,10 +2,32 @@ package com.github.dedis.popstellar.ui.home;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import org.junit.runner.RunWith;
+import com.github.dedis.popstellar.testutils.fragment.ActivityFragmentScenarioRule;
+import com.github.dedis.popstellar.ui.pages.home.HomePageObject;
 
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoTestRule;
+
+import dagger.hilt.android.testing.HiltAndroidRule;
 import dagger.hilt.android.testing.HiltAndroidTest;
 
 @HiltAndroidTest
 @RunWith(AndroidJUnit4.class)
-public class HomeFragmentTest {}
+public class HomeFragmentTest {
+
+  @Rule(order = 0)
+  public final MockitoTestRule mockitoRule = MockitoJUnit.testRule(this);
+
+  @Rule(order = 1)
+  public final HiltAndroidRule hiltRule = new HiltAndroidRule(this);
+
+  @Rule(order = 2)
+  public final ActivityFragmentScenarioRule<HomeActivity, HomeFragment> fragmentRule =
+      ActivityFragmentScenarioRule.launchIn(
+          HomeActivity.class,
+          HomePageObject.homeFragmentId(),
+          HomeFragment.class,
+          HomeFragment::new);
+}
