7112ace92abf47282a35ae8e1f6d3f4227c5690c
Johann Plüss <pluss.j2407@gmail.com>
2022-04-04 14:46:25 +0200

    Intermediate change

--- tests/karate/src/test/java/fe/FrontEndTest.java
+++ tests/karate/src/test/java/fe/FrontEndTest.java
@@ -4,13 +4,8 @@ import com.intuit.karate.junit5.Karate;
 
 public class FrontEndTest {
 
-//  @Karate.Test
-//  Karate testCreateLAO() {
-//    return Karate.run("classpath:fe/LAO/create_lao.feature");
-//  }
-
   @Karate.Test
-  Karate testCreateRollCall() {
-    return Karate.run("classpath:fe/rollCall/roll_call_creation.feature");
+  Karate testCreateLAO() {
+    return Karate.run("classpath:fe/LAO/create_lao.feature");
   }
 }
