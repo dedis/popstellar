29dc31763c9e67d9dfb647496fb54d1034725e17
0xdieman <diego.boros@epfl.ch>
2022-04-04 11:22:11 +0200

    Start draft of the a digital cash transaction object

--- fe1-web/src/features/wallet/network/DigitalCashTransaction.ts
+++ fe1-web/src/features/wallet/network/DigitalCashTransaction.ts
@@ -1,3 +1,30 @@
-export class DigitalCashTransaction{
-  public readonly txId: Hash;
+import { Hash, PublicKey } from 'core/objects';
+
+export interface TxInScript {
+  type: string;
+  pubKey: PublicKey;
+  sig: string; // Hash ?
+}
+export interface TxIn {
+  txOutHash: Hash;
+  txOutIndex: number;
+  script: TxInScript;
+}
+export interface TxOutScript {
+  type: string;
+  pubKeyHash: string; // Hash ?
+}
+export interface TxOut {
+  value: number; // BigInteger ?
+  script: TxOutScript;
+}
+
+export class DigitalCashTransaction {
+  public readonly version: number;
+
+  public readonly txsIn: TxIn[];
+
+  public readonly txsOut: TxOut[];
+
+  public readonly lockTime: number;
 }
