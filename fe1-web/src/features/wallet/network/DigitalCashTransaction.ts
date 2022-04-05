17e529e125e13920439f693e55ea738c07a42f1c
0xdieman <diego.boros@epfl.ch>
2022-04-05 15:42:32 +0200

    Add constructor

--- fe1-web/src/features/wallet/network/DigitalCashTransaction.ts
+++ fe1-web/src/features/wallet/network/DigitalCashTransaction.ts
@@ -1,9 +1,15 @@
 import { Hash, PublicKey } from 'core/objects';
 
+export interface DigitalCashTransactionState {
+  version: number;
+  txsIn: TxIn[];
+  txsOut: TxOut[];
+  lockTime: number;
+}
 export interface TxInScript {
   type: string;
   pubKey: PublicKey;
-  sig: string; // Hash ?
+  sig: string;
 }
 export interface TxIn {
   txOutHash: Hash;
@@ -12,10 +18,10 @@ export interface TxIn {
 }
 export interface TxOutScript {
   type: string;
-  pubKeyHash: string; // Hash ?
+  pubKeyHash: Hash;
 }
 export interface TxOut {
-  value: number; // BigInteger ?
+  value: number;
   script: TxOutScript;
 }
 
@@ -27,4 +33,41 @@ export class DigitalCashTransaction {
   public readonly txsOut: TxOut[];
 
   public readonly lockTime: number;
+
+  constructor(obj: Partial<DigitalCashTransaction>) {
+    if (obj === undefined || obj === null) {
+      throw new Error(
+        'Error encountered while creating a DigitalCashTransaction object: undefined/null parameters',
+      );
+    }
+    if (obj.version === undefined) {
+      throw new Error("Undefined 'version' when creating 'DigitalCashTransaction'");
+    }
+    if (obj.txsIn === undefined) {
+      throw new Error("Undefined 'txsIn' when creating 'DigitalCashTransaction'");
+    }
+    if (obj.txsOut === undefined) {
+      throw new Error("Undefined 'txsOut' when creating 'DigitalCashTransaction'");
+    }
+    if (obj.lockTime === undefined) {
+      throw new Error("Undefined 'lockTime' when creating 'DigitalCashTransaction'");
+    }
+
+    this.version = obj.version;
+    this.txsIn = obj.txsIn;
+    this.txsOut = obj.txsOut;
+    this.lockTime = obj.lockTime;
+  }
+
+  public static fromState(
+    digitalCashTransactionState: DigitalCashTransactionState,
+  ): DigitalCashTransaction {
+    return new DigitalCashTransaction(digitalCashTransactionState);
+  }
+
+  public toState(): DigitalCashTransactionState {
+    return {
+      ...this,
+    };
+  }
 }
