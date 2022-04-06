0c84788b48b255b065cc95286062d7d96eb4b91a
0xdieman <diego.boros@epfl.ch>
2022-04-06 19:12:58 +0200

    Remove unuseful screen order

--- fe1-web/src/features/wallet/navigation/WalletNavigation.tsx
+++ fe1-web/src/features/wallet/navigation/WalletNavigation.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import STRINGS from 'resources/strings';
 
 import { WalletError, WalletHome, WalletSetSeed, WalletSetup, WalletCreateSeed } from '../screens';
-import { WalletStore } from '../store';
 
 const Stack = createStackNavigator();
 
@@ -13,20 +12,13 @@ const Stack = createStackNavigator();
  * Allows to navigate between the wallet screens.
  */
 export default function WalletNavigation() {
-  const home = { name: STRINGS.navigation_wallet_home_tab, Component: WalletHome };
-  const setup = { name: STRINGS.navigation_wallet_setup_tab, Component: WalletSetup };
-
-  const hasSeed = WalletStore.hasSeed();
-
-  const screens = hasSeed ? [home, setup] : [setup, home];
   return (
     <Stack.Navigator
       screenOptions={{
         headerShown: false,
       }}>
-      {screens.map(({ name, Component }) => (
-        <Stack.Screen name={name} component={Component} />
-      ))}
+      <Stack.Screen name={STRINGS.navigation_wallet_setup_tab} component={WalletSetup} />
+      <Stack.Screen name={STRINGS.navigation_wallet_home_tab} component={WalletHome} />
       <Stack.Screen name={STRINGS.navigation_wallet_create_seed} component={WalletCreateSeed} />
       <Stack.Screen name={STRINGS.navigation_wallet_insert_seed} component={WalletSetSeed} />
       <Stack.Screen name={STRINGS.navigation_wallet_error} component={WalletError} />
