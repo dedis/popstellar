197766ce97fc8e446843b436e810107b196fbe70
zoemarin <zoe@marin.info>
2022-04-07 22:55:10 +0200

    Add the connection of the view model and the menu to the main
    Signed-off-by: zoemarin <zoe@marin.info>

--- fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/digitalcash/DigitalCashMain.java
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/digitalcash/DigitalCashMain.java
@@ -1,8 +1,11 @@
 package com.github.dedis.popstellar.ui.digitalcash;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.github.dedis.popstellar.R;
 
@@ -11,10 +14,23 @@ import dagger.hilt.android.AndroidEntryPoint;
 /** Activity for the digital cash */
 @AndroidEntryPoint
 public class DigitalCashMain extends AppCompatActivity {
+  private DigitalCashViewModel mViewModel;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.digital_cash_main_activity);
+
+    mViewModel = obtainViewModel(this);
+  }
+
+  public static DigitalCashViewModel obtainViewModel(FragmentActivity activity) {
+    return new ViewModelProvider(activity).get(DigitalCashViewModel.class);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.digital_cash_menu, menu);
+    return true;
   }
 }
