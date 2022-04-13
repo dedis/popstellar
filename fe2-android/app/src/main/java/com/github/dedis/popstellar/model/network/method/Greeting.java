5454712e47b5e34642743ddccafd4f82afd76a96
ahominal <79504891+ahominal@users.noreply.github.com>
2022-04-13 14:06:39 +0200

    with basic greeting test

--- fe2-android/app/src/main/java/com/github/dedis/popstellar/model/network/method/Greeting.java
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/model/network/method/Greeting.java
@@ -1,7 +1,9 @@
 package com.github.dedis.popstellar.model.network.method;
 
+import androidx.annotation.NonNull;
 import com.github.dedis.popstellar.model.objects.Channel;
 import com.github.dedis.popstellar.model.objects.security.PublicKey;
+import com.google.gson.annotations.SerializedName;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,30 +13,37 @@ import java.util.List;
 public class Greeting extends Message {
 
   // Backend sender address
-  private PublicKey senderKey;
-  // Backend server address
-  private String address;
-  // Backend "peer", list of addresses of future 1 Client -> multiple Server communication
-  private List<String> peers;
+  @NonNull
+  @SerializedName("sender")
+  private String senderKey;
 
+  // Backend server address
+  @NonNull
+  @SerializedName("address")
+  private String address;
+
+  // Backend "peer", list of addresses of future 1 Client -> multiple Server communication
+  @SerializedName("peers")
+  private List<String> peers;
 
   /**
    * Constructor for a Greeting Message
    * @param channel the channel over which the message is sent
    * @throws IllegalArgumentException if channel is null
    */
-  public Greeting(Channel channel, String address, String senderKey, String... peers) {
+  public Greeting(Channel channel,@NonNull String address,
+      @NonNull String senderKey, String... peers) {
     super(channel);
-    if (address == null) {
-      throw new IllegalArgumentException("The address of the backend can't be null");
-    } else if (senderKey == null) {
-      throw new IllegalArgumentException("The public key of the backend can't be null");
-    }
     // Peers can be empty
     this.peers = Arrays.asList(peers);
     this.address = address;
-    // Check of validity should be done via the Public Key class
-    this.senderKey = new PublicKey(senderKey);
+    // Check of validity of the public key should be done via the Public Key class
+    try {
+      new PublicKey(senderKey);
+    } catch (Exception e) {
+      throw new IllegalArgumentException("Please provide a valid public key");
+    }
+    this.senderKey = senderKey;
   }
 
   //Getter for params
@@ -43,7 +52,7 @@ public class Greeting extends Message {
     return Method.GREETING.getMethod();
   }
 
-  public PublicKey getSenderKey(){return senderKey;}
+  public String getSenderKey(){return senderKey;}
 
   public String getAddress(){return address;}
 
@@ -75,8 +84,19 @@ public class Greeting extends Message {
 
   @Override
   public String toString() {
-    return "Greeting{" + "channel='" + getChannel().toString() + "', method='" + getMethod() + "', "
-        + "sender = " + getSenderKey().toString() + "address=" + getAddress() + "}";
+    return "Greeting={"
+        + "channel='"
+        + getChannel().toString()
+        + '\''
+        + ", sender="
+        + getSenderKey()
+        + '\''
+        + ", address="
+        + getAddress()
+        + '\''
+        + ", params="
+        + Arrays.toString(peers.toArray())
+        + '}';
   }
 
 }
--- fe2-android/app/src/test/java/com/github/dedis/popstellar/model/network/method/GreetingTest.java
+++ fe2-android/app/src/test/java/com/github/dedis/popstellar/model/network/method/GreetingTest.java
@@ -1,24 +1,81 @@
 package com.github.dedis.popstellar.model.network.method;
 
+import static com.github.dedis.popstellar.model.network.method.Method.GREETING;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertThrows;
+
+import com.github.dedis.popstellar.model.objects.Channel;
+import com.github.dedis.popstellar.model.objects.security.PublicKey;
 import java.util.Collections;
 import java.util.List;
 import org.junit.Test;
 
 public class GreetingTest {
 
+  private static final Channel CHANNEL = Channel.ROOT;
+  private static final String RANDOM_KEY = "oOcKZjUeandJOFVgn-E6e-7QksviBBbHUPicdzUgIm8";
+  public static final String RANDOM_ADDRESS = "ws://10.0.2.2:9000/organizer/client";
+  public static final String RANDOM_PEER = "http://128.0.0.2";
 
-  public final String randomKey =
-  public final String randomURL =
-  public final List<String> randomPeers =
-      Collections.singletonList("http://128.0.0.2");
+  public static Greeting GREETING_MSG = new Greeting(CHANNEL,
+      RANDOM_ADDRESS, RANDOM_KEY, RANDOM_PEER);
 
-  public static final Greeting greetingMessage = new Greeting()
-
-
-  @Test
-  public void createdCorrectly(){
-
+  @Test(expected = IllegalArgumentException.class)
+  public void noInstantiationWithWrongPublicKey(){
+    new Greeting(CHANNEL, "IsNotValid", RANDOM_ADDRESS, "");
   }
 
+  @Test
+  public void getMethodTest(){
+    assertEquals(GREETING, GREETING_MSG.getMethod());
+  }
+
+  @Test
+  public void getSenderKeyTest(){
+    assertEquals(RANDOM_KEY, GREETING_MSG.getSenderKey());
+  }
+
+  @Test
+  public void getAddressTest(){
+    assertEquals(RANDOM_ADDRESS, GREETING_MSG.getAddress());
+  }
+
+  @Test
+  public void getPeersTest(){
+    List<String> peersList =
+        Collections.singletonList(RANDOM_PEER);
+    assertEquals(true, GREETING_MSG.getAddress().equals(peersList));
+  }
+
+  @Test
+  public void equalsTest(){
+    Greeting GREETING_MSG_2 = new Greeting(CHANNEL,
+        RANDOM_ADDRESS, RANDOM_KEY, RANDOM_PEER);
+
+    assertEquals(GREETING_MSG, GREETING_MSG_2);
+    assertNotEquals(GREETING_MSG, null);
+    assertEquals(GREETING_MSG_2.hashCode(), GREETING_MSG.hashCode());
+    assertEquals(GREETING_MSG, GREETING_MSG);
+    assertNotEquals(GREETING_MSG,
+        new Greeting(CHANNEL, "123", RANDOM_ADDRESS, RANDOM_PEER));
+    assertNotEquals(GREETING_MSG,
+        new Greeting(CHANNEL, RANDOM_KEY, "123", RANDOM_PEER));
+    assertNotEquals(GREETING_MSG,
+        new Greeting(CHANNEL, RANDOM_KEY, RANDOM_ADDRESS, "123"));
+    assertNotEquals(GREETING_MSG,
+        new Greeting(Channel.fromString("10"), RANDOM_KEY, RANDOM_ADDRESS, "123"));
+  }
+
+  @Test
+  public void toStringTest(){
+    String greetingToString = String.format(
+        "Greeting{channel='ROOT', "
+        + "sender='%s', "
+        + "address='%s', "
+        + "peers='%s'}",
+        RANDOM_KEY, RANDOM_ADDRESS, RANDOM_PEER);
+    assertEquals(greetingToString, GREETING_MSG.toString())
+  }
 
 }
