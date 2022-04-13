57043cf4fddd8a926cc829527ca98fbcc2e5f364
0xdieman <diego.boros@epfl.ch>
2022-04-13 13:31:42 +0200

    Add hooks and helper file

--- fe1-web/src/features/wallet/network/DigitalCashMessageApi.ts
+++ fe1-web/src/features/wallet/network/DigitalCashMessageApi.ts
@@ -2,5 +2,5 @@ import { PublicKey } from "core/objects";
 import { RollCallToken } from "../objects/RollCallToken";
 
 export function requestSentTransaction(from: RollCallToken, to: PublicKey, value: number){
-  // algorithm to find all the outputs from this token
+
 }
