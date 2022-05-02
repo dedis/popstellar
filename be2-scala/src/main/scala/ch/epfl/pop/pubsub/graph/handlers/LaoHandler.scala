ce90d9ff6a99dcf375c15c9cec3a320684029ec8
Ajkunas <ajkuna.seipi@epfl.ch>
2022-05-02 13:26:59 +0200

    started implementing lao#greet

--- be2-scala/src/main/scala/ch/epfl/pop/pubsub/graph/handlers/LaoHandler.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/pubsub/graph/handlers/LaoHandler.scala
@@ -3,7 +3,7 @@ package ch.epfl.pop.pubsub.graph.handlers
 import ch.epfl.pop.model.network.JsonRpcRequest
 import ch.epfl.pop.model.network.method.message.Message
 import ch.epfl.pop.model.network.method.message.data.ObjectType
-import ch.epfl.pop.model.network.method.message.data.lao.{CreateLao, StateLao}
+import ch.epfl.pop.model.network.method.message.data.lao.{CreateLao, GreetLao, StateLao}
 import ch.epfl.pop.model.objects.{Channel, DbActorNAckException, Hash}
 import ch.epfl.pop.pubsub.graph.{ErrorCodes, GraphMessage, PipelineError}
 import ch.epfl.pop.storage.DbActor
@@ -48,6 +48,36 @@ case object LaoHandler extends MessageHandler {
           case Success(_) => Left(rpcMessage)
           case Failure(ex: DbActorNAckException) => Right(PipelineError(ex.code, s"handleCreateLao failed : ${ex.message}", rpcMessage.getId))
           case reply => Right(PipelineError(ErrorCodes.SERVER_ERROR.id, s"handleCreateLao failed : unexpected DbActor reply '$reply'", rpcMessage.getId))
+        }
+
+      case _ => Right(PipelineError(
+        ErrorCodes.SERVER_ERROR.id,
+        s"Unable to handle lao message $rpcMessage. Not a Publish/Broadcast message",
+        rpcMessage.id
+      ))
+    }
+  }
+
+  def handleGreetLao(rpcMessage: JsonRpcRequest): GraphMessage = {
+    rpcMessage.getParamsMessage match {
+      case Some(message) =>
+        val data: GreetLao = message.decodedData.get.asInstanceOf[GreetLao]
+        val channel: Channel = rpcMessage.getParamsChannel
+
+        val combined = for {
+          _ <- {
+            (dbActor ? DbActor.ChannelExists(channel)).transformWith {
+              case Success(_) => Future { () }
+              case _ => Future { throw DbActorNAckException(ErrorCodes.INVALID_ACTION.id, "lao already exists in db") }
+            }
+          }
+          _ <- dbActor ? DbActor.WriteAndPropagate(channel, message)
+        } yield ()
+
+        Await.ready(combined, duration).value.get match {
+          case Success(_) => Left(rpcMessage)
+          case Failure(ex: DbActorNAckException) => Right(PipelineError(ex.code, s"handleGreetLao failed : ${ex.message}", rpcMessage.getId))
+          case reply => Right(PipelineError(ErrorCodes.SERVER_ERROR.id, s"handleGreetLao failed : unexpected DbActor reply '$reply'", rpcMessage.getId))
         }
 
       case _ => Right(PipelineError(
