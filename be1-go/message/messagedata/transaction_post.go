42ee36e656c6704897acb96bf4858479d94abc45
maxime.zammit <maxime.zammit@epfl.ch>
2022-05-02 09:41:40 +0200

    update struct name 2

--- be1-go/message/messagedata/transaction_post.go
+++ be1-go/message/messagedata/transaction_post.go
@@ -2,12 +2,12 @@ package messagedata
 
 // TransactionPost defines a message data
 type TransactionPost struct {
-	Object      string            `json:"object"`
-	Action      string            `json:"action"`
-	Transaction TransactionStruct `json:"transaction"`
+	Object      string      `json:"object"`
+	Action      string      `json:"action"`
+	Transaction Transaction `json:"transaction"`
 }
 
-//TBC
+//Transaction define the input and output account for the transaction
 type Transaction struct {
 	Version  int           `json:"version"`
 	TxIn     []TxInStruct  `json:"txin"`  // min 1
@@ -15,22 +15,26 @@ type Transaction struct {
 	Locktime int           `json:"locktime"`
 }
 
+// TxInStruct define the source from the money used in transaction
 type TxInStruct struct {
 	TxOutHash  string       `json:"txouthash"`
 	TxOutIndex int          `json:"txoutindex"`
 	ScripIn    UnlockScript `json:"script"`
 }
 
+// TxOutStruct define the destination from the money used in transaction
 type TxOutStruct struct {
 	Value     int        `json:"value"`
 	ScriptOut LockScript `json:"script"`
 }
 
+// LockScript define the locking value for transaction
 type LockScript struct {
 	Type       string `json:"Type"`
 	PubKeyHash string `json:"PubkeyHash"`
 }
 
+// UnlockScript define the unlocking value for transaction
 type UnlockScript struct {
 	Type   string `json:"Type"`
 	PubKey string `json:"Pubkey"`
