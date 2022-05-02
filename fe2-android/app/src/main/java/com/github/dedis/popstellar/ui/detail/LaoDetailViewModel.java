22740f4a01bd97113f2471887aa91608c90b3f25
Johann Plüss <pluss.j2407@gmail.com>
2022-05-02 20:08:23 +0200

    Implement fix
    Signed-off-by: Johann Plüss <pluss.j2407@gmail.com>

--- fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/detail/LaoDetailViewModel.java
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/detail/LaoDetailViewModel.java
@@ -1181,7 +1181,7 @@ public class LaoDetailViewModel extends AndroidViewModel
                 lao -> {
                   Log.d(TAG, "got an update for lao: " + lao.getName());
                   mCurrentLao.postValue(lao);
-                  boolean isOrganizer = lao.getOrganizer().equals(keyManager.getMainPublicKey());
+                  boolean isOrganizer = keyManager.getMainPublicKey().equals(lao.getOrganizer());
                   mIsOrganizer.setValue(isOrganizer);
                 }));
   }
