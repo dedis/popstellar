463725d21868186b9061817c50ddba41d2116b5f
Robin Goumaz <robin.goumaz@epfl.ch>
2022-05-02 16:04:56 +0200

    Lao#greet message implementation

---
+++ be1-go/message/messagedata/lao_greet.go
@@ -0,0 +1,32 @@
+package messagedata
+
+// LaoGreet defines a message data
+type LaoGreet struct {
+	Object string `json:"object"`
+	Action string `json:"action"`
+	ID     string `json:"id"`
+
+	Frontend string `json:"frontend"`
+	Address  string `json:"address"`
+	Peers    []Peer `json:"peers"`
+}
+
+// Peer defines a peer server for the LAO
+type Peer struct {
+	Address string `json:"address"`
+}
+
+// GetObject implements MessageData
+func (LaoGreet) GetObject() string {
+	return LAOObject
+}
+
+// GetAction implements MessageData
+func (LaoGreet) GetAction() string {
+	return LAOActionGreet
+}
+
+// NewEmpty implements MessageData
+func (LaoGreet) NewEmpty() MessageData {
+	return &LaoGreet{}
+}
