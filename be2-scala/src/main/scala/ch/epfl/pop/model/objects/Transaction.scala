7c349e17ed1acea0b36ead7186c313be44cc4c5e
Louis Bettens <louis@bettens.info>
2022-05-06 19:02:44 +0200

    [BE2] compute transaction_id

--- be2-scala/src/main/scala/ch/epfl/pop/model/objects/Transaction.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/model/objects/Transaction.scala
@@ -31,7 +31,26 @@ final case class Transaction(
   TxIn: List[TxIn],
   TxOut: List[TxOut],
   LockTime: Int,
-  )
+) {
+  lazy val transactionId = {
+    val strings = collection.mutable.ListBuffer.empty[String]
+    strings += LockTime.toString
+    strings ++= TxIn.foldRight(List.empty[String]) { (txin, acc) =>
+        txin.Script.Pubkey.base64Data.toString ::
+        txin.Script.Sig.toString ::
+        txin.Script.Type ::
+        txin.TxOutHash.toString ::
+        txin.TxOutIndex.toString :: acc
+    }
+    strings ++= TxOut.foldRight(List.empty[String]) { (txout, acc) =>
+        txout.Script.PubkeyHash.base64Data.toString ::
+        txout.Script.Type ::
+        txout.Value.toString :: acc
+      }
+    strings += Version.toString
+    Hash.fromStrings(strings.toSeq: _*)
+  }
+}
 
 object Transaction extends Parsable {
   override def buildFromJson(payload: String): Transaction = payload.parseJson.asJsObject.convertTo[Transaction]
