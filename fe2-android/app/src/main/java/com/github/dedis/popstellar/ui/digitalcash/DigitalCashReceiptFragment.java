b6c7780ad877c9bf584ffbba8699a0135039e65a
zoemarin <zoe@marin.info>
2022-05-07 09:52:41 +0200

    Add the fragments
    Signed-off-by: zoemarin <zoe@marin.info>

---
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/digitalcash/DigitalCashReceiptFragment.java
@@ -0,0 +1,41 @@
+package com.github.dedis.popstellar.ui.digitalcash;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.github.dedis.popstellar.R;
+
+/**
+ * A simple {@link Fragment} subclass. Use the {@link DigitalCashReceiptFragment} factory method to
+ * create an instance of this fragment.
+ */
+public class DigitalCashReceiptFragment extends Fragment {
+
+  public DigitalCashReceiptFragment() {}
+
+  /**
+   * Use this factory method to create a new instance of this fragment using the provided
+   * parameters.
+   *
+   * @return A new instance of fragment DigitalCashReceiveFragment.
+   */
+  public static DigitalCashReceiveFragment newInstance() {
+    return new DigitalCashReceiveFragment();
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+
+  @Override
+  public View onCreateView(
+      LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.digital_cash_receive_fragment, container, false);
+  }
+}
---
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/digitalcash/DigitalCashSendFragment.java
@@ -0,0 +1,42 @@
+package com.github.dedis.popstellar.ui.digitalcash;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.github.dedis.popstellar.R;
+
+/**
+ * A simple {@link Fragment} subclass. Use the {@link DigitalCashSendFragment#newInstance} factory
+ * method to create an instance of this fragment.
+ */
+public class DigitalCashSendFragment extends Fragment {
+
+  public DigitalCashSendFragment() {}
+
+  /**
+   * Use this factory method to create a new instance of this fragment using the provided
+   * parameters.
+   *
+   * @return A new instance of fragment DigitalCashSendFragment.
+   */
+  // TODO: Rename and change types and number of parameters
+  public static DigitalCashSendFragment newInstance() {
+    return new DigitalCashSendFragment();
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+
+  @Override
+  public View onCreateView(
+      LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.digital_cash_send_fragment, container, false);
+  }
+}
