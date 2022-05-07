f67086e57151203db277ef42d81463cbc94d8767
zoemarin <zoe@marin.info>
2022-05-07 16:27:23 +0200

    json with test
    Signed-off-by: zoemarin <zoe@marin.info>

---
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/model/network/method/message/data/digitalcash/ScriptTxIn.java
@@ -0,0 +1,71 @@
+package com.github.dedis.popstellar.model.network.method.message.data.digitalcash;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Objects;
+
+// The script describing the unlock mechanism
+public class ScriptTxIn {
+  @SerializedName("Type")
+  private final String type; // The script describing the unlock mechanism
+
+  @SerializedName("PubKey")
+  private final String pub_key_recipient; // The recipient’s public key
+
+  @SerializedName("Sig")
+  private final String sig; // Signature on all txins and txouts using the recipient's private key
+  // Transaction //with all txin txout
+
+  /**
+   * @param type The script describing the unlock mechanism
+   * @param pub_key_recipient The recipient’s public key
+   * @param sig Signature on all txins and txouts using the recipient's private key
+   */
+  public ScriptTxIn(String type, String pub_key_recipient, String sig) {
+    this.type = type;
+    this.pub_key_recipient = pub_key_recipient;
+    this.sig = sig;
+  }
+
+  public String getSig() {
+    return sig;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public String getPub_key_recipient() {
+    return pub_key_recipient;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    ScriptTxIn that = (ScriptTxIn) o;
+    return Objects.equals(type, that.type)
+        && Objects.equals(pub_key_recipient, that.pub_key_recipient)
+        && Objects.equals(sig, that.sig);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getType(), getPub_key_recipient(), getSig());
+  }
+
+  @Override
+  public String toString() {
+    return "ScriptTxIn{"
+        + "type='"
+        + type
+        + '\''
+        + ", pub_key_recipient='"
+        + pub_key_recipient
+        + '\''
+        + ", sig='"
+        + sig
+        + '\''
+        + '}';
+  }
+}
