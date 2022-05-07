56dc50c85900405412b7f1746957c73f6db3a22b
zoemarin <zoe@marin.info>
2022-05-07 09:52:41 +0200

    Add the fragments
    Signed-off-by: zoemarin <zoe@marin.info>

---
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/digitalcash/DigitalCashIssueFragment.java
@@ -0,0 +1,40 @@
+package com.github.dedis.popstellar.ui.digitalcash;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.github.dedis.popstellar.R;
+
+/**
+ * A simple {@link Fragment} subclass. Use the {@link DigitalCashIssueFragment#newInstance} factory
+ * method to create an instance of this fragment.
+ */
+public class DigitalCashIssueFragment extends Fragment {
+
+  public DigitalCashIssueFragment() {}
+
+  /**
+   * Use this factory method to create a new instance of this fragment using the provided
+   * parameters.
+   *
+   * @return A new instance of fragment DigitalCashIssueFragment.
+   */
+  public static DigitalCashIssueFragment newInstance() {
+    return new DigitalCashIssueFragment();
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+
+  @Override
+  public View onCreateView(
+      LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.digital_cash_issue_fragment, container, false);
+  }
+}
--- fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/digitalcash/DigitalCashMain.java
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/digitalcash/DigitalCashMain.java
@@ -4,12 +4,17 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Menu;
 
+import androidx.annotation.IdRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.github.dedis.popstellar.R;
+import com.github.dedis.popstellar.utility.ActivityUtils;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.function.Supplier;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
@@ -26,6 +31,77 @@ public class DigitalCashMain extends AppCompatActivity {
     mViewModel = obtainViewModel(this);
 
     setupNavigationBar();
+    // Subscribe to "open home"
+    mViewModel
+        .getOpenHomeEvent()
+        .observe(
+            this,
+            booleanEvent -> {
+              Boolean event = booleanEvent.getContentIfNotHandled();
+              if (event != null) {
+                setupFragment(R.id.fragment_digital_cash_home);
+              }
+            });
+
+    // Subscribe to "open history"
+    mViewModel
+        .getOpenHistoryEvent()
+        .observe(
+            this,
+            booleanEvent -> {
+              Boolean event = booleanEvent.getContentIfNotHandled();
+              if (event != null) {
+                setupFragment(R.id.fragment_digital_cash_history);
+              }
+            });
+
+    // Subscribe to "open send"
+    mViewModel
+        .getOpenSendEvent()
+        .observe(
+            this,
+            booleanEvent -> {
+              Boolean event = booleanEvent.getContentIfNotHandled();
+              if (event != null) {
+                setupFragment(R.id.fragment_digital_cash_send);
+              }
+            });
+
+    // Subscribe to "open receive"
+    mViewModel
+        .getOpenReceiveEvent()
+        .observe(
+            this,
+            booleanEvent -> {
+              Boolean event = booleanEvent.getContentIfNotHandled();
+              if (event != null) {
+                setupFragment(R.id.fragment_digital_cash_receive);
+              }
+            });
+
+    // Subscribe to "open issue"
+    mViewModel
+        .getOpenIssueEvent()
+        .observe(
+            this,
+            booleanEvent -> {
+              Boolean event = booleanEvent.getContentIfNotHandled();
+              if (event != null) {
+                setupFragment(R.id.fragment_digital_cash_issue);
+              }
+            });
+
+    // Subscribe to "open receipt"
+    mViewModel
+        .getOpenIssueEvent()
+        .observe(
+            this,
+            booleanEvent -> {
+              Boolean event = booleanEvent.getContentIfNotHandled();
+              if (event != null) {
+                setupFragment(R.id.fragment_digital_cash_receipt);
+              }
+            });
   }
 
   public static DigitalCashViewModel obtainViewModel(FragmentActivity activity) {
@@ -63,5 +139,48 @@ public class DigitalCashMain extends AppCompatActivity {
           }
           return true;
         });
+  }
+
+  public void setupFragment(int id) {
+    switch (id) {
+      case R.id.fragment_digital_cash_home:
+        setCurrentFragment(R.id.fragment_digital_cash_home, DigitalCashHomeFragment::newInstance);
+        break;
+      case R.id.fragment_digital_cash_history:
+        setCurrentFragment(
+            R.id.fragment_digital_cash_history, DigitalCashHistoryFragment::newInstance);
+        break;
+      case R.id.fragment_digital_cash_send:
+        setCurrentFragment(R.id.fragment_digital_cash_send, DigitalCashSendFragment::newInstance);
+        break;
+      case R.id.fragment_digital_cash_receive:
+        setCurrentFragment(
+            R.id.fragment_digital_cash_receive, DigitalCashReceiveFragment::newInstance);
+        break;
+      case R.id.fragment_digital_cash_issue:
+        setCurrentFragment(R.id.fragment_digital_cash_issue, DigitalCashIssueFragment::newInstance);
+        break;
+      case R.id.fragment_digital_cash_receipt:
+        setCurrentFragment(
+            R.id.fragment_digital_cash_receipt, DigitalCashReceiptFragment::newInstance);
+        break;
+      default:
+    }
+  }
+
+  /**
+   * Set the current fragment in the container of the activity
+   *
+   * @param id of the fragment
+   * @param fragmentSupplier provides the fragment if it is missing
+   */
+  private void setCurrentFragment(@IdRes int id, Supplier<Fragment> fragmentSupplier) {
+    Fragment fragment = getSupportFragmentManager().findFragmentById(id);
+    // If the fragment was not created yet, create it now
+    if (fragment == null) fragment = fragmentSupplier.get();
+
+    // Set the new fragment in the container
+    ActivityUtils.replaceFragmentInActivity(
+        getSupportFragmentManager(), fragment, R.id.fragment_container_digital_cash);
   }
 }
