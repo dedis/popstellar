4484c79d22829c07dda27b1618ec1af79378260b
ahominal <79504891+ahominal@users.noreply.github.com>
2022-05-09 14:57:00 +0200

    with protocol

--- protocol/test/main.js
+++ protocol/test/main.js
@@ -19,11 +19,14 @@ const message_data_roll_call_open_schema = require("../query/method/message/data
 const message_data_lao_create_schema = require("../query/method/message/data/dataCreateLao.json");
 const message_data_lao_update_schema = require("../query/method/message/data/dataUpdateLao.json");
 const message_data_lao_state_schema = require("../query/method/message/data/dataStateLao.json");
+const message_data_lao_greet_schema = require("../query/method/message/data/dataGreetLao.json");
 const message_data_vote_cast_schema = require("../query/method/message/data/dataCastVote.json");
+const message_data_election_key_schema = require("../query/method/message/data/dataKeyElection.json");
 const message_data_election_open_schema = require("../query/method/message/data/dataOpenElection.json");
 const message_data_election_end_schema = require("../query/method/message/data/dataEndElection.json");
 const message_data_election_result_schema = require("../query/method/message/data/dataResultElection.json");
 const message_data_election_setup_schema = require("../query/method/message/data/dataSetupElection.json");
+
 const message_data_meeting_create_schema = require("../query/method/message/data/dataCreateMeeting.json");
 const message_data_meeting_state_schema = require("../query/method/message/data/dataStateMeeting.json");
 const message_data_message_witness_schema = require("../query/method/message/data/dataWitnessMessage.json");
@@ -69,9 +72,11 @@ ajv.addSchema([
     message_data_lao_create_schema,
     message_data_lao_update_schema,
     message_data_lao_state_schema,
+    message_data_lao_greet_schema,
 
     message_data_vote_cast_schema,
 
+    message_data_election_key_schema,
     message_data_election_open_schema,
     message_data_election_end_schema,
     message_data_election_result_schema,
