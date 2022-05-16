294012e18ca2eb90fc18ce49a1b91ed4401f9dbb
zoemarin <zoe@marin.info>
2022-05-16 22:57:40 +0200

    Add comment and handler begin
    Signed-off-by: zoemarin <zoe@marin.info>

--- fe2-android/app/src/main/java/com/github/dedis/popstellar/model/objects/Input_object.java
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/model/objects/Input_object.java
@@ -1,18 +1,16 @@
 package com.github.dedis.popstellar.model.objects;
 
-import com.github.dedis.popstellar.model.network.method.message.data.digitalcash.Script_input;
-
 public class Input_object {
   private final String txOutHash;
   private final int txOutIndex;
-  private final Script_input script;
+  private final Script_input_object script;
 
   /**
    * @param txOutHash Previous (to-be-used) transaction hash
    * @param txOutIndex index of the previous to-be-used transaction
    * @param script The script describing the unlock mechanism
    */
-  public Input_object(String txOutHash, int txOutIndex, Script_input script) {
+  public Input_object(String txOutHash, int txOutIndex, Script_input_object script) {
     this.script = script;
     this.txOutHash = txOutHash;
     this.txOutIndex = txOutIndex;
@@ -26,7 +24,7 @@ public class Input_object {
     return txOutIndex;
   }
 
-  public Script_input get_script() {
+  public Script_input_object get_script() {
     return script;
   }
 }
--- fe2-android/app/src/main/java/com/github/dedis/popstellar/utility/handler/data/TransactionCoinHandler.java
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/utility/handler/data/TransactionCoinHandler.java
@@ -1,9 +1,20 @@
 package com.github.dedis.popstellar.utility.handler.data;
 
+import com.github.dedis.popstellar.model.network.method.message.data.digitalcash.Input;
+import com.github.dedis.popstellar.model.network.method.message.data.digitalcash.Output;
 import com.github.dedis.popstellar.model.network.method.message.data.digitalcash.PostTransactionCoin;
 import com.github.dedis.popstellar.model.objects.Channel;
+import com.github.dedis.popstellar.model.objects.Input_object;
 import com.github.dedis.popstellar.model.objects.Lao;
+import com.github.dedis.popstellar.model.objects.Output_object;
+import com.github.dedis.popstellar.model.objects.Script_input_object;
+import com.github.dedis.popstellar.model.objects.Script_output_object;
+import com.github.dedis.popstellar.model.objects.Transaction_object;
 import com.github.dedis.popstellar.repository.LAORepository;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class TransactionCoinHandler {
   public static final String TAG = TransactionCoinHandler.class.getSimpleName();
@@ -23,6 +34,33 @@ public class TransactionCoinHandler {
     LAORepository laoRepository = context.getLaoRepository();
     Channel channel = context.getChannel();
     Lao lao = laoRepository.getLaoByChannel(channel);
+
+    Transaction_object transaction_object = new Transaction_object();
+
+    transaction_object.setChannel(channel);
+    transaction_object.setLock_time(postTransactionCoin.get_transaction().get_lock_time());
+    transaction_object.setVersion(postTransactionCoin.get_transaction().get_version());
+
+    List<Input_object> inputs = new ArrayList<>();
+    List<Output_object> outputs = new ArrayList<>();
+
+    Iterator<Input> iterator_input = postTransactionCoin.get_transaction().get_inputs().iterator();
+    Iterator<Output> iterator_output =
+        postTransactionCoin.get_transaction().get_outputs().iterator();
+    while (iterator_input.hasNext()) {
+      Input current = iterator_input.next();
+      Script_input_object script_ =
+          new Script_input_object(
+              current.get_script().get_type(),
+              current.get_script().get_pubkey(),
+              current.get_script().get_sig());
+
+      inputs.add(new Input_object(current.get_tx_out_hash(), current.get_tx_out_index(), script_));
+    }
+    while (iterator_output.hasNext()) {
+      Output current = iterator_output.next();
+      Script_output_object script = new Script_output_object();
+    }
 
     // update all transaction
 
