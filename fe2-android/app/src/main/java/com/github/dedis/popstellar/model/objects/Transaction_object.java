117bff6a5e11655af6f8195cc29ea13e009c32fe
zoemarin <zoe@marin.info>
2022-05-16 09:58:58 +0200

    Create Transaction_object
    Signed-off-by: zoemarin <zoe@marin.info>

---
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/model/objects/Transaction_object.java
@@ -0,0 +1,29 @@
+package com.github.dedis.popstellar.model.objects;
+
+public class Transaction_object {
+  // private final Channel channel;
+
+  // version
+  // private final int version;
+
+  // inputs
+  // private final List<Input_object> inputs;
+
+  // outputs
+  // private final List<Output> outputs;
+
+  // lock_time
+  // private final long lock_time;
+
+  // function that give the list of sender
+
+  // function that give the list of receiver
+
+  // function that given a receiver get the miniLaoCoin he has
+
+  // function that say if it was a coin base transaction
+
+  // function that given a transaction check if the value send are alright
+
+  // function that given a list of attendees check if the transaction send is correct
+}
