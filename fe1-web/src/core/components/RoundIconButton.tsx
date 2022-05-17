3e90e4e80df676063b04e848c9d321babc9f1658
0xdieman <diego.boros@epfl.ch>
2022-05-17 15:54:16 +0200

    Add a small comment

--- fe1-web/src/core/components/RoundIconButton.tsx
+++ fe1-web/src/core/components/RoundIconButton.tsx
@@ -20,6 +20,11 @@ const styles = StyleSheet.create({
   } as ViewStyle,
 });
 
+/**
+ * This is a round button with an icon that uses Google material icons
+ * The 'name' property is the name of the icon that will be rendered,
+ * you can look for icon and their names here : https://fonts.google.com/icons
+ */
 const RoundIconButton = (props: IPropTypes) => {
   const { name, onClick } = props;
   return (
