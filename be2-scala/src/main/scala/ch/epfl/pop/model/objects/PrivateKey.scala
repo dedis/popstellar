cd2b0f94449d8f904cb12fe10602ef734959b1aa
Romain Birling <rombirli@gmail.com>
2022-05-18 11:13:50 +0200

    Started decryption

--- be2-scala/src/main/scala/ch/epfl/pop/model/objects/PrivateKey.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/model/objects/PrivateKey.scala
@@ -1,8 +1,10 @@
 package ch.epfl.pop.model.objects
 
+import ch.epfl.dedis.lib.crypto._
 import com.google.crypto.tink.subtle.Ed25519Sign
 
 final case class PrivateKey(base64Data: Base64Data) {
+  val scalar: Scalar = new Ed25519Scalar(this.base64Data.data)
 
   def signData(data: Base64Data): Signature = {
     val ed: Ed25519Sign = new Ed25519Sign(base64Data.decode())
--- be2-scala/src/main/scala/ch/epfl/pop/model/objects/PublicKey.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/model/objects/PublicKey.scala
@@ -1,5 +1,9 @@
 package ch.epfl.pop.model.objects
 
+import ch.epfl.dedis.lib.crypto._
+
 final case class PublicKey(base64Data: Base64Data) {
+  val point = new Ed25519Point(base64Data.data)
+
   def equals(that: PublicKey): Boolean = base64Data == that.base64Data
 }
