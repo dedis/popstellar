93ba82699e7c2a4a3187dae0bc4df97c8face0f4
Ajkunas <ajkuna.seipi@epfl.ch>
2022-05-20 14:43:27 +0200

    Revert "Started decryption"
    This reverts commit cd2b0f94449d8f904cb12fe10602ef734959b1aa.

--- be2-scala/src/main/scala/ch/epfl/pop/model/objects/KeyPair.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/model/objects/KeyPair.scala
@@ -1,31 +1,10 @@
 package ch.epfl.pop.model.objects
 
-import ch.epfl.dedis.lib.crypto.{Ed25519Pair, Point, Scalar}
+import ch.epfl.dedis.lib.crypto.Ed25519Pair
 
-case class KeyPair(privateKey: PrivateKey, publicKey: PublicKey) {
-  /*
-   func ElGamalDecrypt(group kyber.Group, prikey kyber.Scalar, K, C kyber.Point) (
-     message []byte, err error) {
-
-     // ElGamal-decrypt the ciphertext (K,C) to reproduce the message.
-     S := group.Point().Mul(prikey, K) // regenerate shared secret
-     M := group.Point().Sub(C, S)      // use to un-blind the message
-     message, err = M.Data()           // extract the embedded data
-     return
-   }
- */
-
-  def decrypt(base64Data: Base64Data): Base64Data = {
-    val K: Point = ???
-    val C: Point = ???
-    val S = K.mul(privateKey.scalar)
-    val M = C.add(S.negate())
-    Base64Data(new String(M.data()))
-  }
-}
+case class KeyPair(privateKey: PrivateKey, publicKey: PublicKey)
 
 object KeyPair {
-
   def apply(): KeyPair = {
     val libKeyPair = new Ed25519Pair()
     val privateKey = PrivateKey(Base64Data.encode(libKeyPair.scalar.toBytes))
--- be2-scala/src/main/scala/ch/epfl/pop/model/objects/PrivateKey.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/model/objects/PrivateKey.scala
@@ -1,10 +1,8 @@
 package ch.epfl.pop.model.objects
 
-import ch.epfl.dedis.lib.crypto._
 import com.google.crypto.tink.subtle.Ed25519Sign
 
 final case class PrivateKey(base64Data: Base64Data) {
-  val scalar: Scalar = new Ed25519Scalar(this.base64Data.data)
 
   def signData(data: Base64Data): Signature = {
     val ed: Ed25519Sign = new Ed25519Sign(base64Data.decode())
