a896bdbbfd4d08007a70e3cc96e10717fba4b5a3
MaximeZmt <maxime.zammit@epfl.ch>
2022-05-20 12:44:48 +0200

    new naming

--- fe2-android/app/src/test/java/com/github/dedis/popstellar/model/objects/ScriptOutputObjectTest.java
+++ fe2-android/app/src/test/java/com/github/dedis/popstellar/model/objects/ScriptOutputObjectTest.java
@@ -8,7 +8,7 @@ import com.github.dedis.popstellar.model.objects.security.PublicKey;
 
 import org.junit.Test;
 
-public class Output_objectTest {
+public class ScriptOutputObjectTest {
   private static final KeyPair SENDER_KEY = generateKeyPair();
   private static final PublicKey SENDER = SENDER_KEY.getPublicKey();
 
@@ -16,17 +16,13 @@ public class Output_objectTest {
   public static String PUBKEYHASH = SENDER.computeHash();
   private static Script_output_object SCRIPTTXOUT = new Script_output_object(TYPE, PUBKEYHASH);
 
-  private static final int VALUE = 32;
-  private static final Output_object OUTPUT = new Output_object(VALUE, SCRIPTTXOUT);
-
   @Test
-  public void get_valueTest() {
-    assertEquals(VALUE, OUTPUT.get_value());
+  public void getTypeTest() {
+    assertEquals(TYPE, SCRIPTTXOUT.get_type());
   }
 
   @Test
-  public void get_scriptTest() {
-    assertEquals(TYPE, OUTPUT.get_script().get_type());
-    assertEquals(PUBKEYHASH, OUTPUT.get_script().get_pubkey_hash());
+  public void getPublicKeyHashTest() {
+    assertEquals(PUBKEYHASH, SCRIPTTXOUT.get_pubkey_hash());
   }
 }
