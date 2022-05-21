6ac6b6cf66c0c05bd3508a5787b3504042eb874e
zoemarin <zoe@marin.info>
2022-05-21 09:48:01 +0200

    implementation of useful function in view model
    Signed-off-by: zoemarin <zoe@marin.info>

--- fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/digitalcash/DigitalCashViewModel.java
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/digitalcash/DigitalCashViewModel.java
@@ -1,21 +1,47 @@
 package com.github.dedis.popstellar.ui.digitalcash;
 
 import android.app.Application;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.LiveDataReactiveStreams;
 import androidx.lifecycle.MutableLiveData;
 
+import com.github.dedis.popstellar.R;
 import com.github.dedis.popstellar.SingleEvent;
+import com.github.dedis.popstellar.model.network.method.message.MessageGeneral;
+import com.github.dedis.popstellar.model.network.method.message.data.digitalcash.PostTransactionCoin;
+import com.github.dedis.popstellar.model.network.method.message.data.digitalcash.Transaction;
+import com.github.dedis.popstellar.model.objects.Channel;
+import com.github.dedis.popstellar.model.objects.Lao;
+import com.github.dedis.popstellar.model.objects.security.PoPToken;
+import com.github.dedis.popstellar.repository.LAORepository;
+import com.github.dedis.popstellar.repository.LAOState;
+import com.github.dedis.popstellar.repository.remote.GlobalNetworkManager;
+import com.github.dedis.popstellar.utility.error.ErrorUtils;
+import com.github.dedis.popstellar.utility.error.keys.KeyException;
+import com.github.dedis.popstellar.utility.security.KeyManager;
+import com.google.gson.Gson;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
 import dagger.hilt.android.lifecycle.HiltViewModel;
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
 
 @HiltViewModel
 public class DigitalCashViewModel extends AndroidViewModel {
   public static final String TAG = DigitalCashViewModel.class.getSimpleName();
+  private static final String LAO_FAILURE_MESSAGE = "failed to retrieve lao";
+  private static final String PUBLISH_MESSAGE = "sending publish message";
+  private static final String COIN = "coin";
 
   /*
    * LiveData objects for capturing events
@@ -27,9 +53,42 @@ public class DigitalCashViewModel extends AndroidViewModel {
   private final MutableLiveData<SingleEvent<Boolean>> mOpenIssueEvent = new MutableLiveData<>();
   private final MutableLiveData<SingleEvent<Boolean>> mOpenReceiptEvent = new MutableLiveData<>();
 
+  private final LiveData<List<Lao>> mLAOs;
+  private final MutableLiveData<String> mLaoId = new MutableLiveData<>();
+  private final MutableLiveData<String> mLaoName = new MutableLiveData<>();
+
+  /*
+   * Dependencies for this class
+   */
+  private final LAORepository laoRepository;
+  private final GlobalNetworkManager networkManager;
+  private final Gson gson;
+  private final KeyManager keyManager;
+  private final CompositeDisposable disposables;
+
   @Inject
-  public DigitalCashViewModel(@NonNull Application application) {
+  public DigitalCashViewModel(
+      @NonNull Application application,
+      LAORepository laoRepository,
+      GlobalNetworkManager networkManager,
+      Gson gson,
+      KeyManager keyManager) {
     super(application);
+    this.laoRepository = laoRepository;
+    this.networkManager = networkManager;
+    this.gson = gson;
+    this.keyManager = keyManager;
+    disposables = new CompositeDisposable();
+
+    mLAOs =
+        LiveDataReactiveStreams.fromPublisher(
+            this.laoRepository.getAllLaos().toFlowable(BackpressureStrategy.BUFFER));
+  }
+
+  @Override
+  protected void onCleared() {
+    super.onCleared();
+    disposables.dispose();
   }
 
   /*
@@ -84,5 +143,85 @@ public class DigitalCashViewModel extends AndroidViewModel {
 
   public void openReceipt() {
     mOpenReceiptEvent.postValue(new SingleEvent<>(true));
+  }
+
+  /**
+   * Post a transaction to your channel
+   *
+   * <p>Publish a Message General containing a PostTransaction data
+   */
+  public void postTransaction(Transaction transactionToPost) {
+    Log.d(TAG, "Post a transaction");
+    Lao lao = getCurrentLao();
+    if (lao == null) {
+      Log.e(TAG, LAO_FAILURE_MESSAGE);
+      return;
+    }
+
+    PostTransactionCoin postTransactionCoin = new PostTransactionCoin(transactionToPost);
+
+    try {
+      PoPToken token = keyManager.getValidPoPToken(lao);
+      Channel channel =
+          lao.getChannel().subChannel(COIN).subChannel(token.getPublicKey().getEncoded());
+
+      Log.d(TAG, PUBLISH_MESSAGE);
+
+      MessageGeneral msg = new MessageGeneral(token, postTransactionCoin, gson);
+
+      Disposable disposable =
+          networkManager
+              .getMessageSender()
+              .publish(token, channel, postTransactionCoin)
+              .subscribe(
+                  () -> {
+                    Log.d(TAG, "Post transaction with the message id: " + msg.getMessageId());
+                    Toast.makeText(
+                            getApplication().getApplicationContext(),
+                            "Post Transaction!",
+                            Toast.LENGTH_LONG)
+                        .show();
+                  },
+                  error ->
+                      ErrorUtils.logAndShow(
+                          getApplication(), TAG, error, R.string.error_post_transaction));
+
+      disposables.add(disposable);
+    } catch (KeyException e) {
+      ErrorUtils.logAndShow(getApplication(), TAG, e, R.string.error_retrieve_own_token);
+    }
+  }
+
+  public LiveData<List<Lao>> getLAOs() {
+    return mLAOs;
+  }
+
+  public LiveData<String> getLaoId() {
+    return mLaoId;
+  }
+
+  public LiveData<String> getLaoName() {
+    return mLaoName;
+  }
+
+  public void setLaoId(String laoId) {
+    this.mLaoId.setValue(laoId);
+  }
+
+  public void setLaoName(String laoName) {
+    mLaoName.setValue(laoName);
+  }
+
+  @Nullable
+  public Lao getCurrentLao() {
+    return getLao(getLaoId().getValue());
+  }
+
+  @Nullable
+  private Lao getLao(String laoId) {
+    LAOState laoState = laoRepository.getLaoById().get(laoId);
+    if (laoState == null) return null;
+
+    return laoState.getLao();
   }
 }
