c92f4e3fbb087d6d505b65296a059c018800083b
MaximeZmt <maxime.zammit@epfl.ch>
2022-05-23 12:39:01 +0200

    address Johann issues

--- fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/digitalcash/DigitalCashIssueFragment.java
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/digitalcash/DigitalCashIssueFragment.java
@@ -4,16 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.github.dedis.popstellar.R;
+import com.github.dedis.popstellar.databinding.DigitalCashIssueFragmentBinding;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass. Use the {@link DigitalCashIssueFragment#newInstance} factory
  * method to create an instance of this fragment.
  */
 public class DigitalCashIssueFragment extends Fragment {
+  private DigitalCashIssueFragmentBinding mBinding;
 
   public DigitalCashIssueFragment() {
     // not implemented yet
@@ -33,6 +42,16 @@ public class DigitalCashIssueFragment extends Fragment {
   @Override
   public View onCreateView(
       LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.digital_cash_issue_fragment, container, false);
+    //DigitalCashViewModel mViewModel = DigitalCashMain.obtainViewModel(getActivity());
+    mBinding = DigitalCashIssueFragmentBinding.inflate(inflater, container, false);
+    return mBinding.getRoot();
+  }
+
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    List<String> myArray = Arrays.asList("Option 1", "Option 2", "Option 3", "Option 4");
+    ArrayAdapter<String> adapter = new ArrayAdapter<>(requireContext(), R.layout.list_item, myArray);
+    mBinding.digitalCashIssueSpinnerTv.setAdapter(adapter);
   }
 }
