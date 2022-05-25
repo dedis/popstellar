1cf6cee17454d4eaf2afada25e81a68076052280
Tesa27 <stefan.eric@epfl.ch>
2022-05-25 15:19:10 +0200

    fmt file

--- be1-go/message/answer/error.go
+++ be1-go/message/answer/error.go
@@ -32,62 +32,62 @@ func NewErrorf(code int, format string, values ...interface{}) *Error {
 // NewInvalidActionError returns an error with the code -1 for an invalid action.
 func NewInvalidActionError(action string, values ...interface{}) *Error {
 	return NewErrorf(
-	-1,
-	"invalid action: "+ action,
-	values,
+		-1,
+		"invalid action: "+action,
+		values,
 	)
 }
 
 // NewInvalidObjectError returns an error with the code -1 for an invalid object.
-func NewInvalidObjectError(action string, values ... interface{}) *Error {
+func NewInvalidObjectError(action string, values ...interface{}) *Error {
 	return NewErrorf(
 		-1,
-		"invalid object: "+ action,
+		"invalid object: "+action,
 		values,
 	)
 }
 
 // NewInvalidResourceError returns an error with -2 for an object with invalid resources
-func NewInvalidResourceError(description string, values ... interface{}) *Error {
+func NewInvalidResourceError(description string, values ...interface{}) *Error {
 	return NewErrorf(
 		-2,
-		"invalid resource: "+ description,
+		"invalid resource: "+description,
 		values,
 	)
 }
 
 // NewDuplicateResourceError returns an error with -3 for a resource that already exists
-func NewDuplicateResourceError(description string, values ... interface{}) *Error {
+func NewDuplicateResourceError(description string, values ...interface{}) *Error {
 	return NewErrorf(
 		-3,
-		"duplicate resource: "+ description,
+		"duplicate resource: "+description,
 		values,
 	)
 }
 
 // NewInvalidMessageFieldError returns an error with -4 when a message field is bogus
-func NewInvalidMessageFieldError(description string, values ... interface{}) *Error {
+func NewInvalidMessageFieldError(description string, values ...interface{}) *Error {
 	return NewErrorf(
 		-4,
-		"invalid message field: "+ description,
+		"invalid message field: "+description,
 		values,
 	)
 }
 
 // NewAccessDeniedError returns an error with -5 when an access is denied for the sender
-func NewAccessDeniedError(description string, values ... interface{}) *Error {
+func NewAccessDeniedError(description string, values ...interface{}) *Error {
 	return NewErrorf(
 		-5,
-		"access denied: "+ description,
+		"access denied: "+description,
 		values,
 	)
 }
 
 // NewInternalServerError returns an error with -6 when there is an internal server error
-func NewInternalServerError(description string, values ... interface{}) *Error {
+func NewInternalServerError(description string, values ...interface{}) *Error {
 	return NewErrorf(
 		-6,
-		"internal server error: "+ description,
+		"internal server error: "+description,
 		values,
 	)
 }
