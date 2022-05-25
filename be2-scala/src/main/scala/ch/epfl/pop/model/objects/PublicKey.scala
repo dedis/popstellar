68117baa16d17d1b813abc32bff6d107d1b93fff
Romain Birling <rombirli@gmail.com>
2022-05-25 12:41:56 +0200

    Use SecureRandom instead of random for security

--- be2-scala/src/main/scala/ch/epfl/pop/model/objects/PublicKey.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/model/objects/PublicKey.scala
@@ -2,7 +2,7 @@ package ch.epfl.pop.model.objects
 
 import ch.epfl.dedis.lib.crypto.{Ed25519, Ed25519Point, Ed25519Scalar}
 
-import java.util.Random
+import java.security.SecureRandom
 
 final case class PublicKey(base64Data: Base64Data) {
   private val asPoint = new Ed25519Point(base64Data.decode())
@@ -13,7 +13,7 @@ final case class PublicKey(base64Data: Base64Data) {
     if (message.length > 29) throw new IllegalArgumentException("The message should contain at maximum 29 bytes")
     val M = Ed25519Point.embed(message)
     val seed = new Array[Byte](Ed25519.field.getb / 8)
-    new Random().nextBytes(seed)
+    new SecureRandom().nextBytes(seed)
     val k = new Ed25519Scalar(seed)
     val K = Ed25519Point.base.mul(k)
     val S = asPoint.mul(k)
