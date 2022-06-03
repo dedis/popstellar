79eb05d213dbafc528222cec6bfdf22f0bfa7b29
Nico Hauser <me@tyratox.ch>
2022-06-03 16:36:47 +0200

    Fix merge issues

--- fe1-web/src/core/styles/stylesheets/circularButtonStyles.ts
+++ fe1-web/src/core/styles/stylesheets/circularButtonStyles.ts
@@ -4,12 +4,18 @@ import { Color } from '../index';
 
 const circularButtonStyles = StyleSheet.create({
   button: {
+    alignContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
     width: 80,
     height: 80,
     backgroundColor: Color.blue,
     borderRadius: 80,
   } as ViewStyle,
   roundButton: {
+    alignContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
     height: 65,
     width: 65,
     shadowColor: Color.gray,
