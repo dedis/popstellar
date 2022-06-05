fb2e463e27ce85ca590adeb9f0c6ed609b61fa8f
zoemarin <zoe@marin.info>
2022-06-05 18:44:32 +0200

    Add Issue LAO Members/Attendees/Witnesses
    Signed-off-by: zoemarin <zoe@marin.info>

--- fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/digitalcash/DigitalCashIssueFragment.java
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/digitalcash/DigitalCashIssueFragment.java
@@ -14,13 +14,19 @@ import androidx.fragment.app.Fragment;
 
 import com.github.dedis.popstellar.R;
 import com.github.dedis.popstellar.databinding.DigitalCashIssueFragmentBinding;
+import com.github.dedis.popstellar.model.objects.RollCall;
+import com.github.dedis.popstellar.model.objects.security.PublicKey;
 import com.github.dedis.popstellar.utility.error.keys.KeyException;
 import com.github.dedis.popstellar.utility.error.keys.NoRollCallException;
 
 import java.time.Instant;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
@@ -75,16 +81,50 @@ public class DigitalCashIssueFragment extends Fragment {
                 String current_public_key_selected =
                     String.valueOf(mBinding.digitalCashIssueSpinner.getEditText().getText());
 
+                int radio_group = mBinding.digitalCashIssueSelect.getCheckedRadioButtonId();
+                Log.d(TAG, "radio group : " + radio_group);
+
                 if ((current_amount.isEmpty()) || (Integer.valueOf(current_amount) < 0)) {
                   // create in View Model a function that toast : please enter amount
                   mViewModel.requireToPutAnAmount();
-                } else if (current_public_key_selected.isEmpty()) {
+                } else if (current_public_key_selected.isEmpty() && (radio_group == -1)) {
                   // create in View Model a function that toast : please enter key
                   mViewModel.requireToPutLAOMember();
                 } else {
+
                   try {
-                    postTransaction(
-                        Collections.singletonMap(current_public_key_selected, current_amount));
+                    if (radio_group == -1) {
+                      postTransaction(
+                          Collections.singletonMap(current_public_key_selected, current_amount));
+                    } else {
+                      Set<PublicKey> attendees = new HashSet<>();
+                      if (radio_group == 2131231314) {
+                        Iterator<RollCall> rollCallIterator =
+                            mViewModel.getCurrentLao().getRollCalls().values().iterator();
+                        while (rollCallIterator.hasNext()) {
+                          RollCall current = rollCallIterator.next();
+                          attendees.addAll(current.getAttendees());
+                        }
+                      } else if (radio_group == 2131231315) {
+                        attendees = mViewModel.getAttendeesFromTheRollCall();
+                      } else {
+                        attendees = mViewModel.getCurrentLao().getWitnesses();
+                        if (attendees.isEmpty()) {
+                          Toast.makeText(
+                              requireContext(),
+                              "Can't issue there are no witnesses",
+                              Toast.LENGTH_LONG);
+                          return;
+                        }
+                      }
+                      Map<String, String> issueMap = new HashMap<>();
+                      Iterator<PublicKey> publicKeyIterator = attendees.iterator();
+                      while (publicKeyIterator.hasNext()) {
+                        PublicKey publicKey = publicKeyIterator.next();
+                        issueMap.putIfAbsent(publicKey.getEncoded(), current_amount);
+                      }
+                      postTransaction(issueMap);
+                    }
                   } catch (KeyException e) {
                     e.printStackTrace();
                     Log.d(TAG, "error couldn't post the transaction due to key exception");
