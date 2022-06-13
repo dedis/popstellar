8178a5589c9b755ee1692d89760be0f63005a116
zoemarin <zoe@marin.info>
2022-06-13 16:39:29 +0200

    add some TransactionObjectTest
    Signed-off-by: zoemarin <zoe@marin.info>

--- fe2-android/app/src/test/java/com/github/dedis/popstellar/model/objects/TransactionObjectTest.java
+++ fe2-android/app/src/test/java/com/github/dedis/popstellar/model/objects/TransactionObjectTest.java
@@ -3,10 +3,12 @@ package com.github.dedis.popstellar.model.objects;
 import static com.github.dedis.popstellar.testutils.Base64DataUtils.generateKeyPair;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertThrows;
 
 import com.github.dedis.popstellar.model.objects.security.KeyPair;
 import com.github.dedis.popstellar.model.objects.security.PublicKey;
 import com.github.dedis.popstellar.model.objects.security.Signature;
+import com.github.dedis.popstellar.utility.security.Hash;
 
 import org.junit.Test;
 
@@ -111,6 +113,43 @@ public class TransactionObjectTest {
         Collections.singletonList(pubKeyHash), transactionObject.getReceiversHashTransaction());
   }
 
+  // test thrown null List<PublicKey> getReceiversTransaction(Map<String, PublicKey> mapHashKey)
+  // test thrown null Map<PublicKey, Long> getReceiversTransactionMap(Map<String, PublicKey>
+  // mapHashKey)
+  @Test
+  public void getReceiversTransactionTestNull() {
+    /*public List<PublicKey> getReceiversTransaction(Map<String, PublicKey> mapHashKey) {
+      Iterator<String> receiverHashIte = getReceiversHashTransaction().iterator();
+      List<PublicKey> receivers = new ArrayList<>();
+      while (receiverHashIte.hasNext()){
+        PublicKey pub = mapHashKey.getOrDefault(receiverHashIte.next(),null);
+        if (pub == null) {
+          throw new IllegalArgumentException("The hash correspond to no key in the dictionary");
+        }
+        receivers.add(pub);
+      }
+      return receivers;
+    }*/
+
+    KeyPair senderKey1 = generateKeyPair();
+    PublicKey sender1 = senderKey1.getPublicKey();
+    PublicKey sender2 = null;
+    String type = "P2PKH";
+    String pubkeyhash1 = sender1.computeHash();
+    String pubkeyhash2 = "none";
+    ScriptOutputObject scriptTxOut = new ScriptOutputObject(type, pubkeyhash1);
+    int value = 32;
+    OutputObject output = new OutputObject(value, scriptTxOut);
+    List<OutputObject> listOutput = Collections.singletonList(output);
+    Map<String, PublicKey> mapHash = Collections.singletonMap(pubkeyhash2, sender2);
+    transactionObject.setOutputs(listOutput);
+    assertThrows(
+        IllegalArgumentException.class, () -> transactionObject.getReceiversTransaction(mapHash));
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> transactionObject.getReceiversTransactionMap(mapHash));
+  }
+
   // test List<PublicKey> get_receivers_transaction(Map<String, PublicKey> map_hash_key)
   @Test
   public void getReceiversTransactionTest() {
@@ -128,7 +167,36 @@ public class TransactionObjectTest {
         Collections.singletonList(sender), transactionObject.getReceiversTransaction(mapHash));
   }
 
-  // test boolean is_receiver(PublicKey publicKey)
+  // test Map<PublicKey, Long> getReceiversTransactionMap(Map<String, PublicKey> mapHashKey)
+  @Test
+  public void getReceiversTransactionMapTest() {
+    /*  public Map<PublicKey, Long> getReceiversTransactionMap(Map<String, PublicKey> mapHashKey) {
+      Iterator<String> receiverHashIte = getReceiversHashTransaction().iterator();
+      Map<PublicKey, Long> receivers = new HashMap<>();
+      while (receiverHashIte.hasNext()) {
+        PublicKey pub = mapHashKey.getOrDefault(receiverHashIte.next(), null);
+        if (pub == null) {
+          throw new IllegalArgumentException("The hash correspond to no key in the dictionary");
+        }
+        receivers.putIfAbsent(pub, this.getMiniLaoPerReceiver(pub));
+      }
+      return receivers;
+    }*/
+    KeyPair senderKey = generateKeyPair();
+    PublicKey sender = senderKey.getPublicKey();
+    String type = "P2PKH";
+    String pubkeyhash = sender.computeHash();
+    ScriptOutputObject scriptTxOut = new ScriptOutputObject(type, pubkeyhash);
+    long value = 32;
+    OutputObject output = new OutputObject(value, scriptTxOut);
+    List<OutputObject> listOutput = Collections.singletonList(output);
+    Map<String, PublicKey> mapHash = Collections.singletonMap(pubkeyhash, sender);
+    Map<PublicKey, Long> mapValue = Collections.singletonMap(sender, value);
+    transactionObject.setOutputs(listOutput);
+    assertEquals(mapValue, transactionObject.getReceiversTransactionMap(mapHash));
+  }
+
+  // test boolean isReceiver(PublicKey publicKey)
   @Test
   public void getIsReceiverTest() {
     // SENDER
@@ -150,6 +218,19 @@ public class TransactionObjectTest {
     assertNotEquals(
         Collections.singletonList(senderDummy.computeHash()),
         transactionObject.getReceiversHashTransaction());
+  }
+
+  // test boolean is Sender(PublicKey publicKey)
+  @Test
+  public void getIsSenderTest() {
+    KeyPair senderKey = generateKeyPair();
+    PublicKey sender = senderKey.getPublicKey();
+    String type = "P2PKH";
+    ScriptInputObject scriptTxInput = new ScriptInputObject(type, sender, new Signature("qqchose"));
+    InputObject input = new InputObject(Hash.hash("none"), 0, scriptTxInput);
+    List<InputObject> listInput = Collections.singletonList(input);
+    transactionObject.setInputs(listInput);
+    assertEquals(true, transactionObject.isSender(sender));
   }
 
   // test String compute_sig_outputs_inputs(KeyPair keyPair)
