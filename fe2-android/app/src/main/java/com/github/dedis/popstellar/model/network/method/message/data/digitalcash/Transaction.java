fb14a22422c976f318bd80bf910b4237f4cb27bb
MaximeZmt <maxime.zammit@epfl.ch>
2022-06-14 12:28:18 +0200

    remove code smells

--- fe2-android/app/src/main/java/com/github/dedis/popstellar/model/network/method/message/data/digitalcash/Transaction.java
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/model/network/method/message/data/digitalcash/Transaction.java
@@ -6,7 +6,6 @@ import com.google.gson.annotations.SerializedName;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
