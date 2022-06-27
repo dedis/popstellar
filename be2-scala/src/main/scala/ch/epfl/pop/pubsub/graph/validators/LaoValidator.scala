3fa596f7078744393126b998dc5fbcbe0ccf9740
Tesa27 <stefan.eric@epfl.ch>
2022-06-27 17:41:46 +0200

    last

--- be2-scala/src/main/scala/ch/epfl/pop/pubsub/graph/validators/LaoValidator.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/pubsub/graph/validators/LaoValidator.scala
@@ -82,7 +82,7 @@ case object LaoValidator extends MessageDataContentValidator {
         val expectedLaoId: Hash = rpcMessage.extractLaoId
 
         if (expectedLaoId != data.lao) {
-          Right(validationError("unexpected id "+ expectedLaoId+ "1111111"+data.lao))
+          Right(validationError("unexpected id, was " + data.lao + " but expected " + expectedLaoId))
         } else if (data.frontend != message.sender) {
           Right(validationError("unexpected frontend"))
         } else if (!data.address.startsWith("ws://")) {
