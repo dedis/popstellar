b468a573517d4e6df414dffa64a8d8aa2b0aaeed
Nico Hauser <me@tyratox.ch>
2022-07-01 17:27:57 +0200

    Add timestamp back to build output

--- fe1-web/build/web-postbuild.js
+++ fe1-web/build/web-postbuild.js
@@ -6,6 +6,6 @@ const gitHash = execSync('git rev-parse HEAD', { encoding: 'ascii' }).trim();
 const isDirty = execSync("git diff --quiet || echo '-dirty'", { encoding: 'ascii' }).trim();
 
 const oldName = path.resolve(__dirname, '..', 'dist', 'fe1.zip');
-const newName = path.resolve(__dirname, '..', 'dist', `fe1-${gitHash}${isDirty}.zip`);
+const newName = path.resolve(__dirname, '..', 'dist', `fe1-${gitHash}${isDirty}-${Date.now()}.zip`);
 
 fs.renameSync(oldName, newName);
