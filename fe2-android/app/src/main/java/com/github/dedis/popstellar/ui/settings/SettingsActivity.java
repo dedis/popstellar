36345318cbe695a3adf6a76ff2ab2703b29401b4
Johann Pluss <johann.pluss@epfl.ch>
2022-07-26 23:20:16 +0200

    Refactor settings Replaced activity + fragment + viewmodel by standalone activity
    Signed-off-by: Johann Plüss <pluss.j2407@gmail.com>

--- fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/settings/SettingsActivity.java
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/settings/SettingsActivity.java
@@ -1,38 +1,77 @@
 package com.github.dedis.popstellar.ui.settings;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.github.dedis.popstellar.R;
-import com.github.dedis.popstellar.utility.ActivityUtils;
+import com.github.dedis.popstellar.repository.remote.GlobalNetworkManager;
+import com.github.dedis.popstellar.ui.home.HomeActivity;
+
+import javax.inject.Inject;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
 public class SettingsActivity extends AppCompatActivity {
 
+  public final String TAG = SettingsActivity.class.getSimpleName();
+
+  @Inject GlobalNetworkManager networkManager;
+
+  private Button applyButton;
+  private String initialUrl;
+  private EditText entryBoxServerUrl;
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.settings_activity);
 
-    setupSettingsFragment();
+    initialUrl = networkManager.getCurrentUrl();
+
+    setupApplyButton();
+    setupEntryBox();
   }
 
-  public static SettingsViewModel obtainViewModel(FragmentActivity activity) {
-    return new ViewModelProvider(activity).get(SettingsViewModel.class);
+  private void setupEntryBox() {
+    entryBoxServerUrl = findViewById(R.id.entry_box_server_url);
+    entryBoxServerUrl.setText(initialUrl);
+    entryBoxServerUrl.addTextChangedListener(
+        new TextWatcher() {
+          @Override
+          public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            // Do nothing
+          }
+
+          @Override
+          public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            // Do nothing
+          }
+
+          @Override
+          public void afterTextChanged(Editable editable) {
+            String newAddress = editable.toString();
+            applyButton.setEnabled(newAddress.length() > 0 && !newAddress.equals(initialUrl));
+          }
+        });
   }
 
-  private void setupSettingsFragment() {
-    SettingsFragment settingsFragment =
-        (SettingsFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_settings);
-    if (settingsFragment == null) {
-      settingsFragment = SettingsFragment.newInstance();
-    }
-    ActivityUtils.replaceFragmentInActivity(
-        getSupportFragmentManager(), settingsFragment, R.id.fragment_container_settings);
+  private void setupApplyButton() {
+    applyButton = findViewById(R.id.button_apply);
+    applyButton.setOnClickListener(v -> applyChanges());
+  }
+
+  private void applyChanges() {
+    networkManager.connect(entryBoxServerUrl.getText().toString());
+    Intent intent = new Intent(this, HomeActivity.class);
+    Log.d(TAG, "Trying to open home");
+    startActivity(intent);
   }
 }
