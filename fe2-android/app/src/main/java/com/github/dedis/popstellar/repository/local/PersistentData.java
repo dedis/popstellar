5fbdfd8af5bc19cc8437800a59575b69695d5e86
Johann Pluss <johann.pluss@epfl.ch>
2022-08-15 13:08:33 +0200

    Add class representing data stored persistently
    Signed-off-by: Johann Plüss <pluss.j2407@gmail.com>

---
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/repository/local/PersistentData.java
@@ -0,0 +1,54 @@
+package com.github.dedis.popstellar.repository.local;
+
+import androidx.annotation.NonNull;
+
+import com.github.dedis.popstellar.model.objects.Channel;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class PersistentData implements Serializable {
+
+  private final List<String> walletSeed;
+  private final String serverAddress;
+  private final Set<Channel> subscriptions;
+
+  public PersistentData(String[] walletSeed, String serverAddress, Set<Channel> subscription) {
+    if (walletSeed == null || serverAddress == null || subscription == null) {
+      throw new IllegalArgumentException("Null argument");
+    }
+    this.walletSeed = Collections.unmodifiableList(Arrays.asList(walletSeed));
+    this.serverAddress = serverAddress;
+    this.subscriptions = new HashSet<>(subscription);
+  }
+
+  public String[] getWalletSeed() {
+    return walletSeed.toArray(new String[0]);
+  }
+
+  public String getServerAddress() {
+    return serverAddress;
+  }
+
+  public Set<Channel> getSubscriptions() {
+    return new HashSet<>(subscriptions);
+  }
+
+  @NonNull
+  @Override
+  public String toString() {
+    return "PersistentData{"
+        + "walletSeed="
+        + walletSeed
+        + ", serverAddress='"
+        + serverAddress
+        + '\''
+        + ", subscriptions="
+        + subscriptions
+        + '}';
+  }
+}
