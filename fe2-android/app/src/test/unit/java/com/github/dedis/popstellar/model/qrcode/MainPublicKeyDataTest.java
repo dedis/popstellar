ad1348144f7875ca76b6ff3e01c79c7e457f7e6e
Johann Pluss <johann.pluss@epfl.ch>
2022-09-27 22:57:09 +0200

    Fix tests due to missing customization of Gson

--- fe2-android/app/src/test/unit/java/com/github/dedis/popstellar/model/qrcode/MainPublicKeyDataTest.java
+++ fe2-android/app/src/test/unit/java/com/github/dedis/popstellar/model/qrcode/MainPublicKeyDataTest.java
@@ -2,16 +2,21 @@ package com.github.dedis.popstellar.model.qrcode;
 
 import static org.junit.Assert.assertEquals;
 
+import com.github.dedis.popstellar.model.network.serializer.JsonBase64DataSerializer;
 import com.github.dedis.popstellar.model.objects.security.PublicKey;
 import com.github.dedis.popstellar.testutils.Base64DataUtils;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import org.junit.Test;
 
 public class MainPublicKeyDataTest {
 
-  Gson gson = new Gson();
-  PublicKey pk = Base64DataUtils.generatePublicKey();
+  private final Gson gson =
+      new GsonBuilder()
+          .registerTypeAdapter(PublicKey.class, new JsonBase64DataSerializer<>(PublicKey::new))
+          .create();
+  private final PublicKey pk = Base64DataUtils.generatePublicKey();
 
   @Test
   public void constructorAndGetterAreCoherent() {
--- fe2-android/app/src/test/unit/java/com/github/dedis/popstellar/model/qrcode/PopTokenDataTest.java
+++ fe2-android/app/src/test/unit/java/com/github/dedis/popstellar/model/qrcode/PopTokenDataTest.java
@@ -4,13 +4,18 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+import com.github.dedis.popstellar.model.network.serializer.JsonBase64DataSerializer;
 import com.github.dedis.popstellar.model.objects.security.PublicKey;
 import com.github.dedis.popstellar.testutils.Base64DataUtils;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class PopTokenDataTest {
 
-  private final Gson gson = new Gson();
+  private final Gson gson =
+      new GsonBuilder()
+          .registerTypeAdapter(PublicKey.class, new JsonBase64DataSerializer<>(PublicKey::new))
+          .create();
   private final PublicKey pk = Base64DataUtils.generatePublicKey();
 
   @Test
