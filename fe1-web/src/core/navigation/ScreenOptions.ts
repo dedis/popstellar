9e2b92ed63db5898e8541242eb9200006879bce6
Nico Hauser <me@tyratox.ch>
2022-11-03 11:25:38 +0100

    Improve comment wording

--- fe1-web/src/core/navigation/ScreenOptions.ts
+++ fe1-web/src/core/navigation/ScreenOptions.ts
@@ -5,8 +5,8 @@ import { Color, Spacing, Typography } from 'core/styles';
 
 export const stackScreenOptionsWithoutHeader: StackNavigationOptions = {
   headerShown: false,
-  // Since we explicitly use scroll views for screens, disable scrolling for
-  // the navigator so that the header sticks to the top
+  // Since we explicitly use scroll views for screens, we should disable scrolling for
+  // the navigator so that the header sticks to the top of the screen
   // https://github.com/react-navigation/react-navigation/issues/9344#issuecomment-782520454
   cardStyle: { flex: 1 },
 };
@@ -24,8 +24,8 @@ export const stackScreenOptionsWithHeader: StackNavigationOptions = {
     backgroundColor: Color.contrast,
     borderColor: Color.separator,
   },
-  // Since we explicitly use scroll views for screens, disable scrolling for
-  // the navigator so that the header sticks to the top
+  // Since we explicitly use scroll views for screens, we should disable scrolling for
+  // the navigator so that the header sticks to the top of the screen
   // https://github.com/react-navigation/react-navigation/issues/9344#issuecomment-782520454
   cardStyle: { flex: 1 },
 };
