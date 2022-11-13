9e273ae0513adc627bc44332f7f700f4ff4ac8ef
Nico Hauser <me@tyratox.ch>
2022-11-13 15:50:11 +0100

    export useConnectedToLao hook

--- fe1-web/src/features/lao/interface/Configuration.ts
+++ fe1-web/src/features/lao/interface/Configuration.ts
@@ -121,6 +121,12 @@ export interface LaoConfigurationInterface extends FeatureInterface {
     useAssertCurrentLaoId: () => Hash;
 
     /**
+     * Returns true if currently connected to a lao, false if in offline mode
+     * and undefined if there is no current lao
+     */
+    useConnectedToLao: () => boolean | undefined;
+
+    /**
      * Returns the public key of the organizer's backend for a given lao id
      * @param laoId The lao id for which the key should be retrieved
      * @returns The public key or undefined if there is none
