230a0bef76ab1cf63cb4b9bd72582c357c1dc3f9
GabrielFleischer <gabriel.fleischer@epfl.ch>
2022-11-17 14:33:08 +0100

    Improve test function and coverage of the connection object
    Signed-off-by: GabrielFleischer <gabriel.fleischer@epfl.ch>

--- fe2-android/app/src/test/unit/java/com/github/dedis/popstellar/repository/remote/ConnectionTest.java
+++ fe2-android/app/src/test/unit/java/com/github/dedis/popstellar/repository/remote/ConnectionTest.java
@@ -1,20 +1,23 @@
 package com.github.dedis.popstellar.repository.remote;
 
 import com.github.dedis.popstellar.model.network.GenericMessage;
+import com.github.dedis.popstellar.model.network.answer.Result;
 import com.github.dedis.popstellar.model.network.method.Message;
 import com.github.dedis.popstellar.model.network.method.Subscribe;
 import com.github.dedis.popstellar.model.objects.Channel;
-import com.tinder.scarlet.Lifecycle;
-import com.tinder.scarlet.ShutdownReason;
+import com.tinder.scarlet.*;
 
 import org.junit.Test;
 
+import io.reactivex.Observable;
 import io.reactivex.subjects.BehaviorSubject;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class ConnectionTest {
+
+  public static final String URL = "url";
 
   @Test
   public void sendMessageDelegatesToService() {
@@ -45,9 +48,15 @@ public class ConnectionTest {
 
     BehaviorSubject<Lifecycle.State> manualState = BehaviorSubject.create();
 
-    Connection connection = new Connection("url", service, manualState);
+    // Create connection and retrieve events
+    Connection connection = new Connection(URL, service, manualState);
+    Observable<GenericMessage> connectionMessages = connection.observeMessage();
+    // Publish message to the pipeline
+    GenericMessage message = new Result(5);
+    messages.onNext(message);
 
-    connection.observeMessage();
+    // Make sure the event was receive
+    connectionMessages.test().assertValueCount(1).assertValue(message);
 
     verify(service).observeMessage();
     verify(service, atLeastOnce()).observeWebsocket();
@@ -58,16 +67,22 @@ public class ConnectionTest {
   public void observeWebsocketDelegatesToService() {
     LAOService service = mock(LAOService.class);
     BehaviorSubject<GenericMessage> messages = BehaviorSubject.create();
+    BehaviorSubject<WebSocket.Event> events = BehaviorSubject.create();
     when(service.observeMessage()).thenReturn(messages);
-    when(service.observeWebsocket()).thenReturn(BehaviorSubject.create());
+    when(service.observeWebsocket()).thenReturn(events);
 
     BehaviorSubject<Lifecycle.State> manualState = BehaviorSubject.create();
 
+    // Create connection and retrieve events
     Connection connection = new Connection("url", service, manualState);
+    Observable<WebSocket.Event> connectionEvents = connection.observeConnectionEvents();
+    // Publish event to the pipeline
+    WebSocket.Event event = new WebSocket.Event.OnConnectionOpened<>("Fake WebSocket");
+    events.onNext(event);
 
-    connection.observeConnectionEvents();
+    // Make sure the event was receive
+    connectionEvents.test().assertValueCount(1).assertValue(event);
 
-    verify(service).observeWebsocket();
     verify(service).observeMessage();
     verify(service, atLeastOnce()).observeWebsocket();
     verifyNoMoreInteractions(service);
