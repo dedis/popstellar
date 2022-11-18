4a8ea49d61879c45fa7f997de9c00d86a543f928
Ajkunas <ajkuna.seipi@epfl.ch>
2022-11-18 10:36:18 +0100

    parametersToBroadcast function

--- be2-scala/src/main/scala/ch/epfl/pop/pubsub/graph/handlers/SocialMediaHandler.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/pubsub/graph/handlers/SocialMediaHandler.scala
@@ -43,25 +43,16 @@ class SocialMediaHandler(dbRef: => AskableActorRef) extends MessageHandler {
   def handleAddChirp(rpcMessage: JsonRpcRequest): GraphMessage = {
     val ask =
       for {
-        _ <- checkLaoChannel(rpcMessage, "Server failed to extract LAO id for the broadcast")
-        _ <- checkParameters(rpcMessage, "Server failed to extract chirp id for the broadcast")
+        _ <- extractLaoChannel(rpcMessage, "Server failed to extract LAO id for the broadcast")
+        _ <- extractParameters[AddChirp](rpcMessage, "Server failed to extract chirp id for the broadcast")
         _ <- dbActor ? DbActor.WriteAndPropagate(rpcMessage.getParamsChannel, rpcMessage.getParamsMessage.get)
       } yield ()
 
     Await.ready(ask, duration).value match {
       case Some(Success(_)) =>
-        val channelChirp: Channel = rpcMessage.getParamsChannel
-        val lao_id: Hash = channelChirp.decodeChannelLaoId.get
-        val broadcastChannel: Channel = generateSocialChannel(lao_id)
-
-        val params: Message = rpcMessage.getParamsMessage.get
-        val chirp_id: Hash = params.message_id
-        val timestamp: Timestamp = params.decodedData.get.asInstanceOf[AddChirp].timestamp
-        val notifyAddChirp: NotifyAddChirp = NotifyAddChirp(chirp_id, channelChirp, timestamp)
-        val broadcastData: Base64Data = Base64Data.encode(notifyAddChirp.toJson.toString)
-
-        Await.result(dbBroadcast(rpcMessage, rpcMessage.getParamsChannel, broadcastData, broadcastChannel), duration)
-
+        val (chirp_id, channelChirp, data, broadcastChannel) = parametersToBroadcast[AddChirp](rpcMessage)
+        val notifyAddChirp: NotifyAddChirp = NotifyAddChirp(chirp_id, channelChirp, data.timestamp)
+        Await.result(dbBroadcast(rpcMessage, channelChirp, Base64Data.encode(notifyAddChirp.toJson.toString), broadcastChannel), duration)
       case Some(Failure(ex: DbActorNAckException)) => Right(PipelineError(ex.code, s"handleAddChirp failed : ${ex.message}", rpcMessage.getId))
       case _                                       => Right(PipelineError(ErrorCodes.SERVER_ERROR.id, unknownAnswerDatabase, rpcMessage.getId))
     }
@@ -70,25 +61,16 @@ class SocialMediaHandler(dbRef: => AskableActorRef) extends MessageHandler {
   def handleDeleteChirp(rpcMessage: JsonRpcRequest): GraphMessage = {
     val ask =
       for {
-        _ <- checkLaoChannel(rpcMessage, "Server failed to extract LAO id for the broadcast")
-        _ <- checkParameters(rpcMessage, "Server failed to extract chirp id for the broadcast")
+        _ <- extractLaoChannel(rpcMessage, "Server failed to extract LAO id for the broadcast")
+        _ <- extractParameters(rpcMessage, "Server failed to extract chirp id for the broadcast")
         _ <- dbActor ? DbActor.WriteAndPropagate(rpcMessage.getParamsChannel, rpcMessage.getParamsMessage.get)
       } yield ()
 
     Await.ready(ask, duration).value match {
       case Some(Success(_)) =>
-        val channelChirp: Channel = rpcMessage.getParamsChannel
-        val lao_id: Hash = channelChirp.decodeChannelLaoId.get
-        val broadcastChannel: Channel = generateSocialChannel(lao_id)
-
-        val params: Message = rpcMessage.getParamsMessage.get
-        val chirp_id: Hash = params.message_id
-        val timestamp: Timestamp = params.decodedData.get.asInstanceOf[DeleteChirp].timestamp
-        val notifyDeleteChirp: NotifyDeleteChirp = NotifyDeleteChirp(chirp_id, channelChirp, timestamp)
-        val broadcastData: Base64Data = Base64Data.encode(notifyDeleteChirp.toJson.toString)
-
-        Await.result(dbBroadcast(rpcMessage, rpcMessage.getParamsChannel, broadcastData, broadcastChannel), duration)
-
+        val (chirp_id, channelChirp, data, broadcastChannel) = parametersToBroadcast[DeleteChirp](rpcMessage)
+        val notifyDeleteChirp: NotifyDeleteChirp = NotifyDeleteChirp(chirp_id, channelChirp, data.timestamp)
+        Await.result(dbBroadcast(rpcMessage, channelChirp, Base64Data.encode(notifyDeleteChirp.toJson.toString), broadcastChannel), duration)
       case Some(Failure(ex: DbActorNAckException)) => Right(PipelineError(ex.code, s"handleDeleteChirp failed : ${ex.message}", rpcMessage.getId))
       case _                                       => Right(PipelineError(ErrorCodes.SERVER_ERROR.id, unknownAnswerDatabase, rpcMessage.getId))
     }
@@ -111,5 +93,17 @@ class SocialMediaHandler(dbRef: => AskableActorRef) extends MessageHandler {
   def handleDeleteReaction(rpcMessage: JsonRpcRequest): GraphMessage = {
     val ask: Future[GraphMessage] = dbAskWritePropagate(rpcMessage)
     Await.result(ask, duration)
+  }
+
+  // generates the parameters that will be used to broadcast the chirps
+  private def parametersToBroadcast[T](rpcMessage: JsonRpcRequest): (Hash, Channel, T, Channel) = {
+    val channelChirp: Channel = rpcMessage.getParamsChannel
+    val lao_id: Hash = channelChirp.decodeChannelLaoId.get
+    val broadcastChannel: Channel = generateSocialChannel(lao_id)
+    val params: Message = rpcMessage.getParamsMessage.get
+    val chirp_id: Hash = params.message_id
+    val data: T = params.decodedData.get.asInstanceOf[T]
+
+    (chirp_id, channelChirp, data, broadcastChannel)
   }
 }
