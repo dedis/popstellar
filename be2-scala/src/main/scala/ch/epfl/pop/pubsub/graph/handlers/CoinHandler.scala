84fde3a33807a107ec9db679585c5e261e82f242
Ajkunas <ajkuna.seipi@epfl.ch>
2022-11-27 22:32:35 +0100

    create meeting refactoring and coin handler refactoring

--- be2-scala/src/main/scala/ch/epfl/pop/pubsub/graph/handlers/CoinHandler.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/pubsub/graph/handlers/CoinHandler.scala
@@ -1,29 +1,27 @@
 package ch.epfl.pop.pubsub.graph.handlers
 
 import ch.epfl.pop.model.network.JsonRpcRequest
-import ch.epfl.pop.model.network.method.message.Message
 import ch.epfl.pop.model.objects.DbActorNAckException
 import ch.epfl.pop.pubsub.graph.{ErrorCodes, GraphMessage, PipelineError}
-import ch.epfl.pop.storage.DbActor
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.{Await, Future}
+import scala.concurrent.{Await}
 import scala.util.{Failure, Success}
 
 case object CoinHandler extends MessageHandler {
 
   def handlePostTransaction(rpcMessage: JsonRpcRequest): GraphMessage = {
-    val ask =
+    val ask = {
       for {
         _ <- checkParameters(rpcMessage, s"Unable to handle coin message $rpcMessage. Not a post message")
-        message: Message = rpcMessage.getParamsMessage.get
-        _ <- dbActor ? DbActor.WriteAndPropagate(rpcMessage.getParamsChannel, message)
+        _ <- dbAskWritePropagate(rpcMessage)
       } yield ()
+    }
 
-      Await.ready(ask, duration).value match {
-        case Some(Success(_))                        => Left(rpcMessage)
-        case Some(Failure(ex: DbActorNAckException)) => Right(PipelineError(ex.code, s"handlePostTransaction failed : ${ex.message}", rpcMessage.getId))
-        case reply                                   => Right(PipelineError(ErrorCodes.SERVER_ERROR.id, s"handlePostTransaction failed : unexpected DbActor reply '$reply'", rpcMessage.getId))
-      }
+    Await.ready(ask, duration).value match {
+      case Some(Success(_))                        => Left(rpcMessage)
+      case Some(Failure(ex: DbActorNAckException)) => Right(PipelineError(ex.code, s"handlePostTransaction failed : ${ex.message}", rpcMessage.getId))
+      case reply                                   => Right(PipelineError(ErrorCodes.SERVER_ERROR.id, s"handlePostTransaction failed : unexpected DbActor reply '$reply'", rpcMessage.getId))
+    }
   }
 }
--- be2-scala/src/main/scala/ch/epfl/pop/pubsub/graph/handlers/MeetingHandler.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/pubsub/graph/handlers/MeetingHandler.scala
@@ -1,22 +1,27 @@
 package ch.epfl.pop.pubsub.graph.handlers
 
 import ch.epfl.pop.model.network.JsonRpcRequest
+import ch.epfl.pop.model.objects.DbActorNAckException
 import ch.epfl.pop.pubsub.graph.{ErrorCodes, GraphMessage, PipelineError}
 
-import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{Await}
+import scala.util.{Failure, Success}
 
 case object MeetingHandler extends MessageHandler {
 
   def handleCreateMeeting(rpcMessage: JsonRpcRequest): GraphMessage = {
-    rpcMessage.getParamsChannel.decodeChannelLaoId match {
-      case Some(_) =>
-        val ask: Future[GraphMessage] = dbAskWritePropagate(rpcMessage)
-        Await.result(ask, duration)
-      case _ => Right(PipelineError(
-          ErrorCodes.INVALID_DATA.id,
-          s"Unable to create meeting: invalid encoded laoId '${rpcMessage.getParamsChannel}'",
-          rpcMessage.id
-        ))
+    val ask = {
+      for {
+        _ <- checkParameters(rpcMessage, s"Unable to handle coin message $rpcMessage. Not a post message")
+        _ <- dbAskWritePropagate(rpcMessage)
+      } yield ()
+    }
+
+    Await.ready(ask, duration).value match {
+      case Some(Success(_))                        => Left(rpcMessage)
+      case Some(Failure(ex: DbActorNAckException)) => Right(PipelineError(ex.code, s"Unable to create meeting: invalid encoded laoId '${rpcMessage.getParamsChannel}'", rpcMessage.getId))
+      case reply                                   => Right(PipelineError(ErrorCodes.SERVER_ERROR.id, s"handleCreateMeeting failed : unexpected DbActor reply '$reply'", rpcMessage.getId))
     }
   }
 
