4b72bd2a8be482644ccfbfa9002fd7b6bc7c4136
Johann Pluss <johann.pluss@epfl.ch>
2022-12-24 17:01:22 +0100

    Add DigitalCashRepository
    Signed-off-by: Johann Plüss <pluss.j2407@gmail.com>

--- fe2-android/app/src/main/java/com/github/dedis/popstellar/repository/DigitalCashRepository.java
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/repository/DigitalCashRepository.java
@@ -1,12 +1,12 @@
 package com.github.dedis.popstellar.repository;
 
-import android.util.Log;
-
+import com.github.dedis.popstellar.model.objects.OutputObject;
 import com.github.dedis.popstellar.model.objects.digitalcash.TransactionObject;
-import com.github.dedis.popstellar.model.objects.security.MessageID;
 import com.github.dedis.popstellar.model.objects.security.PublicKey;
+import com.github.dedis.popstellar.utility.error.keys.NoRollCallException;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
@@ -15,37 +15,79 @@ public class DigitalCashRepository {
   public static final String TAG = DigitalCashRepository.class.getSimpleName();
 
   private final Map<String, LaoTransactions> transactionsByLao = new HashMap<>();
-  private final Map<String, LaoTransactions> transactionHistoryByLao = new HashMap<>();
-  private final Map<String, PublicKey> publicKeyByHash = new HashMap<>();
 
   @Inject
   public DigitalCashRepository() {
     // Constructor required by Hilt
   }
 
+  private List<TransactionObject> getTransactions(String laoId, PublicKey user) {
+    return getLaoTransactions(laoId).getTransactions(user);
+  }
+
+  public void updateTransactions(String laoId, TransactionObject transaction)
+      throws NoRollCallException {
+    getLaoTransactions(laoId).updateTransactions(transaction);
+  }
+
+  public void initializeDigitalCash(String laoId, List<PublicKey> attendees) {
+    getLaoTransactions(laoId).initializeDigitalCash(attendees);
+  }
+
   private synchronized LaoTransactions getLaoTransactions(String laoId) {
-    // Create the lao chirps object if it is not present yet
-    return transactionsByLao.computeIfAbsent(laoId, id -> new LaoTransactions());
-  }
-
-  private synchronized LaoTransactions getLaoTransactionHistory(String laoId){
-    // Create the lao chirps object if it is not present yet
-    return transactionHistoryByLao.computeIfAbsent(laoId, id -> new LaoTransactions());
-  }
-
-  private synchronized TransactionObject getTransaction(String laoId, MessageID id){
-    return getLaoTransactions(laoId)
-  }
-
-  public void addTransaction(String laoId, TransactionObject transaction) {
-     Log.d(TAG, )
+    // Create the lao transactions object if it is not present yet
+    return transactionsByLao.computeIfAbsent(laoId, lao -> new LaoTransactions());
   }
 
   private static final class LaoTransactions {
-    private final Map<PublicKey, Set<TransactionObject>> transactionsByUser = new HashMap<>();
 
-    public synchronized void add(PublicKey user, TransactionObject transaction){
-      transactionsByUser.computeIfAbsent(user)
+    private final Map<PublicKey, List<TransactionObject>> transactionsByUser = new HashMap<>();
+    private final Map<String, PublicKey> hashDictionary = new HashMap<>();
+
+    /**
+     * This resets the digital cash state. This should be called each time a rc is closed
+     *
+     * @param attendees the attendees of the closed roll call
+     */
+    public synchronized void initializeDigitalCash(List<PublicKey> attendees) {
+      transactionsByUser.clear();
+      hashDictionary.clear();
+      attendees.forEach(publicKey -> hashDictionary.put(publicKey.computeHash(), publicKey));
+    }
+
+    public synchronized void updateTransactions(TransactionObject transaction)
+        throws NoRollCallException {
+      if (hashDictionary.isEmpty()) {
+        throw new NoRollCallException("No roll call attendees could be found");
+      }
+      for (PublicKey current : getReceiversTransaction(transaction)) {
+        List<TransactionObject> transactions =
+            transactionsByUser.getOrDefault(current, new ArrayList<>());
+        if (!transactions.contains(transaction)) {
+          transactions.add(transaction);
+        }
+        transactionsByUser.put(current, new ArrayList<>(transactions));
+      }
+    }
+
+    public List<TransactionObject> getTransactions(PublicKey user) {
+      return new ArrayList<>(transactionsByUser.get(user));
+    }
+
+    private List<PublicKey> getReceiversTransaction(TransactionObject transaction) {
+      // For each output, we get the hash of the public key. Then for each hash we return the
+      // preimage (the public key)
+      return transaction.getOutputs().stream()
+          .map(OutputObject::getPubKeyHash)
+          .map(
+              hash -> {
+                PublicKey key = hashDictionary.get(hash);
+                if (key == null) {
+                  throw new IllegalStateException("The hash is not in dictionary of known hashes");
+                }
+                return key;
+              })
+          .collect(Collectors.toList());
     }
   }
 }
