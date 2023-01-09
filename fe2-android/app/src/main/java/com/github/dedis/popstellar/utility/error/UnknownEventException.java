b5067642654026cb97cd8063d989cbca62d07b2b
GabrielFleischer <gabriel.fleischer@epfl.ch>
2023-01-09 15:51:59 +0100

    Fix codesmells
    Signed-off-by: GabrielFleischer <gabriel.fleischer@epfl.ch>

--- fe2-android/app/src/main/java/com/github/dedis/popstellar/utility/error/UnknownEventException.java
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/utility/error/UnknownEventException.java
@@ -2,7 +2,7 @@ package com.github.dedis.popstellar.utility.error;
 
 public abstract class UnknownEventException extends GenericException {
 
-  public UnknownEventException(String eventType, String id) {
+  protected UnknownEventException(String eventType, String id) {
     super(eventType + " with id " + id + " is unknown.");
   }
 }
