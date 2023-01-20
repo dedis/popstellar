e6ffeaaba3d079e85237b6423d0e5cd267e6fbba
Noémien Kocher <nkcr.je@gmail.com>
2023-01-20 17:35:37 +0100

    Adds a unit test

--- be1-go/network/server_test.go
+++ be1-go/network/server_test.go
@@ -2,6 +2,8 @@ package network
 
 import (
 	"io"
+	"net/http"
+	"net/http/httptest"
 	"popstellar/crypto"
 	"popstellar/hub"
 	"popstellar/hub/standard_hub"
@@ -24,4 +26,38 @@ func TestServerStartAndShutdown(t *testing.T) {
 	err = srv.Shutdown()
 	require.NoError(t, err)
 	<-srv.Stopped
+}
+
+func TestInfoHandler(t *testing.T) {
+	s := Server{
+		hub: fakeHub{},
+		st:  "fake",
+	}
+
+	rr := httptest.NewRecorder()
+	s.infoHandler(rr, &http.Request{Method: http.MethodGet})
+
+	res, err := io.ReadAll(rr.Body)
+	require.NoError(t, err)
+
+	expected := `{
+	"version": "unknown",
+	"commit": "unknown",
+	"buildTime": "unknown",
+	"hubType": "fake",
+	"socketType": "fake"
+}`
+
+	require.Equal(t, expected, string(res))
+}
+
+// -----------------------------------------------------------------------------
+// Utility functions
+
+type fakeHub struct {
+	hub.Hub
+}
+
+func (fakeHub) Type() hub.HubType {
+	return "fake"
 }
