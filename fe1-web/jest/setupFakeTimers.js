828f6f1251bb3a00df36bac1dc739a9632897b05
Nico Hauser <me@tyratox.ch>
2023-02-10 18:11:04 +0100

    Fix failing tests

--- fe1-web/jest/setupFakeTimers.js
+++ fe1-web/jest/setupFakeTimers.js
@@ -1,24 +1,7 @@
-import FakeTimers from '@sinonjs/fake-timers';
-
-/**
- * Sets up fake timers for tests.
- *
- * @remarks
- * We use a library because there is an issue between jest.useFakeTimers and asynchronous tests.
- * (see https://github.com/facebook/jest/issues/10221)
- */
+import { jest } from '@jest/globals';
 
 const FIXED_SYSTEM_TIME = new Date(1620255600 * 1000); // 5 May 2021
 
-let clock;
 global.beforeAll(() => {
-  clock = FakeTimers.install({
-    now: FIXED_SYSTEM_TIME,
-    shouldAdvanceTime: true,
-    toFake: ['Date'],
-  });
-});
-
-global.afterAll(() => {
-  clock.uninstall();
+  jest.useFakeTimers({ advanceTimers: true, now: FIXED_SYSTEM_TIME });
 });
