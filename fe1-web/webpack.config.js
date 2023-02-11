98626ae54665875d700734c42521dd4faeb92677
Nico Hauser <me@tyratox.ch>
2023-02-11 10:55:17 +0100

    Add comment regarding #1244

--- fe1-web/webpack.config.js
+++ fe1-web/webpack.config.js
@@ -13,7 +13,9 @@ async function configAsync(env, argv) {
           // This is a workaround for https://github.com/react-native-elements/react-native-elements/issues/3607
           // to get the new version of react-native-elements working
           // Revert this commit as soon as the new version is properly released and this
-          // issue is resolved
+          // issue is resolved.
+          // Update (2023-02-11, Tyratox): ^4.0.0-rc.7 fixes this issue but some other issue causes the icons
+          // to not render properly, hence I had to revert the changes in 203194cc9ac6126fbbe03aae3efa69740ff15e7a
           dangerouslyAddModulePathsToTranspile: ['@rneui/base', '@rneui/themed'],
       },
   }, argv);
