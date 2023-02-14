2bdef00cfd2014896c8a7f0680ed3716c7a54fdf
Johann Pluss <johann.pluss@epfl.ch>
2023-02-14 12:01:16 +0100

    Slight improvement

--- fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/digitalcash/DigitalCashActivity.java
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/digitalcash/DigitalCashActivity.java
@@ -4,11 +4,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.*;
 import android.widget.Toast;
 
 import androidx.annotation.IdRes;
-import androidx.annotation.NonNull;
 import androidx.fragment.app.*;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -37,7 +35,7 @@ public class DigitalCashActivity extends LaoActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    com.github.dedis.popstellar.databinding.DigitalCashMainActivityBinding binding =
+    DigitalCashMainActivityBinding binding =
         DigitalCashMainActivityBinding.inflate(getLayoutInflater());
     setContentView(binding.getRoot());
     laoViewModel = viewModel = obtainViewModel(this);
@@ -83,25 +81,6 @@ public class DigitalCashActivity extends LaoActivity {
 
   public static DigitalCashViewModel obtainViewModel(FragmentActivity activity) {
     return new ViewModelProvider(activity).get(DigitalCashViewModel.class);
-  }
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    MenuInflater inflater = getMenuInflater();
-    inflater.inflate(R.menu.digital_cash_menu, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-    if (item.getItemId() == R.id.history_menu_toolbar) {
-      setCurrentFragment(
-          getSupportFragmentManager(),
-          R.id.fragment_digital_cash_history,
-          DigitalCashHistoryFragment::newInstance);
-      return true;
-    }
-    return super.onOptionsItemSelected(item);
   }
 
   @Override
