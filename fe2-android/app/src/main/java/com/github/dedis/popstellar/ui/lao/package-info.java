55f767e4362e50f769e2c6b159662c44060a61d9
Johann Pluss <johann.pluss@epfl.ch>
2023-02-15 17:26:18 +0100

    Update lao package info

--- fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/lao/package-info.java
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/lao/package-info.java
@@ -1,10 +1,10 @@
 /**
  * The package detail represents the LAO-specific view.
  *
- * <p>The package contains one activity, one view model and the necessary adapters and listeners for
- * the view. The view model prepares and manages the data used by the activity and the fragments.
+ * <p>The package contains one activity, several view models which are roughly feature separated,
+ * and the necessary adapters and listeners for the views.
  *
- * <p>The fragments represent the event-related UIs (creation, details, etc.) and the token
- * information UI (wallet and identity QR code).
+ * <p>The fragments represent screens with every LAO related feature (e.g. events, social media,
+ * digital cash)
  */
 package com.github.dedis.popstellar.ui.lao;
