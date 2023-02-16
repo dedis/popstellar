e65673fca6d7fa03d60d60dd6724dccf8c33e0c9
Johann Pluss <johann.pluss@epfl.ch>
2023-02-16 13:08:55 +0100

    Improve redundancy for Events

--- fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/lao/event/UpcomingEventsFragment.java
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/lao/event/UpcomingEventsFragment.java
@@ -55,17 +55,15 @@ public class UpcomingEventsFragment extends Fragment {
   }
 
   private void handleBackNav() {
-    requireActivity()
-        .getOnBackPressedDispatcher()
-        .addCallback(
-            getViewLifecycleOwner(),
-            new OnBackPressedCallback(true) {
-              @Override
-              public void handleOnBackPressed() {
-                Log.d(TAG, "Back pressed, going to event list");
-                LaoActivity.setCurrentFragment(
-                    getParentFragmentManager(), R.id.fragment_event_list, EventListFragment::new);
-              }
-            });
+    LaoActivity.addBackNavigationInstruction(
+        requireActivity(),
+        getViewLifecycleOwner(),
+        new OnBackPressedCallback(true) {
+          @Override
+          public void handleOnBackPressed() {
+            Log.d(TAG, "Back pressed, going to event list");
+            EventListFragment.openFragment(getParentFragmentManager());
+          }
+        });
   }
 }
