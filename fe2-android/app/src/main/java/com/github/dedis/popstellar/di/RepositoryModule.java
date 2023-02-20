dec0ab6bc1542cee34719a0ac3054876b508001f
Johann Pluss <johann.pluss@epfl.ch>
2023-02-20 13:52:55 +0100

    Remove unused class

--- fe2-android/app/src/main/java/com/github/dedis/popstellar/di/RepositoryModule.java
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/di/RepositoryModule.java
@@ -1,16 +1,10 @@
 package com.github.dedis.popstellar.di;
 
-import android.app.Application;
-
-import androidx.room.Room;
-
-import com.github.dedis.popstellar.repository.local.LAODatabase;
 import com.github.dedis.popstellar.utility.scheduler.ProdSchedulerProvider;
 import com.github.dedis.popstellar.utility.scheduler.SchedulerProvider;
 
-import javax.inject.Singleton;
-
-import dagger.*;
+import dagger.Binds;
+import dagger.Module;
 import dagger.hilt.InstallIn;
 import dagger.hilt.components.SingletonComponent;
 
@@ -21,14 +15,6 @@ public abstract class RepositoryModule {
   private static final String DATABASE_NAME = "lao_database";
 
   private RepositoryModule() {}
-
-  @Provides
-  @Singleton
-  public static LAODatabase provideDatabase(Application application) {
-    return Room.databaseBuilder(
-            application.getApplicationContext(), LAODatabase.class, DATABASE_NAME)
-        .build();
-  }
 
   @Binds
   public abstract SchedulerProvider bindsSchedulerProvider(
