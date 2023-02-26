93dd9b4b7c8fb25214d1081140d0fe1b6aa0ec71
lauener <kilian.lauener@epfl.ch>
2023-02-26 17:32:03 +0100

    refactors validateCreateLao

--- be2-scala/src/main/scala/ch/epfl/pop/pubsub/graph/validators/LaoValidator.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/pubsub/graph/validators/LaoValidator.scala
@@ -1,10 +1,11 @@
 package ch.epfl.pop.pubsub.graph.validators
 
-import ch.epfl.pop.model.network.JsonRpcRequest
+import akka.pattern.AskableActorRef
+import ch.epfl.pop.model.network.{JsonRpcMessage, JsonRpcRequest}
 import ch.epfl.pop.model.network.method.message.Message
 import ch.epfl.pop.model.network.method.message.data.ObjectType
 import ch.epfl.pop.model.network.method.message.data.lao.{CreateLao, GreetLao, StateLao, UpdateLao}
-import ch.epfl.pop.model.objects.{Channel, DbActorNAckException, Hash}
+import ch.epfl.pop.model.objects.{Channel, DbActorNAckException, Hash, PublicKey}
 import ch.epfl.pop.pubsub.graph.validators.MessageValidator._
 import ch.epfl.pop.pubsub.graph.{ErrorCodes, GraphMessage, PipelineError}
 import ch.epfl.pop.storage.DbActor
@@ -12,34 +13,142 @@ import ch.epfl.pop.storage.DbActor
 import scala.concurrent._
 import scala.util.{Failure, Success}
 
-case object LaoValidator extends MessageDataContentValidator {
-  def validateCreateLao(rpcMessage: JsonRpcRequest): GraphMessage = {
-    def validationError(reason: String): PipelineError = super.validationError(reason, "CreateLao", rpcMessage.id)
+object LaoValidator extends MessageDataContentValidator {
 
-    rpcMessage.getParamsMessage match {
-      case Some(message: Message) =>
-        val data: CreateLao = message.decodedData.get.asInstanceOf[CreateLao]
-        val expectedHash: Hash = Hash.fromStrings(data.organizer.base64Data.toString, data.creation.toString, data.name)
+  val laoValidator = new LaoValidator(DbActor.getInstance)
 
-        val channel: Channel = rpcMessage.getParamsChannel
+  def validateCreateLao(rpcMessage: JsonRpcRequest): GraphMessage = laoValidator.validateCreateLao(rpcMessage)
 
-        if (!validateTimestampStaleness(data.creation)) {
-          Right(validationError(s"stale 'creation' timestamp (${data.creation})"))
-        } else if (!validateWitnesses(data.witnesses)) {
-          Right(validationError("duplicate witnesses keys"))
-        } else if (expectedHash != data.id) {
-          Right(validationError("unexpected id"))
-        } else if (data.organizer != message.sender) {
-          Right(validationError("unexpected organizer public key"))
-        } else if (channel != Channel.ROOT_CHANNEL) {
-          Right(validationError(s"trying to send a CreateLao message on a channel $channel other than ${Channel.ROOT_CHANNEL}"))
-        } else if (data.name == "") {
-          Right(validationError(s"LAO name must not be empty"))
-        } else {
-          Left(rpcMessage)
-        }
-      case _ => Right(validationErrorNoMessage(rpcMessage.id))
+  sealed class LaoValidator(dbActorRef: => AskableActorRef) extends MessageDataContentValidator {
+
+    def validateCreateLao(rpcMessage: JsonRpcRequest): GraphMessage = {
+      def validationError(reason: String): PipelineError = super.validationError(reason, "CreateLao", rpcMessage.id)
+
+      rpcMessage.getParamsMessage match {
+        case Some(message: Message) =>
+          val (data, hash, sender, channel) = extractData[CreateLao](rpcMessage)
+          val expectedHash: Hash = Hash.fromStrings( // needs Checking in docs or protocol.
+            data.organizer.base64Data.toString,
+            data.creation.toString,
+            data.name
+          )
+
+          runChecks(
+            checkTimestampStaleness(
+              rpcMessage,
+              data.creation,
+              validationError(s"stale 'creation' timestamp (${data.creation})")
+            ),
+            checkWitnesses(
+              rpcMessage,
+              data.witnesses,
+              validationError("duplicate witnesses keys")
+            ),
+            checkId(
+              rpcMessage,
+              expectedHash,
+              data.id,
+              validationError("unexpected id")
+            ),
+            checkMsgSenderKey(
+              rpcMessage,
+              data.organizer,
+              sender,
+              validationError("unexpected organizer public key")
+            ),
+            checkChannel(
+              rpcMessage,
+              channel,
+              Channel.ROOT_CHANNEL,
+              validationError(s"trying to send a CreateLao message on a channel $channel other than ${Channel.ROOT_CHANNEL}")
+            ),
+            checkLAOName(
+              rpcMessage,
+              data.name,
+              validationError("LAO name must not be empty")
+            )
+          )
+        case _ => Right(validationErrorNoMessage(rpcMessage.id))
+      }
+
     }
+
+    /** Check if all witnesses are distinct
+      *
+      * @param rpcMessage
+      *   rpc message to validate
+      * @param witnesses
+      *   witnesses to check
+      * @param error
+      *   the error to forward in case the witnesses are not all distinct
+      * @return
+      *   GraphMessage: passes the rpcMessages to Left if successful right with pipeline error
+      */
+    private def checkWitnesses(rpcMessage: JsonRpcMessage, witnesses: List[PublicKey], error: PipelineError): GraphMessage = {
+      if (validateWitnesses(witnesses))
+        Left(rpcMessage)
+      else
+        Right(error)
+    }
+
+    /** Checks if the msg sender is the expected one
+      *
+      * @param rpcMessage
+      *   rpc message to validate
+      * @param expectedKey
+      *   the expected key
+      * @param msgSenderKey
+      *   the rpc message sender
+      * @param error
+      *   the error to forward in case the sender doesn't match the expected one
+      * @return
+      *   GraphMessage: passes the rpcMessages to Left if successful right with pipeline error
+      */
+    private def checkMsgSenderKey(rpcMessage: JsonRpcMessage, expectedKey: PublicKey, msgSenderKey: PublicKey, error: PipelineError): GraphMessage = {
+      if (expectedKey == msgSenderKey)
+        Left(rpcMessage)
+      else
+        Right(error)
+    }
+
+    /** Check for chan1 and chan2 equality
+      *
+      * @param rpcMessage
+      *   rpc message to validate
+      * @param chan1
+      *   the first channel to compare
+      * @param chan2
+      *   the second channel to compare
+      * @param error
+      *   the error to forward in case the channels are not equals
+      * @return
+      *   GraphMessage: passes the rpcMessages to Left if successful right with pipeline error
+      */
+    private def checkChannel(rpcMessage: JsonRpcMessage, chan1: Channel, chan2: Channel, error: PipelineError): GraphMessage = {
+      if (chan1 == chan2)
+        Left(rpcMessage)
+      else
+        Right(error)
+    }
+
+    /** Check if the LAO's name is empty
+      *
+      * @param rpcMessage
+      *   rpc message to validate
+      * @param name
+      *   the name to check
+      * @param error
+      *   the error to forward in case the name si empty
+      * @return
+      *   GraphMessage: passes the rpcMessages to Left if successful right with pipeline error
+      */
+    private def checkLAOName(rpcMessage: JsonRpcMessage, name: String, error: PipelineError): GraphMessage = {
+      if (name.nonEmpty)
+        Left(rpcMessage)
+      else
+        Right(error)
+    }
+
   }
 
   def validateStateLao(rpcMessage: JsonRpcRequest): GraphMessage = {
