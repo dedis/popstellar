08a193c3b1335775d2be5f53c075814f6421b891
BelghmiAmine <amine.belghmi@epfl.ch>
2023-03-11 22:32:00 +0100

    canceled refactoring of checkSumOutputs, made code more readable and deleted unused import statements.

--- be2-scala/src/main/scala/ch/epfl/pop/pubsub/graph/validators/CoinValidator.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/pubsub/graph/validators/CoinValidator.scala
@@ -2,18 +2,14 @@ package ch.epfl.pop.pubsub.graph.validators
 
 import ch.epfl.pop.model.network.JsonRpcRequest
 import ch.epfl.pop.model.network.method.message.Message
-import ch.epfl.pop.model.network.method.message.data.ObjectType
 import ch.epfl.pop.model.network.method.message.data.coin.PostTransaction
-import ch.epfl.pop.model.objects.{Channel, Hash, Transaction}
+import ch.epfl.pop.model.objects.Transaction
 import ch.epfl.pop.pubsub.graph.validators.MessageValidator._
 import ch.epfl.pop.pubsub.graph.{GraphMessage, PipelineError}
-
-import scala.concurrent._
 
 case object CoinValidator extends MessageDataContentValidator {
   def validatePostTransaction(rpcMessage: JsonRpcRequest): GraphMessage = {
     def validationError(reason: String): PipelineError = super.validationError(reason, "PostTransaction", rpcMessage.id)
-
     rpcMessage.getParamsMessage match {
       case Some(message: Message) =>
         val data: PostTransaction = message.decodedData.get.asInstanceOf[PostTransaction]
@@ -56,12 +52,10 @@ case object CoinValidator extends MessageDataContentValidator {
     */
   private def checkSumOutputs(rpcMessage: JsonRpcRequest, transaction: Transaction, error: PipelineError): GraphMessage = {
     def validationError(reason: String): PipelineError = super.validationError(reason, "PostTransaction", rpcMessage.id)
-    val sumOutput = transaction.sumOutputs()
-    if (sumOutput.isLeft) {
-      val err = sumOutput.swap.getOrElse(error)
-      Right(validationError(err.asInstanceOf[Error].getMessage))
-    } else
-      Left(rpcMessage)
+    transaction.sumOutputs() match {
+      case Left(err) => Right(validationError(err.getMessage))
+      case Right(_)  => Left(rpcMessage)
+    }
   }
 
 }
