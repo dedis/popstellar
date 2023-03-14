35bef26655e6c32ad64d1d4243b19c1c59a3aaf2
matteosz <mattsuez01@gmail.com>
2023-03-14 16:01:41 +0100

    backnav exit app test

--- fe2-android/app/src/test/ui/robolectric/com/github/dedis/popstellar/ui/home/HomeActivityTest.java
+++ fe2-android/app/src/test/ui/robolectric/com/github/dedis/popstellar/ui/home/HomeActivityTest.java
@@ -1,7 +1,13 @@
 package com.github.dedis.popstellar.ui.home;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.SystemClock;
+
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -9,11 +15,14 @@ import org.junit.rules.RuleChain;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnit;
 
+import java.util.List;
+
 import dagger.hilt.android.testing.HiltAndroidRule;
 import dagger.hilt.android.testing.HiltAndroidTest;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.pressBack;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
 import static com.github.dedis.popstellar.testutils.UITestUtils.*;
@@ -22,6 +31,7 @@ import static com.github.dedis.popstellar.testutils.pages.home.LaoCreatePageObje
 import static com.github.dedis.popstellar.testutils.pages.home.WalletPageObject.confirmButton;
 import static com.github.dedis.popstellar.testutils.pages.home.WalletPageObject.walletId;
 import static org.hamcrest.Matchers.allOf;
+import static org.junit.Assert.assertTrue;
 
 @HiltAndroidTest
 @RunWith(AndroidJUnit4.class)
@@ -69,6 +79,29 @@ public class HomeActivityTest {
     clearDataMenuItem().perform(click());
     assertThat(dialogPositiveButton(), allOf(withText("YES"), isDisplayed()));
     assertThat(dialogNegativeButton(), allOf(withText("NO"), isDisplayed()));
+  }
+
+  @Test
+  public void testMoveAppToBackground() {
+    // Launch HomeActivity
+    ActivityScenario<HomeActivity> scenario = activityScenarioRule.getScenario();
+
+    // Press back button
+    onView(isRoot()).perform(pressBack());
+
+    // Wait for 1 second
+    SystemClock.sleep(1000);
+
+    // Get the list of running tasks
+    ActivityManager activityManager =
+        (ActivityManager)
+            InstrumentationRegistry.getInstrumentation()
+                .getTargetContext()
+                .getSystemService(Context.ACTIVITY_SERVICE);
+    List<ActivityManager.AppTask> appTasks = activityManager.getAppTasks();
+
+    // Check that the task is moved to the back
+    assertTrue(appTasks.isEmpty());
   }
 
   public static void initializeWallet(
