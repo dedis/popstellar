dbae115fa1ec71b6d7d465999e161a8d35ec3b95
nazianze <victor.nazianzeno-lejamtel@epfl.ch>
2023-03-15 12:54:44 +0100

    Adding unit test for hash (including with emojis)

--- be1-go/message/test/messagedata/mod_test.go
+++ be1-go/message/test/messagedata/mod_test.go
@@ -50,3 +50,17 @@ func Test_GetTime(t *testing.T) {
 	t.Run("timestamp is missing", testWrongExamples("wrong_message_no_action.json"))
 	t.Run("json is invalid", testWrongExamples("wrong_message_invalid_json.json"))
 }
+
+func Test_Hash(t *testing.T) {
+	data1 := []string{"abcd", "1234"}
+	data2 := "test 😀"
+	data3 := "🫡"
+	data4 := []string{"text 🥰", "🏉", "more text🎃️", "♠️"}
+
+	require.Equal(t, messagedata.Hash(data1...), "61I7DQkiMtdHFM5VygjbFqrVmn4NAl0wSVxkj6Q5iDw=")
+	require.Equal(t, messagedata.Hash(), "47DEQpj8HBSa-_TImW-5JCeuQeRkm5NMpJWZG3hSuFU=")
+	require.Equal(t, messagedata.Hash(data2), "8BMmJjQMPhtD0QwVor1uVB3B_PyMMyIbIvaDHcOQnTg=")
+	require.Equal(t, messagedata.Hash(data3), "ht7cQAkPdd6o-ZFVW6gTbt0gEIEUcr5FTDgOaeW8BOU=")
+	require.Equal(t, messagedata.Hash(data4...), "wANKJFj9q_ncRKalYmK4yozUpet33JaFXVQEpMcHdfU=")
+
+}
