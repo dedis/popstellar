43c5f92161d360ea35d75e8a31fca44530ad68a6
BelghmiAmine <amine.belghmi@epfl.ch>
2023-03-15 01:59:02 +0100

    formatted the code.

--- be2-scala/src/main/scala/ch/epfl/pop/pubsub/graph/validators/CoinValidator.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/pubsub/graph/validators/CoinValidator.scala
@@ -15,16 +15,22 @@ case object CoinValidator extends MessageDataContentValidator {
         val data: PostTransaction = message.decodedData.get.asInstanceOf[PostTransaction]
 
         runChecks(
-          checkId(rpcMessage,
+          checkId(
+            rpcMessage,
             data.transactionId,
             data.transaction.transactionId,
-            validationError("incorrect transaction id")),
-          checkTransactionSignatures(rpcMessage,
+            validationError("incorrect transaction id")
+          ),
+          checkTransactionSignatures(
+            rpcMessage,
             data.transaction,
-            validationError("bad signature")),
-          checkSumOutputs(rpcMessage,
+            validationError("bad signature")
+          ),
+          checkSumOutputs(
+            rpcMessage,
             data.transaction,
-            s => validationError(s))
+            s => validationError(s)
+          )
         )
 
       case _ => Right(validationErrorNoMessage(rpcMessage.id))
