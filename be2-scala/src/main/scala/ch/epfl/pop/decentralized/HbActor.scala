c55436551b58247588f8356cf095fbf77aa7507e
BelghmiAmine <amine.belghmi@epfl.ch>
2023-03-26 18:52:20 +0200

    version for handling retrieveHeartBeatMessages.

--- be2-scala/src/main/scala/ch/epfl/pop/decentralized/HbActor.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/decentralized/HbActor.scala
@@ -16,12 +16,13 @@ import akka.util.Timeout
 import akka.pattern.ask
 import ch.epfl.pop.pubsub.AskPatternConstants
 
+import scala.collection.mutable
 import scala.util.{Failure, Success, Try}
 import scala.collection.mutable.{HashMap, SortedSet}
 import scala.concurrent.Await
 
 final case class HbActor(
-    private val dbRef : ActorRef[DbActor]
+    private val dbRef : AskableActorRef
 ) extends Actor {
 
   implicit val timeout: Timeout = 3.seconds
@@ -32,6 +33,18 @@ final case class HbActor(
     case RetrieveHeartBeat() =>
       val ask = dbRef ? DbActor.GetSetOfChannels()
       val answer = Await.result(ask, duration)
+      val setOfChannels: Set[String] = answer.asInstanceOf[DbActor.DbActorGetSetOfChannelsAck].channels
+      var res : Map[String, List[Hash]] = Map()
+      setOfChannels.foreach(channel => {
+        val ask = dbRef ? DbActor.Catchup(Channel(channel))
+        val answer = Await.result(ask, duration)
+        val listOfIds : List[Hash] = answer.asInstanceOf[DbActor.DbActorCatchupAck].messages.map(message => message.message_id)
+        res += (channel,listOfIds)
+      })
+      sender() ! HbActorRetrieveHeartBeatAck(res)
+
+
+
   }
 }
 
@@ -53,5 +66,5 @@ object HbActor {
    * Receiving a [[HbActorRetrieveHeartBeatAck]] means that the retrieving of the HeartBeatContent was successfull.
    * @param heartBeatContent the content of the HeartBeat the server should send.
    */
-  final case class HbActorRetrieveHeartBeatAck(heartBeatContent : Map[Channel, List[String]])
+  final case class HbActorRetrieveHeartBeatAck(heartBeatContent : Map[String, List[Hash]])
 }
