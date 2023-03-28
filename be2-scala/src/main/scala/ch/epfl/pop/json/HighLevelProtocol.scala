f57532d1aa9c20a9112a669896925197df6c0780
lauener <kilian.lauener@epfl.ch>
2023-03-28 10:41:14 +0200

    Add new methods Read/write from/to Json support

--- be2-scala/src/main/scala/ch/epfl/pop/json/HighLevelProtocol.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/json/HighLevelProtocol.scala
@@ -8,9 +8,12 @@ import ch.epfl.pop.model.network.method.message.Message
 import ch.epfl.pop.model.objects._
 import spray.json._
 
-import scala.collection.immutable.ListMap
+import scala.collection.immutable.{HashMap, ListMap}
+import scala.collection.mutable
 
 object HighLevelProtocol extends DefaultJsonProtocol {
+
+  final private val PARAM_CHANNEL: String = "channel"
 
   // ----------------------------------- ENUM FORMATTERS ----------------------------------- //
   implicit object methodTypeFormat extends RootJsonFormat[MethodType] {
@@ -76,6 +79,46 @@ object HighLevelProtocol extends DefaultJsonProtocol {
     }
   }
 
+  implicit object ParamsWithMapFormat extends RootJsonFormat[ParamsWithMap] {
+
+    override def read(json: JsValue): ParamsWithMap = {
+      val map = mutable.HashMap[Channel, Set[Hash]]()
+      json.asJsObject.fields.foreach {
+        case (k: String, JsArray(v)) => map.put(Channel(k), v.map(_.convertTo[Hash]).toSet)
+        case _                       => throw new IllegalArgumentException(s"Unrecognizable value in $json")
+      }
+
+      new ParamsWithMap(HashMap.from(map))
+    }
+
+    override def write(obj: ParamsWithMap): JsValue = {
+      val map = mutable.HashMap[String, JsValue]()
+      obj.channelsToMessageIds.foreach {
+        case (channel, set) =>
+          map.put(channel.channel, set.toJson)
+      }
+
+      JsObject.apply(HashMap.from(map))
+    }
+  }
+
+  implicit object ParamsFormat extends RootJsonFormat[Params] {
+
+    override def read(json: JsValue): Params =
+      json.asJsObject.getFields(PARAM_CHANNEL) match {
+        case Seq(_) => ParamsWithChannelFormat.read(json)
+        // if no Seq can be retrieved, the json is of type ParamsWithMap or faulty
+        case _ => ParamsWithMapFormat.read(json)
+      }
+
+    override def write(obj: Params): JsValue =
+      obj match {
+        case paramsWithChannel: ParamsWithChannel => paramsWithChannel.toJson
+        case paramsWithMap: ParamsWithMap         => paramsWithMap.toJson
+      }
+
+  }
+
   implicit object BroadcastFormat extends RootJsonFormat[Broadcast] {
     override def read(json: JsValue): Broadcast = {
       val params: ParamsWithMessage = json.convertTo[ParamsWithChannel].asInstanceOf[ParamsWithMessage]
@@ -110,6 +153,20 @@ object HighLevelProtocol extends DefaultJsonProtocol {
     override def read(json: JsValue): Unsubscribe = Unsubscribe(json.convertTo[ParamsWithChannel].channel)
 
     override def write(obj: Unsubscribe): JsValue = obj.toJson(ParamsFormat.write)
+  }
+
+  implicit object HeartbeatFormat extends RootJsonFormat[Heartbeat] {
+    override def read(json: JsValue): Heartbeat =
+      Heartbeat(json.convertTo[ParamsWithMap].channelsToMessageIds)
+
+    override def write(obj: Heartbeat): JsValue = obj.toJson(ParamsWithMapFormat.write)
+  }
+
+  implicit object GetMessagesByIdFormat extends RootJsonFormat[GetMessagesById] {
+    override def read(json: JsValue): GetMessagesById =
+      GetMessagesById(json.convertTo[ParamsWithMap].channelsToMessageIds)
+
+    override def write(obj: GetMessagesById): JsValue = obj.toJson(ParamsWithMapFormat.write)
   }
 
   implicit val errorObjectFormat: JsonFormat[ErrorObject] = jsonFormat2(ErrorObject.apply)
@@ -155,12 +212,14 @@ object HighLevelProtocol extends DefaultJsonProtocol {
 
         JsonRpcRequest(version, method, params, id)
 
-      // Broadcast does not have an Id and should be treated separately
+      // Heartbeat, GetMessagesById and Broadcast do not have an Id and should be treated separately
       case Seq(JsString(version), methodJsString @ JsString(_), paramsJsObject @ JsObject(_)) =>
         val method: MethodType = methodJsString.convertTo[MethodType]
-        val params: Params = method match {
-          case MethodType.BROADCAST => paramsJsObject.convertTo[Broadcast]
-          case _                    => throw new IllegalArgumentException(s"Can't parse json value $json with unknown method ${method.toString}")
+        val params = method match {
+          case MethodType.HEARTBEAT          => paramsJsObject.convertTo[Heartbeat]
+          case MethodType.GET_MESSAGES_BY_ID => paramsJsObject.convertTo[GetMessagesById]
+          case MethodType.BROADCAST          => paramsJsObject.convertTo[Broadcast]
+          case _                             => throw new IllegalArgumentException(s"Can't parse json value $json with unknown method ${method.toString}")
         }
         JsonRpcRequest(version, method, params, None)
       case _ => throw new IllegalArgumentException(s"Can't parse json value $json to a JsonRpcRequest object")
