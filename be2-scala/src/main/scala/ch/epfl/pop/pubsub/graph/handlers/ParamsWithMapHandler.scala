1d55d79bc59eb7fce01c181d02daa094b1e30d07
BelghmiAmine <amine.belghmi@epfl.ch>
2023-04-01 16:18:43 +0200

    Add heartbeatHandler to support entering heartbeat messages.

--- be2-scala/src/main/scala/ch/epfl/pop/pubsub/graph/handlers/ParamsWithMapHandler.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/pubsub/graph/handlers/ParamsWithMapHandler.scala
@@ -1,17 +1,18 @@
 package ch.epfl.pop.pubsub.graph.handlers
 
 import akka.NotUsed
-import akka.actor.ActorRef
 import akka.pattern.AskableActorRef
-import akka.stream.FlowShape
 import akka.stream.scaladsl.{Flow, GraphDSL, Merge, Partition}
 import ch.epfl.pop.model.network.method.{Catchup, GetMessagesById, Heartbeat, Subscribe, Unsubscribe}
 import ch.epfl.pop.model.network.{JsonRpcRequest, JsonRpcResponse}
-import ch.epfl.pop.model.objects.Channel
+import ch.epfl.pop.model.objects.{Channel, Hash}
 import ch.epfl.pop.pubsub.graph.{ErrorCodes, GraphMessage, PipelineError}
 import ch.epfl.pop.pubsub.{AskPatternConstants, ClientActor, PubSubMediator}
+import ch.epfl.pop.storage.DbActor
+import ch.epfl.pop.model.network.MethodType
+import ch.epfl.pop.pubsub.graph.validators.RpcValidator
 
-import scala.concurrent.ExecutionContext.Implicits.global
+import scala.collection.mutable
 import scala.concurrent.{Await, Future}
 
 object ParamsWithMapHandler extends AskPatternConstants{
@@ -40,9 +41,35 @@ object ParamsWithMapHandler extends AskPatternConstants{
           }
         ))
 
+        val heartbeatHandler = builder.add(ParamsWithMapHandler.heartBeatHandler(dbActorRef))
+
+
         // ajouter les handlers
 
       }
+  }
+
+  def heartBeatHandler(dbActorRef: AskableActorRef): Flow[GraphMessage, GraphMessage, NotUsed] = Flow[GraphMessage].map ({
+    case Right(jsonRpcMessage: JsonRpcRequest) =>
+      val receivedHeartBeat: Map[Channel, Set[Hash]] = jsonRpcMessage.getParams.asInstanceOf[Heartbeat].channelsToMessageIds // pas sûr
+      val ask = dbActorRef ? DbActor.GetSetOfChannels()
+      val answer = Await.result(ask, duration)
+      val setOfChannels: Set[Channel] = answer.asInstanceOf[DbActor.DbActorGetSetOfChannelsAck].channels.map(s => Channel(s))
+      var localHeartBeat: mutable.HashMap[Channel, List[Hash]] = mutable.HashMap()
+      setOfChannels.foreach(channel => {
+        val ask = dbActorRef ? DbActor.ReadChannelData(channel)
+        val answer = Await.result(ask, duration)
+        val setOfIds = answer.asInstanceOf[DbActor.DbActorReadChannelDataAck].channelData.messages.to(collection.mutable.Set)
+        localHeartBeat += (channel -> setOfIds)
+      })
+      var missingIds: mutable.HashMap[Channel, Set[Hash]] = mutable.HashMap()
+      receivedHeartBeat.keys.foreach(channel =>{
+        if(localHeartBeat.contains(channel)){
+          missingIds += (channel -> receivedHeartBeat.get(channel).get.filter(id =>
+          !localHeartBeat.get(channel).contains(id)))
+        }
+      })
+      Right(JsonRpcRequest(RpcValidator.JSON_RPC_VERSION,MethodType.HEARTBEAT,Heartbeat(missingIds.toMap),None)) // how to pass the missing ids?? répondre par un getmsgsbyid et gérer les ids.
   })
 
 
