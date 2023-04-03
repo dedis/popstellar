32360c725b3ef28244079451fcd004a6ad875cf1
Michael Greub <michael.greub@epfl.ch>
2023-04-03 14:34:29 +0200

    Add the authentication redirection

--- example-third-party/server/authentication.py
+++ example-third-party/server/authentication.py
@@ -1,10 +1,11 @@
 import urllib.parse
+import secrets
 
 
-def getUrl(server: str, userToken: str, clientId: str) -> str :
-    baseUrl = "https://{}/authorize?response_type=id_token%20token&client_id={}&redirect_uri={}cb&" +\
-              "scope=openid%20profile&state={}nonce={}"
+def get_url(server: str, user_token: str, client_id: str) -> str :
+    baseUrl = "https://{server_url}/authorize?response_type=id_token%20token&client_id={id}" \
+              "&redirect_uri=https%3A%2F%2F{serverURL}%2Fcb&scope=openid%20profile&state={state}nonce={nonce}"
 
-    url = .format()
+    url = baseUrl.format(server_url = server, id = client_id ,state = user_token, nonce = secrets.token_urlsafe(32))
 
     return url
--- example-third-party/server/main.py
+++ example-third-party/server/main.py
@@ -1,14 +1,19 @@
 from typing import Optional, Any
-from flask import Flask, request
+from flask import Flask, request, redirect
+from authentication import get_url
 import json
 
-def on_startup():
-    global homePageHTML, providers
-    providers = json.loads(open("data/providers.json").read())
-    htmlProviders = [f'<option value="{str(i)}">{provider.get("laoId")}@{provider.get("url")}</option>' for i, provider in enumerate(providers)]
-    homePageHTML = open("model/index.html", "r").read().replace("<!-- Insert options -->", ''.join(htmlProviders))
 
-#Creates a subclass to be able to load some data on startup
+def on_startup():
+    global home_page_html, providers, config
+    providers = json.loads(open("data/providers.json").read())
+    config = json.loads(open("data/config.json").read())
+    htmlProviders = [f'<option value="{str(i)}">{provider.get("laoId")}@{provider.get("url")}</option>' for i, provider
+                     in enumerate(providers)]
+    home_page_html = open("model/index.html", "r").read().replace("<!-- Insert options -->", ''.join(htmlProviders))
+
+
+# The only usage of this class is to call the on_
 class ExampleFlask(Flask):
     def run(
             self,
@@ -21,23 +26,28 @@ class ExampleFlask(Flask):
         with self.app_context():
             on_startup()
         super(ExampleFlask, self).run(host=host, port=port, debug=debug, load_dotenv=load_dotenv, **options)
+
+
 app = ExampleFlask(__name__)
 
 # Step1: Returns the homepage
 app.get("/home/")
+
+
 @app.get("/")
 def root():
-    return homePageHTML
+    return home_page_html
+
 
 # Step2: Process user connection data prepare the OIDC request
 @app.route("/authenticate")
 def authentication():
     id = request.args.get("serverAndLaoId")
     token = request.args.get("token").strip()
+    url = get_url()
+    return redirect(url, code = 302)
 
 
-
-
- # Step 0: Starts the server
+# Step 0: Starts the server
 if __name__ == "__main__":
-    app.run(debug = True, ssl_context= "adhoc")
+    app.run(debug=True, ssl_context="adhoc")
