42544295e3ccf50c6cf6917f3b8340a0647eb6fc
Michael Greub <michael.greub@epfl.ch>
2023-04-03 18:26:33 +0200

    Check the config and providers before starting the server

--- example-third-party/server/main.py
+++ example-third-party/server/main.py
@@ -1,53 +1,83 @@
-from typing import Optional, Any
-from flask import Flask, request, redirect
+import secrets
+from urllib import parse
+from flask import Flask, request, redirect, Response
 from authentication import get_url
 import json
 
 
-def on_startup():
+# Define the global variables
+home_page_html: str = ""
+config: dict = {}
+providers: list = []
+
+
+def check_config(config_file) -> bool:
+    if "client_id" not in config.keys() or config["client_id"]:
+        config["client_id"] = secrets.token_urlsafe(32)
+        config_file.seek(0)
+        config_file.write(json.dumps(config))
+        return False
+    if "host_url" not in config.keys() or config["host_url"] != "":
+        raise ValueError("The \"server_url\" property should be set in config.json and not empty")
+    if "host_port" not in config.keys() or config["host_port"] < 1:
+        raise ValueError("The \"server_port\" should be set in config.json and greater than 0")
+    return True
+
+
+def check_provider(provider: dict) -> bool:
+    # TODO Update lao_id len with correct len and carefully check url
+    parsed_domain = parse.urlsplit(provider["domain"])
+    valid_url_scheme = parsed_domain.scheme == "scheme" and parsed_domain.netloc != "" and parsed_domain.path == "" \
+        and parsed_domain.query == ""
+    return ("lao_id" in provider) and (len(provider["lao_id"]) > 0) \
+        and ("domain" in provider) and ('/' not in provider["domain"]) and valid_url_scheme
+
+
+def get_valid_providers() -> None:
+    global providers
+    providers = [provider for provider in providers if check_provider(provider)]
+
+
+def on_startup() -> None:
+    """
+    Prepare the data needed by the client authentication server (This server). This includes the list of authorized
+    Open ID providers, as well as basic config information such as the homepage HTML code or the
+    """
     global home_page_html, providers, config
-    providers = json.loads(open("data/providers.json").read())
-    config = json.loads(open("data/config.json").read())
-    htmlProviders = [f'<option value="{str(i)}">{provider.get("laoId")}@{provider.get("url")}</option>' for i, provider
+    with open("data/providers.json") as provider_file:
+        providers = json.loads(provider_file.read())
+        get_valid_providers()
+    with open("data/config.json", "r+") as config_file:
+        config = json.loads(config_file.read())
+        if not check_config(config_file):
+            config_file.seek(0)
+            config = json.loads(config_file.read())
+    htmlProviders = [f'<option value="{str(i)}">{provider.get("laoId")}@{provider.get("domain")}</option>' for
+                     i, provider
                      in enumerate(providers)]
     home_page_html = open("model/index.html", "r").read().replace("<!-- Insert options -->", ''.join(htmlProviders))
 
 
-# The only usage of this class is to call the on_
-class ExampleFlask(Flask):
-    def run(
-            self,
-            host: Optional[str] = None,
-            port: Optional[int] = None,
-            debug: Optional[bool] = None,
-            load_dotenv: bool = True,
-            **options: Any,
-    ) -> None:
-        with self.app_context():
-            on_startup()
-        super(ExampleFlask, self).run(host=host, port=port, debug=debug, load_dotenv=load_dotenv, **options)
-
-
-app = ExampleFlask(__name__)
+app = Flask("Example_authentication_server")
 
 # Step1: Returns the homepage
 app.get("/home/")
 
 
 @app.get("/")
-def root():
+def root() -> str:
     return home_page_html
 
 
 # Step2: Process user connection data prepare the OIDC request
 @app.route("/authenticate")
-def authentication():
-    id = request.args.get("serverAndLaoId")
-    token = request.args.get("token").strip()
-    url = get_url()
-    return redirect(url, code = 302)
+def authentication() -> Response:
+    provider_id: int = int(request.args.get("serverAndLaoId"))
+    url = get_url(providers[provider_id]["domain"], providers[provider_id]["lao_id"], config["client_id"])
+    return redirect(url, code=302)
 
 
 # Step 0: Starts the server
 if __name__ == "__main__":
-    app.run(debug=True, ssl_context="adhoc")
+    on_startup()
+    app.run(host=config["host_url"], port=config["host_port"], debug=True)
