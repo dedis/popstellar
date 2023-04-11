7a46d05123b4ad0ca84ed6b065190951e1170471
dayan9265 <82887324+dayan9265@users.noreply.github.com>
2023-04-11 17:40:32 +0200

    renaming some variables

--- fe1-web/src/features/popcha/index.ts
+++ fe1-web/src/features/popcha/index.ts
@@ -1,9 +1,9 @@
 import { PoPchaConfiguration, PoPchaInterface } from './interface';
-import { simpleScreenScreen } from './screens/simpleScreen';
+import { popchaScannerScreen } from './screens/PoPchaScanner';
 
 export const configure = (configuration: PoPchaConfiguration): PoPchaInterface => {
   console.log('PoPcha configuration: ', configuration);
   return {
-    laoScreens: [simpleScreenScreen],
+    laoScreens: [popchaScannerScreen],
   };
 };
--- fe1-web/src/features/popcha/screens/PoPchaScanner.tsx
+++ fe1-web/src/features/popcha/screens/PoPchaScanner.tsx
@@ -3,21 +3,23 @@ import { Text, View } from 'react-native';
 
 import ScreenWrapper from '../../../core/components/ScreenWrapper';
 import { Typography } from '../../../core/styles';
+import STRINGS from '../../../resources/strings';
 import { PoPchaFeature } from '../interface';
 
-const simpleScreen = () => {
+const PoPchaScanner = () => {
   return (
     <ScreenWrapper>
       <View>
-        <Text style={Typography.paragraph}>Hello World</Text>
+        <Text style={Typography.paragraph}>Hello World!</Text>
       </View>
     </ScreenWrapper>
   );
 };
 
-export default simpleScreen;
+export default PoPchaScanner;
 
-export const simpleScreenScreen: PoPchaFeature.LaoScreen = {
-  id: 'simpleScreen',
-  Component: simpleScreen,
+export const popchaScannerScreen: PoPchaFeature.LaoScreen = {
+  id: STRINGS.navigation_lao_popcha,
+  Component: PoPchaScanner,
+  order: 100000,
 };
