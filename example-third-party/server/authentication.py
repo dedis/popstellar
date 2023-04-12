8cf8875adbd14b0d886409c53cf8ff9a26e488de
Michael Greub <michael.greub@epfl.ch>
2023-04-12 13:21:37 +0200

    add requirments and update query URL

--- example-third-party/server/authentication.py
+++ example-third-party/server/authentication.py
@@ -3,9 +3,7 @@ import secrets
 
 
 def get_url(server: str, lao_id: str, client_id: str) -> str :
-    baseUrl = "https://{}/authorize?response_type=id_token%20token&client_id={}" \
-              "&redirect_uri=https%3A%2F%2F{}%2Fcb&scope=openid%20profile&state={}&nonce={}"
-
+    baseUrl = "https://{}/authorize?response_mode=query&response_type=id_token%20token&client_id={}" \
+              "&redirect_uri=https%3A%2F%2F{}%2Fcb&scope=openid%20profile&login_hint={}&nonce={}"
     url = baseUrl.format(server, client_id, server, lao_id, secrets.token_urlsafe(32))
-
     return url
