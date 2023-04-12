57e0d2daccbb90a6c862fdbc8f5451e8628da108
dayan9265 <82887324+dayan9265@users.noreply.github.com>
2023-04-12 14:55:33 +0200

    some cleaning

--- fe1-web/src/features/popcha/hooks/PoPchaHooks.ts
+++ fe1-web/src/features/popcha/hooks/PoPchaHooks.ts
@@ -6,9 +6,8 @@ import { POPCHA_FEATURE_IDENTIFIER, PoPchaReactContext } from '../interface';
 export namespace PoPchaHooks {
   export const usePoPchaContext = (): PoPchaReactContext => {
     const featureContext = useContext(FeatureContext);
-    // assert that the social context exists
+
     if (!(POPCHA_FEATURE_IDENTIFIER in featureContext)) {
-      console.log('featureContext: ', featureContext);
       throw new Error('PoPcha context could not be found!');
     }
     return featureContext[POPCHA_FEATURE_IDENTIFIER] as PoPchaReactContext;
