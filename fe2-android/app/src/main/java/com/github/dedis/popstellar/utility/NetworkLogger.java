1bd105dfc431266464d27b0f80c96b4c947d6c3f
matteosz <mattsuez01@gmail.com>
2023-04-22 18:58:13 +0200

    added websocket to server

--- fe2-android/app/src/main/java/com/github/dedis/popstellar/utility/NetworkLogger.java
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/utility/NetworkLogger.java
@@ -7,40 +7,82 @@ import androidx.annotation.NonNull;
 import java.time.Clock;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import okhttp3.*;
 import timber.log.Timber;
 
 public class NetworkLogger extends Timber.Tree {
   private static final String SERVER_URL = "url";
 
+  private static WebSocket webSocket;
+  private static final Object lock = new Object();
+
   public static final AtomicBoolean sendToServer = new AtomicBoolean(false);
 
   @Override
   protected void log(int priority, String tag, @NonNull String message, Throwable t) {
+    String error = t == null ? "" : Log.getStackTraceString(t);
     // Print the log to console
-    Timber.tag(tag).log(priority, message, t);
+    if (t != null) {
+      Log.println(priority, tag, message + "\n" + error);
+    } else {
+      Log.println(priority, tag, message);
+    }
 
     // Send the log message to the remote server
     if (sendToServer.get()) {
-      sendToServer(
-          String.format(
-              "[%s] [%s] %s: %s\nE/: %s",
-              Clock.systemUTC().instant(),
-              getPriorityString(priority),
-              tag,
-              message,
-              t.getMessage()));
+      String log =
+          t == null
+              ? String.format(
+                  "[%s]-[%s]-%s: %s",
+                  Clock.systemUTC().instant(), getPriorityString(priority), tag, message)
+              : String.format(
+                  "[%s]-[%s]-%s: %s\nERROR: %s",
+                  Clock.systemUTC().instant(), getPriorityString(priority), tag, message, error);
+      sendToServer(log);
     }
   }
 
   public static void enableRemote() {
+    connectWebSocket();
     sendToServer.set(true);
   }
 
   public static void disableRemote() {
     sendToServer.set(false);
+    closeWebSocket();
   }
 
-  private void sendToServer(String log) {}
+  private static void connectWebSocket() {
+    Request request = new Request.Builder().url(SERVER_URL).build();
+    OkHttpClient client = new OkHttpClient();
+    webSocket =
+        client.newWebSocket(
+            request,
+            new WebSocketListener() {
+              @Override
+              public void onMessage(@NonNull WebSocket webSocket, @NonNull String text) {
+                // For now no message from the remote server is sent, so no handling required
+              }
+            });
+  }
+
+  private static void closeWebSocket() {
+    synchronized (lock) {
+      if (webSocket != null) {
+        webSocket.close(1000, null);
+        webSocket = null;
+      }
+    }
+  }
+
+  private void sendToServer(String log) {
+    synchronized (lock) {
+      if (webSocket == null) {
+        connectWebSocket();
+      }
+      webSocket.send(log);
+    }
+  }
 
   private String getPriorityString(int priority) {
     switch (priority) {
