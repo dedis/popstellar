de7c798e4a05c5654a4706ac406885e480c2311b
lauener <kilian.lauener@epfl.ch>
2023-04-22 11:38:40 +0200

    Add parsing support for get_messages_by_id answers

--- be2-scala/src/main/scala/ch/epfl/pop/json/HighLevelProtocol.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/json/HighLevelProtocol.scala
@@ -175,6 +175,7 @@ object HighLevelProtocol extends DefaultJsonProtocol {
     override def read(json: JsValue): ResultObject = json match {
       case JsNumber(resultInt)  => new ResultObject(resultInt.toInt)
       case JsArray(resultArray) => new ResultObject(resultArray.map(_.convertTo[Message]).toList)
+      case JsObject(resultMap)  => new ResultObject(resultMap.map { case (k, v) => (Channel(k), v.convertTo[Set[Message]]) })
       case _                    => throw new IllegalArgumentException(s"Unrecognizable channel value in $json")
     }
 
