0eb4c4f93a1225b3b6918de4a2080e0470e287af
Michael Greub <michael.greub@epfl.ch>
2023-04-23 22:03:00 +0200

    Few authentication code

--- example-third-party/authentication.py
+++ example-third-party/authentication.py
@@ -2,14 +2,16 @@
 This file includes all the authentication logic. No external server logic is
 done here.
 """
-import jwt
+from __future__ import annotations
+
 import secrets
 import time
 from urllib import parse
 
+import jwt
 from werkzeug.datastructures import MultiDict
 
-valid_nonces: list[(str, float)]
+valid_nonces: dict[(str, float)]
 
 
 def get_url(server: str, lao_id: str, client_id: str) -> str:
@@ -22,8 +24,8 @@ def get_url(server: str, lao_id: str, client_id: str) -> str:
     :return: A url to the authentication server
     """
     global valid_nonces
-    nonce = (secrets.token_urlsafe(64), time.time())
-    valid_nonces.append(nonce)
+    nonce = secrets.token_urlsafe(64)
+    valid_nonces[nonce] = (server, time.time())
     parameters = {
         "response_mode": "query",
         "response_type": "id_token token",
@@ -31,25 +33,32 @@ def get_url(server: str, lao_id: str, client_id: str) -> str:
         "redirect_uri": f"https%3A%2F%2F{server}%2Fcb",
         "scope": "openid profile",
         "login_hint": lao_id,
-        "nonce": nonce[0],
-    }
+        "nonce": nonce,
+        }
     return f"https://{server}/authorize?{parse.urlencode(parameters)}"
 
 
-def validate_args(args: MultiDict[str, str], public_key) -> bool:
+def validate_args(args: MultiDict[str, str], public_key: str, client_id: str) \
+        -> str | None:
     """
     Validate the args as if they were coming from OpenID Authentication request
     :param args: A MultiDict containing the arguments (almost the same as a
     dict).
-    :param public_key: 
-    :return: True if the args are valid.
+    :param public_key:
+    :param client_id:
+    :return: The unique user identifier if logged in None otherwise
     """
     missing_arg: bool = ("access_token" not in args or "id_token" not in args
-                         or "access_token" not in args)
-
+                         or "token_type" not in args)
     if missing_arg:
-        id_token = jwt.decode(args.get(), default="", type=str)
-    return True
+        return None
+    token: dict = jwt.decode(args.get("id_token", type = str))
+
+    valid_client_id = client_id == token.get("aud") == client_id
+    if not valid_client_id:
+        return None
+
+    return None
 
 
 def valid_login_url(args: MultiDict[str, str]) -> str:
