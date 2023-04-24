5af56fb54b77448396f633a08ca51b16fe134266
BelghmiAmine <amine.belghmi@epfl.ch>
2023-04-24 23:41:17 +0200

    bug fix in tests to make failing db send back a message.

--- be2-scala/src/test/scala/ch/epfl/pop/decentralized/FailingToyDbActor.scala
+++ be2-scala/src/test/scala/ch/epfl/pop/decentralized/FailingToyDbActor.scala
@@ -7,7 +7,9 @@ import ch.epfl.pop.storage.DbActor
 
 class FailingToyDbActor extends Actor {
   override def receive: Receive = {
-    case _ => DbActorNAckException(0, "")
+    case _ => {
+      sender() ! DbActorNAckException(0, "")
+    }
   }
 
 }
