a63f4dd8c712b8c913f1eb9a5cc402ad81d318d8
BelghmiAmine <amine.belghmi@epfl.ch>
2023-04-26 14:06:09 +0200

    add retries to write on db.

--- be2-scala/src/main/scala/ch/epfl/pop/pubsub/graph/handlers/GetMessagesByIdResponseHandler.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/pubsub/graph/handlers/GetMessagesByIdResponseHandler.scala
@@ -2,16 +2,25 @@ package ch.epfl.pop.pubsub.graph.handlers
 
 import akka.NotUsed
 import akka.actor.ActorRef
-import akka.stream.scaladsl.{Flow, GraphDSL, Merge, Partition, Sink}
-import ch.epfl.pop.model.network.{JsonRpcRequest, JsonRpcResponse}
-import ch.epfl.pop.pubsub.graph.{ErrorCodes, GraphMessage, PipelineError}
-import ch.epfl.pop.pubsub.{AskPatternConstants, ClientActor, PubSubMediator}
+import akka.pattern.AskableActorRef
+import akka.stream.scaladsl.{Flow, GraphDSL, Merge, Partition}
+import ch.epfl.pop.model.network.JsonRpcResponse
+import ch.epfl.pop.pubsub.graph.GraphMessage
+import ch.epfl.pop.pubsub.AskPatternConstants
 import ch.epfl.pop.storage.DbActor
 import akka.stream.FlowShape
+import ch.epfl.pop.model.network.method.message.Message
+import ch.epfl.pop.model.objects.Channel
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 /** This object's job is to handle responses it receives from other servers after sending a heartbeat. When receiving the missing messages, the server's job is to write them on the data base.
   */
 object GetMessagesByIdResponseHandler extends AskPatternConstants {
+
+  private final val MAX_ATTEMPT : Int = 3
+
 
   def graph(dbActorRef: ActorRef): Flow[GraphMessage, GraphMessage, NotUsed] = Flow.fromGraph(GraphDSL.create() {
     implicit builder: GraphDSL.Builder[NotUsed] =>
@@ -29,7 +38,7 @@ object GetMessagesByIdResponseHandler extends AskPatternConstants {
           {
             case Right(jsonRpcMessage: JsonRpcResponse) => jsonRpcMessage.result match {
                 case Some(result) => result.resultMap match {
-                    case Some(map) => portResponseHandler
+                    case Some(_) => portResponseHandler
                     case _         => portPipelineError
                   }
                 case _ => portPipelineError
@@ -56,11 +65,23 @@ object GetMessagesByIdResponseHandler extends AskPatternConstants {
       val receivedResponse = jsonRpcMessage.result.get.resultMap.get
       receivedResponse.keys.foreach(channel => {
         receivedResponse(channel).foreach(message => {
-          dbActorRef ! DbActor.Write(channel, message)
+          writeOnDb(channel,message,dbActorRef)
         })
       })
       Right(jsonRpcMessage)
     case value @ _ => value
   }.filter(_ => false)
+
+  private def writeOnDb(channel : Channel, message : Message, dbActorRef: AskableActorRef): Unit = {
+    retry(MAX_ATTEMPT){
+      dbActorRef ? DbActor.Write(channel, message)
+    }
+  }
+
+  private def retry[T](n: Int)(fn: => Future[T]): Future[T] = {
+    fn recoverWith {
+      case _ if n > 1 => retry(n - 1)(fn)
+    }
+  }
 
 }
--- be2-scala/src/test/scala/ch/epfl/pop/pubsub/graph/handlers/GetMessagesByIdResponseHandlerSuite.scala
+++ be2-scala/src/test/scala/ch/epfl/pop/pubsub/graph/handlers/GetMessagesByIdResponseHandlerSuite.scala
@@ -6,12 +6,13 @@ import akka.stream.scaladsl.{Flow, Sink, Source}
 import akka.testkit.{TestKit, TestProbe}
 import ch.epfl.pop.model.network.{JsonRpcResponse, ResultObject}
 import ch.epfl.pop.model.network.method.message.Message
-import ch.epfl.pop.model.objects.{Base64Data, Channel, Hash}
+import ch.epfl.pop.model.objects.{Base64Data, Channel, DbActorNAckException, Hash}
 import ch.epfl.pop.pubsub.AskPatternConstants
 import ch.epfl.pop.pubsub.graph.GraphMessage
 import ch.epfl.pop.pubsub.graph.validators.RpcValidator
 import ch.epfl.pop.storage.DbActor
 import org.scalatest.funsuite.AnyFunSuiteLike
+
 import scala.concurrent.Await
 
 class GetMessagesByIdResponseHandlerSuite extends TestKit(ActorSystem("GetMessagesByIdResponseHandlerSuiteSystem")) with AnyFunSuiteLike with AskPatternConstants {
@@ -22,9 +23,15 @@ class GetMessagesByIdResponseHandlerSuite extends TestKit(ActorSystem("GetMessag
         testProbe ! (channel, message)
     }
   }
+  class FailingTestDb(testProbe: ActorRef) extends Actor {
+    override def receive: Receive = {
+      case DbActor.Write(channel, message) =>
+        testProbe ! (channel, message)
+        throw(DbActorNAckException(0,""))
+    }
+  }
 
   val testProbe = TestProbe()
-  final val boxUnderTest: Flow[GraphMessage, GraphMessage, NotUsed] = GetMessagesByIdResponseHandler.graph(system.actorOf(Props(new TestDb(testProbe.ref))))
   final val CHANNEL1: Channel = Channel("/root/wex/lao1Id")
   final val CHANNEL2: Channel = Channel("/root/wex/lao2Id")
   final val MESSAGE1_ID: Hash = Hash(Base64Data.encode("message1Id"))
@@ -33,7 +40,9 @@ class GetMessagesByIdResponseHandlerSuite extends TestKit(ActorSystem("GetMessag
   final val MESSAGE2: Message = Message(null, null, null, MESSAGE2_ID, null, null)
   final val missingMessages = Map((CHANNEL1, Set(MESSAGE1)), (CHANNEL2, Set(MESSAGE2)))
   final val receivedResponse: JsonRpcResponse = JsonRpcResponse(RpcValidator.JSON_RPC_VERSION, new ResultObject(missingMessages), None)
+
   test("receiving a get messages by id response sends a write message to the data base") {
+    val boxUnderTest: Flow[GraphMessage, GraphMessage, NotUsed] = GetMessagesByIdResponseHandler.graph(system.actorOf(Props(new TestDb(testProbe.ref))))
     val input: List[GraphMessage] = List(Right(receivedResponse))
     val source = Source(input)
     val s = source.via(boxUnderTest).runWith(Sink.seq[GraphMessage])
@@ -41,4 +50,13 @@ class GetMessagesByIdResponseHandlerSuite extends TestKit(ActorSystem("GetMessag
     testProbe.expectMsgAllOf((CHANNEL1, MESSAGE1), (CHANNEL2, MESSAGE2))
   }
 
+
+  test("failing to write a get messages by id response in the data base retries exactly three times before giving up") {
+    val boxUnderTest: Flow[GraphMessage, GraphMessage, NotUsed] = GetMessagesByIdResponseHandler.graph(system.actorOf(Props(new FailingTestDb(testProbe.ref))))
+    val input: List[GraphMessage] = List(Right(receivedResponse))
+    val source = Source(input)
+    val s = source.via(boxUnderTest).runWith(Sink.seq[GraphMessage])
+    Await.ready(s, duration)
+    testProbe.expectMsgAllOf((CHANNEL1, MESSAGE1),(CHANNEL1, MESSAGE1),(CHANNEL1, MESSAGE1),(CHANNEL2, MESSAGE2),(CHANNEL2, MESSAGE2),(CHANNEL2, MESSAGE2))
+  }
 }
