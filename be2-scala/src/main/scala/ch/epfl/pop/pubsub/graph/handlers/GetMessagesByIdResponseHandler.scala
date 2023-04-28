047b6f47c000cdb7daa3e8e513e672fc7c64495d
lauener <kilian.lauener@epfl.ch>
2023-04-28 22:31:00 +0200

    Initial implementation of get_messages_by_id answer verification

--- be2-scala/src/main/scala/ch/epfl/pop/pubsub/graph/handlers/GetMessagesByIdResponseHandler.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/pubsub/graph/handlers/GetMessagesByIdResponseHandler.scala
@@ -1,16 +1,18 @@
 package ch.epfl.pop.pubsub.graph.handlers
 
 import akka.NotUsed
-import akka.actor.ActorRef
+import akka.actor.{ActorRef, ActorSystem}
 import akka.pattern.AskableActorRef
-import akka.stream.scaladsl.{Flow, GraphDSL, Merge, Partition}
-import ch.epfl.pop.model.network.JsonRpcResponse
-import ch.epfl.pop.pubsub.graph.GraphMessage
-import ch.epfl.pop.pubsub.AskPatternConstants
+import akka.stream.scaladsl.{Flow, GraphDSL, Merge, Partition, Sink, Source}
+import ch.epfl.pop.model.network.{JsonRpcRequest, JsonRpcResponse, MethodType}
+import ch.epfl.pop.pubsub.graph.{GraphMessage, Validator}
+import ch.epfl.pop.pubsub.{AskPatternConstants, ClientActor, MessageRegistry}
 import ch.epfl.pop.storage.DbActor
 import akka.stream.FlowShape
+import ch.epfl.pop.model.network.method.Publish
 import ch.epfl.pop.model.network.method.message.Message
 import ch.epfl.pop.model.objects.{Channel, DbActorNAckException}
+import ch.epfl.pop.pubsub.graph.validators.RpcValidator
 
 import scala.annotation.tailrec
 import scala.concurrent.Await
@@ -20,51 +22,97 @@ import scala.util.Failure
   */
 object GetMessagesByIdResponseHandler extends AskPatternConstants {
 
-  private final val MAX_ATTEMPT: Int = 3
+  def graph(mediatorActorRef: ActorRef, messageRegistry: MessageRegistry)(implicit system: ActorSystem): Flow[GraphMessage, GraphMessage, NotUsed] =
+    Flow.fromGraph(GraphDSL.create() {
+      implicit builder: GraphDSL.Builder[NotUsed] =>
+        {
+          import GraphDSL.Implicits._
 
-  def graph(dbActorRef: ActorRef): Flow[GraphMessage, GraphMessage, NotUsed] = Flow.fromGraph(GraphDSL.create() {
+          /* partitioner port numbers */
+          val portPipelineError = 0
+          val portResponseHandler = 1
+          val totalPorts = 2
+
+          /* building blocks */
+          val handlerPartitioner = builder.add(Partition[GraphMessage](
+            totalPorts,
+            {
+              case Right(jsonRpcMessage: JsonRpcResponse) => jsonRpcMessage.result match {
+                  case Some(result) => result.resultMap match {
+                      case Some(_) => portResponseHandler
+                      case _       => portPipelineError
+                    }
+                  case _ => portPipelineError
+                }
+              case _ => portPipelineError // Pipeline error goes directly in handlerMerger
+            }
+          ))
+
+          val responseHandler = builder.add(GetMessagesByIdResponseHandler.responseHandler(mediatorActorRef, messageRegistry)(system))
+          val handlerMerger = builder.add(Merge[GraphMessage](totalPorts))
+
+          /* glue the components together */
+          handlerPartitioner.out(portPipelineError) ~> handlerMerger
+          handlerPartitioner.out(portResponseHandler) ~> responseHandler ~> handlerMerger
+
+          /* close the shape */
+          FlowShape(handlerPartitioner.in, handlerMerger.out)
+        }
+    })
+  private def validator(mediatorActorRef: ActorRef, messageRegistry: MessageRegistry)(implicit system: ActorSystem): Flow[GraphMessage, GraphMessage, NotUsed] = Flow.fromGraph(GraphDSL.create() {
     implicit builder: GraphDSL.Builder[NotUsed] =>
       {
         import GraphDSL.Implicits._
 
+        val input = builder.add(Flow[GraphMessage].collect { case msg: GraphMessage => msg })
+
+        val localClient: ActorRef = system.actorOf(ClientActor.props(mediatorActorRef, ActorRef.noSender, isServer = false))
+
         /* partitioner port numbers */
         val portPipelineError = 0
-        val portResponseHandler = 1
-        val totalPorts = 2
+        val portParamsWithMessage = 1
+        val portParamsWithChannel = 2
+        val totalPorts = 3
 
-        /* building blocks */
-        val handlerPartitioner = builder.add(Partition[GraphMessage](
+        val methodPartitioner = builder.add(Partition[GraphMessage](
           totalPorts,
           {
-            case Right(jsonRpcMessage: JsonRpcResponse) => jsonRpcMessage.result match {
-                case Some(result) => result.resultMap match {
-                    case Some(_) => portResponseHandler
-                    case _       => portPipelineError
-                  }
-                case _ => portPipelineError
-              }
-            case _ => portPipelineError // Pipeline error goes directly in handlerMerger
+            case Right(m: JsonRpcRequest) if m.hasParamsMessage => portParamsWithMessage // Publish and Broadcast messages
+            case Right(m: JsonRpcRequest) if m.hasParamsChannel => portParamsWithChannel
+            case _                                              => portPipelineError // Pipeline error goes directly in merger
           }
         ))
 
-        val responseHandler = builder.add(GetMessagesByIdResponseHandler.responseHandler(dbActorRef))
-        val handlerMerger = builder.add(Merge[GraphMessage](totalPorts))
+        val jsonRpcContentValidator = builder.add(Validator.jsonRpcContentValidator)
+        val hasMessagePartition = builder.add(ParamsWithMessageHandler.graph(messageRegistry))
+        val hasChannelPartition = builder.add(ParamsWithChannelHandler.graph(localClient))
+
+        val merger = builder.add(Merge[GraphMessage](totalPorts))
+
+        // output message (answer) for the client
+        val output = builder.add(Flow[GraphMessage])
 
         /* glue the components together */
-        handlerPartitioner.out(portPipelineError) ~> handlerMerger
-        handlerPartitioner.out(portResponseHandler) ~> responseHandler ~> handlerMerger
+        input ~> jsonRpcContentValidator ~> methodPartitioner
+
+        methodPartitioner.out(portPipelineError) ~> merger
+        methodPartitioner.out(portParamsWithMessage) ~> hasMessagePartition ~> merger
+        methodPartitioner.out(portParamsWithChannel) ~> hasChannelPartition ~> merger
+        merger ~> output
 
         /* close the shape */
-        FlowShape(handlerPartitioner.in, handlerMerger.out)
+        FlowShape(input.in, output.out)
       }
   })
 
-  private def responseHandler(dbActorRef: ActorRef): Flow[GraphMessage, GraphMessage, NotUsed] = Flow[GraphMessage].map {
+  private def responseHandler(mediatorActorRef: ActorRef, messageRegistry: MessageRegistry)(implicit system: ActorSystem): Flow[GraphMessage, GraphMessage, NotUsed] = Flow[GraphMessage].map {
     case Right(jsonRpcMessage: JsonRpcResponse) =>
       val receivedResponse = jsonRpcMessage.result.get.resultMap.get
+      val validatorFlow = validator(mediatorActorRef, messageRegistry)
       receivedResponse.keys.foreach(channel => {
         receivedResponse(channel).foreach(message => {
-          writeOnDb(channel, message, dbActorRef, MAX_ATTEMPT)
+          val publishedMessage = Right(JsonRpcRequest(RpcValidator.JSON_RPC_VERSION, MethodType.PUBLISH, new Publish(channel, message), Some(0)))
+          Source.single(publishedMessage).via(validatorFlow).runWith(Sink.foreach(msg => println("OUTPUT OF PIPE: " + msg)))
         })
       })
       Right(jsonRpcMessage)
