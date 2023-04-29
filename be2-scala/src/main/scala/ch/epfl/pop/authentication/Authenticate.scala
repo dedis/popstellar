fd64ec7d2b2c51e9d8b74e69adbab2d1859d7b11
Jharaxus <hugo.majerczyk@gmail.com>
2023-04-30 00:22:42 +0200

    Fix review comments in Authenticate

--- be2-scala/src/main/scala/ch/epfl/pop/authentication/Authenticate.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/authentication/Authenticate.scala
@@ -1,21 +1,26 @@
 package ch.epfl.pop.authentication
 
-import akka.http.scaladsl.model.StatusCodes.ClientError
 import akka.http.scaladsl.model.{AttributeKey, HttpRequest, HttpResponse, StatusCodes}
 import akka.http.scaladsl.server
 import akka.http.scaladsl.server.Directives._
-import ch.epfl.pop.authentication.Authenticate.verifyResponseType
 
+/** Helper object used to build a server route for handling authentication requests
+  */
 object Authenticate {
-  case class RequestParameters(response_type: String, client_id: String, redirect_uri: String, scope: String, state: Option[String], response_mode: Option[String], login_hint: String, nonce: String)
+  private case class RequestParameters(response_type: String, client_id: String, redirect_uri: String, scope: String, state: Option[String], response_mode: Option[String], login_hint: String, nonce: String)
 
-  type VerificationState = Either[(String, String), Unit]
+  private type VerificationState = Either[(String, String), Unit]
 
   private val mandatoryParameters = Set("response_type", "client_id", "redirect_uri", "scope", "login_hint", "nonce")
 
+  /** Builds a route that can handle an authentication request
+    * @return
+    *   A route either successfully handling a given authentication request or rejecting it with an error
+    */
   def buildRoute(): server.Route = {
     extractRequest { request =>
-      parameters(
+
+      val validParametersRoute = parameters(
         "response_type",
         "client_id",
         "redirect_uri",
@@ -33,14 +38,18 @@ object Authenticate {
               case Right(_)                        => complete(generateChallenge(request))
             }
           }
-      } ~ extractUri { uri =>
+      }
+
+      val invalidParametersRoute = extractUri { uri =>
         complete {
-          val attributesKeys = uri.query().toMap.keys.toSet
-          val missingParams = mandatoryParameters.diff(attributesKeys).map(name => s"[$name]")
-          val errorDescription = missingParams.mkString("Missing parameters: ", " ", "")
+          val parametersFound = uri.query().toMap.keys.toSet
+          val missingParams = mandatoryParameters.diff(parametersFound)
+          val errorDescription = missingParams.map(name => s"[$name]").mkString("Missing parameters: ", " ", "")
           authenticationFailure("invalid_request", errorDescription, None)
         }
       }
+
+      validParametersRoute ~ invalidParametersRoute
     }
   }
 
@@ -55,7 +64,8 @@ object Authenticate {
 
   private def verifyResponseType(response_type: String): VerificationState = {
     val expectedResponseType = "id_token token"
-    if (response_type == expectedResponseType) Right(())
+    if (response_type == expectedResponseType)
+      Right(())
     else
       Left("unsupported_response_type" -> s"expected \"$expectedResponseType\" but received \"$response_type\"")
   }
@@ -63,7 +73,8 @@ object Authenticate {
   private def verifyScope(scope: String): VerificationState = {
     val expectedScope = "openid"
     val scopes = scope.split(" ")
-    if (scopes.contains(expectedScope)) Right(())
+    if (scopes.contains(expectedScope))
+      Right(())
     else
       Left("invalid_scope" -> s"expected scope to contain \"$expectedScope\" but received \"$scope\"")
   }
@@ -73,14 +84,16 @@ object Authenticate {
     // see https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url for regex source
     val httpRegex =
       "https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)"
-    if (uri.matches(httpRegex)) Right(())
+    if (uri.matches(httpRegex))
+      Right(())
     else
       Left("invalid_request" -> s"expected http or https url format for redirect uri")
   }
 
   private def verifyResponseMode(mode: Option[String]): VerificationState = {
     val supportedModes = List("query", "fragment")
-    if (mode.isEmpty || supportedModes.contains(mode.get)) Right(())
+    if (mode.isEmpty || supportedModes.contains(mode.get))
+      Right(())
     else
       Left("invalid_request" -> s"only [${supportedModes.mkString(",")}] response modes are supported but received $mode")
   }
