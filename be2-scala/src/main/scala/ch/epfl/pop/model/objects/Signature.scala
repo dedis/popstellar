6f30008706bd649ce1b9bb6646ef798c824097d7
Jharaxus <hugo.majerczyk@gmail.com>
2023-04-30 19:34:29 +0200

    Add part of Authenticate message data validation

--- be2-scala/src/main/scala/ch/epfl/pop/model/objects/Signature.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/model/objects/Signature.scala
@@ -4,7 +4,17 @@ import com.google.crypto.tink.subtle.Ed25519Verify
 
 import scala.util.{Success, Try}
 
+/**
+ * Represents a signature by a private key. It may be used to verify that a given publicKey is the pair of the private key originally used for the signature.
+ * @param signature data resulting from an encryption using a private key
+ */
 final case class Signature(signature: Base64Data) {
+  /**
+   * Verifies that the given key indeed decodes back to the given message
+   * @param key Key to use to decode the signature
+   * @param message Message expected to be found after decoding the signature
+   * @return True iif the decoded message is equal to the message expected
+   */
   def verify(key: PublicKey, message: Base64Data): Boolean = {
     val ed = new Ed25519Verify(key.base64Data.decode())
 
--- be2-scala/src/main/scala/ch/epfl/pop/pubsub/graph/validators/PopchaValidator.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/pubsub/graph/validators/PopchaValidator.scala
@@ -1,20 +1,76 @@
 package ch.epfl.pop.pubsub.graph.validators
 
-import ch.epfl.pop.model.network.JsonRpcRequest
+import akka.pattern.AskableActorRef
+import ch.epfl.pop.model.network.{JsonRpcMessage, JsonRpcRequest}
 import ch.epfl.pop.model.network.method.message.Message
 import ch.epfl.pop.model.network.method.message.data.popcha.Authenticate
+import ch.epfl.pop.model.objects.{Base64Data, Channel, Hash, PublicKey, Signature}
 import ch.epfl.pop.pubsub.graph.validators.CoinValidator.validationErrorNoMessage
+import ch.epfl.pop.pubsub.graph.validators.MessageValidator.extractData
 import ch.epfl.pop.pubsub.graph.{GraphMessage, PipelineError}
+import ch.epfl.pop.storage.DbActor
 
+/**
+ * Validator for Popcha messages' data contents
+ */
 case object PopchaValidator extends MessageDataContentValidator {
+
+  private val popchaValidator = new PopchaValidator(DbActor.getInstance)
+
+  /**
+   * Validates a Popcha Authenticate message data content
+   * @param rpcMessage Message received
+   * @return A graph message representing the result of the validation
+   */
+  def validateAuthenticateRequest(rpcMessage: JsonRpcRequest): GraphMessage = popchaValidator.validateAuthenticateRequest(rpcMessage)
+}
+sealed class PopchaValidator(dbActorRef: => AskableActorRef) extends MessageDataContentValidator {
+
   def validateAuthenticateRequest(rpcMessage: JsonRpcRequest): GraphMessage = {
-    def validationError(reason: String): PipelineError = super.validationError(reason, "CreateLao", rpcMessage.id)
+    def validationError(reason: String): PipelineError = super.validationError(reason, "Authenticate", rpcMessage.id)
 
     rpcMessage.getParamsMessage match {
-      case Some(message: Message) =>
-        val authenticate = message.decodedData.get.asInstanceOf[Authenticate]
-        Right(rpcMessage)
+      case Some(_: Message) =>
+        val (authenticate, laoId, sender, channel) = extractData[Authenticate](rpcMessage)
+
+        for {
+          _ <- checkResponseMode(rpcMessage, authenticate.responseMode, validationError(s"Invalid response mode ${authenticate.responseMode}"))
+          _ <- checkChannelName(rpcMessage, channel, laoId, validationError(s"Incorrect channel $channel for lao $laoId"))
+          _ <- checkIdentifierProof(rpcMessage, authenticate.identifier, authenticate.identifierProof, authenticate.nonce,
+            validationError("Failed to verify the identifier proof with the given identifier/nonce pair"))
+          result <- checkNoDuplicateIdentifiers(rpcMessage, sender, authenticate.identifier, validationError("An identifier is already paired with this user token"))
+        } yield result
+
       case None => Left(validationErrorNoMessage(rpcMessage.id))
     }
+  }
+
+  private def checkResponseMode(rpcMessage: JsonRpcRequest, responseMode: String, error: PipelineError): GraphMessage = {
+    if (responseMode == "query" || responseMode == "fragment")
+      Right(rpcMessage)
+    else
+      Left(error)
+  }
+
+  private def checkChannelName(rpcMessage: JsonRpcMessage, channel: Channel, laodId: Hash, error: PipelineError): GraphMessage = {
+    val expectedChannelName = s"/root/$laodId/authentication"
+    if (channel.channel == expectedChannelName)
+      Right(rpcMessage)
+    else
+      Left(error)
+  }
+
+  private def checkIdentifierProof(rpcMessage: JsonRpcMessage, identifier: PublicKey, identifierProof: Signature, nonce: String,
+                           error: PipelineError): GraphMessage = {
+    val verified = identifierProof.verify(identifier, Base64Data.encode(nonce))
+    if (verified)
+      Right(rpcMessage)
+    else
+      Left(error)
+  }
+
+  private def checkNoDuplicateIdentifiers(rpcMessage: JsonRpcMessage, user: PublicKey, identifier: PublicKey, error: PipelineError): GraphMessage = {
+    // TODO: store in DBActor a map [user -> identifier] to check and prevent duplicate
+    Right(rpcMessage)
   }
 }
