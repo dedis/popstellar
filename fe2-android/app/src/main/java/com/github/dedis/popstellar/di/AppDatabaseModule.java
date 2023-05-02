50c39fa9ba0b0b1f548704cfd74804d1629a1aa4
matteosz <mattsuez01@gmail.com>
2023-05-02 19:48:58 +0200

    removed close from prod

--- fe2-android/app/src/main/java/com/github/dedis/popstellar/di/AppDatabaseModule.java
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/di/AppDatabaseModule.java
@@ -1,11 +1,7 @@
 package com.github.dedis.popstellar.di;
 
-import android.app.Activity;
 import android.app.Application;
-import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.room.Room;
 
 import com.github.dedis.popstellar.repository.database.AppDatabase;
@@ -28,51 +24,9 @@ public class AppDatabaseModule {
   @Provides
   @Singleton
   public static AppDatabase provideAppDatabase(Application application) {
-    AppDatabase appDatabase =
-        Room.databaseBuilder(application, AppDatabase.class, "POP-Database")
-            .fallbackToDestructiveMigration()
-            .allowMainThreadQueries()
-            .build();
-    // Register a callback that closes the db when application stops
-    application.registerActivityLifecycleCallbacks(
-        new Application.ActivityLifecycleCallbacks() {
-          @Override
-          public void onActivityCreated(
-              @NonNull Activity activity, @Nullable Bundle savedInstanceState) {
-            // Nothing
-          }
-
-          @Override
-          public void onActivityStarted(@NonNull Activity activity) {
-            // Nothing
-          }
-
-          @Override
-          public void onActivityResumed(@NonNull Activity activity) {
-            // Nothing
-          }
-
-          @Override
-          public void onActivityPaused(@NonNull Activity activity) {
-            // Nothing
-          }
-
-          @Override
-          public void onActivityStopped(@NonNull Activity activity) {
-            appDatabase.close();
-          }
-
-          @Override
-          public void onActivitySaveInstanceState(
-              @NonNull Activity activity, @NonNull Bundle outState) {
-            // Nothing
-          }
-
-          @Override
-          public void onActivityDestroyed(@NonNull Activity activity) {
-            // Nothing
-          }
-        });
-    return appDatabase;
+    return Room.databaseBuilder(application, AppDatabase.class, "POP-Database")
+        .fallbackToDestructiveMigration()
+        .allowMainThreadQueries()
+        .build();
   }
 }
