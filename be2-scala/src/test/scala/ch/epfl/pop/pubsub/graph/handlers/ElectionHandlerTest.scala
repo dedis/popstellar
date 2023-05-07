2fac8fcad8ac21720380d19a9b0405a9dded9082
lauener <kilian.lauener@epfl.ch>
2023-05-07 22:37:21 +0200

    Fix ElectionHandlerTest

--- be2-scala/src/test/scala/ch/epfl/pop/pubsub/graph/handlers/ElectionHandlerTest.scala
+++ be2-scala/src/test/scala/ch/epfl/pop/pubsub/graph/handlers/ElectionHandlerTest.scala
@@ -64,7 +64,11 @@ class ElectionHandlerTest extends TestKit(ActorSystem("Election-DB-System")) wit
     val dbActorMock = Props(new Actor() {
       override def receive: Receive = {
         // You can modify the following match case to include more args, names...
-        case DbActor.WriteAndPropagate(_, _) | DbActor.ChannelExists(_) | DbActor.CreateChannel(_, _) | DbActor.CreateElectionData(_, _, _) =>
+        case DbActor.WriteAndPropagate(_, _)
+            | DbActor.ChannelExists(_)
+            | DbActor.CreateChannel(_, _)
+            | DbActor.CreateElectionData(_, _, _)
+            | DbActor.WriteSetupElectionMessage(_, _) =>
           system.log.info(f"Received a message")
           system.log.info("Responding with a Ack")
           sender() ! DbActor.DbActorAck()
@@ -85,7 +89,10 @@ class ElectionHandlerTest extends TestKit(ActorSystem("Election-DB-System")) wit
     val dbActorMock = Props(new Actor() {
       override def receive: Receive = {
         // You can modify the following match case to include more args, names...
-        case DbActor.WriteAndPropagate(_, _) | DbActor.CreateChannel(_, _) | DbActor.CreateElectionData(_, _, _) =>
+        case DbActor.WriteAndPropagate(_, _)
+            | DbActor.CreateChannel(_, _)
+            | DbActor.CreateElectionData(_, _, _)
+            | DbActor.WriteSetupElectionMessage(_, _) =>
           system.log.info("Received a message")
           system.log.info("Responding with a Ack")
           sender() ! DbActor.DbActorAck()
@@ -110,7 +117,10 @@ class ElectionHandlerTest extends TestKit(ActorSystem("Election-DB-System")) wit
     val dbActorMock = Props(new Actor() {
       override def receive: Receive = {
         // You can modify the following match case to include more args, names...
-        case DbActor.WriteAndPropagate(_, _) | DbActor.CreateChannel(_, _) | DbActor.CreateElectionData(_, _, _) =>
+        case DbActor.WriteAndPropagate(_, _)
+            | DbActor.CreateChannel(_, _)
+            | DbActor.CreateElectionData(_, _, _)
+            | DbActor.WriteSetupElectionMessage(_, _) =>
           system.log.info("Received a message")
           system.log.info("Responding with a Ack")
           sender() ! DbActor.DbActorAck()
