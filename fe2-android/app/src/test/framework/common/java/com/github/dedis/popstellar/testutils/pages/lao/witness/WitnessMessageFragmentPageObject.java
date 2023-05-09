9d915b4b89a655cfffe9d3dded534e6e7dc9eef0
Simone Kalbermatter <simone.kalbermatter@epfl.ch>
2023-05-09 17:14:52 +0200

    add more tests

--- fe2-android/app/src/test/framework/common/java/com/github/dedis/popstellar/testutils/pages/lao/witness/WitnessMessageFragmentPageObject.java
+++ fe2-android/app/src/test/framework/common/java/com/github/dedis/popstellar/testutils/pages/lao/witness/WitnessMessageFragmentPageObject.java
@@ -3,8 +3,10 @@ package com.github.dedis.popstellar.testutils.pages.lao.witness;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
+import android.view.View;
 import androidx.test.espresso.ViewInteraction;
 import com.github.dedis.popstellar.R;
+import org.hamcrest.Matcher;
 
 public class WitnessMessageFragmentPageObject {
 
@@ -12,8 +14,28 @@ public class WitnessMessageFragmentPageObject {
     throw new IllegalStateException("Page object");
   }
 
+  public static Matcher<View> witnessMessageListMatcher() {
+    return withId(R.id.witness_message_list);
+  }
+
   public static ViewInteraction witnessMessageList() {
-    return onView(withId(R.id.witness_message_list));
+    return onView(witnessMessageListMatcher());
+  }
+
+  public static Matcher<View> messageDescriptionArrowMatcher() {
+    return withId(R.id.message_description_arrow);
+  }
+
+  public static Matcher<View> messageSignaturesArrowMatcher() {
+    return withId(R.id.signatures_arrow);
+  }
+  
+  public static ViewInteraction messageDescriptionText() {
+    return onView(withId(R.id.message_description_text));
+  }
+
+  public static ViewInteraction witnessSignaturesText() {
+    return onView(withId(R.id.witnesses_text));
   }
 
   public static ViewInteraction witnessMessageFragment() {
