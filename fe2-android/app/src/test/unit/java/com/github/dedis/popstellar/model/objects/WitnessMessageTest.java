ec7949cb119f3ba68d455376472a88befdd2f882
Simone Kalbermatter <simone.kalbermatter@epfl.ch>
2023-05-09 14:09:55 +0200

    add witness message test

--- fe2-android/app/src/test/unit/java/com/github/dedis/popstellar/model/objects/WitnessMessageTest.java
+++ fe2-android/app/src/test/unit/java/com/github/dedis/popstellar/model/objects/WitnessMessageTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 public class WitnessMessageTest {
@@ -16,6 +17,13 @@ public class WitnessMessageTest {
   private static final MessageID MSG_ID = new MessageID("foo");
   private static final String DESCRIPTION = "description";
   private static final String TITLE = "title";
+  private static final MessageID MESSAGE_ID1 = Base64DataUtils.generateMessageID();
+  private static final MessageID MESSAGE_ID2 = MESSAGE_ID1;
+  private static final MessageID MESSAGE_ID3 = Base64DataUtils.generateMessageID();
+
+  private static final WitnessMessage MESSAGE1 = new WitnessMessage(MESSAGE_ID1);
+  private static final WitnessMessage MESSAGE2 = new WitnessMessage(MESSAGE_ID2);
+  private static final WitnessMessage MESSAGE3 = new WitnessMessage(MESSAGE_ID3);
 
   @Test
   public void addWitnessTest() {
@@ -35,5 +43,36 @@ public class WitnessMessageTest {
             "WitnessMessage{messageId='%s', witnesses=%s, title='%s', description='%s'}",
             MSG_ID, Collections.singletonList(PK), TITLE, DESCRIPTION);
     assertEquals(expected, witnessMessage.toString());
+  }
+
+  @Test
+  public void equalsTest() {
+    assertEquals(MESSAGE1, MESSAGE2);
+    assertNotEquals(MESSAGE1, MESSAGE3);
+
+    WitnessMessage message1 = new WitnessMessage(MESSAGE1);
+    message1.addWitness(PK);
+    message1.setDescription(DESCRIPTION);
+    message1.setTitle(TITLE);
+
+    WitnessMessage message2 = new WitnessMessage(message1);
+    assertEquals(message1, message2);
+
+    message2.addWitness(Base64DataUtils.generatePublicKey());
+    assertNotEquals(message1, message2);
+
+    message2 = new WitnessMessage(message1);
+    message2.setDescription("new description");
+    assertNotEquals(message1, message2);
+
+    message2 = new WitnessMessage(message1);
+    message2.setTitle("new title");
+    assertNotEquals(message1, message2);
+  }
+
+  @Test
+  public void hashCodeTest() {
+    assertEquals(MESSAGE1.hashCode(), MESSAGE2.hashCode());
+    assertNotEquals(MESSAGE2.hashCode(), MESSAGE3.hashCode());
   }
 }
