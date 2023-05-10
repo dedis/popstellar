b17fbe1a431272ac4d725deda472ee1b645cd5eb
Michael <michael.greub@epfl.ch>
2023-05-10 10:30:12 +0200

    Switch to id_token only + Some tests

--- demo/popcha-client/web/authentication.py
+++ demo/popcha-client/web/authentication.py
@@ -28,7 +28,7 @@ def get_url(server: str, lao_id: str, client_id: str) -> str:
     login_nonces[nonce] = (server, time.time())
     parameters = {
         "response_mode": "query",
-        "response_type": "id_token token",
+        "response_type": "id_token",
         "client_id": client_id,
         "redirect_uri": f"https://{server}/cb",
         "scope": "openid profile",
@@ -38,22 +38,30 @@ def get_url(server: str, lao_id: str, client_id: str) -> str:
     return f"https://{server}/authorize?{parse.urlencode(parameters)}"
 
 
-def validate_args(args: MultiDict[str, str], client_id: str) \
+def validate_args(args: MultiDict[str, str], client_id: str,
+                  auth_server_pub_key: str) \
         -> str | None:
     """
     Validate the args as if they were coming from OpenID Authentication request
     :param args: A MultiDict containing the arguments (almost the same as a
     dict).
-    :param client_id:
+    :param client_id: The ID of this client
+    :param auth_server_pub_key: The public key of the authentication server
     :return: The unique user identifier if logged in None otherwise
     """
-    missing_arg: bool = ("access_token" not in args or "id_token" not in args
-                         or "token_type" not in args)
+    missing_arg: bool = ("id_token" not in args or "token_type" not in args)
     if missing_arg:
         return None
-    token: dict = jwt.decode(args.get("id_token", type=str))
 
-    valid_client_id = client_id == token.get("aud") == client_id
+    token: dict = jwt.decode(jwt=args.get("id_token", type=str),
+                             key=auth_server_pub_key,
+                             algorithms=['RS256', 'RS384', 'RS512'],
+                             audience=client_id,
+                             leeway= 3127680000,
+                             options={"verify_signature":False}
+                             )
+
+    valid_client_id = client_id in token.get("aud")
     if not valid_client_id:
         return None
 
@@ -61,8 +69,8 @@ def validate_args(args: MultiDict[str, str], client_id: str) \
     if not valid_provided_nonce:
         return None
 
-    nonce_data: (str, float) = login_nonces.pop(token.get("nonce", None), None)
-    valid_issuer = nonce_data[0] == token.get("iss", None)
+    nonce_data: (str, float) = login_nonces.pop(token.get("nonce"))
+    valid_issuer = nonce_data[0] == token.get("iss")
     if not valid_issuer:
         return None
     # Nonce is not older than 5 minutes
--- demo/popcha-client/web/tests/test_authentication.py
+++ demo/popcha-client/web/tests/test_authentication.py
@@ -1,6 +1,9 @@
 """
 Test file for authentication.py
 """
+from werkzeug.datastructures import MultiDict
+from time import time
+
 import authentication
 
 
@@ -25,9 +28,33 @@ class TestAuthenticationResponse:
     """
     Simple test class for the authentication response related functions
     """
+    jwt: str = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Nl" \
+               "cnZlci5leGFtcGxlLmNvbSIsInN1YiI6IjEyMzQ1Njc4IiwiYXVkIjpbImNsM" \
+               "WVudCJdLCJleHAiOjE2ODM2NDY0NjAsImlhdCI6MTY4MzY0Mjg2MH0.cSfvw8" \
+               "zhBXNJ6YWrtb7e2hBRye45q5eXpBWRia1KMEwz9E3Ka2qlsLRNQ7VpTJrBDL-" \
+               "so_ESOUwDLl34JjJCvJWxtLg56r1jUQNyp8CTKX64xWT9kBFOF8VNi45MpAVX" \
+               "fgY7TkLgaSiqZzboAP8q_P8rB1FFgHN42FcRvLFIT6u7KkOyiyfo_yba0DL5g" \
+               "dHGDiORoG9O-DUZNr0HAyj2TZHqDn81j8Rg2eZ4w8yzjFu4wERsvlYyR1wL2-" \
+               "tBGI25XSAbCLjrLr4vLjDrl6Ztw7LdlD1bMyLNShnjtg0dArkNw07LtQmxLMb" \
+               "AblYEOXD1QFhwiBoLA21TDKl1KibbMQ"
+    pub_key: str = b"-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOC" \
+                   b"AQ8AMIIBCgKCAQEAsbR9Ip84tR4vc1IEefBJdHMlQAQm1UltYE3vs875" \
+                   b"eY8ASZ4lzlLG6iVRe7LH4VN6j7GB4Tjj2EtgUFUAQqbFs5mn7cFO7DR9" \
+                   b"riQDgLGekAQ5g/mLz9QhuAGjU2am0mPBOSBME08Ek9vNRfAOGVWk9fDU" \
+                   b"hdRceRdKOXnnz+YvqYfe3vz4jx9XXJHZHmG2wNB6egCnsbZOuEqiWVMj" \
+                   b"5+3wKt1prUGKEAHtPqC+olDaLwZw1didYotPgaZDwedkcAVSWNHvOkkY" \
+                   b"3uMqvKI+CpoxP+uqtdy9tM54sNQjoWdq4LIaWF/nLRy5fM2JAVbAqwPW" \
+                   b"6z23YMi4HIsfwuj+d8UZtQIDAQAB\n-----END PUBLIC KEY-----"
+    args: MultiDict[str, str] = MultiDict({
+        "id_token": jwt,
+        "token_type": "bearer"
+    })
+
 
     def test_validate_args_validates_a_correct_one(self):
-        assert 0 == 0
+        authentication.login_nonces["n0nce"] = ("https://server.example.com", time())
+        assert authentication.validate_args(self.args, "cl1ent",
+                                            self.pub_key) is not None
 
     def test_validate_args_fails_if_a_required_arg_is_missing(self):
         assert 0 == 0
