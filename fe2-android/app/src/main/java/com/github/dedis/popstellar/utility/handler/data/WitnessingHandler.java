e691ef889dc8ad4610d388bc13bf57a6285d57bd
kalberma <simone.kalbermatter@epfl.ch>
2023-05-16 17:41:29 +0200

    implement witness message signature handler

--- fe2-android/app/src/main/java/com/github/dedis/popstellar/utility/handler/data/WitnessingHandler.java
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/utility/handler/data/WitnessingHandler.java
@@ -1,7 +1,13 @@
 package com.github.dedis.popstellar.utility.handler.data;
 
 import com.github.dedis.popstellar.model.network.method.message.data.message.WitnessMessageSignature;
+import com.github.dedis.popstellar.model.objects.*;
+import com.github.dedis.popstellar.model.objects.security.MessageID;
+import com.github.dedis.popstellar.model.objects.security.PublicKey;
+import com.github.dedis.popstellar.model.objects.view.LaoView;
 import com.github.dedis.popstellar.repository.LAORepository;
+import com.github.dedis.popstellar.utility.error.DataHandlingException;
+import com.github.dedis.popstellar.utility.error.UnknownLaoException;
 
 import javax.inject.Inject;
 
@@ -23,7 +29,31 @@ public class WitnessingHandler {
    * @param witnessMessageSignature the message that was received
    */
   public void handleWitnessMessageSignature(
-      HandlerContext context, WitnessMessageSignature witnessMessageSignature) {
-    Timber.tag(TAG).d("WitnessMessageSignature handler not implemented");
+      HandlerContext context, WitnessMessageSignature witnessMessageSignature)
+      throws UnknownLaoException, DataHandlingException {
+    Timber.tag(TAG)
+        .d("Received a WitnessMessageSignature Broadcast msg: %s", witnessMessageSignature);
+    Channel channel = context.getChannel();
+    LaoView laoView = laoRepo.getLaoViewByChannel(channel);
+    PublicKey witnessPk = context.getSenderPk();
+
+    if (!laoView.getWitnesses().contains(witnessPk)) {
+      throw new DataHandlingException(
+          witnessMessageSignature, "The sender of the WitnessMessageSignature is not a witness");
+    }
+
+    MessageID messageID = witnessMessageSignature.getMessageId();
+    WitnessMessage witnessMessage = laoView.getWitnessMessages().get(messageID);
+    if (witnessMessage == null) {
+      throw new DataHandlingException(
+          witnessMessageSignature,
+          "No witness signature with message id " + messageID + " exists in the lao");
+    }
+
+    witnessMessage.addWitness(witnessPk);
+    Lao lao = laoView.createLaoCopy();
+    lao.updateWitnessMessage(messageID, witnessMessage);
+
+    laoRepo.updateLao(lao);
   }
 }
