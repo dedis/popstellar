c795a319238ea1bb1dd5182627ae4d417cca12de
Michael Greub <michael.greub@epfl.ch>
2023-05-18 12:40:10 +0200

    Updat the run.sh

--- demo/popcha-client/web/run.sh
+++ demo/popcha-client/web/run.sh
@@ -1,8 +1,13 @@
+#!/bin/sh
 echo "Creating Virtual environment"
 python -m venv venv;
 echo "Entering Virtual environment"
 source venv/bin/activate;
 echo "Installing dependencies"
 pip install -r requirements.txt;
-echo "Starting server"
+if [ $# -eq 2 ] ; then
+    echo "Creating config file (This will reset your client id)"
+    printf "{\"client_id\":\"\", \"host_url\": \"$1\", \"host_port\": $2}" > data/config.json
+fi
+echo "Starting server with $# arguments"
 gunicorn -w 4 --chdir src "main:app" | tee -a log.out;
