c6942ff4cb89c968f93b04045de6bf1785c6d01e
BelghmiAmine <amine.belghmi@epfl.ch>
2023-05-24 11:33:00 +0200

    resolved nitpick.

--- be2-scala/src/main/scala/ch/epfl/pop/model/network/MethodType.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/model/network/MethodType.scala
@@ -5,17 +5,15 @@ object MethodType extends Enumeration {
 
   // uninitialized placeholder
   val INVALID: Value = MatchingValue("__INVALID_METHOD__")
-
   val BROADCAST: Value = MatchingValue("broadcast")
   val PUBLISH: Value = MatchingValue("publish")
   val SUBSCRIBE: Value = MatchingValue("subscribe")
   val UNSUBSCRIBE: Value = MatchingValue("unsubscribe")
   val CATCHUP: Value = MatchingValue("catchup")
-
   val HEARTBEAT: Value = MatchingValue("heartbeat")
   val GET_MESSAGES_BY_ID: Value = MatchingValue("get_messages_by_id")
-
   val GREET_SERVER: Value = MatchingValue("greet_server")
+
   def MatchingValue(v: String): Value with Matching = new Val(nextId, v) with Matching
 
   def unapply(s: String): Option[Value] = values.find(s == _.toString)
