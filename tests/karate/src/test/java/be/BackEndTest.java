4da9e8e771c67319dfde94c349cba77e79720db1
kalberma <simone.kalbermatter@epfl.ch>
2023-05-25 22:40:25 +0200

    add documentation

--- tests/karate/src/test/java/be/BackEndTest.java
+++ tests/karate/src/test/java/be/BackEndTest.java
@@ -25,7 +25,7 @@ public class BackEndTest {
 
 
   /**
-   * This test will execute the tests for back-ends connected to another backend with the Go back-end
+   * This test will execute the tests for back-ends connected to another back-end with the Go back-end
    * It will not generate a clean report, but it can be used in development
    *
    * @return the Karate builder
@@ -51,7 +51,7 @@ public class BackEndTest {
   }
 
   /**
-   * This test will execute the tests for back-ends connected to another backend with the Scala back-end
+   * This test will execute the tests for back-ends connected to another back-end with the Scala back-end
    * It will not generate a clean report, but it can be used in development
    *
    * @return the Karate builder
