cfd294722d4195574202bcf96f5549b4357e9f32
dayan9265 <82887324+dayan9265@users.noreply.github.com>
2023-06-14 14:24:42 +0200

    remove useless valueof

--- fe1-web/src/features/popcha/network/PopchaMessageApi.ts
+++ fe1-web/src/features/popcha/network/PopchaMessageApi.ts
@@ -34,7 +34,7 @@ export const sendPopchaAuthRequest = (
     const signedToken = t.sign(nonceEnc);
     const popchaChannel = getPopchaAuthenticationChannel(laoId);
     const message = new PopchaAuthMsg({
-      client_id: client_id.valueOf(),
+      client_id: client_id,
       nonce: nonceEnc.valueOf(),
       identifier: t.publicKey,
       identifier_proof: signedToken,
