2346dfd5e2c0fedb3e52a0bf8a48c1858530343d
matteosz <mattsuez01@gmail.com>
2023-12-28 17:13:26 +0100

    Migrated utility.security files to Kotlin

--- fe2-android/app/src/main/java/com/github/dedis/popstellar/utility/security/package-info.kt
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/utility/security/package-info.kt
@@ -2,4 +2,4 @@
  * The package security contains the utility classes for hashing, signing and retrieving the public
  * key of the user.
  */
-package com.github.dedis.popstellar.utility.security;
+package com.github.dedis.popstellar.utility.security
