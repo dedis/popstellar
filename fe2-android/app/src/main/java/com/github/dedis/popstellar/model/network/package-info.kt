ad14d8b5773d95334086f41dd35b0962d95c0047
matteosz <mattsuez01@gmail.com>
2024-01-23 10:51:25 +0100

    Network generic methods refactoring

---
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/model/network/package-info.kt
@@ -0,0 +1,7 @@
+/**
+ * Classes used to model a publish/subscribe communication
+ *
+ *
+ * Every data class defined here is expected to be immutable
+ */
+package com.github.dedis.popstellar.model.network
