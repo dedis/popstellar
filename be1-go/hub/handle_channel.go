4733d4880f19256e1ef81f31d13e76b8faf17796
stuart <stuart.gueissaz@epfl.ch>
2024-03-29 19:26:49 +0100

    Update handle_channel.go error msgs

--- be1-go/hub/handle_channel.go
+++ be1-go/hub/handle_channel.go
@@ -12,33 +12,33 @@ import (
 func handleChannel(params handlerParameters, channelType string, msg message.Message) *answer.Error {
 	dataBytes, err := base64.URLEncoding.DecodeString(msg.Data)
 	if err != nil {
-		return answer.NewInvalidMessageFieldError("handleChannel: failed to decode data string: %v", err)
+		return answer.NewInvalidMessageFieldError("failed to decode data string: %v", err).Wrap("handleChannel")
 	}
 
 	publicKeySender, err := base64.URLEncoding.DecodeString(msg.Sender)
 	if err != nil {
-		return answer.NewInvalidMessageFieldError("handleChannel: failed to decode public key string: %v", err)
+		return answer.NewInvalidMessageFieldError("failed to decode public key string: %v", err).Wrap("handleChannel")
 	}
 
 	signatureBytes, err := base64.URLEncoding.DecodeString(msg.Signature)
 	if err != nil {
-		return answer.NewInvalidMessageFieldError("handleChannel: failed to decode signature string: %v", err)
+		return answer.NewInvalidMessageFieldError("failed to decode signature string: %v", err).Wrap("handleChannel")
 	}
 
 	err = schnorr.VerifyWithChecks(crypto.Suite, publicKeySender, dataBytes, signatureBytes)
 	if err != nil {
-		return answer.NewInvalidMessageFieldError("handleChannel: failed to verify signature : %v", err)
+		return answer.NewInvalidMessageFieldError("failed to verify signature : %v", err).Wrap("handleChannel")
 	}
 
 	expectedMessageID := messagedata.Hash(msg.Data, msg.Signature)
 	if expectedMessageID != msg.MessageID {
-		return answer.NewInvalidActionError("handleChannel: message_id is wrong: expected %q found %q",
-			expectedMessageID, msg.MessageID)
+		return answer.NewInvalidActionError("message_id is wrong: expected %q found %q",
+			expectedMessageID, msg.MessageID).Wrap("handleChannel")
 	}
 
 	_, err = params.db.GetMessageByID(msg.MessageID)
 	if err == nil {
-		return answer.NewInvalidActionError("handleChannel: message %s was already received", msg.MessageID)
+		return answer.NewInvalidActionError("message %s was already received", msg.MessageID).Wrap("handleChannel")
 	}
 
 	var errAnswer *answer.Error
