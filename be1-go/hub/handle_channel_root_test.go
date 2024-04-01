e840671c41bbac97fa44fa7532762e076d8b9497
emonnin-epfl <etienne.monnin@epfl.ch>
2024-04-01 17:47:58 +0200

    add remaining tests to Test_verifyLaoCreation()

--- be1-go/hub/handle_channel_root_test.go
+++ be1-go/hub/handle_channel_root_test.go
@@ -98,11 +98,10 @@ func Test_handleChannelRoot(t *testing.T) {
 
 func Test_verifyLaoCreation(t *testing.T) {
 	keypair := generateKeyPair(t)
+	wrongKeyPair := generateKeyPair(t)
 	now := time.Now().Unix()
 	name := "LAO X"
 	laoID := messagedata.Hash(base64.URLEncoding.EncodeToString(keypair.publicBuf), fmt.Sprintf("%d", now), name)
-
-	// Test 1: verifyLaoCreation should return a "duplicate" error when the lao already exists
 
 	laoCreate := messagedata.LaoCreate{
 		Object:    messagedata.LAOObject,
@@ -129,30 +128,167 @@ func Test_verifyLaoCreation(t *testing.T) {
 		MessageID:         messagedata.Hash(dataBase64, signatureBase64),
 		WitnessSignatures: []message.WitnessSignature{},
 	}
-
 	laoPath := rootPrefix + laoID
-	mockRepository := mocks.NewRepository(t)
-	mockRepository.On("HasChannel", laoPath).Return(true, nil)
-	params := newHandlerParameters(mockRepository)
-	_, errAnswer := verifyLAOCreation(params, msg, laoCreate, laoPath)
+
+	// Test 1: error when the lao id is not base64URL encoded
+	wrongLaoCreate := laoCreate
+	wrongLaoCreate.ID = "wrongID"
+
+	params := newHandlerParameters(nil)
+
+	_, errAnswer := verifyLAOCreation(params, msg, wrongLaoCreate, laoPath)
 	fmt.Println(errAnswer)
 	require.Error(t, errAnswer)
 
-	// Test 2: verifyLaoCreation should return an error when the lao id is not base64URL encoded
+	// Test 2: error when the lao id is not the expected one
+	wrongLaoCreate = laoCreate
+	wrongLaoCreate.ID = base64.URLEncoding.EncodeToString([]byte("wrongID"))
 
-	wrongKeyPair := generateKeyPair(t)
-	msgWrongSender := message.Message{
-		Data:              dataBase64,
-		Sender:            base64.URLEncoding.EncodeToString(wrongKeyPair.publicBuf),
-		Signature:         signatureBase64,
-		MessageID:         messagedata.Hash(dataBase64, signatureBase64),
-		WitnessSignatures: []message.WitnessSignature{},
-	}
+	params = newHandlerParameters(nil)
+
+	_, errAnswer = verifyLAOCreation(params, msg, wrongLaoCreate, laoPath)
+	fmt.Println(errAnswer)
+	require.Error(t, errAnswer)
+
+	// Test 3: error when the lao name is empty
+	wrongLaoCreate = laoCreate
+	wrongLaoCreate.Name = ""
+	wrongLaoCreate.ID = messagedata.Hash(wrongLaoCreate.Organizer, fmt.Sprintf("%d", wrongLaoCreate.Creation), wrongLaoCreate.Name)
+
+	params = newHandlerParameters(nil)
+
+	_, errAnswer = verifyLAOCreation(params, msg, wrongLaoCreate, laoPath)
+	fmt.Println(errAnswer)
+	require.Error(t, errAnswer)
+
+	// Test 4: error when the lao creation is negative
+	wrongLaoCreate = laoCreate
+	wrongLaoCreate.Creation = -1
+	wrongLaoCreate.ID = messagedata.Hash(wrongLaoCreate.Organizer, fmt.Sprintf("%d", wrongLaoCreate.Creation), wrongLaoCreate.Name)
+
+	params = newHandlerParameters(nil)
+
+	_, errAnswer = verifyLAOCreation(params, msg, wrongLaoCreate, laoPath)
+	fmt.Println(errAnswer)
+	require.Error(t, errAnswer)
+
+	// Test 5: error when the organizer is not base64URL encoded
+	wrongLaoCreate = laoCreate
+	wrongLaoCreate.Organizer = "wrongOrganizer"
+	wrongLaoCreate.ID = messagedata.Hash(wrongLaoCreate.Organizer, fmt.Sprintf("%d", wrongLaoCreate.Creation), wrongLaoCreate.Name)
+
+	params = newHandlerParameters(nil)
+
+	_, errAnswer = verifyLAOCreation(params, msg, wrongLaoCreate, laoPath)
+	fmt.Println(errAnswer)
+	require.Error(t, errAnswer)
+
+	// Test 6: error when a witness is not base64URL encoded
+	wrongLaoCreate = laoCreate
+	wrongLaoCreate.Witnesses = []string{"a wrong witness"}
+
+	params = newHandlerParameters(nil)
+
+	_, errAnswer = verifyLAOCreation(params, msg, wrongLaoCreate, laoPath)
+	fmt.Println(errAnswer)
+	require.Error(t, errAnswer)
+
+	// Test 7: error when the lao already exists
+	mockRepository := mocks.NewRepository(t)
+	mockRepository.On("HasChannel", laoPath).Return(true, nil)
+	params = newHandlerParameters(mockRepository)
+
+	_, errAnswer = verifyLAOCreation(params, msg, laoCreate, laoPath)
+	fmt.Println(errAnswer)
+	require.Error(t, errAnswer)
+
+	// Test 8: error when querying the channel
+	mockRepository = mocks.NewRepository(t)
+	mockRepository.On("HasChannel", laoPath).Return(false, fmt.Errorf("db is disconnected"))
+	params = newHandlerParameters(mockRepository)
+
+	_, errAnswer = verifyLAOCreation(params, msg, laoCreate, laoPath)
+	fmt.Println(errAnswer)
+	require.Error(t, errAnswer)
+
+	// Test 9: error when the sender's public key is not base64URL encoded
+	wrongMsg := msg
+	wrongMsg.Sender = "wrongSender"
+
 	mockRepository = mocks.NewRepository(t)
 	mockRepository.On("HasChannel", laoPath).Return(false, nil)
 	params = newHandlerParameters(mockRepository)
-	_, errAnswer = verifyLAOCreation(params, msgWrongSender, laoCreate, laoPath)
+
+	_, errAnswer = verifyLAOCreation(params, wrongMsg, laoCreate, laoPath)
 	fmt.Println(errAnswer)
 	require.Error(t, errAnswer)
 
+	// Test 10: error when the sender's public key is not unmarshable using Kyber
+	wrongMsg = msg
+	wrongMsg.Sender = base64.URLEncoding.EncodeToString([]byte("wrongSender"))
+
+	mockRepository = mocks.NewRepository(t)
+	mockRepository.On("HasChannel", laoPath).Return(false, nil)
+	params = newHandlerParameters(mockRepository)
+
+	_, errAnswer = verifyLAOCreation(params, wrongMsg, laoCreate, laoPath)
+	fmt.Println(errAnswer)
+	require.Error(t, errAnswer)
+
+	// Test 11: error when the organizer's public key is not base64URL encoded
+	wrongLaoCreate = laoCreate
+	wrongLaoCreate.Organizer = "wrongOrganizer"
+	wrongLaoCreate.ID = messagedata.Hash(wrongLaoCreate.Organizer, fmt.Sprintf("%d", wrongLaoCreate.Creation), wrongLaoCreate.Name)
+
+	params = newHandlerParameters(nil)
+
+	_, errAnswer = verifyLAOCreation(params, msg, wrongLaoCreate, laoPath)
+	fmt.Println(errAnswer)
+	require.Error(t, errAnswer)
+
+	// Test 12: error when the organizer's public key is not unmarshable using Kyber
+	wrongLaoCreate = laoCreate
+	wrongLaoCreate.Organizer = base64.URLEncoding.EncodeToString([]byte("wrongOrganizer"))
+	wrongLaoCreate.ID = messagedata.Hash(wrongLaoCreate.Organizer, fmt.Sprintf("%d", wrongLaoCreate.Creation), wrongLaoCreate.Name)
+
+	mockRepository = mocks.NewRepository(t)
+	mockRepository.On("HasChannel", laoPath).Return(false, nil)
+	params = newHandlerParameters(mockRepository)
+
+	_, errAnswer = verifyLAOCreation(params, msg, wrongLaoCreate, laoPath)
+	fmt.Println(errAnswer)
+	require.Error(t, errAnswer)
+
+	// Test 13: error when the organizer's public key is not the same as the sender's public key
+	wrongLaoCreate = laoCreate
+	wrongLaoCreate.Organizer = base64.URLEncoding.EncodeToString(wrongKeyPair.publicBuf)
+	wrongLaoCreate.ID = messagedata.Hash(wrongLaoCreate.Organizer, fmt.Sprintf("%d", wrongLaoCreate.Creation), wrongLaoCreate.Name)
+
+	mockRepository = mocks.NewRepository(t)
+	mockRepository.On("HasChannel", laoPath).Return(false, nil)
+	params = newHandlerParameters(mockRepository)
+
+	_, errAnswer = verifyLAOCreation(params, msg, wrongLaoCreate, laoPath)
+	fmt.Println(errAnswer)
+	require.Error(t, errAnswer)
+
+	// Test 14: error when querying the owner's public key
+	mockRepository = mocks.NewRepository(t)
+	mockRepository.On("HasChannel", laoPath).Return(false, nil)
+	mockRepository.On("GetOwnerPubKey").Return(nil, fmt.Errorf("db is disconnected"))
+	params = newHandlerParameters(mockRepository)
+
+	_, errAnswer = verifyLAOCreation(params, msg, laoCreate, laoPath)
+	fmt.Println(errAnswer)
+	require.Error(t, errAnswer)
+
+	// Test 15: error when the owner's public key is not the same as the sender's public key
+	mockRepository = mocks.NewRepository(t)
+	mockRepository.On("HasChannel", laoPath).Return(false, nil)
+	mockRepository.On("GetOwnerPubKey").Return(wrongKeyPair.public, nil)
+	params = newHandlerParameters(mockRepository)
+
+	_, errAnswer = verifyLAOCreation(params, msg, laoCreate, laoPath)
+	fmt.Println(errAnswer)
+	require.Error(t, errAnswer)
 }
