3497607ac26ad9d5e0b590eaf9b91653a5522b31
Daniel Tavares Agostinho <comptesdaniel@gmail.com>
2024-04-03 15:03:29 +0200

    added rumor to existing methods

--- be2-scala/src/main/scala/ch/epfl/pop/model/network/MethodType.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/model/network/MethodType.scala
@@ -10,16 +10,18 @@ enum MethodType:
   case heartbeat extends MethodType
   case get_messages_by_id extends MethodType
   case greet_server extends MethodType
+  case rumor extends MethodType
 
 object MethodType:
   def apply(method: String): MethodType =
     method match
-      case "broadcast"          => broadcast
-      case "publish"            => publish
-      case "subscribe"          => subscribe
-      case "unsubscribe"        => unsubscribe
-      case "catchup"            => catchup
-      case "heartbeat"          => heartbeat
+      case "broadcast" => broadcast
+      case "publish" => publish
+      case "subscribe" => subscribe
+      case "unsubscribe" => unsubscribe
+      case "catchup" => catchup
+      case "heartbeat" => heartbeat
       case "get_messages_by_id" => get_messages_by_id
-      case "greet_server"       => greet_server
-      case _                    => INVALID
+      case "greet_server" => greet_server
+      case "rumor" => rumor
+      case _ => INVALID
