acb0a56950d02136d6c162f29160e90a5bf84261
stuart <stuart.gueissaz@epfl.ch>
2024-04-03 14:42:26 +0200

    Split in two handleGetMessagesByIDAnswer

--- be1-go/hub/handle_message_answer.go
+++ be1-go/hub/handle_message_answer.go
@@ -10,32 +10,49 @@ import (
 const maxRetry = 10
 
 func handleGetMessagesByIDAnswer(params handlerParameters, msg answer.Answer) *answer.Error {
+	blacklist, errorAnswers := handleMessagesByChannel(params, msg)
+	if len(errorAnswers) != 0 {
+		for _, errorAnswer := range errorAnswers {
+			params.log.Error().Msg(errorAnswer.Error())
+		}
+	}
+
+	errAnswer := params.db.AddNewBlackList(blacklist)
+	if errAnswer != nil {
+		errAnswer = errAnswer.Wrap("handleGetMessagesByIDAnswer")
+	}
+
+	return nil
+}
+
+func handleMessagesByChannel(params handlerParameters, msg answer.Answer) (map[string]map[string]message.Message, []*answer.Error) {
 	result := msg.Result.GetMessagesByChannel()
-	resultMsgs := make(map[string]map[string]message.Message)
+	blacklist := make(map[string]map[string]message.Message)
+	errorAnswers := make([]*answer.Error, 0)
 
 	// Unmarshal each message
 	for channelID, rawMsgs := range result {
-		resultMsgs[channelID] = make(map[string]message.Message)
+		blacklist[channelID] = make(map[string]message.Message)
 		for _, rawMsg := range rawMsgs {
 			var msg message.Message
 			err := json.Unmarshal(rawMsg, &msg)
 			if err == nil {
-				resultMsgs[channelID][msg.MessageID] = msg
+				blacklist[channelID][msg.MessageID] = msg
 				continue
 			}
 
-			errAnswer := answer.NewInvalidMessageFieldError("failed to unmarshal: %v", err).Wrap("handleGetMessagesByIDAnswer")
-			params.log.Error().Msg(errAnswer.Error())
+			errorAnswers = append(errorAnswers, answer.NewInvalidMessageFieldError("failed to unmarshal: %v",
+				err).Wrap("handleGetMessagesByIDAnswer"))
 		}
 
-		if len(resultMsgs[channelID]) == 0 {
-			delete(resultMsgs, channelID)
+		if len(blacklist[channelID]) == 0 {
+			delete(blacklist, channelID)
 		}
 	}
 
 	// Sort by channelID length
 	sortedChannelIDs := make([]string, 0)
-	for channelID := range resultMsgs {
+	for channelID := range blacklist {
 		sortedChannelIDs = append(sortedChannelIDs, channelID)
 	}
 	sort.Slice(sortedChannelIDs, func(i, j int) bool {
@@ -45,37 +62,31 @@ func handleGetMessagesByIDAnswer(params handlerParameters, msg answer.Answer) *a
 	// Handle every messages
 	for i := 0; i < maxRetry; i++ {
 		for j, channelID := range sortedChannelIDs {
-			msgs := resultMsgs[channelID]
+			msgs := blacklist[channelID]
 			for msgID, msg := range msgs {
 				errAnswer := handleChannel(params, channelID, msg)
 				if errAnswer == nil {
-					delete(resultMsgs[channelID], msgID)
+					delete(blacklist[channelID], msgID)
 					continue
 				}
 
 				if errAnswer.Code == answer.InvalidMessageFieldErrorCode {
-					delete(resultMsgs[channelID], msgID)
+					delete(blacklist[channelID], msgID)
 				}
 
-				errAnswer = errAnswer.Wrap("handleGetMessagesByIDAnswer")
-				params.log.Error().Msgf(errAnswer.Error())
+				errorAnswers = append(errorAnswers, errAnswer.Wrap(msgID).Wrap("handleGetMessagesByIDAnswer"))
 			}
 
-			if len(resultMsgs[channelID]) == 0 {
-				delete(resultMsgs, channelID)
+			if len(blacklist[channelID]) == 0 {
+				delete(blacklist, channelID)
 				sortedChannelIDs = append(sortedChannelIDs[:j], sortedChannelIDs[j+1:]...)
 			}
 		}
 
-		if len(resultMsgs) == 0 {
-			return nil
+		if len(blacklist) == 0 {
+			break
 		}
 	}
 
-	errAnswer := params.db.AddNewBlackList(resultMsgs)
-	if errAnswer != nil {
-		errAnswer = errAnswer.Wrap("handleGetMessagesByIDAnswer")
-	}
-
-	return nil
+	return blacklist, errorAnswers
 }
