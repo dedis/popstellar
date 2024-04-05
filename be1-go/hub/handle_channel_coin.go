3a71002178e1c8cf3063196606a13092a4c8c68c
stuart <stuart.gueissaz@epfl.ch>
2024-04-05 17:07:47 +0200

    Implement handle_channel_coin.go

--- be1-go/hub/handle_channel_coin.go
+++ be1-go/hub/handle_channel_coin.go
@@ -1,9 +1,11 @@
 package hub
 
 import (
+	"encoding/base64"
 	"popstellar/message/answer"
 	"popstellar/message/messagedata"
 	"popstellar/message/query/method/message"
+	"popstellar/validation"
 )
 
 func handleChannelCoin(params handlerParameters, channel string, msg message.Message) *answer.Error {
@@ -15,7 +17,7 @@ func handleChannelCoin(params handlerParameters, channel string, msg message.Mes
 
 	switch object + "#" + action {
 	case messagedata.CoinObject + "#" + messagedata.CoinActionPostTransaction:
-		errAnswer = handleCoinPostTransaction(msg, params)
+		errAnswer = handleCoinPostTransaction(params, msg)
 	default:
 		errAnswer = answer.NewInvalidMessageFieldError("failed to handle %s#%s, invalid object#action", object, action)
 	}
@@ -30,9 +32,46 @@ func handleChannelCoin(params handlerParameters, channel string, msg message.Mes
 		errAnswer = errAnswer.Wrap("handleChannelCoin")
 		return errAnswer
 	}
+
+	errAnswer = broadcastToAllClients(msg, params, channel)
+	if errAnswer != nil {
+		errAnswer = errAnswer.Wrap("handleChannelCoin")
+		return errAnswer
+	}
+
 	return nil
 }
 
-func handleCoinPostTransaction(msg message.Message, params handlerParameters) *answer.Error {
+func handleCoinPostTransaction(params handlerParameters, msg message.Message) *answer.Error {
+	jsonData, err := base64.URLEncoding.DecodeString(msg.Data)
+	if err != nil {
+		errAnswer := answer.NewInvalidMessageFieldError("failed to decode message data: %v", err).
+			Wrap("handleCoinPostTransaction")
+		return errAnswer
+	}
+
+	err = params.schemaValidator.VerifyJSON(jsonData, validation.Data)
+	if err != nil {
+		errAnswer := answer.NewInvalidMessageFieldError("failed to verify json schema: %w", err).
+			Wrap("handleCoinPostTransaction")
+		return errAnswer
+	}
+
+	var data messagedata.PostTransaction
+
+	err = msg.UnmarshalData(&data)
+	if err != nil {
+		errAnswer := answer.NewInvalidMessageFieldError("failed to unmarshal: %v", err).
+			Wrap("handleCoinPostTransaction")
+		return errAnswer
+	}
+
+	err = data.Verify()
+	if err != nil {
+		errAnswer := answer.NewInvalidMessageFieldError("invalid data: %v", err).
+			Wrap("handleCoinPostTransaction")
+		return errAnswer
+	}
+
 	return nil
 }
