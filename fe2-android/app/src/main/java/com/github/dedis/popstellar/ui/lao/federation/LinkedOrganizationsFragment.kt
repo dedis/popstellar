0003dcdea81f0823c5b3f99ebc3732f80ff82c6c
johan <johan.belloy@epfl.ch>
2024-04-11 13:52:41 +0200

    Added all buttons and other UI things

--- fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/lao/federation/LinkedOrganizationsFragment.kt
+++ fe2-android/app/src/main/java/com/github/dedis/popstellar/ui/lao/federation/LinkedOrganizationsFragment.kt
@@ -10,6 +10,9 @@ import com.github.dedis.popstellar.databinding.LinkedOrganizationsFragmentBindin
 import com.github.dedis.popstellar.model.Role
 import com.github.dedis.popstellar.ui.lao.LaoActivity.Companion.obtainViewModel
 import com.github.dedis.popstellar.ui.lao.LaoViewModel
+import com.github.dedis.popstellar.ui.lao.event.LaoDetailAnimation.rotateFab
+import com.github.dedis.popstellar.ui.lao.event.LaoDetailAnimation.showIn
+import com.github.dedis.popstellar.ui.lao.event.LaoDetailAnimation.showOut
 
 
 /**
@@ -22,23 +25,47 @@ class LinkedOrganizationsFragment : Fragment() {
     private lateinit var binding: LinkedOrganizationsFragmentBinding
     private lateinit var laoViewModel: LaoViewModel
 
+    private var buttonClicked = false
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         // Inflate the layout for this fragment
         binding = LinkedOrganizationsFragmentBinding.inflate(inflater, container, false)
         laoViewModel = obtainViewModel(requireActivity())
 
-        // Sets the text depending on the user's role
+        // Sets the text and the button depending on the user's role
         laoViewModel.role.observe(viewLifecycleOwner) {role: Role ->
-            binding.noOrganizationsText.setText(
-                    if(role == Role.ORGANIZER) R.string.no_organizations_organizer_text
-                    else R.string.no_organizations_non_organizer_text
-            )
+            if(role == Role.ORGANIZER) {
+                binding.noOrganizationsText.setText(R.string.no_organizations_organizer_text)
+                binding.addLinkedOrganization.visibility = View.VISIBLE
+            } else {
+                binding.noOrganizationsText.setText(R.string.no_organizations_non_organizer_text)
+                binding.addLinkedOrganization.visibility = View.GONE
+            }
         }
+
+        binding.addLinkedOrganization.setOnClickListener(observeButton)
 
         return binding.root
         //return inflater.inflate(R.layout.linked_organizations_fragment, container, false)
     }
+
+    //Changes the display when the + button is pressed
+    private var observeButton =
+            View.OnClickListener { v: View ->
+                buttonClicked = rotateFab(v, !buttonClicked)
+                if(buttonClicked) {
+                    showIn(binding.inviteOtherOrganization)
+                    showIn(binding.inviteOtherOrganizationText)
+                    showIn(binding.joinOtherOrganizationInvitation)
+                    showIn(binding.joinOtherOrganizationInvitationText)
+                } else {
+                    showOut(binding.inviteOtherOrganization)
+                    showOut(binding.inviteOtherOrganizationText)
+                    showOut(binding.joinOtherOrganizationInvitation)
+                    showOut(binding.joinOtherOrganizationInvitationText)
+                }
+            }
 
     companion object {
         @JvmStatic
