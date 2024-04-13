c0f8163986fcd57ba989b41197b86d4fa645299b
onsriahi14 <ons.riahi@epfl.ch>
2024-04-13 22:25:11 +0200

    commented non defined messages

--- protocol/test/main.js
+++ protocol/test/main.js
@@ -43,8 +43,8 @@ const message_data_federation_init_schema = require("../query/method/message/dat
 const message_data_federation_expect_schema = require("../query/method/message/data/dataFederationExpect.json")
 const message_data_federation_challenge_request_schema = require("../query/method/message/data/dataFederationChallengeRequest.json")
 const message_data_federation_challenge_schema = require("../query/method/message/data/dataFederationChallenge.json")
-const message_data_federation_authenticate_schema = require("../query/method/message/data/dataFederationAuthenticate.json")
-const message_data_federation_result_schema = require("../query/method/message/data/dataFederationResult.json")
+//const message_data_federation_authenticate_schema = require("../query/method/message/data/dataFederationAuthenticate.json")
+//const message_data_federation_result_schema = require("../query/method/message/data/dataFederationResult.json")
 
 const message_data_chirp_add_schema = require("../query/method/message/data/dataAddChirp.json");
 const message_data_chirp_notify_add_schema = require("../query/method/message/data/dataNotifyAddChirp.json");
@@ -114,8 +114,8 @@ ajv.addSchema([
     message_data_federation_expect_schema,
     message_data_federation_challenge_request_schema,
     message_data_federation_challenge_schema,
-    message_data_federation_authenticate_schema,
-    message_data_federation_result_schema,
+    //message_data_federation_authenticate_schema,
+    //message_data_federation_result_schema,
 
     message_data_chirp_notify_add_schema,
     message_data_chirp_add_schema,
