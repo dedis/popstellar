17563f7bdd29016a0ba372aa7dfd4e02e2666f07
johan <johan.belloy@epfl.ch>
2024-04-16 14:01:14 +0200

    Some tests for the LinkedOrganizations fragment

---
+++ fe2-android/app/src/test/framework/common/java/com/github/dedis/popstellar/testutils/pages/lao/fragment/LinkedOrganizationsFragmentPageObject.java
@@ -0,0 +1,27 @@
+package com.github.dedis.popstellar.testutils.pages.lao.fragment;
+
+import com.github.dedis.popstellar.R;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import androidx.test.espresso.ViewInteraction;
+
+public class LinkedOrganizationsFragmentPageObject {
+
+    private LinkedOrganizationsFragmentPageObject() {
+        throw new IllegalStateException("Page object");
+    }
+
+    public static ViewInteraction createLinkButton() {
+        return onView(withId(R.id.add_linked_organization));
+    }
+
+    public static ViewInteraction inviteButton() {
+        return onView(withId(R.id.invite_other_organization));
+    }
+
+    public static ViewInteraction joinInvitationButton() {
+        return onView(withId(R.id.join_other_organization_invitation));
+    }
+}
