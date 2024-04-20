65471042ba62d4d1f058796d500c3da6b34681b1
Thibault Czarniak <thibault.czarniak@epfl.ch>
2024-04-20 19:11:30 +0200

    Added interpolate strings to ArraysInFormat

--- be2-scala/linter/input/src/main/scala/fix/ArraysInFormat.scala
+++ be2-scala/linter/input/src/main/scala/fix/ArraysInFormat.scala
@@ -8,10 +8,12 @@ import java.io.FileNotFoundException
 object ArraysInFormat {
   def test() = {
     val array = new Array[String](3)
+    val str = f"Here are my cool elements ${array}" // assert: ArraysInFormat
+    val str2 = s"Here are my cool elements ${array}" // assert: ArraysInFormat
     String.format("Here are my cool elements %d", array) // assert: ArraysInFormat
     String.format("Here are my cool elements %d", array) /* assert: ArraysInFormat
                                                   ^^^^^
-    Array passed to String.format
+    Array passed to format / interpolate string
     */
   }
 
