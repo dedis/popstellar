ce7bc9f24dabf3984e546a35bf773ee651ab37e5
stuart <stuart.gueissaz@epfl.ch>
2024-05-10 15:14:20 +0200

    reduce complexity of getMessagesByIDAnswer

--- be1-go/internal/popserver/handler/answer.go
+++ be1-go/internal/popserver/handler/answer.go
@@ -78,38 +78,47 @@ func handleMessagesByChannel(msgsByChannel map[string]map[string]message.Message
 	// Handle every messages
 	for i := 0; i < maxRetry; i++ {
 		// Sort by channelID length
-		sortedChannelIDs := make([]string, 0)
-		for channelID := range msgsByChannel {
-			sortedChannelIDs = append(sortedChannelIDs, channelID)
-		}
-		sort.Slice(sortedChannelIDs, func(i, j int) bool {
-			return len(sortedChannelIDs[i]) < len(sortedChannelIDs[j])
-		})
+		sortedChannelIDs := getSortedChannels(msgsByChannel)
 
-		for _, channelID := range sortedChannelIDs {
-			msgs := msgsByChannel[channelID]
-			for msgID, msg := range msgs {
-				errAnswer := handleChannel(channelID, msg)
-				if errAnswer == nil {
-					delete(msgsByChannel[channelID], msgID)
-					continue
-				}
-
-				if errAnswer.Code == answer.InvalidMessageFieldErrorCode {
-					delete(msgsByChannel[channelID], msgID)
-				}
-
-				errAnswer = errAnswer.Wrap(msgID).Wrap("handleGetMessagesByIDAnswer")
-				utils.LogError(errAnswer)
-			}
-
-			if len(msgsByChannel[channelID]) == 0 {
-				delete(msgsByChannel, channelID)
-			}
-		}
+		TryToHandleMessages(msgsByChannel, sortedChannelIDs)
 
 		if len(msgsByChannel) == 0 {
 			return
 		}
 	}
+}
+
+func TryToHandleMessages(msgsByChannel map[string]map[string]message.Message, sortedChannelIDs []string) {
+	for _, channelID := range sortedChannelIDs {
+		msgs := msgsByChannel[channelID]
+		for msgID, msg := range msgs {
+			errAnswer := handleChannel(channelID, msg)
+			if errAnswer == nil {
+				delete(msgsByChannel[channelID], msgID)
+				continue
+			}
+
+			if errAnswer.Code == answer.InvalidMessageFieldErrorCode {
+				delete(msgsByChannel[channelID], msgID)
+			}
+
+			errAnswer = errAnswer.Wrap(msgID).Wrap("handleGetMessagesByIDAnswer")
+			utils.LogError(errAnswer)
+		}
+
+		if len(msgsByChannel[channelID]) == 0 {
+			delete(msgsByChannel, channelID)
+		}
+	}
+}
+
+func getSortedChannels(msgsByChannel map[string]map[string]message.Message) []string {
+	sortedChannelIDs := make([]string, 0)
+	for channelID := range msgsByChannel {
+		sortedChannelIDs = append(sortedChannelIDs, channelID)
+	}
+	sort.Slice(sortedChannelIDs, func(i, j int) bool {
+		return len(sortedChannelIDs[i]) < len(sortedChannelIDs[j])
+	})
+	return sortedChannelIDs
 }
