a043fd9217bb0663f943b1e4ce75e9247b5b7ecd
Omar Majzoub <omarmajzoub1@hotmail.com>
2024-05-12 16:30:15 +0200

    Added PagedCatchup object to method folder in network

---
+++ be2-scala/src/main/scala/ch/epfl/pop/model/network/method/PagedCatchup.scala
@@ -0,0 +1,24 @@
+package ch.epfl.pop.model.network.method
+
+import ch.epfl.pop.json.HighLevelProtocol.PagedCatchupFormat
+import ch.epfl.pop.model.network.Parsable
+import ch.epfl.pop.model.objects.{Channel}
+import spray.json._
+
+final case class PagedCatchup(channel: Channel, numberOfMessages: Int, beforeMessageID: String) extends Params {
+
+  override def hasChannel: Boolean = true
+
+  override def hasMessage: Boolean = false
+
+}
+
+object PagedCatchup extends Parsable {
+
+  def apply(channel: Channel, numberOfMessages: Int, beforeMessageID: String): PagedCatchup = {
+    new PagedCatchup(channel, numberOfMessages, beforeMessageID)
+  }
+
+  override def buildFromJson(payload: String): PagedCatchup = payload.parseJson.asJsObject.convertTo[PagedCatchup]
+
+}
