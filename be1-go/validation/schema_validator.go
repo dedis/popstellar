b61cec1aad70d1e36f41874ef01709f3b8077533
Florentin Aigner <contact@florentinaigner.at>
2024-05-13 11:35:15 +0200

    add schema validator

--- be1-go/validation/schema_validator.go
+++ be1-go/validation/schema_validator.go
@@ -49,6 +49,7 @@ func init() {
 	// Override the defaults for loading files and decoding base64 encoded data
 	jsonschema.Loaders["file"] = loadFileURL
 	jsonschema.Decoders["base64"] = base64.URLEncoding.DecodeString
+	jsonschema.Loaders["https"] = loadHttpURL
 }
 
 // VerifyJSON verifies that the `msg` follow the schema protocol of name
@@ -137,3 +138,11 @@ func loadFileURL(s string) (io.ReadCloser, error) {
 
 	return protocolFS.Open(path)
 }
+
+func loadHttpURL(s string) (io.ReadCloser, error) {
+	// If for example a message data use a $ref to ../message.json,
+	// it will try to load using the baseURL => replace path,
+	// so that only local file is loaded.
+	return loadFileURL(strings.ReplaceAll(s, baseURL, "file://"))
+}
+
