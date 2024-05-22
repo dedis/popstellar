dc9cf71945be817c4f2267e8f0fd25d04b99342d
Thibault Czarniak <thibault.czarniak@epfl.ch>
2024-05-22 15:51:47 +0200

    Implemented IncorrectlyNamedExceptions

---
+++ be2-scala/linter/input/src/main/scala/fix/IncorrectlyNamedExceptions.scala
@@ -0,0 +1,24 @@
+/*
+rule = IncorrectlyNamedExceptions
+ */
+package fix
+
+object IncorrectlyNamedExceptions {
+  class NotException // scalafix: ok;
+
+  class IsException extends Exception // scalafix: ok;
+
+  class IsNotNamedCorrectly extends Exception // assert: IncorrectlyNamedExceptions
+
+  class Is extends Exception // assert: IncorrectlyNamedExceptions
+
+  class IsChild extends Is // assert: IncorrectlyNamedExceptions
+
+  class IsChildException // scalafix: ok;
+
+  class IsRuntimeException extends RuntimeException // scalafix: ok;
+
+  class IsRuntime extends Exception // assert: IncorrectlyNamedExceptions
+
+}
+
