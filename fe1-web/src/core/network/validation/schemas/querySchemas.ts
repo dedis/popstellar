4a2ac3b40a533d4894dfcc0fc1e333c748075928
Daniel Tavares Agostinho <comptesdaniel@gmail.com>
2024-05-27 17:58:26 +0200

    lint fix

--- fe1-web/src/core/network/validation/schemas/querySchemas.ts
+++ fe1-web/src/core/network/validation/schemas/querySchemas.ts
@@ -3,12 +3,12 @@ import catchup from 'protocol/query/method/catchup.json';
 import get_messages_by_id from 'protocol/query/method/get_messages_by_id.json';
 import greet_server from 'protocol/query/method/greet_server.json';
 import heartbeat from 'protocol/query/method/heartbeat.json';
+import rumor_object from 'protocol/query/method/object/rumor.json';
+import rumor_state_object from 'protocol/query/method/object/rumor_state.json';
 import paged_catchup from 'protocol/query/method/paged_catchup.json';
 import publish from 'protocol/query/method/publish.json';
 import rumor from 'protocol/query/method/rumor.json';
-import rumor_object from 'protocol/query/method/object/rumor.json';
 import rumor_state from 'protocol/query/method/rumor_state.json';
-import rumor_state_object from 'protocol/query/method/object/rumor_state.json'
 import subscribe from 'protocol/query/method/subscribe.json';
 import unsubscribe from 'protocol/query/method/unsubscribe.json';
 import query from 'protocol/query/query.json';
