e5b9a22e191293a23f0e953172b1a9849758203c
Daniel Tavares Agostinho <comptesdaniel@gmail.com>
2024-05-27 17:50:21 +0200

    added rumor objects

--- fe1-web/src/core/network/validation/schemas/querySchemas.ts
+++ fe1-web/src/core/network/validation/schemas/querySchemas.ts
@@ -6,7 +6,9 @@ import heartbeat from 'protocol/query/method/heartbeat.json';
 import paged_catchup from 'protocol/query/method/paged_catchup.json';
 import publish from 'protocol/query/method/publish.json';
 import rumor from 'protocol/query/method/rumor.json';
+import rumor_object from 'protocol/query/method/object/rumor.json';
 import rumor_state from 'protocol/query/method/rumor_state.json';
+import rumor_state_object from 'protocol/query/method/object/rumor_state.json'
 import subscribe from 'protocol/query/method/subscribe.json';
 import unsubscribe from 'protocol/query/method/unsubscribe.json';
 import query from 'protocol/query/query.json';
@@ -22,7 +24,9 @@ const querySchemas = [
   greet_server,
   query,
   rumor,
+  rumor_object,
   rumor_state,
+  rumor_state_object,
   paged_catchup,
 ];
 
