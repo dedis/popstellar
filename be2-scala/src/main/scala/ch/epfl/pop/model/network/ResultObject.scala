52fdb1b69bc03258e49bbf13f75016e4a27d772f
Daniel Tavares Agostinho <comptesdaniel@gmail.com>
2024-05-27 12:04:40 +0200

    result object changed

--- be2-scala/src/main/scala/ch/epfl/pop/model/network/ResultObject.scala
+++ be2-scala/src/main/scala/ch/epfl/pop/model/network/ResultObject.scala
@@ -1,15 +1,71 @@
 package ch.epfl.pop.model.network
 
+import ch.epfl.pop.model.network.method.Rumor
 import ch.epfl.pop.model.network.method.message.Message
-import ch.epfl.pop.model.objects.Channel
+import ch.epfl.pop.model.objects.{Channel, RumorStateAns}
 
-class ResultObject(val resultInt: Option[Int], val resultMessages: Option[List[Message]], val resultMap: Option[Map[Channel, Set[Message]]]) {
+sealed trait ResultType
+final case class ResultInt(result: Int) extends ResultType
+final case class ResultMessage(result: List[Message]) extends ResultType
+final case class ResultMap(result: Map[Channel, Set[Message]]) extends ResultType
+final case class ResultRumor(result: List[Rumor]) extends ResultType
 
-  def this(result: Int) = this(Some(result), None, None)
 
-  def this(result: List[Message]) = this(None, Some(result), None)
+class ResultObject(val result: Option[ResultType]) {
 
-  def this(mapResult: Map[Channel, Set[Message]]) = this(None, None, Some(mapResult))
+  // sugar syntax and legacy purposes
+  def this(result : Int) = this(Some(ResultInt(result)))
+  def this(result: Map[Channel, Set[Message]]) = this(Some(ResultMap(result)))
+    
+  def this(result : ResultType) = this(Some(result))
+
+  def resultInt : Option[Int] = {
+    result match
+      case Some(resultInt: ResultInt) => Some(resultInt.result)
+      case _ => None
+  }
+
+  def resultMessages: Option[List[Message]] = {
+    result match
+      case Some(resultMessage: ResultMessage) => Some(resultMessage.result)
+      case _ => None
+  }
+
+  def resultMap: Option[Map[Channel, Set[Message]]] = {
+    result match
+      case Some(resultMap: ResultMap) => Some(resultMap.result)
+      case _ => None
+  }
+
+  def resultRumor: Option[List[Rumor]] = {
+    result match
+      case Some(resultRumor: ResultRumor) => Some(resultRumor.result)
+      case _ => None
+  }
+
+  def isIntResult: Boolean = result match
+    case Some(_: ResultInt) => true
+    case _ => false
+
+  override def equals(obj: Any): Boolean = {
+    obj match
+      case that: ResultObject =>
+        (this.result, that.result) match
+          case (Some(a), Some(b)) => a == b
+          case (None, None) => true
+          case _ => false
+  }
+}
+
+class ResultObject2(val resultInt: Option[Int], val resultMessages: Option[List[Message]], val resultMap: Option[Map[Channel, Set[Message]]], val resultRumor: Option[RumorStateAns]) {
+
+  def this(result: Int) = this(Some(result), None, None, None)
+
+  def this(result: List[Message]) = this(None, Some(result), None, None)
+
+  def this(mapResult: Map[Channel, Set[Message]]) = this(None, None, Some(mapResult), None)
+
+  def this(rumorResult: RumorStateAns) = this(None, None, None, Some(rumorResult))
 
   def isIntResult: Boolean = resultInt.isDefined
 
---
+++ be2-scala/src/main/scala/ch/epfl/pop/model/objects/RumorStateAns.scala
@@ -0,0 +1,5 @@
+package ch.epfl.pop.model.objects
+
+class RumorStateAns {
+
+}
