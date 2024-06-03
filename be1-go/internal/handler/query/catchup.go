4f9cf8aa31af71c4fe425305ff4980bf0527f901
stuart <stuart.gueissaz@epfl.ch>
2024-06-03 10:54:46 +0200

    update catchup handler errors

--- be1-go/internal/handler/query/catchup.go
+++ be1-go/internal/handler/query/catchup.go
@@ -2,30 +2,28 @@ package query
 
 import (
 	"encoding/json"
-	"popstellar/internal/message/answer"
+	"popstellar/internal/errors"
 	"popstellar/internal/message/query/method"
 	"popstellar/internal/network/socket"
 	"popstellar/internal/singleton/database"
 )
 
-func handleCatchUp(socket socket.Socket, msg []byte) (*int, *answer.Error) {
+func handleCatchUp(socket socket.Socket, msg []byte) (*int, error) {
 	var catchup method.Catchup
 
 	err := json.Unmarshal(msg, &catchup)
 	if err != nil {
-		errAnswer := answer.NewJsonUnmarshalError(err.Error())
-		return nil, errAnswer.Wrap("handleCatchUp")
+		return nil, errors.NewJsonUnmarshalError(err.Error())
 	}
 
 	db, err := database.GetQueryRepositoryInstance()
 	if err != nil {
-		return &catchup.ID, answer.NewInternalServerError(err.Error())
+		return &catchup.ID, err
 	}
 
 	result, err := db.GetAllMessagesFromChannel(catchup.Params.Channel)
 	if err != nil {
-		errAnswer := answer.NewQueryDatabaseError("all message from channel %s: %v", catchup.Params.Channel, err)
-		return &catchup.ID, errAnswer.Wrap("handleCatchUp")
+		return &catchup.ID, errors.NewQueryDatabaseError("all message from channel %s: %v", catchup.Params.Channel, err)
 	}
 
 	socket.SendResult(catchup.ID, result, nil)
