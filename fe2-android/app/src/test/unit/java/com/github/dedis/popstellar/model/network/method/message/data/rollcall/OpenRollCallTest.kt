a792fad342ff77f22fe6919b88d16d85d2cf88b0
Kaz <maxime.teuber@epfl.ch>
2024-06-27 22:10:57 +0200

    data input validation for OpenRollCall + constructor tests

--- fe2-android/app/src/test/unit/java/com/github/dedis/popstellar/model/network/method/message/data/rollcall/OpenRollCallTest.kt
+++ fe2-android/app/src/test/unit/java/com/github/dedis/popstellar/model/network/method/message/data/rollcall/OpenRollCallTest.kt
@@ -6,7 +6,9 @@ import com.github.dedis.popstellar.model.network.method.message.data.Action
 import com.github.dedis.popstellar.model.network.method.message.data.Objects
 import com.github.dedis.popstellar.model.objects.event.EventState
 import com.github.dedis.popstellar.model.objects.event.EventType
+import com.github.dedis.popstellar.model.objects.security.Base64URLData
 import com.github.dedis.popstellar.utility.security.HashSHA256.hash
+import junit.framework.TestCase.assertNotNull
 import java.time.Instant
 import org.hamcrest.CoreMatchers
 import org.hamcrest.MatcherAssert
@@ -54,6 +56,82 @@ class OpenRollCallTest {
   }
 
   @Test
+  fun constructor1SucceedsWithValidData() {
+    val openRollCall = OpenRollCall(LAO_ID, CREATE_ROLL_CALL.id, TIME, EventState.CREATED)
+    assertNotNull(openRollCall)
+  }
+
+  @Test(expected = IllegalArgumentException::class)
+  fun constructor1FailsWhenLaoIdEmpty() {
+    OpenRollCall(EMPTY_B64, CREATE_ROLL_CALL.id, TIME, EventState.CREATED)
+  }
+
+  @Test(expected = IllegalArgumentException::class)
+  fun constructor1FailsWhenLaoIdNotBase64() {
+    OpenRollCall(INVALID_B64, CREATE_ROLL_CALL.id, TIME, EventState.CREATED)
+  }
+
+  @Test(expected = IllegalArgumentException::class)
+  fun constructor1FailsWhenOpensNotBase64() {
+    OpenRollCall(LAO_ID, INVALID_B64, TIME, EventState.CREATED)
+  }
+
+  @Test(expected = IllegalArgumentException::class)
+  fun constructor1FailsWhenOpensEmpty() {
+    OpenRollCall(LAO_ID, EMPTY_B64, TIME, EventState.CREATED)
+  }
+
+  @Test(expected = IllegalArgumentException::class)
+  fun constructor1FailsWhenOpenedAtNegative() {
+    OpenRollCall(LAO_ID, CREATE_ROLL_CALL.id, -1, EventState.CREATED)
+  }
+
+  @Test
+  fun constructor2SucceedsWithValidDataOPENAction() {
+    val openRollCall = OpenRollCall(ID, CREATE_ROLL_CALL.id, TIME, Action.OPEN.action)
+    assertNotNull(openRollCall)
+  }
+
+  fun constructor2SucceedsWithValidDataREOPENAction() {
+    val openRollCall = OpenRollCall(ID, CREATE_ROLL_CALL.id, TIME, Action.REOPEN.action)
+    assertNotNull(openRollCall)
+  }
+
+  @Test(expected = IllegalArgumentException::class)
+  fun constructor2FailsWhenUpdateIdEmpty() {
+    OpenRollCall(EMPTY_B64, CREATE_ROLL_CALL.id, TIME, Action.OPEN.action)
+  }
+
+  @Test(expected = IllegalArgumentException::class)
+  fun constructor2FailsWhenUpdateIdNotBase64() {
+    OpenRollCall(INVALID_B64, CREATE_ROLL_CALL.id, TIME, Action.OPEN.action)
+  }
+
+  @Test(expected = IllegalArgumentException::class)
+  fun constructor2FailsWhenOpensEmpty() {
+    OpenRollCall(ID, EMPTY_B64, TIME, Action.OPEN.action)
+  }
+
+  @Test(expected = IllegalArgumentException::class)
+  fun constructor2FailsWhenOpensNotBase64() {
+    OpenRollCall(ID, INVALID_B64, TIME, Action.OPEN.action)
+  }
+
+  @Test(expected = IllegalArgumentException::class)
+  fun constructor2FailsWhenOpenedAtNegative() {
+    OpenRollCall(ID, CREATE_ROLL_CALL.id, -1, Action.OPEN.action)
+  }
+
+  @Test(expected = IllegalArgumentException::class)
+  fun constructor2FailsWhenActionInvalid() {
+    for (action in Action.values()) {
+      if (action != Action.OPEN && action != Action.REOPEN) {
+        OpenRollCall(ID, CREATE_ROLL_CALL.id, TIME, action.action)
+      }
+    }
+  }
+
+  @Test
   fun jsonValidationTest() {
     testData(REOPEN_ROLL_CALL)
   }
@@ -81,8 +159,10 @@ class OpenRollCallTest {
     private const val LOCATION = "Location"
     private val CREATE_ROLL_CALL = CreateRollCall(NAME, TIME, TIME, TIME, LOCATION, null, LAO_ID)
     private val OPEN_ROLL_CALL = OpenRollCall(LAO_ID, CREATE_ROLL_CALL.id, TIME, EventState.CREATED)
-    private val REOPEN_ROLL_CALL =
-      OpenRollCall(LAO_ID, CREATE_ROLL_CALL.id, TIME, EventState.CLOSED)
+    private val REOPEN_ROLL_CALL = OpenRollCall(LAO_ID, CREATE_ROLL_CALL.id, TIME, EventState.CLOSED)
     private val ID = hash(EventType.ROLL_CALL.suffix, LAO_ID, CREATE_ROLL_CALL.id, TIME.toString())
+
+    private const val INVALID_B64 = "invalidBase64String"
+    private val EMPTY_B64 = Base64URLData("".toByteArray()).encoded
   }
 }
